867524087c49484aa74449263eccd09f85e4fc89
Pawel Lebioda <pawel.lebioda@intel.com>
2015-04-07 17:11:57 +0200

    obj: redo log implementation

--- src/libpmemobj/redo.c
+++ src/libpmemobj/redo.c
@@ -35,8 +35,45 @@
  */
 #include <stdlib.h>
 #include <stdint.h>
+
+#include "libpmem.h"
 #include "libpmemobj.h"
+#include "util.h"
+#include "obj.h"
 #include "redo.h"
+#include "out.h"
+
+#define	_POBJ_REDO_MIN_OFFSET	8192
+
+/*
+ * redo_log_check_offset -- (internal) check if offset is valid
+ */
+static inline int
+redo_log_check_offset(PMEMobjpool *pop, uint64_t offset)
+{
+	/* XXX change the minimum offset */
+	return offset >= _POBJ_REDO_MIN_OFFSET &&
+		offset <= (pop->size - sizeof (uint64_t));
+}
+
+/*
+ * redo_log_nflags -- (internal) get number of finish flags set
+ */
+static size_t
+redo_log_nflags(struct redo_log *redo, size_t nentries)
+{
+	size_t ret = 0;
+	size_t i;
+
+	for (i = 0; i < nentries; i++) {
+		if (redo[i].offset & REDO_FINISH_FLAG)
+			ret++;
+	}
+
+	LOG(15, "redo %p nentries %zu nflags %zu", redo, nentries, ret);
+
+	return ret;
+}
 
 /*
  * redo_log_store -- (internal) store redo log entry at specified index
@@ -45,7 +82,13 @@ void
 redo_log_store(PMEMobjpool *pop, struct redo_log *redo, size_t index,
 		uint64_t offset, uint64_t value)
 {
-	/* stub */
+	LOG(15, "redo %p index %zu offset %ju value %ju",
+			redo, index, offset, value);
+
+	ASSERTeq(offset & REDO_FINISH_FLAG, 0);
+
+	redo[index].offset = offset;
+	redo[index].value = value;
 }
 
 /*
@@ -55,7 +98,28 @@ void
 redo_log_store_last(PMEMobjpool *pop, struct redo_log *redo, size_t index,
 		uint64_t offset, uint64_t value)
 {
-	/* stub */
+	LOG(15, "redo %p index %zu offset %ju value %ju",
+			redo, index, offset, value);
+
+	ASSERTeq(offset & REDO_FINISH_FLAG, 0);
+
+	/* store value of last entry */
+	redo[index].value = value;
+
+	/* persist all redo log entries */
+	/* XXX function pointer from pop */
+	if (pop->is_pmem)
+		pmem_persist(redo, index * sizeof (struct redo_log));
+	else
+		pmem_msync(redo, index * sizeof (struct redo_log));
+
+	/* store and persist offset of last entry */
+	redo[index].offset = offset | REDO_FINISH_FLAG;
+	/* XXX function pointer from pop */
+	if (pop->is_pmem)
+		pmem_persist(&redo[index].offset, sizeof (redo[index].offset));
+	else
+		pmem_msync(&redo[index].offset, sizeof (redo[index].offset));
 }
 
 /*
@@ -65,17 +129,63 @@ void
 redo_log_process(PMEMobjpool *pop, struct redo_log *redo,
 		size_t nentries)
 {
-	/* stub */
+	LOG(15, "redo %p nentries %zu", redo, nentries);
+
+	ASSERTeq(redo_log_check(pop, redo, nentries), 1);
+
+	uint64_t *val;
+	while ((redo->offset & REDO_FINISH_FLAG) == 0) {
+		val = (uint64_t *)((uintptr_t)pop->addr + redo->offset);
+		*val = redo->value;
+
+		/* XXX function pointer from pop */
+		if (pop->is_pmem)
+			pmem_flush(val, sizeof (uint64_t));
+		else
+			pmem_msync(val, sizeof (uint64_t));
+
+		redo++;
+	}
+
+	uint64_t offset = redo->offset & REDO_FLAG_MASK;
+	val = (uint64_t *)((uintptr_t)pop->addr + offset);
+	*val = redo->value;
+
+	/* XXX function pointer from pop */
+	if (pop->is_pmem)
+		pmem_flush(val, sizeof (uint64_t));
+	else
+		pmem_msync(val, sizeof (uint64_t));
+
+	/* XXX function pointer from pop */
+	if (pop->is_pmem)
+		pmem_drain();
+
+	redo->offset = 0;
+
+	/* XXX function pointer from pop */
+	if (pop->is_pmem)
+		pmem_persist(&redo->offset, sizeof (redo->offset));
+	else
+		pmem_msync(&redo->offset, sizeof (redo->offset));
 }
 
 /*
  * redo_log_recover -- (internal) recovery of redo log
+ *
+ * The redo_log_recover shall be preceded by redo_log_check call.
  */
 void
 redo_log_recover(PMEMobjpool *pop, struct redo_log *redo,
 		size_t nentries)
 {
-	/* stub */
+	LOG(15, "redo %p nentries %zu", redo, nentries);
+
+	size_t nflags = redo_log_nflags(redo, nentries);
+	ASSERT(nflags < 2);
+
+	if (nflags == 1)
+		redo_log_process(pop, redo, nentries);
 }
 
 /*
@@ -84,6 +194,31 @@ redo_log_recover(PMEMobjpool *pop, struct redo_log *redo,
 int
 redo_log_check(PMEMobjpool *pop, struct redo_log *redo, size_t nentries)
 {
-	/* stub */
-	return 0;
+	LOG(15, "redo %p nentries %zu", redo, nentries);
+
+	size_t nflags = redo_log_nflags(redo, nentries);
+
+	if (nflags > 1) {
+		LOG(15, "redo %p to many finish flags", redo);
+		return 0;
+	}
+
+	if (nflags == 1) {
+		while ((redo->offset & REDO_FINISH_FLAG) == 0) {
+			if (!redo_log_check_offset(pop, redo->offset)) {
+				LOG(15, "redo %p invalid offset %ju",
+						redo, redo->offset);
+				return 0;
+			}
+			redo++;
+		}
+
+		uint64_t offset = redo->offset & REDO_FLAG_MASK;
+		if (!redo_log_check_offset(pop, offset)) {
+			LOG(15, "redo %p invalid offset %ju", redo, offset);
+			return 0;
+		}
+	}
+
+	return 1;
 }
