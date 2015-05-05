e003c87de3aa26c0ba1a9d87dc08e4813dec58c8
Piotr Balcer <piotr.balcer@intel.com>
2015-05-05 12:44:05 +0200

    obj: implement basic transaction flow

--- src/include/libpmemobj.h
+++ src/include/libpmemobj.h
@@ -548,9 +548,10 @@ int pmemobj_tx_process();
 #define	_POBJ_TX_BEGIN(pop, ...)\
 {\
 	jmp_buf _tx_env;\
-	int _stage;\
-	pmemobj_tx_begin(pop, _tx_env, __VA_ARGS__, TX_LOCK_NONE);\
+	int _stage = TX_STAGE_NONE;\
 	setjmp(_tx_env);\
+	if (_stage == TX_STAGE_NONE)\
+		pmemobj_tx_begin(pop, _tx_env, __VA_ARGS__, TX_LOCK_NONE);\
 	while ((_stage = pmemobj_tx_stage()) != TX_STAGE_NONE) {\
 		switch (_stage) {\
 			case TX_STAGE_WORK:
