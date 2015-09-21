ddca85fb5472f3447d245f0b71e21684df5b6b60
Krzysztof Czurylo <krzysztof.czurylo@intel.com>
2015-09-21 14:02:15 +0200

    win: temporarily disable _pobj_pcache

--- src/libpmemobj/obj.c
+++ src/libpmemobj/obj.c
@@ -57,8 +57,10 @@
 static struct cuckoo *pools_ht; /* hash table used for searching by UUID */
 static struct ctree *pools_tree; /* tree used for searching by address */
 
+#ifndef WIN32
 int _pobj_cache_invalidate;
 __thread struct _pobj_pcache _pobj_cached_pool;
+#endif
 
 /*
  * User may decide to map all pools with MAP_PRIVATE flag using
@@ -977,7 +979,9 @@ pmemobj_close(PMEMobjpool *pop)
 {
 	LOG(3, "pop %p", pop);
 
+#ifndef WIN32
 	_pobj_cache_invalidate++;
+#endif
 
 	if (cuckoo_remove(pools_ht, pop->uuid_lo) != pop) {
 		ERR("cuckoo_remove");
@@ -987,10 +991,12 @@ pmemobj_close(PMEMobjpool *pop)
 		ERR("ctree_remove");
 	}
 
+#ifndef WIN32
 	if (_pobj_cached_pool.pop == pop) {
 		_pobj_cached_pool.pop = NULL;
 		_pobj_cached_pool.uuid_lo = 0;
 	}
+#endif
 
 	pmemobj_cleanup(pop);
 }
