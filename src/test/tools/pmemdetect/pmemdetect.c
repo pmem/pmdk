2a38fcec48d8b590c75222a4db20fe50ea157bd8
Krzysztof Czurylo <krzysztof.czurylo@intel.com>
2016-01-08 19:30:07 +0100

    test: check the actual filesystem type
    Fail the test if pmem/non-pmem filesystem is required by the test,
    and PMEM_FS_DIR or NON_PMEM_FS_DIR do not point to the actual PMEM or
    non-PMEM device.

---
+++ src/test/tools/pmemdetect/pmemdetect.c
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2016, Intel Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * pmemdetect.c -- detect pmem device
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "util.h"
+#include "libpmem.h"
+
+#define	SIZE 4096
+
+int
+main(int argc, char *argv[])
+{
+	int ret;
+
+	if (argc != 2) {
+		fprintf(stderr, "usage: %s path\n", argv[0]);
+		exit(2);
+	}
+
+	const char *path = argv[1];
+
+	util_init();
+	void *addr = util_map_tmpfile(path, SIZE, 0);
+	if (addr == NULL) {
+		fprintf(stderr, "file creation failed\n");
+		exit(2);
+	}
+
+	if (pmem_is_pmem(addr, SIZE))
+		ret = 0;
+	else
+		ret = 1;
+
+	util_unmap(addr, SIZE);
+
+	return ret;
+}
