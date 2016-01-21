09ea55f54c28804c62b6312df1fc6515ef3337be
Bianka Golik <bianka.golik@intel.com>
2016-01-21 12:34:18 +0100

    ut: pmemobj_tx_commit case added

--- src/test/obj_tx_flow/obj_tx_flow.c
+++ src/test/obj_tx_flow/obj_tx_flow.c
@@ -42,88 +42,83 @@
 #define	TEST_VALUE_A 5
 #define	TEST_VALUE_B 10
 #define	TEST_VALUE_C 15
+#define	OPS_NUM 8
+TOID_DECLARE(struct test_obj, 1);
 
-int
-main(int argc, char *argv[])
+struct test_obj {
+	int a;
+	int b;
+	int c;
+};
+
+
+static void
+do_tx_macro_commit(PMEMobjpool *pop, TOID(struct test_obj) *obj)
 {
-	START(argc, argv, "obj_tx_flow");
+	TX_BEGIN(pop) {
+		D_RW(*obj)->a = TEST_VALUE_A;
+	} TX_ONCOMMIT {
+		ASSERT(D_RW(*obj)->a == TEST_VALUE_A);
+		D_RW(*obj)->b = TEST_VALUE_B;
+	} TX_ONABORT { /* not called */
+		D_RW(*obj)->a = TEST_VALUE_B;
+	} TX_FINALLY {
+		ASSERT(D_RW(*obj)->b == TEST_VALUE_B);
+		D_RW(*obj)->c = TEST_VALUE_C;
+	} TX_END
+}
 
-	if (argc != 2)
-		FATAL("usage: %s [file]", argv[0]);
+static void
+do_tx_macro_abort(PMEMobjpool *pop, TOID(struct test_obj) *obj)
+{
+	D_RW(*obj)->a = TEST_VALUE_A;
+	D_RW(*obj)->b = TEST_VALUE_B;
+	TX_BEGIN(pop) {
+		TX_ADD(*obj);
+		D_RW(*obj)->a = TEST_VALUE_B;
+		pmemobj_tx_abort(EINVAL);
+		D_RW(*obj)->b = TEST_VALUE_A;
+	} TX_ONCOMMIT { /* not called */
+		D_RW(*obj)->a = TEST_VALUE_B;
+	} TX_ONABORT {
+		ASSERT(D_RW(*obj)->a == TEST_VALUE_A);
+		ASSERT(D_RW(*obj)->b == TEST_VALUE_B);
+		D_RW(*obj)->b = TEST_VALUE_B;
+	} TX_FINALLY {
+		ASSERT(D_RW(*obj)->b == TEST_VALUE_B);
+		D_RW(*obj)->c = TEST_VALUE_C;
+	} TX_END
+}
 
-	PMEMobjpool *pop;
-	if ((pop = pmemobj_create(argv[1], LAYOUT_NAME, PMEMOBJ_MIN_POOL,
-	    S_IWUSR | S_IRUSR)) == NULL)
-		FATAL("!pmemobj_create");
+static void
+do_tx_macro_commit_nested(PMEMobjpool *pop, TOID(struct test_obj) *obj)
+{
+	TX_BEGIN(pop) {
+		TX_BEGIN(pop) {
+			D_RW(*obj)->a = TEST_VALUE_A;
+		} TX_ONCOMMIT {
+			ASSERT(D_RW(*obj)->a == TEST_VALUE_A);
+			D_RW(*obj)->b = TEST_VALUE_B;
+		} TX_END
+	} TX_ONCOMMIT {
+		D_RW(*obj)->c = TEST_VALUE_C;
+	} TX_END
+}
 
+static void
+do_tx_macro_abort_nested(PMEMobjpool *pop, TOID(struct test_obj) *obj)
+{
 	int a = 0;
 	int b = 0;
 	int c = 0;
-
+	D_RW(*obj)->a = TEST_VALUE_A;
+	D_RW(*obj)->b = TEST_VALUE_B;
 	TX_BEGIN(pop) {
-		a = TEST_VALUE_A;
-	} TX_ONCOMMIT {
-		ASSERT(a == TEST_VALUE_A);
-		b = TEST_VALUE_B;
-	} TX_ONABORT { /* not called */
-		a = TEST_VALUE_B;
-	} TX_FINALLY {
-		ASSERT(b == TEST_VALUE_B);
-		c = TEST_VALUE_C;
-	} TX_END
-
-	ASSERT(a == TEST_VALUE_A);
-	ASSERT(b == TEST_VALUE_B);
-	ASSERT(c == TEST_VALUE_C);
-
-	a = 0;
-	b = 0;
-	c = 0;
-
-	TX_BEGIN(pop) {
-		a = TEST_VALUE_A;
-		pmemobj_tx_abort(EINVAL);
-		a = TEST_VALUE_B;
-	} TX_ONCOMMIT { /* not called */
-		a = TEST_VALUE_B;
-	} TX_ONABORT {
-		ASSERT(a == TEST_VALUE_A);
-		b = TEST_VALUE_B;
-	} TX_FINALLY {
-		ASSERT(b == TEST_VALUE_B);
-		c = TEST_VALUE_C;
-	} TX_END
-
-	ASSERT(a == TEST_VALUE_A);
-	ASSERT(b == TEST_VALUE_B);
-	ASSERT(c == TEST_VALUE_C);
-
-	a = 0;
-	b = 0;
-	c = 0;
-
-	TX_BEGIN(pop) {
-		TX_BEGIN(pop) {
-			a = TEST_VALUE_A;
-		} TX_ONCOMMIT {
-			ASSERT(a == TEST_VALUE_A);
-			b = TEST_VALUE_B;
-		} TX_END
-	} TX_ONCOMMIT {
-		c = TEST_VALUE_C;
-	} TX_END
-
-	ASSERT(a == TEST_VALUE_A);
-	ASSERT(b == TEST_VALUE_B);
-	ASSERT(c == TEST_VALUE_C);
-
-	a = 0;
-	b = 0;
-	c = 0;
-
-	TX_BEGIN(pop) {
+		TX_ADD(*obj);
+		D_RW(*obj)->a = TEST_VALUE_B;
 		a = TEST_VALUE_C;
 		TX_BEGIN(pop) {
+			D_RW(*obj)->b = TEST_VALUE_C;
 			a = TEST_VALUE_A;
 			pmemobj_tx_abort(EINVAL);
 			a = TEST_VALUE_B;
@@ -144,28 +139,153 @@ main(int argc, char *argv[])
 		ASSERT(a == TEST_VALUE_A);
 		ASSERT(b == TEST_VALUE_B);
 		ASSERT(c == TEST_VALUE_C);
-		a = TEST_VALUE_B;
-	} TX_FINALLY {
-		ASSERT(a == TEST_VALUE_B);
 		b = TEST_VALUE_A;
+	} TX_FINALLY {
+		ASSERT(b == TEST_VALUE_A);
+		D_RW(*obj)->c = TEST_VALUE_C;
+		a = TEST_VALUE_B;
 	} TX_END
-
 	ASSERT(a == TEST_VALUE_B);
-	ASSERT(b == TEST_VALUE_A);
-	ASSERT(c == TEST_VALUE_C);
+}
 
-	a = 0;
-	b = 0;
-	c = 0;
+static void
+do_tx_commit(PMEMobjpool *pop, TOID(struct test_obj) *obj)
+{
 
 	pmemobj_tx_begin(pop, NULL, TX_LOCK_NONE);
+	D_RW(*obj)->a = TEST_VALUE_A;
+	TX_ADD(*obj);
+	D_RW(*obj)->b = TEST_VALUE_B;
+	pmemobj_tx_commit();
+	ASSERT(pmemobj_tx_stage() == TX_STAGE_ONCOMMIT);
+	D_RW(*obj)->c = TEST_VALUE_C;
+	pmemobj_tx_end();
+}
+
+static void
+do_tx_commit_nested(PMEMobjpool *pop, TOID(struct test_obj) *obj)
+{
+
+	pmemobj_tx_begin(pop, NULL, TX_LOCK_NONE);
+	TX_ADD(*obj);
+	D_RW(*obj)->a = TEST_VALUE_A;
+		pmemobj_tx_begin(pop, NULL, TX_LOCK_NONE);
+		TX_ADD(*obj);
+		D_RW(*obj)->b = TEST_VALUE_B;
+		pmemobj_tx_commit();
+		ASSERT(pmemobj_tx_stage() == TX_STAGE_ONCOMMIT);
+		pmemobj_tx_end();
+	ASSERT(pmemobj_tx_stage() == TX_STAGE_WORK);
+	pmemobj_tx_commit();
+	ASSERT(pmemobj_tx_stage() == TX_STAGE_ONCOMMIT);
+	D_RW(*obj)->c = TEST_VALUE_C;
+	pmemobj_tx_end();
+}
+
+static void
+do_tx_abort(PMEMobjpool *pop, TOID(struct test_obj) *obj)
+{
+	D_RW(*obj)->a = TEST_VALUE_A;
+	pmemobj_tx_begin(pop, NULL, TX_LOCK_NONE);
+	D_RW(*obj)->b = TEST_VALUE_B;
+	TX_ADD(*obj);
+	D_RW(*obj)->a = 0;
 	pmemobj_tx_abort(EINVAL);
 	ASSERT(pmemobj_tx_stage() == TX_STAGE_ONABORT);
-	a = TEST_VALUE_A;
+	D_RW(*obj)->c = TEST_VALUE_C;
 	pmemobj_tx_end();
+}
 
-	ASSERT(a == TEST_VALUE_A);
+static void
+do_tx_abort_nested(PMEMobjpool *pop, TOID(struct test_obj) *obj)
+{
+	D_RW(*obj)->a = TEST_VALUE_A;
+	D_RW(*obj)->b = TEST_VALUE_B;
+	pmemobj_tx_begin(pop, NULL, TX_LOCK_NONE);
+	TX_ADD(*obj);
+	D_RW(*obj)->a = 0;
+		pmemobj_tx_begin(pop, NULL, TX_LOCK_NONE);
+		TX_ADD(*obj);
+		D_RW(*obj)->b = 0;
+		pmemobj_tx_abort(EINVAL);
+		ASSERT(pmemobj_tx_stage() == TX_STAGE_ONABORT);
+		pmemobj_tx_end();
+	ASSERT(pmemobj_tx_stage() == TX_STAGE_ONABORT);
+	D_RW(*obj)->c = TEST_VALUE_C;
+	pmemobj_tx_end();
+}
 
+typedef void (*fn_op)(PMEMobjpool *pop, TOID(struct test_obj) *obj);
+fn_op tx_op[OPS_NUM] = {do_tx_macro_commit, do_tx_macro_abort,
+			do_tx_macro_commit_nested, do_tx_macro_abort_nested,
+			do_tx_commit, do_tx_commit_nested, do_tx_abort,
+			do_tx_abort_nested};
+
+static void
+do_tx_process(PMEMobjpool *pop)
+{
+	pmemobj_tx_begin(pop, NULL, TX_LOCK_NONE);
+	ASSERT(pmemobj_tx_stage() == TX_STAGE_WORK);
+	pmemobj_tx_process();
+	ASSERT(pmemobj_tx_stage() == TX_STAGE_ONCOMMIT);
+	pmemobj_tx_process();
+	ASSERT(pmemobj_tx_stage() == TX_STAGE_FINALLY);
+	pmemobj_tx_process();
+	ASSERT(pmemobj_tx_stage() == TX_STAGE_NONE);
+	pmemobj_tx_end();
+	ASSERT(pmemobj_tx_stage() == TX_STAGE_NONE);
+}
+
+static void
+do_tx_process_nested(PMEMobjpool *pop)
+{
+	pmemobj_tx_begin(pop, NULL, TX_LOCK_NONE);
+	ASSERT(pmemobj_tx_stage() == TX_STAGE_WORK);
+		pmemobj_tx_begin(pop, NULL, TX_LOCK_NONE);
+		pmemobj_tx_process();
+		ASSERT(pmemobj_tx_stage() == TX_STAGE_ONCOMMIT);
+		pmemobj_tx_process();
+		ASSERT(pmemobj_tx_stage() == TX_STAGE_FINALLY);
+		pmemobj_tx_end();
+	ASSERT(pmemobj_tx_stage() == TX_STAGE_WORK);
+	pmemobj_tx_abort(EINVAL);
+	ASSERT(pmemobj_tx_stage() == TX_STAGE_ONABORT);
+	pmemobj_tx_process();
+	ASSERT(pmemobj_tx_stage() == TX_STAGE_FINALLY);
+	pmemobj_tx_process();
+	ASSERT(pmemobj_tx_stage() == TX_STAGE_NONE);
+	pmemobj_tx_end();
+	ASSERT(pmemobj_tx_stage() == TX_STAGE_NONE);
+}
+
+int
+main(int argc, char *argv[])
+{
+	START(argc, argv, "obj_tx_flow");
+
+	if (argc != 2)
+		FATAL("usage: %s [file]", argv[0]);
+
+	PMEMobjpool *pop;
+	if ((pop = pmemobj_create(argv[1], LAYOUT_NAME, PMEMOBJ_MIN_POOL,
+	    S_IWUSR | S_IRUSR)) == NULL)
+		FATAL("!pmemobj_create");
+
+	TOID(struct test_obj) obj;
+	POBJ_ZNEW(pop, &obj, struct test_obj);
+
+	for (int i = 0; i < OPS_NUM; i++) {
+		D_RW(obj)->a = 0;
+		D_RW(obj)->b = 0;
+		D_RW(obj)->c = 0;
+		tx_op[i](pop, &obj);
+
+		ASSERT(D_RO(obj)->a == TEST_VALUE_A);
+		ASSERT(D_RO(obj)->b == TEST_VALUE_B);
+		ASSERT(D_RO(obj)->c == TEST_VALUE_C);
+	}
+	do_tx_process(pop);
+	do_tx_process_nested(pop);
 	pmemobj_close(pop);
 
 	DONE(NULL);
