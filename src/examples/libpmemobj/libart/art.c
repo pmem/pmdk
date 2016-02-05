2f214f72741eb4e75e8aff852adb8d7c068f8a7f
Andreas Bluemle <andreas.bluemle@itxperts.de>
2016-02-05 08:22:09 +0100

    and remove a trailing blank

--- src/examples/libpmemobj/libart/art.c
+++ src/examples/libpmemobj/libart/art.c
@@ -14,7 +14,7 @@
  *       the documentation and/or other materials provided with the
  *       distribution.
  *
- *     * Neither the name of the copyright holder nor the names of its 
+ *     * Neither the name of the copyright holder nor the names of its
  *       contributors may be used to endorse or promote products derived
  *       from this software without specific prior written permission.
  *
