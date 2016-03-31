6e2167425f46b48cf135e01eccfdd6f5da6d3ed1
Tomasz Kapela <tomasz.kapela@intel.com>
2016-03-31 08:57:56 +0200

    obj: add C++ bindings documentation

---
+++ cpp_obj/cpp_html/search/classes_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['lock_5ferror',['lock_error',['../classnvml_1_1lock__error.html',1,'nvml']]]
+];
---
+++ cpp_obj/cpp_html/search/functions_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['exec_5ftx',['exec_tx',['../classnvml_1_1obj_1_1transaction.html#a751a3ce8102a977ea6ab3532fa1bf202',1,'nvml::obj::transaction']]]
+];
