023977b6faaef963d96d863d83a4000ca58b45fc
Pawel Lebioda <pawel.lebioda@intel.com>
2016-06-03 22:56:11 +0200

    rpmem: implement all operations

---
+++ src/test/rpmem_basic/setup.sh
@@ -0,0 +1,79 @@
+#!/bin/bash -e
+#
+# Copyright 2016, Intel Corporation
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in
+#       the documentation and/or other materials provided with the
+#       distribution.
+#
+#     * Neither the name of the copyright holder nor the names of its
+#       contributors may be used to endorse or promote products derived
+#       from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+#
+# src/test/rpmem_basic/setup.sh -- common part for TEST* scripts
+#
+
+require_nodes 2
+require_node_libfabric 0 $RPMEM_PROVIDER
+require_node_libfabric 1 $RPMEM_PROVIDER
+require_node_log_files 0 $RPMEMD_LOG_FILE
+require_node_log_files 1 $RPMEM_LOG_FILE
+require_node_log_files 1 $PMEM_LOG_FILE
+
+POOLS_DIR=pools
+POOLS_PART=pool_parts
+PART_DIR=${NODE_TEST_DIR[0]}/$POOLS_PART
+POOLS_DIR_NODE=${NODE_TEST_DIR[0]}/$POOLS_DIR
+
+RPMEM_CMD="\"cd ${NODE_TEST_DIR[0]} && "
+RPMEM_CMD="$RPMEM_CMD LD_LIBRARY_PATH=.:${NODE_LD_LIBRARY_PATH[0]}"
+RPMEM_CMD="$RPMEM_CMD ./rpmemd"
+RPMEM_CMD="$RPMEM_CMD --log-file=$RPMEMD_LOG_FILE"
+RPMEM_CMD="$RPMEM_CMD --poolset-dir=$POOLS_DIR_NODE"
+
+if [ "$RPMEM_PM" == "APM" ]; then
+	RPMEM_CMD="$RPMEM_CMD --persist-apm"
+fi
+
+RPMEM_CMD="$RPMEM_CMD \""
+
+RPMEM_ENABLE_SOCKETS=0
+RPMEM_ENABLE_VERBS=0
+
+case $RPMEM_PROVIDER in
+	sockets)
+		RPMEM_ENABLE_SOCKETS=1
+		;;
+	verbs)
+		RPMEM_ENABLE_VERBS=1
+		;;
+esac
+
+export_vars_node 1 RPMEM_CMD
+export_vars_node 1 RPMEM_ENABLE_SOCKETS
+export_vars_node 1 RPMEM_ENABLE_VERBS
+export_vars_node 1 RPMEM_LOG_LEVEL
+export_vars_node 1 RPMEM_LOG_FILE
+
