a4f1b7991c46b730f331346f2c9dc08b7d2eac84
Lukasz Dorau <lukasz.dorau@intel.com>
2016-06-08 17:16:22 +0200

    util: adjust util_compare_arch_flags()
    Adjust util_compare_arch_flags() to librpmem.

--- src/common/util.c
+++ src/common/util.c
@@ -324,35 +324,12 @@ int
 util_compare_arch_flags(const struct arch_flags *arch_flags,
 				const struct arch_flags *comp_flags)
 {
-	int ret = 0;
-
-	if (!util_is_zeroed(&arch_flags->reserved,
-				sizeof(arch_flags->reserved))) {
-		ERR("invalid reserved values");
-		ret = -1;
+	if (memcmp(arch_flags, comp_flags, sizeof(struct arch_flags))) {
+		ERR("invalid architecture flags");
+		return -1;
 	}
 
-	if (arch_flags->e_machine != comp_flags->e_machine) {
-		ERR("invalid e_machine value");
-		ret = -1;
-	}
-
-	if (arch_flags->ei_data != comp_flags->ei_data) {
-		ERR("invalid ei_data value");
-		ret = -1;
-	}
-
-	if (arch_flags->ei_class != comp_flags->ei_class) {
-		ERR("invalid ei_class value");
-		ret = -1;
-	}
-
-	if (arch_flags->alignment_desc != comp_flags->alignment_desc) {
-		ERR("invalid alignment_desc value");
-		ret = -1;
-	}
-
-	return ret;
+	return 0;
 }
 
 /*
