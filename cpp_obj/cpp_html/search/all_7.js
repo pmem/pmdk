d49cc34597a8df41e6995ae782d64a1a8d29fcf4
Tomasz Kapela <tomasz.kapela@intel.com>
2016-06-17 16:09:23 +0200

    cpp: updated doxygen docs
    This is the last manual update to be made for the C++ documentation.

--- cpp_obj/cpp_html/search/all_7.js
+++ cpp_obj/cpp_html/search/all_7.js
@@ -1,10 +1,7 @@
 var searchData=
 [
-  ['manual',['manual',['../classnvml_1_1obj_1_1transaction_1_1manual.html',1,'nvml::obj::transaction']]],
-  ['manual',['manual',['../classnvml_1_1obj_1_1transaction_1_1manual.html#a8be401d972a1cb1b1b6dc1e7e627b510',1,'nvml::obj::transaction::manual::manual(obj::pool_base &amp;pop, L &amp;...locks)'],['../classnvml_1_1obj_1_1transaction_1_1manual.html#a54c171ab4d1e66ddc5c8d1858ca54d19',1,'nvml::obj::transaction::manual::manual(const manual &amp;p)=delete'],['../classnvml_1_1obj_1_1transaction_1_1manual.html#ad047b6fadf1505412134eda9fb64ecd3',1,'nvml::obj::transaction::manual::manual(const manual &amp;&amp;p)=delete']]],
-  ['manual_5ftx_5fabort',['manual_tx_abort',['../classnvml_1_1manual__tx__abort.html',1,'nvml']]],
-  ['memcpy_5fpersist',['memcpy_persist',['../classnvml_1_1obj_1_1pool__base.html#a33703cd394da6f835d177045eb86ac32',1,'nvml::obj::pool_base']]],
-  ['memset_5fpersist',['memset_persist',['../classnvml_1_1obj_1_1pool__base.html#ad9b919d3beeb64d987a9103262245dd2',1,'nvml::obj::pool_base']]],
-  ['mutex',['mutex',['../classnvml_1_1obj_1_1mutex.html#ac38afe6912eb8dd6ccca14745858ad8f',1,'nvml::obj::mutex::mutex() noexcept=default'],['../classnvml_1_1obj_1_1mutex.html#aebc05f2c269e6e701506797588e241cc',1,'nvml::obj::mutex::mutex(const mutex &amp;)=delete']]],
-  ['mutex',['mutex',['../classnvml_1_1obj_1_1mutex.html',1,'nvml::obj']]]
+  ['lock',['lock',['../classnvml_1_1obj_1_1mutex.html#a787e04aea62ea7499c0e9a94da3d756b',1,'nvml::obj::mutex::lock()'],['../classnvml_1_1obj_1_1shared__mutex.html#a50148f545233f3adb815ab72f024892a',1,'nvml::obj::shared_mutex::lock()'],['../classnvml_1_1obj_1_1timed__mutex.html#a2a8c83fad7f2bb95cd002cdcbb7f7a30',1,'nvml::obj::timed_mutex::lock()']]],
+  ['lock_5ferror',['lock_error',['../classnvml_1_1lock__error.html',1,'nvml']]],
+  ['lock_5fshared',['lock_shared',['../classnvml_1_1obj_1_1shared__mutex.html#a6c3af58c04aae0b80498dc3bbd37f35d',1,'nvml::obj::shared_mutex']]],
+  ['lock_5ftype',['lock_type',['../classnvml_1_1obj_1_1mutex.html#a608fdf9ef4a6226441317de440d9a536',1,'nvml::obj::mutex::lock_type()'],['../classnvml_1_1obj_1_1shared__mutex.html#a9bcb181d1e27a8805f2c7b315a69434e',1,'nvml::obj::shared_mutex::lock_type()']]]
 ];
--- cpp_obj/cpp_html/search/all_d.js
+++ cpp_obj/cpp_html/search/all_d.js
@@ -1,14 +1,8 @@
 var searchData=
 [
-  ['timed_5fmutex',['timed_mutex',['../classnvml_1_1obj_1_1timed__mutex.html',1,'nvml::obj']]],
-  ['timed_5fmutex',['timed_mutex',['../classnvml_1_1obj_1_1timed__mutex.html#a04fc861e28e667eeff0eb8cecb0befba',1,'nvml::obj::timed_mutex::timed_mutex() noexcept=default'],['../classnvml_1_1obj_1_1timed__mutex.html#ab7bf974caa19bddcaaf83aa682ae47bc',1,'nvml::obj::timed_mutex::timed_mutex(const timed_mutex &amp;)=delete']]],
-  ['timedlock_5fimpl',['timedlock_impl',['../classnvml_1_1obj_1_1timed__mutex.html#ae1e2cd29138ff443e831c7dfc99d6485',1,'nvml::obj::timed_mutex']]],
-  ['transaction',['transaction',['../classnvml_1_1obj_1_1transaction.html',1,'nvml::obj']]],
-  ['transaction_5falloc_5ferror',['transaction_alloc_error',['../classnvml_1_1transaction__alloc__error.html',1,'nvml']]],
-  ['transaction_5ferror',['transaction_error',['../classnvml_1_1transaction__error.html',1,'nvml']]],
-  ['transaction_5fscope_5ferror',['transaction_scope_error',['../classnvml_1_1transaction__scope__error.html',1,'nvml']]],
-  ['try_5flock',['try_lock',['../classnvml_1_1obj_1_1mutex.html#af9b67e5aa997e1bd78966b1bda536ddc',1,'nvml::obj::mutex::try_lock()'],['../classnvml_1_1obj_1_1shared__mutex.html#a58c0ff8220185a381c61f8955bdb688c',1,'nvml::obj::shared_mutex::try_lock()'],['../classnvml_1_1obj_1_1timed__mutex.html#a56cda52cea035f3868549d14ef0da276',1,'nvml::obj::timed_mutex::try_lock()']]],
-  ['try_5flock_5ffor',['try_lock_for',['../classnvml_1_1obj_1_1timed__mutex.html#a68d4f623bf0f4ab6052972bf2d315563',1,'nvml::obj::timed_mutex']]],
-  ['try_5flock_5fshared',['try_lock_shared',['../classnvml_1_1obj_1_1shared__mutex.html#a9a52629ecfc893dc24d8f7754b54ba63',1,'nvml::obj::shared_mutex']]],
-  ['try_5flock_5funtil',['try_lock_until',['../classnvml_1_1obj_1_1timed__mutex.html#a40639d1926cb858425838832ebd2d364',1,'nvml::obj::timed_mutex']]]
+  ['shared_5fmutex',['shared_mutex',['../classnvml_1_1obj_1_1shared__mutex.html',1,'nvml::obj']]],
+  ['shared_5fmutex',['shared_mutex',['../classnvml_1_1obj_1_1shared__mutex.html#a343ec83e072d49cd5ec27063481cf554',1,'nvml::obj::shared_mutex::shared_mutex() noexcept=default'],['../classnvml_1_1obj_1_1shared__mutex.html#af34d84d27c1f6f96edc9b4f00398bd97',1,'nvml::obj::shared_mutex::shared_mutex(const shared_mutex &amp;)=delete']]],
+  ['shared_5fmutex_2ehpp',['shared_mutex.hpp',['../shared__mutex_8hpp.html',1,'']]],
+  ['specialization_2ehpp',['specialization.hpp',['../specialization_8hpp.html',1,'']]],
+  ['swap',['swap',['../classnvml_1_1obj_1_1p.html#adfd2909bbbe7d3694eefb22d8a009ef3',1,'nvml::obj::p::swap()'],['../classnvml_1_1obj_1_1persistent__ptr.html#a224446552101ff45f098c8a84145c9c8',1,'nvml::obj::persistent_ptr::swap()'],['../p_8hpp.html#a583b96064e11bcb8cb50f3f6718d7862',1,'nvml::obj::swap(p&lt; T &gt; &amp;a, p&lt; T &gt; &amp;b) noexcept'],['../persistent__ptr_8hpp.html#aeff9076f9f19fb718e4f3e20b1eecac2',1,'nvml::obj::swap(persistent_ptr&lt; T &gt; &amp;a, persistent_ptr&lt; T &gt; &amp;b) noexcept']]]
 ];
