5792c1110195a709019314f6053b84a2c8b73e7f
Wojciech Uss <wojciech.uss@intel.com>
2016-06-24 15:25:53 +0200

    Removed dependency on connection to apt's key server

--- utils/docker/prepare-environment.sh
+++ utils/docker/prepare-environment.sh
@@ -36,11 +36,9 @@
 #
 
 # Install the newest docker engine
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 sudo sh -c "echo deb https://apt.dockerproject.org/repo ubuntu-precise main >> /etc/apt/sources.list.d/docker.list"
 sudo apt-get update
-sudo apt-get install -y docker-engine apt-transport-https ca-certificates
-sudo usermod -aG docker $USER
+sudo apt-get install --force-yes docker-engine apt-transport-https ca-certificates
 
 # Pull the docker image with environment for building nvml
 sudo docker pull nvml/${OS}:${OS_VER}
