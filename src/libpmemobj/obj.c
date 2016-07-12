pmem/pmdk

commit ec117b1fb4058fdda57d408628584eafda0fcbc1

obj: decouple redo.c from libpmemobj
In the future it will probably be used outside of pmemobj.

src/libpmemobj/obj.c
@@ -677,6 +677,16 @@ pmemobj_replica_init(PMEMobjpool *pop, int is_pmem)
 }
 
 /*
+ * redo_log_check_offset -- (internal) check if offset is valid
+ */
+static int
+redo_log_check_offset(void *ctx, uint64_t offset)
+{
+	PMEMobjpool *pop = ctx;
+	return OBJ_OFF_IS_VALID(pop, offset);
+}
+
+/*
  * pmemobj_runtime_init -- (internal) initialize runtime part of the pool header
  */
 static int
@@ -707,6 +717,16 @@ pmemobj_runtime_init(PMEMobjpool *pop, int rdonly, int boot)
 	pop->rdonly = rdonly;
 
 	pop->uuid_lo = pmemobj_get_uuid_lo(pop);
+
+	pop->redo = redo_log_config_new(pop->addr,
+			(redo_persist_fn)pop->persist,
+			(redo_flush_fn)pop->flush,
+			redo_log_check_offset,
+			pop,
+			pop,
+			REDO_NUM_ENTRIES);
+	if (!pop->redo)
+		return -1;
 
 	if (boot) {
 		if ((errno = pmemobj_boot(pop)) != 0)
@@ -1028,6 +1048,9 @@ pmemobj_cleanup(PMEMobjpool *pop)
 	heap_cleanup(pop);
 
 	lane_cleanup(pop);
+
+	redo_log_config_delete(pop->redo);
+	pop->redo = NULL;
 
 	VALGRIND_DO_DESTROY_MEMPOOL(pop);
 

src/libpmemobj/redo.c
@@ -34,28 +34,71 @@
  * redo.c -- redo log implementation
  */
 
-#include "libpmem.h"
-#include "libpmemobj.h"
-#include "util.h"
-#include "lane.h"
 #include "redo.h"
-#include "obj.h"
 #include "out.h"
+#include "util.h"
 #include "valgrind_internal.h"
 
 /*
- * redo_log_check_offset -- (internal) check if offset is valid
+ * Finish flag at the least significant bit
  */
-static inline int
-redo_log_check_offset(PMEMobjpool *pop, uint64_t offset)
+#define REDO_FINISH_FLAG	((uint64_t)1<<0)
+#define REDO_FLAG_MASK		(~REDO_FINISH_FLAG)
+
+struct redo_ctx {
+	void *base;
+
+	redo_persist_fn persist;
+	redo_flush_fn flush;
+
+	void *flush_ctx;
+
+	redo_check_offset_fn check_offset;
+	void *check_offset_ctx;
+
+	unsigned redo_num_entries;
+};
+
+/*
+ * redo_log_config_new -- allocates redo context
+ */
+struct redo_ctx *
+redo_log_config_new(void *base,
+		redo_persist_fn persist,
+		redo_flush_fn flush,
+		redo_check_offset_fn check_offset,
+		void *flush_ctx,
+		void *check_offset_ctx,
+		unsigned redo_num_entries)
 {
-	return OBJ_OFF_IS_VALID(pop, offset);
+	struct redo_ctx *cfg = Malloc(sizeof(*cfg));
+	if (!cfg)
+		return NULL;
+
+	cfg->base = base;
+	cfg->persist = persist;
+	cfg->flush = flush;
+	cfg->check_offset = check_offset;
+	cfg->flush_ctx = flush_ctx;
+	cfg->check_offset_ctx = check_offset_ctx;
+	cfg->redo_num_entries = redo_num_entries;
+
+	return cfg;
+}
+
+/*
+ * redo_log_config_delete -- frees redo context
+ */
+void
+redo_log_config_delete(struct redo_ctx *ctx)
+{
+	Free(ctx);
 }
 
 /*
  * redo_log_nflags -- (internal) get number of finish flags set
  */
-static size_t
+size_t
 redo_log_nflags(struct redo_log *redo, size_t nentries)
 {
 	size_t ret = 0;
@@ -75,14 +118,14 @@ redo_log_nflags(struct redo_log *redo, size_t nentries)
  * redo_log_store -- (internal) store redo log entry at specified index
  */
 void
-redo_log_store(PMEMobjpool *pop, struct redo_log *redo, size_t index,
+redo_log_store(struct redo_ctx *ctx, struct redo_log *redo, size_t index,
 		uint64_t offset, uint64_t value)
 {
 	LOG(15, "redo %p index %zu offset %ju value %ju",
 			redo, index, offset, value);
 
 	ASSERTeq(offset & REDO_FINISH_FLAG, 0);
-	ASSERT(index < REDO_NUM_ENTRIES);
+	ASSERT(index < ctx->redo_num_entries);
 
 	redo[index].offset = offset;
 	redo[index].value = value;
@@ -92,80 +135,84 @@ redo_log_store(PMEMobjpool *pop, struct redo_log *redo, size_t index,
  * redo_log_store_last -- (internal) store last entry at specified index
  */
 void
-redo_log_store_last(PMEMobjpool *pop, struct redo_log *redo, size_t index,
+redo_log_store_last(struct redo_ctx *ctx, struct redo_log *redo, size_t index,
 		uint64_t offset, uint64_t value)
 {
 	LOG(15, "redo %p index %zu offset %ju value %ju",
 			redo, index, offset, value);
 
 	ASSERTeq(offset & REDO_FINISH_FLAG, 0);
-	ASSERT(index < REDO_NUM_ENTRIES);
+	ASSERT(index < ctx->redo_num_entries);
 
 	/* store value of last entry */
 	redo[index].value = value;
 
+	void *fctx = ctx->flush_ctx;
+
 	/* persist all redo log entries */
-	pop->persist(pop, redo, (index + 1) * sizeof(struct redo_log));
+	ctx->persist(fctx, redo, (index + 1) * sizeof(struct redo_log));
 
 	/* store and persist offset of last entry */
 	redo[index].offset = offset | REDO_FINISH_FLAG;
-	pop->persist(pop, &redo[index].offset, sizeof(redo[index].offset));
+	ctx->persist(fctx, &redo[index].offset, sizeof(redo[index].offset));
 }
 
 /*
  * redo_log_set_last -- (internal) set finish flag in specified entry
  */
 void
-redo_log_set_last(PMEMobjpool *pop, struct redo_log *redo, size_t index)
+redo_log_set_last(struct redo_ctx *ctx, struct redo_log *redo, size_t index)
 {
 	LOG(15, "redo %p index %zu", redo, index);
 
-	ASSERT(index < REDO_NUM_ENTRIES);
+	ASSERT(index < ctx->redo_num_entries);
+
+	void *fctx = ctx->flush_ctx;
 
 	/* persist all redo log entries */
-	pop->persist(pop, redo, (index + 1) * sizeof(struct redo_log));
+	ctx->persist(fctx, redo, (index + 1) * sizeof(struct redo_log));
 
 	/* set finish flag of last entry and persist */
 	redo[index].offset |= REDO_FINISH_FLAG;
-	pop->persist(pop, &redo[index].offset, sizeof(redo[index].offset));
+	ctx->persist(fctx, &redo[index].offset, sizeof(redo[index].offset));
 }
 
 /*
  * redo_log_process -- (internal) process redo log entries
  */
 void
-redo_log_process(PMEMobjpool *pop, struct redo_log *redo,
-		size_t nentries)
+redo_log_process(struct redo_ctx *ctx, struct redo_log *redo, size_t nentries)
 {
 	LOG(15, "redo %p nentries %zu", redo, nentries);
 
 #ifdef DEBUG
-	ASSERTeq(redo_log_check(pop, redo, nentries), 0);
+	ASSERTeq(redo_log_check(ctx, redo, nentries), 0);
 #endif
 
 	uint64_t *val;
+	void *fctx = ctx->flush_ctx;
 	while ((redo->offset & REDO_FINISH_FLAG) == 0) {
-		val = (uint64_t *)((uintptr_t)pop->addr + redo->offset);
+		val = (uint64_t *)((uintptr_t)ctx->base + redo->offset);
 		VALGRIND_ADD_TO_TX(val, sizeof(*val));
 		*val = redo->value;
 		VALGRIND_REMOVE_FROM_TX(val, sizeof(*val));
 
-		pop->flush(pop, val, sizeof(uint64_t));
+		ctx->flush(fctx, val, sizeof(uint64_t));
 
 		redo++;
 	}
 
 	uint64_t offset = redo->offset & REDO_FLAG_MASK;
-	val = (uint64_t *)((uintptr_t)pop->addr + offset);
+	val = (uint64_t *)((uintptr_t)ctx->base + offset);
 	VALGRIND_ADD_TO_TX(val, sizeof(*val));
 	*val = redo->value;
 	VALGRIND_REMOVE_FROM_TX(val, sizeof(*val));
 
-	pop->persist(pop, val, sizeof(uint64_t));
+	ctx->persist(fctx, val, sizeof(uint64_t));
 
 	redo->offset = 0;
 
-	pop->persist(pop, &redo->offset, sizeof(redo->offset));
+	ctx->persist(fctx, &redo->offset, sizeof(redo->offset));
 }
 
 /*
@@ -174,8 +221,7 @@ redo_log_process(PMEMobjpool *pop, struct redo_log *redo,
  * The redo_log_recover shall be preceded by redo_log_check call.
  */
 void
-redo_log_recover(PMEMobjpool *pop, struct redo_log *redo,
-		size_t nentries)
+redo_log_recover(struct redo_ctx *ctx, struct redo_log *redo, size_t nentries)
 {
 	LOG(15, "redo %p nentries %zu", redo, nentries);
 
@@ -183,14 +229,14 @@ redo_log_recover(PMEMobjpool *pop, struct redo_log *redo,
 	ASSERT(nflags < 2);
 
 	if (nflags == 1)
-		redo_log_process(pop, redo, nentries);
+		redo_log_process(ctx, redo, nentries);
 }
 
 /*
  * redo_log_check -- (internal) check consistency of redo log entries
  */
 int
-redo_log_check(PMEMobjpool *pop, struct redo_log *redo, size_t nentries)
+redo_log_check(struct redo_ctx *ctx, struct redo_log *redo, size_t nentries)
 {
 	LOG(15, "redo %p nentries %zu", redo, nentries);
 
@@ -202,8 +248,10 @@ redo_log_check(PMEMobjpool *pop, struct redo_log *redo, size_t nentries)
 	}
 
 	if (nflags == 1) {
+		void *cctx = ctx->check_offset_ctx;
+
 		while ((redo->offset & REDO_FINISH_FLAG) == 0) {
-			if (!redo_log_check_offset(pop, redo->offset)) {
+			if (!ctx->check_offset(cctx, redo->offset)) {
 				LOG(15, "redo %p invalid offset %ju",
 						redo, redo->offset);
 				return -1;
@@ -212,11 +260,29 @@ redo_log_check(PMEMobjpool *pop, struct redo_log *redo, size_t nentries)
 		}
 
 		uint64_t offset = redo->offset & REDO_FLAG_MASK;
-		if (!redo_log_check_offset(pop, offset)) {
+		if (!ctx->check_offset(cctx, offset)) {
 			LOG(15, "redo %p invalid offset %ju", redo, offset);
 			return -1;
 		}
 	}
 
 	return 0;
+}
+
+/*
+ * redo_log_offset -- returns offset
+ */
+uint64_t
+redo_log_offset(struct redo_log *redo)
+{
+	return redo->offset & REDO_FLAG_MASK;
+}
+
+/*
+ * redo_log_is_last -- returns 1/0
+ */
+int
+redo_log_is_last(struct redo_log *redo)
+{
+	return redo->offset & REDO_FINISH_FLAG;
 }

src/libpmemobj/lane.h
@@ -35,6 +35,9 @@
  */
 #define LANE_SECTION_LEN 1024
 
+#define REDO_NUM_ENTRIES \
+	((LANE_SECTION_LEN - 2 * sizeof(uint64_t)) / sizeof(struct redo_log))
+
 /*
  * Distance between lanes used by threads required to prevent threads from
  * false sharing part of lanes array. Used if properly spread lanes are

src/libpmemobj/list.c
@@ -198,13 +198,14 @@ list_set_oid_redo_log(PMEMobjpool *pop,
 			ASSERTeq(oidp->pool_uuid_lo, 0);
 		uint64_t oid_uuid_off = OBJ_PTR_TO_OFF(pop,
 				&oidp->pool_uuid_lo);
-		redo_log_store(pop, redo, redo_index, oid_uuid_off,
+		redo_log_store(pop->redo, redo, redo_index, oid_uuid_off,
 				pop->uuid_lo);
 		redo_index += 1;
 	}
 
 	uint64_t oid_off_off = OBJ_PTR_TO_OFF(pop, &oidp->off);
-	redo_log_store(pop, redo, redo_index, oid_off_off, obj_doffset);
+	redo_log_store(pop->redo, redo, redo_index, oid_off_off,
+			obj_doffset);
 
 	return redo_index + 1;
 }
@@ -222,14 +223,14 @@ list_update_head(PMEMobjpool *pop,
 
 	uint64_t pe_first_off_off = OBJ_PTR_TO_OFF(pop, &head->pe_first.off);
 
-	redo_log_store(pop, redo, redo_index + 0,
+	redo_log_store(pop->redo, redo, redo_index + 0,
 			pe_first_off_off, first_offset);
 
 	if (head->pe_first.pool_uuid_lo == 0) {
 		uint64_t pe_first_uuid_off = OBJ_PTR_TO_OFF(pop,
 				&head->pe_first.pool_uuid_lo);
 
-		redo_log_store(pop, redo, redo_index + 1,
+		redo_log_store(pop->redo, redo, redo_index + 1,
 				pe_first_uuid_off, pop->uuid_lo);
 
 		return redo_index + 2;
@@ -319,8 +320,10 @@ list_fill_entry_redo_log(PMEMobjpool *pop,
 	u64_add_offset(&next_off_off, args->pe_offset);
 	u64_add_offset(&prev_off_off, args->pe_offset);
 
-	redo_log_store(pop, redo, redo_index + 0, next_off_off, next_offset);
-	redo_log_store(pop, redo, redo_index + 1, prev_off_off, prev_offset);
+	redo_log_store(pop->redo, redo, redo_index + 0, next_off_off,
+			next_offset);
+	redo_log_store(pop->redo, redo, redo_index + 1, prev_off_off,
+			prev_offset);
 
 	return redo_index + 2;
 }
@@ -350,9 +353,9 @@ list_remove_single(PMEMobjpool *pop,
 		uint64_t prev_next_off = prev_off + NEXT_OFF;
 		u64_add_offset(&prev_next_off, args->pe_offset);
 
-		redo_log_store(pop, redo, redo_index + 0,
+		redo_log_store(pop->redo, redo, redo_index + 0,
 				next_prev_off, prev_off);
-		redo_log_store(pop, redo, redo_index + 1,
+		redo_log_store(pop->redo, redo, redo_index + 1,
 				prev_next_off, next_off);
 		redo_index += 2;
 
@@ -388,9 +391,9 @@ list_insert_before(PMEMobjpool *pop,
 					NEXT_OFF;
 	u64_add_offset(&dest_prev_next_off, args_common->pe_offset);
 
-	redo_log_store(pop, redo, redo_index + 0,
+	redo_log_store(pop->redo, redo, redo_index + 0,
 			dest_prev_off, args_common->obj_doffset);
-	redo_log_store(pop, redo, redo_index + 1,
+	redo_log_store(pop->redo, redo, redo_index + 1,
 			dest_prev_next_off, args_common->obj_doffset);
 
 	return redo_index + 2;
@@ -418,9 +421,9 @@ list_insert_after(PMEMobjpool *pop,
 					PREV_OFF;
 	u64_add_offset(&dest_next_prev_off, args_common->pe_offset);
 
-	redo_log_store(pop, redo, redo_index + 0,
+	redo_log_store(pop->redo, redo, redo_index + 0,
 			dest_next_off, args_common->obj_doffset);
-	redo_log_store(pop, redo, redo_index + 1,
+	redo_log_store(pop->redo, redo, redo_index + 1,
 			dest_next_prev_off, args_common->obj_doffset);
 
 	return redo_index + 2;
@@ -582,9 +585,9 @@ list_insert_new(PMEMobjpool *pop,
 	}
 
 	/* clear the obj_offset in lane section */
-	redo_log_store_last(pop, redo, redo_index, sec_off_off, 0);
+	redo_log_store_last(pop->redo, redo, redo_index, sec_off_off, 0);
 
-	redo_log_process(pop, redo, REDO_NUM_ENTRIES);
+	redo_log_process(pop->redo, redo, REDO_NUM_ENTRIES);
 
 	ret = 0;
 
@@ -700,9 +703,9 @@ list_insert(PMEMobjpool *pop,
 	redo_index = list_fill_entry_redo_log(pop, redo, redo_index,
 			&args_common, next_offset, prev_offset, 1);
 
-	redo_log_set_last(pop, redo, redo_index - 1);
+	redo_log_set_last(pop->redo, redo, redo_index - 1);
 
-	redo_log_process(pop, redo, REDO_NUM_ENTRIES);
+	redo_log_process(pop->redo, redo, REDO_NUM_ENTRIES);
 
 	pmemobj_mutex_unlock_nofail(pop, &head->lock);
 err:
@@ -770,9 +773,10 @@ list_remove_free(PMEMobjpool *pop, size_t pe_offset,
 	else
 		oidp->off = 0;
 
-	redo_log_store_last(pop, redo, redo_index, sec_off_off, obj_doffset);
+	redo_log_store_last(pop->redo, redo, redo_index, sec_off_off,
+			obj_doffset);
 
-	redo_log_process(pop, redo, REDO_NUM_ENTRIES);
+	redo_log_process(pop->redo, redo, REDO_NUM_ENTRIES);
 
 	/*
 	 * Don't need to fill next and prev offsets of removing element
@@ -870,9 +874,9 @@ list_remove(PMEMobjpool *pop,
 	redo_index = list_fill_entry_redo_log(pop, redo, redo_index,
 			&args_common, 0, 0, 0);
 
-	redo_log_set_last(pop, redo, redo_index - 1);
+	redo_log_set_last(pop->redo, redo, redo_index - 1);
 
-	redo_log_process(pop, redo, REDO_NUM_ENTRIES);
+	redo_log_process(pop->redo, redo, REDO_NUM_ENTRIES);
 
 	pmemobj_mutex_unlock_nofail(pop, &head->lock);
 err:
@@ -1009,9 +1013,9 @@ list_move(PMEMobjpool *pop,
 			&args_common, next_offset, prev_offset, set_uuid);
 
 redo_last:
-	redo_log_set_last(pop, redo, redo_index - 1);
+	redo_log_set_last(pop->redo, redo, redo_index - 1);
 
-	redo_log_process(pop, redo, REDO_NUM_ENTRIES);
+	redo_log_process(pop->redo, redo, REDO_NUM_ENTRIES);
 
 unlock:
 	list_mutexes_unlock(pop, head_new, head_old);
@@ -1032,7 +1036,7 @@ lane_list_recovery(PMEMobjpool *pop, struct lane_section_layout *section_layout)
 	struct lane_list_layout *section =
 		(struct lane_list_layout *)section_layout;
 
-	redo_log_recover(pop, section->redo, REDO_NUM_ENTRIES);
+	redo_log_recover(pop->redo, section->redo, REDO_NUM_ENTRIES);
 
 	if (section->obj_offset) {
 		/* alloc or free recovery */
@@ -1054,7 +1058,7 @@ lane_list_check(PMEMobjpool *pop, struct lane_section_layout *section_layout)
 		(struct lane_list_layout *)section_layout;
 
 	int ret = 0;
-	if ((ret = redo_log_check(pop,
+	if ((ret = redo_log_check(pop->redo,
 			section->redo, REDO_NUM_ENTRIES)) != 0) {
 		ERR("list lane: redo log check failed");
 

src/libpmemobj/memops.c
@@ -150,17 +150,18 @@ static void
 operation_process_persistent_redo(struct operation_context *ctx)
 {
 	struct operation_entry *e;
+	struct redo_ctx *redo = ctx->pop->redo;
 
 	size_t i;
 	for (i = 0; i < ctx->nentries[ENTRY_PERSISTENT]; ++i) {
 		e = &ctx->entries[ENTRY_PERSISTENT][i];
 
-		redo_log_store(ctx->pop, ctx->redo, i,
+		redo_log_store(redo, ctx->redo, i,
 			OBJ_PTR_TO_OFF(ctx->pop, e->ptr), e->value);
 	}
 
-	redo_log_set_last(ctx->pop, ctx->redo, i - 1);
-	redo_log_process(ctx->pop, ctx->redo, i);
+	redo_log_set_last(redo, ctx->redo, i - 1);
+	redo_log_process(redo, ctx->redo, i);
 }
 
 /*

src/libpmemobj/obj.h
@@ -35,6 +35,7 @@
  */
 
 #include <stddef.h>
+#include "redo.h"
 
 #define PMEMOBJ_LOG_PREFIX "libpmemobj"
 #define PMEMOBJ_LOG_LEVEL_VAR "PMEMOBJ_LOG_LEVEL"
@@ -147,6 +148,7 @@ struct pmemobjpool {
 	uint64_t uuid_lo;
 
 	struct pmemobjpool *replica;	/* next replica */
+	struct redo_ctx *redo;
 
 	/* per-replica functions: pmem or non-pmem */
 	persist_local_fn persist_local;	/* persist function */
@@ -164,7 +166,7 @@ struct pmemobjpool {
 
 	PMEMmutex rootlock;	/* root object lock */
 	int is_master_replica;
-	char unused2[1804];
+	char unused2[1796];
 };
 
 /*

src/libpmemobj/pmalloc.c
@@ -685,7 +685,7 @@ lane_allocator_recovery(PMEMobjpool *pop, struct lane_section_layout *section)
 	struct lane_alloc_layout *sec =
 		(struct lane_alloc_layout *)section;
 
-	redo_log_recover(pop, sec->redo, ALLOC_REDO_LOG_SIZE);
+	redo_log_recover(pop->redo, sec->redo, ALLOC_REDO_LOG_SIZE);
 
 	return 0;
 }
@@ -701,8 +701,8 @@ lane_allocator_check(PMEMobjpool *pop, struct lane_section_layout *section)
 	struct lane_alloc_layout *sec =
 		(struct lane_alloc_layout *)section;
 
-	int ret;
-	if ((ret = redo_log_check(pop, sec->redo, ALLOC_REDO_LOG_SIZE)) != 0)
+	int ret = redo_log_check(pop->redo, sec->redo, ALLOC_REDO_LOG_SIZE);
+	if (ret != 0)
 		ERR("allocator lane: redo log check failed");
 
 	return ret;

src/libpmemobj/redo.h
@@ -30,18 +30,17 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-/*
- * redo.h -- internal definitions for redo log
- */
+#ifndef LIBPMEMOBJ_REDO_H
+#define LIBPMEMOBJ_REDO_H
 
 /*
- * Finish flag at the least significant bit
+ * redo.h -- redo log public interface
  */
-#define REDO_FINISH_FLAG	((uint64_t)1<<0)
-#define REDO_FLAG_MASK		(~REDO_FINISH_FLAG)
 
-#define REDO_NUM_ENTRIES \
-((LANE_SECTION_LEN - 2 * sizeof(uint64_t)) / sizeof(struct redo_log))
+#include <stddef.h>
+#include <stdint.h>
+
+struct redo_ctx;
 
 /*
  * redo_log -- redo log entry
@@ -51,13 +50,35 @@ struct redo_log {
 	uint64_t value;
 };
 
-void redo_log_store(PMEMobjpool *pop, struct redo_log *redo, size_t index,
+typedef void (*redo_persist_fn)(void *ctx, const void *, size_t);
+typedef void (*redo_flush_fn)(void *ctx, const void *, size_t);
+typedef int  (*redo_check_offset_fn)(void *ctx, uint64_t offset);
+
+struct redo_ctx *redo_log_config_new(void *base,
+		redo_persist_fn persist,
+		redo_flush_fn flush,
+		redo_check_offset_fn check_offset,
+		void *flush_ctx,
+		void *check_offset_ctx,
+		unsigned redo_num_entries);
+
+void redo_log_config_delete(struct redo_ctx *ctx);
+
+void redo_log_store(struct redo_ctx *ctx, struct redo_log *redo, size_t index,
 		uint64_t offset, uint64_t value);
-void redo_log_store_last(PMEMobjpool *pop, struct redo_log *redo, size_t index,
-		uint64_t offset, uint64_t value);
-void redo_log_set_last(PMEMobjpool *pop, struct redo_log *redo, size_t index);
-void redo_log_process(PMEMobjpool *pop, struct redo_log *redo,
+void redo_log_store_last(struct redo_ctx *ctx, struct redo_log *redo,
+		size_t index, uint64_t offset, uint64_t value);
+void redo_log_set_last(struct redo_ctx *ctx, struct redo_log *redo,
+		size_t index);
+void redo_log_process(struct redo_ctx *ctx, struct redo_log *redo,
 		size_t nentries);
-void redo_log_recover(PMEMobjpool *pop, struct redo_log *redo,
+void redo_log_recover(struct redo_ctx *ctx, struct redo_log *redo,
 		size_t nentries);
-int redo_log_check(PMEMobjpool *pop, struct redo_log *redo, size_t nentries);
+int redo_log_check(struct redo_ctx *ctx, struct redo_log *redo,
+		size_t nentries);
+
+size_t redo_log_nflags(struct redo_log *redo, size_t nentries);
+uint64_t redo_log_offset(struct redo_log *redo);
+int redo_log_is_last(struct redo_log *redo);
+
+#endif

commit 63b25b30bc47b8f18a967fe2c5bf1668b0016498

obj: fix redo log initialization
We need to initialize pop->redo before recovery and do it for all
replicas.

Please note that this code will be simplified when the next part
of the refactoring will land.

Broken since ec117b1fb4058fdda57d408628584eafda0fcbc1.

src/libpmemobj/obj.c
@@ -838,6 +838,16 @@ pmemobj_replica_init_remote(PMEMobjpool *rep, struct pool_set *set,
 }
 
 /*
+ * redo_log_check_offset -- (internal) check if offset is valid
+ */
+static int
+redo_log_check_offset(void *ctx, uint64_t offset)
+{
+	PMEMobjpool *pop = ctx;
+	return OBJ_OFF_IS_VALID(pop, offset);
+}
+
+/*
  * pmemobj_replica_init -- (internal) initialize runtime part of the replica
  */
 static int
@@ -876,21 +886,26 @@ pmemobj_replica_init(PMEMobjpool *rep, struct pool_set *set, unsigned repidx,
 		rep->memset_persist = NULL;
 	}
 
+	int ret;
 	if (repset->remote)
-		return pmemobj_replica_init_remote(rep, set, repidx,
+		ret = pmemobj_replica_init_remote(rep, set, repidx,
 							runtime_nlanes, create);
 	else
-		return pmemobj_replica_init_local(rep, repset->is_pmem);
-}
+		ret = pmemobj_replica_init_local(rep, repset->is_pmem);
+	if (ret)
+		return ret;
 
-/*
- * redo_log_check_offset -- (internal) check if offset is valid
- */
-static int
-redo_log_check_offset(void *ctx, uint64_t offset)
-{
-	PMEMobjpool *pop = ctx;
-	return OBJ_OFF_IS_VALID(pop, offset);
+	rep->redo = redo_log_config_new(rep->addr,
+			(redo_persist_fn)rep->persist,
+			(redo_flush_fn)rep->flush,
+			redo_log_check_offset,
+			rep,
+			rep,
+			REDO_NUM_ENTRIES);
+	if (!rep->redo)
+		return -1;
+
+	return 0;
 }
 
 /*
@@ -915,16 +930,6 @@ pmemobj_runtime_init(PMEMobjpool *pop, int rdonly, int boot, unsigned nlanes)
 	pop->rdonly = rdonly;
 
 	pop->uuid_lo = pmemobj_get_uuid_lo(pop);
-
-	pop->redo = redo_log_config_new(pop->addr,
-			(redo_persist_fn)pop->persist,
-			(redo_flush_fn)pop->flush,
-			redo_log_check_offset,
-			pop,
-			pop,
-			REDO_NUM_ENTRIES);
-	if (!pop->redo)
-		return -1;
 
 	pop->lanes_desc.runtime_nlanes = nlanes;
 
@@ -1358,6 +1363,9 @@ pmemobj_unmap_replicas(PMEMobjpool *pop)
 
 	while (pop) {
 		PMEMobjpool *next_replica = pop->replica;
+
+		redo_log_config_delete(pop->redo);
+
 		if (pop->remote == NULL) {
 			VALGRIND_REMOVE_PMEM_MAPPING(pop->addr, pop->size);
 			util_unmap(pop->addr, pop->size);
@@ -1388,9 +1396,6 @@ pmemobj_cleanup(PMEMobjpool *pop)
 	heap_cleanup(pop);
 
 	lane_cleanup(pop);
-
-	redo_log_config_delete(pop->redo);
-	pop->redo = NULL;
 
 	VALGRIND_DO_DESTROY_MEMPOOL(pop);
 

src/libpmemobj/redo.c
@@ -72,8 +72,10 @@ redo_log_config_new(void *base,
 		unsigned redo_num_entries)
 {
 	struct redo_ctx *cfg = Malloc(sizeof(*cfg));
-	if (!cfg)
+	if (!cfg) {
+		ERR("!can't create redo log config");
 		return NULL;
+	}
 
 	cfg->base = base;
 	cfg->persist = persist;
@@ -224,6 +226,7 @@ void
 redo_log_recover(struct redo_ctx *ctx, struct redo_log *redo, size_t nentries)
 {
 	LOG(15, "redo %p nentries %zu", redo, nentries);
+	ASSERTne(ctx, NULL);
 
 	size_t nflags = redo_log_nflags(redo, nentries);
 	ASSERT(nflags < 2);
@@ -239,6 +242,7 @@ int
 redo_log_check(struct redo_ctx *ctx, struct redo_log *redo, size_t nentries)
 {
 	LOG(15, "redo %p nentries %zu", redo, nentries);
+	ASSERTne(ctx, NULL);
 
 	size_t nflags = redo_log_nflags(redo, nentries);
 