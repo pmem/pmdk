8b857a230c05f79ba9ea733ac114845f8bc5621c
Krzysztof Filipek <krzysztof.filipek@intel.com>
2016-07-19 12:20:03 +0200

    common: allow empty string in xxx_LOG_FILE env var
    If PMEM*_LOG_FILE environment variable is empty, it is treated as if
    it is not set at all.

--- src/common/out.c
+++ src/common/out.c
@@ -200,7 +200,7 @@ out_init(const char *log_prefix, const char *log_level_var,
 		}
 	}
 
-	if ((log_file = getenv(log_file_var)) != NULL) {
+	if ((log_file = getenv(log_file_var)) != NULL && log_file[0] != '\0') {
 		size_t cc = strlen(log_file);
 
 		/* reserve more than enough space for a PID + '\0' */
