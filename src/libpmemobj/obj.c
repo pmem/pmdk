pmem/pmdk

commit 90218d45a214a9e33678abe45390a6565afbc351

win: add initial support for pool sets
- Changes memory mapping alignment to 64K (Windows only).
  This is because 'offset' argument of MapViewOfFile() must be aligned
  to allocation granularity (64K), not the page size.  This also affects
  minimum blk pool size (PMEMBLK_MIN_POOL).
- Modifies pool set mapping/unmaping algorithm.
  On Windows, it is not allowed to make a mapping that is larger than
  the file size, so we cannot create a mapping for the first part of the
  pool set with the size of the entire pool set to reserve a contiguous
  memory region, then partially override it with subsequent part's
  mappings.

src/libpmemobj/obj.c
@@ -143,7 +143,7 @@ obj_init(void)
 {
 	LOG(3, NULL);
 
-	COMPILE_ERROR_ON(sizeof(struct pmemobjpool) != 8192);
+	COMPILE_ERROR_ON(sizeof(struct pmemobjpool) != POOL_HDR_SIZE + 4096);
 
 #ifdef USE_COW_ENV
 	char *env = getenv("PMEMOBJ_COW");
@@ -311,7 +311,7 @@ obj_norep_drain(PMEMobjpool *pop)
 	pop->drain_local();
 }
 
-static void pmemobj_cleanup(PMEMobjpool *pop);
+static void obj_pool_cleanup(PMEMobjpool *pop);
 
 /*
  * obj_handle_remote_persist_error -- (internal) handle remote persist
@@ -323,7 +323,7 @@ obj_handle_remote_persist_error(PMEMobjpool *pop)
 	LOG(1, "pop %p", pop);
 
 	ERR("error clean up...");
-	pmemobj_cleanup(pop);
+	obj_pool_cleanup(pop);
 
 	FATAL("Fatal error of remote persist. Aborting...");
 }
@@ -1045,6 +1045,8 @@ pmemobj_create(const char *path, const char *layout, size_t poolsize,
 			rep->replica = set->replica[r + 1]->part[0].addr;
 	}
 
+	pop->set = set;
+
 	/* create pool descriptor */
 	if (pmemobj_descr_create(pop, layout, set->poolsize) != 0) {
 		LOG(2, "creation of pool descriptor failed");
@@ -1062,10 +1064,6 @@ pmemobj_create(const char *path, const char *layout, size_t poolsize,
 
 	if (util_poolset_chmod(set, mode))
 		goto err;
-
-	util_poolset_fdclose(set);
-
-	util_poolset_free(set);
 
 	LOG(3, "pop %p", pop);
 
@@ -1261,6 +1259,8 @@ pmemobj_open_common(const char *path, const char *layout, int cow, int boot)
 			rep->replica = set->replica[r + 1]->part[0].addr;
 	}
 
+	pop->set = set;
+
 	if (boot) {
 		/* check consistency of 'master' replica */
 		if (pmemobj_check_basic(pop) == 0) {
@@ -1320,9 +1320,6 @@ pmemobj_open_common(const char *path, const char *layout, int cow, int boot)
 		goto err;
 	}
 
-	util_poolset_fdclose(set);
-	util_poolset_free(set);
-
 #ifdef USE_VG_MEMCHECK
 	if (boot)
 		pmemobj_vg_boot(pop);
@@ -1354,25 +1351,21 @@ pmemobj_open(const char *path, const char *layout)
 }
 
 /*
- * pmemobj_unmap_replicas -- (internal) unmap all the local replicas,
- *                           close remote ones and free their headers
+ * obj_replicas_cleanup -- (internal) free resources allocated for replicas
  */
 static void
-pmemobj_unmap_replicas(PMEMobjpool *pop)
+obj_replicas_cleanup(struct pool_set *set)
 {
-	LOG(3, "pop %p", pop);
+	LOG(3, "set %p", set);
 
-	while (pop) {
-		PMEMobjpool *next_replica = pop->replica;
+	for (unsigned r = 0; r < set->nreplicas; r++) {
+		struct pool_replica *rep = set->replica[r];
 
+		PMEMobjpool *pop = rep->part[0].addr;
 		redo_log_config_delete(pop->redo);
 
-		if (pop->remote == NULL) {
-			VALGRIND_REMOVE_PMEM_MAPPING(pop->addr, pop->size);
-			util_unmap(pop->addr, pop->size);
-		} else {
+		if (pop->remote != NULL) {
 			LOG(4, "closing remote replica %p", pop->remote);
-			util_pool_close_remote(pop->remote);
 			pop->remote = NULL;
 
 			Free(pop->node_addr);
@@ -1381,16 +1374,14 @@ pmemobj_unmap_replicas(PMEMobjpool *pop)
 			LOG(4, "freeing remote pool's header %p", pop->addr);
 			Free(pop->addr);
 		}
-		pop = next_replica;
 	}
-	util_remote_unload();
 }
 
 /*
- * pmemobj_cleanup -- (internal) cleanup the pool and unmap
+ * obj_pool_cleanup -- (internal) cleanup the pool and unmap
  */
 static void
-pmemobj_cleanup(PMEMobjpool *pop)
+obj_pool_cleanup(PMEMobjpool *pop)
 {
 	LOG(3, "pop %p", pop);
 
@@ -1400,7 +1391,9 @@ pmemobj_cleanup(PMEMobjpool *pop)
 
 	VALGRIND_DO_DESTROY_MEMPOOL(pop);
 
-	pmemobj_unmap_replicas(pop);
+	/* unmap all the replicas */
+	obj_replicas_cleanup(pop->set);
+	util_poolset_close(pop->set, 0);
 }
 
 /*
@@ -1440,7 +1433,7 @@ pmemobj_close(PMEMobjpool *pop)
 
 #endif /* _WIN32 */
 
-	pmemobj_cleanup(pop);
+	obj_pool_cleanup(pop);
 }
 
 /*
@@ -1470,9 +1463,11 @@ pmemobj_check(const char *path, const char *layout)
 	}
 
 	if (consistent) {
-		pmemobj_cleanup(pop);
+		obj_pool_cleanup(pop);
 	} else {
-		pmemobj_unmap_replicas(pop);
+		/* unmap all the replicas */
+		obj_replicas_cleanup(pop->set);
+		util_poolset_close(pop->set, 0);
 	}
 
 	if (consistent)

src/common/mmap.h
@@ -67,7 +67,7 @@ void *util_map_tmpfile(const char *dir, size_t size, size_t req_align);
 #define RANGE_RO(addr, len)
 #define RANGE_RW(addr, len)
 
-#endif	/* DEBUG */
+#endif /* DEBUG */
 
 void util_mmap_init(void);
 
@@ -77,5 +77,27 @@ int util_range_none(void *addr, size_t len);
 
 char *util_map_hint_unused(void *addr, size_t len, size_t align);
 char *util_map_hint(size_t len, size_t req_align);
+
+#define MEGABYTE ((uintptr_t)1 << 20)
+#define GIGABYTE ((uintptr_t)1 << 30)
+
+/*
+ * util_map_hint_align -- choose the desired mapping alignment
+ *
+ * Use 2MB/1GB page alignment only if the mapping length is at least
+ * twice as big as the page size.
+ */
+static inline size_t
+util_map_hint_align(size_t len, size_t req_align)
+{
+	size_t align = Mmap_align;
+	if (req_align)
+		align = req_align;
+	else if (len >= 2 * GIGABYTE)
+		align = GIGABYTE;
+	else if (len >= 4 * MEGABYTE)
+		align = 2 * MEGABYTE;
+	return align;
+}
 
 #endif

src/common/mmap_linux.c
@@ -42,9 +42,6 @@
 
 #define PROCMAXLEN 2048 /* maximum expected line length in /proc files */
 
-#define MEGABYTE ((uintptr_t)1 << 20)
-#define GIGABYTE ((uintptr_t)1 << 30)
-
 /*
  * util_map_hint_unused -- use /proc to determine a hint address for mmap()
  *
@@ -149,18 +146,8 @@ util_map_hint(size_t len, size_t req_align)
 
 	char *addr;
 
-	/*
-	 * Choose the desired alignment based on the requested length.
-	 * Use 2MB/1GB page alignment only if the mapping length is at least
-	 * twice as big as the page size.
-	 */
-	size_t align = Pagesize;
-	if (req_align)
-		align = req_align;
-	else if (len >= 2 * GIGABYTE)
-		align = GIGABYTE;
-	else if (len >= 4 * MEGABYTE)
-		align = 2 * MEGABYTE;
+	/* choose the desired alignment based on the requested length */
+	size_t align = util_map_hint_align(len, req_align);
 
 	if (Mmap_no_random) {
 		LOG(4, "user-defined hint %p", (void *)Mmap_hint);

src/common/mmap_windows.c
@@ -41,12 +41,39 @@
 
 /*
  * util_map_hint -- determine hint address for mmap()
- *
- * XXX - no Windows implementation yet
  */
 char *
 util_map_hint(size_t len, size_t req_align)
 {
-	LOG(4, "hint not supported on windows");
+	LOG(3, "len %zu req_align %zu", len, req_align);
+
+#if 0
+	/*
+	 * XXX - for large mappings, we can end up with error
+	 * ERROR_COMMITMENT_LIMIT (0x5AF) - The paging file is too small
+	 * for this operation to complete.
+	 * Need to find an equivalent of Linux memory overcommit feature.
+	 */
+
+	/* choose the desired alignment based on the requested length */
+	size_t align = util_map_hint_align(len, req_align);
+
+	/*
+	 * Create dummy mapping to find an unused region of given size.
+	 * Request for increased size for later address alignment.
+	 */
+	char *addr = mmap(NULL, len + align, PROT_READ|PROT_WRITE,
+			MAP_PRIVATE|MAP_ANONYMOUS|MAP_NORESERVE, -1, 0);
+	if (addr != MAP_FAILED) {
+		LOG(4, "system choice %p", addr);
+		munmap(addr, len + align);
+		addr = (char *)roundup((uintptr_t)addr, align);
+	}
+
+	LOG(4, "hint %p", addr);
+	return addr;
+#else
+	LOG(4, "hint %p", NULL);
 	return NULL;
+#endif
 }

src/common/set.c
@@ -280,7 +280,7 @@ static const char *parser_errstr[PARSER_MAX_CODE] = {
 };
 
 /*
- * util_map_part -- map a header of a pool set
+ * util_map_hdr -- map a header of a pool set
  */
 int
 util_map_hdr(struct pool_set_part *part, int flags)
@@ -326,7 +326,6 @@ util_unmap_hdr(struct pool_set_part *part)
 	return 0;
 }
 
-
 /*
  * util_map_part -- map a part of a pool set
  */
@@ -337,13 +336,13 @@ util_map_part(struct pool_set_part *part, void *addr, size_t size,
 	LOG(3, "part %p addr %p size %zu offset %zu flags %d",
 		part, addr, size, offset, flags);
 
-	ASSERTeq((uintptr_t)addr % Pagesize, 0);
-	ASSERTeq(offset % Pagesize, 0);
-	ASSERTeq(size % Pagesize, 0);
+	ASSERTeq((uintptr_t)addr % Mmap_align, 0);
+	ASSERTeq(offset % Mmap_align, 0);
+	ASSERTeq(size % Mmap_align, 0);
 	ASSERT(((off_t)offset) >= 0);
 
 	if (!size)
-		size = (part->filesize & ~(Pagesize - 1)) - offset;
+		size = (part->filesize & ~(Mmap_align - 1)) - offset;
 
 	void *addrp = mmap(addr, size,
 		PROT_READ|PROT_WRITE, flags, part->fd, (off_t)offset);
@@ -430,10 +429,10 @@ util_poolset_close(struct pool_set *set, int del)
 	int oerrno = errno;
 
 	for (unsigned r = 0; r < set->nreplicas; r++) {
+		util_replica_close(set, r);
+
 		struct pool_replica *rep = set->replica[r];
 		if (rep->remote == NULL) {
-			/* it's enough to unmap part[0] only */
-			util_unmap_part(&rep->part[0]);
 			for (unsigned p = 0; p < rep->nparts; p++) {
 				if (rep->part[p].fd != -1)
 					(void) close(rep->part[p].fd);
@@ -443,12 +442,6 @@ util_poolset_close(struct pool_set *set, int del)
 				}
 			}
 		} else {
-			LOG(4, "freeing volatile header of remote "
-				"replica #%u", r);
-			Free(rep->part[0].addr);
-			rep->part[0].addr = NULL;
-			rep->part[0].size = 0;
-
 			LOG(4, "closing remote replica #%u", r);
 			Rpmem_close(rep->remote->rpp);
 			rep->remote->rpp = NULL;
@@ -515,8 +508,10 @@ util_poolset_fdclose(struct pool_set *set)
 		for (unsigned p = 0; p < rep->nparts; p++) {
 			struct pool_set_part *part = &rep->part[p];
 
-			if (part->fd != -1)
+			if (part->fd != -1) {
 				close(part->fd);
+				part->fd = -1;
+			}
 		}
 	}
 }
@@ -682,11 +677,11 @@ util_poolset_set_size(struct pool_set *set)
 	set->poolsize = SIZE_MAX;
 	for (unsigned r = 0; r < set->nreplicas; r++) {
 		struct pool_replica *rep = set->replica[r];
-		rep->repsize = sizeof(struct pool_hdr);
+		rep->repsize = Mmap_align;
 		for (unsigned p = 0; p < rep->nparts; p++) {
 			rep->repsize +=
-				(rep->part[p].filesize & ~(Pagesize - 1)) -
-				sizeof(struct pool_hdr);
+				(rep->part[p].filesize & ~(Mmap_align - 1)) -
+				Mmap_align;
 		}
 
 		/*
@@ -952,8 +947,8 @@ util_poolset_single(const char *path, size_t filesize, int create)
 	rep->remote = NULL;
 	set->remote = 0;
 
-	/* round down to the nearest page boundary */
-	rep->repsize = rep->part[0].filesize & ~(Pagesize - 1);
+	/* round down to the nearest mapping alignment boundary */
+	rep->repsize = rep->part[0].filesize & ~(Mmap_align - 1);
 
 	set->poolsize = rep->repsize;
 
@@ -1597,9 +1592,14 @@ util_replica_create_local(struct pool_set *set, unsigned repidx, int flags,
 		return -1;
 	}
 
+	size_t mapsize = rep->part[0].filesize & ~(Mmap_align - 1);
+
 	/* map the first part and reserve space for remaining parts */
-	/* XXX investigate this idea of reserving space on Windows */
+#ifndef _WIN32
 	if (util_map_part(&rep->part[0], addr, rep->repsize, 0, flags) != 0) {
+#else
+	if (util_map_part(&rep->part[0], addr, mapsize, 0, flags) != 0) {
+#endif
 		LOG(2, "pool mapping failed - replica #%u part #0", repidx);
 		return -1;
 	}
@@ -1633,22 +1633,22 @@ util_replica_create_local(struct pool_set *set, unsigned repidx, int flags,
 
 	set->zeroed &= rep->part[0].created;
 
-	size_t mapsize = rep->part[0].filesize & ~(Pagesize - 1);
 	addr = (char *)rep->part[0].addr + mapsize;
 
 	/*
-	 * map the remaining parts of the usable pool space (4K-aligned)
+	 * map the remaining parts of the usable pool space
+	 * (aligned to memory mapping granularity)
 	 */
 	for (unsigned p = 1; p < rep->nparts; p++) {
 		/* map data part */
-		if (util_map_part(&rep->part[p], addr, 0, POOL_HDR_SIZE,
+		if (util_map_part(&rep->part[p], addr, 0, Mmap_align,
 				flags | MAP_FIXED) != 0) {
 			LOG(2, "usable space mapping failed - part #%d", p);
 			goto err;
 		}
 
 		VALGRIND_REGISTER_PMEM_FILE(rep->part[p].fd,
-			rep->part[p].addr, rep->part[p].size, POOL_HDR_SIZE);
+			rep->part[p].addr, rep->part[p].size, Mmap_align);
 
 		mapsize += rep->part[p].size;
 		set->zeroed &= rep->part[p].created;
@@ -1971,8 +1971,14 @@ util_replica_open_local(struct pool_set *set, unsigned repidx, int flags)
 		return -1;
 	}
 
+	size_t mapsize = rep->part[0].filesize & ~(Mmap_align - 1);
+
 	/* map the first part and reserve space for remaining parts */
+#ifndef _WIN32
 	if (util_map_part(&rep->part[0], addr, rep->repsize, 0, flags) != 0) {
+#else
+	if (util_map_part(&rep->part[0], addr, mapsize, 0, flags) != 0) {
+#endif
 		LOG(2, "pool mapping failed - part #0");
 		return -1;
 	}
@@ -1989,23 +1995,22 @@ util_replica_open_local(struct pool_set *set, unsigned repidx, int flags)
 		}
 	}
 
-	size_t mapsize = rep->part[0].filesize & ~(Pagesize - 1);
 	addr = (char *)rep->part[0].addr + mapsize;
 
 	/*
 	 * map the remaining parts of the usable pool space
-	 * (4K-aligned)
+	 * (aligned to memory mapping granularity)
 	 */
 	for (unsigned p = 1; p < rep->nparts; p++) {
 		/* map data part */
-		if (util_map_part(&rep->part[p], addr, 0, POOL_HDR_SIZE,
+		if (util_map_part(&rep->part[p], addr, 0, Mmap_align,
 				flags | MAP_FIXED) != 0) {
 			LOG(2, "usable space mapping failed - part #%d", p);
 			goto err;
 		}
 
 		VALGRIND_REGISTER_PMEM_FILE(rep->part[p].fd,
-			rep->part[p].addr, rep->part[p].size, POOL_HDR_SIZE);
+			rep->part[p].addr, rep->part[p].size, Mmap_align);
 
 		mapsize += rep->part[p].size;
 		addr = (char *)addr + rep->part[p].size;

src/common/util.c
@@ -46,6 +46,9 @@
 /* library-wide page size */
 unsigned long long Pagesize;
 
+/* allocation/mmap granularity */
+unsigned long long Mmap_align;
+
 /*
  * our versions of malloc & friends start off pointing to the libc versions
  */
@@ -211,8 +214,19 @@ util_parse_size(const char *str, size_t *sizep)
 void
 util_init(void)
 {
+	/* XXX - replace sysconf() with util_get_sys_xxx() */
 	if (Pagesize == 0)
 		Pagesize = (unsigned long) sysconf(_SC_PAGESIZE);
+
+#ifndef _WIN32
+	Mmap_align = Pagesize;
+#else
+	if (Mmap_align == 0) {
+		SYSTEM_INFO si;
+		GetSystemInfo(&si);
+		Mmap_align = si.dwAllocationGranularity;
+	}
+#endif
 
 #if defined(USE_VG_PMEMCHECK) || defined(USE_VG_HELGRIND) ||\
 	defined(USE_VG_MEMCHECK)

src/common/util.h
@@ -41,6 +41,7 @@
 #include <stdint.h>
 
 extern unsigned long long Pagesize;
+extern unsigned long long Mmap_align;
 
 /*
  * overridable names for malloc & friends used by this library

src/include/libpmemblk.h
@@ -66,8 +66,15 @@ const char *pmemblk_check_version(
 		unsigned major_required,
 		unsigned minor_required);
 
-/* minimum pool size: 16MB + 8KB (minimum BTT size + header size) */
+/* XXX - unify minimum pool size for both OS-es */
+
+#ifndef _WIN32
+/* minimum pool size: 16MB + 4KB (minimum BTT size + mmap alignment) */
 #define PMEMBLK_MIN_POOL ((size_t)((1u << 20) * 16 + (1u << 10) * 8))
+#else
+/* minimum pool size: 16MB + 64KB (minimum BTT size + mmap alignment) */
+#define PMEMBLK_MIN_POOL ((size_t)((1u << 20) * 16 + (1u << 10) * 64))
+#endif
 
 #define PMEMBLK_MIN_BLK ((size_t)512)
 

commit e9fc51f2001f250b11f6be0d3f0ab665c4ab775a

obj: fix double free corruption
Fix regression introduced by 90218d4

src/libpmemobj/obj.c
@@ -1365,14 +1365,13 @@ obj_replicas_cleanup(struct pool_set *set)
 		redo_log_config_delete(pop->redo);
 
 		if (pop->rpp != NULL) {
-			LOG(4, "closing remote replica %p", pop->rpp);
+			/*
+			 * remote replica will be closed in util_poolset_close
+			 */
 			pop->rpp = NULL;
 
 			Free(pop->node_addr);
 			Free(pop->pool_desc);
-
-			LOG(4, "freeing remote pool's header %p", pop->addr);
-			Free(pop->addr);
 		}
 	}
 }