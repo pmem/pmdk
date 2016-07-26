7dda47b92cf44ab2ba282d7e0d76527897b9c84e
Jan M Michalski <jan.m.michalski@intel.com>
2016-07-26 16:05:58 +0200

    test: fix and enable obj_rpmem_constructor unittest

--- src/test/obj_constructor/obj_constructor.c
+++ src/test/obj_constructor/obj_constructor.c
@@ -39,20 +39,36 @@
 #include "unittest.h"
 
 /*
+ * Command line toggle indicating use of a bigger node structure for querying
+ * pool size expressed in a number of possible allocations. A small node
+ * structure results in a great number of allocations impossible to replicate
+ * in assumed timeout. It is required by unittests using remote replication to
+ * pass on Travis.
+ */
+#define USE_BIG_ALLOC "--big-alloc"
+
+/*
  * Layout definition
  */
 POBJ_LAYOUT_BEGIN(constr);
 POBJ_LAYOUT_ROOT(constr, struct root);
 POBJ_LAYOUT_TOID(constr, struct node);
+POBJ_LAYOUT_TOID(constr, struct node_big);
 POBJ_LAYOUT_END(constr);
 
 struct root {
 	TOID(struct node) n;
 	POBJ_LIST_HEAD(head, struct node) list;
+	POBJ_LIST_HEAD(head_big, struct node_big) list_big;
 };
 
 struct node {
 	POBJ_LIST_ENTRY(struct node) next;
+};
+
+struct node_big {
+	POBJ_LIST_ENTRY(struct node_big) next;
+	int weight[2048];
 };
 
 static int
@@ -86,10 +102,21 @@ main(int argc, char *argv[])
 	START(argc, argv, "obj_constructor");
 
 	/* root doesn't count */
-	UT_COMPILE_ERROR_ON(POBJ_LAYOUT_TYPES_NUM(constr) != 1);
+	UT_COMPILE_ERROR_ON(POBJ_LAYOUT_TYPES_NUM(constr) != 2);
 
-	if (argc != 2)
-		UT_FATAL("usage: %s file-name", argv[0]);
+	int big = (argc == 3 && strcmp(argv[2], USE_BIG_ALLOC) == 0);
+	size_t node_size;
+	size_t next_off;
+
+	if (big) {
+		node_size = sizeof(struct node_big);
+		next_off = offsetof(struct node_big, next);
+	} else if (argc == 2) {
+		node_size = sizeof(struct node);
+		next_off = offsetof(struct node, next);
+	} else {
+		UT_FATAL("usage: %s file-name [ %s ]", argv[0], USE_BIG_ALLOC);
+	}
 
 	const char *path = argv[1];
 
@@ -98,6 +125,7 @@ main(int argc, char *argv[])
 	int ret;
 	TOID(struct root) root;
 	TOID(struct node) node;
+	TOID(struct node_big) node_big;
 
 	if ((pop = pmemobj_create(path, POBJ_LAYOUT_NAME(constr),
 			0, S_IWUSR | S_IRUSR)) == NULL)
@@ -108,8 +136,7 @@ main(int argc, char *argv[])
 	 * cancellation didn't damage the heap in any way.
 	 */
 	int allocs = 0;
-	while (pmemobj_alloc(pop, NULL, sizeof(struct node), 1,
-			NULL, NULL) == 0)
+	while (pmemobj_alloc(pop, NULL, node_size, 1, NULL, NULL) == 0)
 		allocs++;
 
 	UT_ASSERTne(allocs, 0);
@@ -126,14 +153,12 @@ main(int argc, char *argv[])
 	UT_ASSERTeq(errno, ECANCELED);
 
 	errno = 0;
-	ret = pmemobj_alloc(pop, NULL, sizeof(struct node), 1,
-			node_constr_cancel, NULL);
+	ret = pmemobj_alloc(pop, NULL, node_size, 1, node_constr_cancel, NULL);
 	UT_ASSERTeq(ret, -1);
 	UT_ASSERTeq(errno, ECANCELED);
 
 	/* the same number of allocations should be possible. */
-	while (pmemobj_alloc(pop, NULL, sizeof(struct node), 1,
-			NULL, NULL) == 0)
+	while (pmemobj_alloc(pop, NULL, node_size, 1, NULL, NULL) == 0)
 		allocs--;
 	UT_ASSERTeq(allocs, 0);
 	POBJ_FOREACH_SAFE(pop, oid, next)
@@ -144,10 +169,17 @@ main(int argc, char *argv[])
 	UT_ASSERT(!TOID_IS_NULL(root));
 
 	errno = 0;
-	node.oid = pmemobj_list_insert_new(pop, offsetof(struct node, next),
-			&D_RW(root)->list, OID_NULL, 0, sizeof(struct node),
-			1, node_constr_cancel, NULL);
-	UT_ASSERT(TOID_IS_NULL(node));
+	if (big) {
+		node_big.oid = pmemobj_list_insert_new(pop, next_off,
+				&D_RW(root)->list_big, OID_NULL, 0, node_size,
+				1, node_constr_cancel, NULL);
+		UT_ASSERT(TOID_IS_NULL(node_big));
+	} else {
+		node.oid = pmemobj_list_insert_new(pop, next_off,
+				&D_RW(root)->list, OID_NULL, 0, node_size,
+				1, node_constr_cancel, NULL);
+		UT_ASSERT(TOID_IS_NULL(node));
+	}
 	UT_ASSERTeq(errno, ECANCELED);
 
 	pmemobj_alloc(pop, &oid, sizeof(struct foo), 1,
