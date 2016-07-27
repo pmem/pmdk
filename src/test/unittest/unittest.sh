b1058f55ce8d856036fb31889555cb568b98538d
Jan M Michalski <jan.m.michalski@intel.com>
2016-07-27 13:59:16 +0200

    test: compress files before copying from a remote node

--- src/test/unittest/unittest.sh
+++ src/test/unittest/unittest.sh
@@ -1342,21 +1342,19 @@ function copy_files_from_node() {
 	local N=$1
 	local DIR=$2
 	shift 2
-	[ "$*" == "" ] &&\
-		echo "error: copy_files_from_node(): no files provided" >&2 && exit 1
+	[ "$*" == "" ] \
+		&& echo "error: copy_files_from_node(): no files provided" >&2 \
+		&& exit 1
 
-	# copy all required files
+	# compress required files, copy and extract
 	local REMOTE_DIR=${NODE_WORKING_DIR[$N]}/$curtestdir
-
-	if [ $# -ne 1 ]; then
-		FILE_STRING=$(printf ",%s" "$@")
-		FILE_STRING=\{${FILE_STRING:1}\}
-	else
-		FILE_STRING=$1
-	fi
-
-	run_command scp $SCP_OPTS ${NODE[$N]}:$REMOTE_DIR/$FILE_STRING $DIR
-
+	local temp_file=node_${N}_temp_file.tar
+	run_command ssh $SSH_OPTS ${NODE[$N]} "cd $REMOTE_DIR && tar -cf $temp_file $@"
+	run_command scp $SCP_OPTS ${NODE[$N]}:$REMOTE_DIR/$temp_file $DIR
+	cd $DIR \
+		&& tar -xf $temp_file \
+		&& rm $temp_file \
+		&& cd - > /dev/null
 	return 0
 }
 
