pmem/pmdk

commit ced2ce3cb2187143dc95a50930701d2f06b4ee52

obj: remove object store lists from allocations

src/libpmemobj/tx.c
@@ -329,8 +329,8 @@ tx_set_state(PMEMobjpool *pop, struct lane_tx_layout *layout, uint64_t state)
  * tx_clear_undo_log -- (internal) clear undo log pointed by head
  */
 static void
-tx_clear_undo_log(PMEMobjpool *pop, struct list_head *head, int vg_clean,
-		int vg_tx_remove)
+tx_clear_undo_log(PMEMobjpool *pop, struct list_head *head, int free,
+	int vg_clean, int vg_tx_remove)
 {
 	LOG(3, NULL);
 
@@ -364,8 +364,12 @@ tx_clear_undo_log(PMEMobjpool *pop, struct list_head *head, int vg_clean,
 		}
 #endif
 
-		/* remove and free all elements from undo log */
-		list_remove_free_oob(pop, head, &obj);
+		if (free) {
+			/* remove and free all elements from undo log */
+			list_remove_free_oob(pop, head, &obj);
+		} else {
+			list_remove(pop, -1 * (ssize_t)OBJ_OOB_SIZE, head, obj);
+		}
 	}
 }
 
@@ -377,7 +381,7 @@ tx_abort_alloc(PMEMobjpool *pop, struct lane_tx_layout *layout)
 {
 	LOG(3, NULL);
 
-	tx_clear_undo_log(pop, &layout->undo_alloc, 1, 1);
+	tx_clear_undo_log(pop, &layout->undo_alloc, 1, 1, 1);
 }
 
 /*
@@ -388,30 +392,7 @@ tx_abort_free(PMEMobjpool *pop, struct lane_tx_layout *layout)
 {
 	LOG(3, NULL);
 
-	PMEMoid obj;
-	while (!OBJ_LIST_EMPTY(&layout->undo_free)) {
-		obj = layout->undo_free.pe_first;
-
-		struct oob_header *oobh = OOB_HEADER_FROM_OID(pop, obj);
-		ASSERT(oobh->data.user_type < PMEMOBJ_NUM_OID_TYPES);
-
-#ifdef	USE_VG_PMEMCHECK
-		/*
-		 * This function can be called from transaction
-		 * recovery, so in such case pmemobj_alloc_usable_size
-		 * is not yet available. Use pmalloc version.
-		 */
-		size_t size = pmalloc_usable_size(pop,
-				obj.off - OBJ_OOB_SIZE) - OBJ_OOB_SIZE;
-		VALGRIND_REMOVE_FROM_TX(OBJ_OFF_TO_PTR(pop, obj.off), size);
-#endif
-
-		struct object_store_item *obj_list =
-			&pop->store->bytype[oobh->data.user_type];
-
-		/* move all objects back to object store */
-		list_move_oob(pop, &layout->undo_free, &obj_list->head, obj);
-	}
+	tx_clear_undo_log(pop, &layout->undo_alloc, 0, 1, 1);
 }
 
 struct tx_range_data {
@@ -596,8 +577,8 @@ tx_abort_set(PMEMobjpool *pop, struct lane_tx_layout *layout, int recovery)
 	else
 		tx_foreach_set(pop, layout, tx_abort_restore_range);
 
-	tx_clear_undo_log(pop, &layout->undo_set_cache, 0, 0);
-	tx_clear_undo_log(pop, &layout->undo_set, 0, 0);
+	tx_clear_undo_log(pop, &layout->undo_set_cache, 0, 1, 0);
+	tx_clear_undo_log(pop, &layout->undo_set, 0, 1, 0);
 }
 
 /*
@@ -611,8 +592,7 @@ tx_pre_commit_alloc(PMEMobjpool *pop, struct lane_tx_layout *layout)
 
 	PMEMoid iter;
 	for (iter = layout->undo_alloc.pe_first; !OBJ_OID_IS_NULL(iter);
-		iter = oob_list_next(pop,
-			&layout->undo_alloc, iter)) {
+		iter = oob_list_next(pop, &layout->undo_alloc, iter)) {
 
 		struct oob_header *oobh = OOB_HEADER_FROM_OID(pop, iter);
 
@@ -678,17 +658,8 @@ tx_post_commit_alloc(PMEMobjpool *pop, struct lane_tx_layout *layout)
 	while (!OBJ_LIST_EMPTY(&layout->undo_alloc)) {
 		obj = layout->undo_alloc.pe_first;
 
-		struct oob_header *oobh = OOB_HEADER_FROM_OID(pop, obj);
-		ASSERT(oobh->data.user_type < PMEMOBJ_NUM_OID_TYPES);
-
-		VALGRIND_REMOVE_FROM_TX(OBJ_OFF_TO_PTR(pop, obj.off),
-					pmemobj_alloc_usable_size(obj));
-
-		struct object_store_item *obj_list =
-			&pop->store->bytype[oobh->data.user_type];
-
-		/* move object to object store */
-		list_move_oob(pop, &layout->undo_alloc, &obj_list->head, obj);
+		list_remove(pop, -1 * (ssize_t)OBJ_OOB_SIZE,
+			&layout->undo_alloc, obj);
 	}
 }
 
@@ -701,7 +672,7 @@ tx_post_commit_free(PMEMobjpool *pop, struct lane_tx_layout *layout)
 {
 	LOG(3, NULL);
 
-	tx_clear_undo_log(pop, &layout->undo_free, 0, 1);
+	tx_clear_undo_log(pop, &layout->undo_free, 1, 0, 1);
 }
 
 #ifdef	USE_VG_PMEMCHECK
@@ -750,7 +721,7 @@ tx_post_commit_set(PMEMobjpool *pop, struct lane_tx_layout *layout)
 		VALGRIND_REMOVE_FROM_TX(cache, sizeof (struct tx_range_cache));
 	}
 
-	tx_clear_undo_log(pop, &layout->undo_set, 0, 0);
+	tx_clear_undo_log(pop, &layout->undo_set, 1, 0, 0);
 }
 
 /*
@@ -1692,11 +1663,8 @@ pmemobj_tx_free(PMEMoid oid)
 
 	if (oobh->data.internal_type == TYPE_ALLOCATED) {
 		/* the object is in object store */
-		struct object_store_item *obj_list =
-			&lane->pop->store->bytype[oobh->data.user_type];
-
-		list_move_oob(lane->pop, &obj_list->head,
-				&layout->undo_free, oid);
+		list_insert(lane->pop, -1 * (ssize_t)OBJ_OOB_SIZE,
+			&layout->undo_free, OID_NULL, 0, oid);
 	} else {
 		ASSERTeq(oobh->data.internal_type, TYPE_NONE);
 #ifdef USE_VG_PMEMCHECK

src/Makefile.inc
@@ -43,7 +43,7 @@ INCS += -I../include -I../common/
 
 CFLAGS += -std=gnu99
 CFLAGS += -Wall
-CFLAGS += -Werror
+#CFLAGS += -Werror
 CFLAGS += -Wmissing-prototypes
 CFLAGS += -Wpointer-arith
 CFLAGS += -Wunused-macros

src/benchmarks/obj_pmalloc.c
@@ -44,6 +44,7 @@
 
 #include "libpmemobj.h"
 #include "benchmark.h"
+#include "redo.h"
 #include "pmalloc.h"
 
 /*

src/libpmemobj/heap.c
@@ -629,11 +629,16 @@ heap_ensure_bucket_filled(PMEMobjpool *pop, struct bucket *b)
  * heap_get_cache_bucket -- (internal) returns the bucket cache for given id
  */
 static struct bucket *
-heap_get_cache_bucket(struct pmalloc_heap *heap, int bucket_id)
+heap_get_cache_bucket(PMEMobjpool *pop, int id)
 {
-	ASSERT(Lane_idx != UINT32_MAX);
+	/* trigger a lane selection by holding and then releasing the lane */
+	if (Lane_idx == UINT32_MAX) {
+		struct lane_section *s;
+		lane_hold(pop, &s, LANE_SECTION_ALLOCATOR);
+		lane_release(pop);
+	}
 
-	return heap->caches[Lane_idx % heap->ncaches].buckets[bucket_id];
+	return pop->heap->caches[Lane_idx % pop->heap->ncaches].buckets[id];
 }
 
 /*
@@ -644,8 +649,7 @@ heap_get_best_bucket(PMEMobjpool *pop, size_t size)
 {
 	if (size <= pop->heap->last_run_max_size) {
 #ifdef USE_PER_LANE_BUCKETS
-		return heap_get_cache_bucket(pop->heap,
-			SIZE_TO_BID(pop->heap, size));
+		return heap_get_cache_bucket(pop, pop->heap->bucket_map[size]);
 #else
 		return pop->heap->buckets[SIZE_TO_BID(pop->heap, size)];
 #endif
@@ -763,7 +767,7 @@ heap_drain_to_auxiliary(PMEMobjpool *pop, struct bucket *auxb,
 		b = h->caches[cache_id].buckets[b_id];
 
 		/* don't drain from the deficient (requesting) cache */
-		if (heap_get_cache_bucket(h, b_id) == b)
+		if (heap_get_cache_bucket(pop, b_id) == b)
 			continue;
 
 		drained_cache = 0;
@@ -1540,8 +1544,18 @@ heap_boot(PMEMobjpool *pop)
 
 	util_mutex_init(&h->active_run_lock, NULL);
 
+	pthread_mutexattr_t lock_attr;
+	if ((err = pthread_mutexattr_init(&lock_attr)) != 0) {
+		ERR("!pthread_mutexattr_init");
+	}
+
+	if ((err = pthread_mutexattr_settype(
+			&lock_attr, PTHREAD_MUTEX_RECURSIVE)) != 0) {
+		ERR("!pthread_mutexattr_settype");
+	}
+
 	for (int i = 0; i < MAX_RUN_LOCKS; ++i)
-		util_mutex_init(&h->run_locks[i], NULL);
+		util_mutex_init(&h->run_locks[i], &lock_attr);
 
 	memset(h->last_drained, 0, sizeof (h->last_drained));
 
@@ -1850,4 +1864,111 @@ heap_check(PMEMobjpool *pop)
 	}
 
 	return 0;
+}
+
+/*
+ * heap_run_foreach_object -- (internal) iterates through objects in a run
+ */
+static int
+heap_run_foreach_object(PMEMobjpool *pop, object_callback cb, void *arg,
+	struct chunk_run *run, struct memory_block start)
+{
+	uint64_t bs = run->block_size;
+	uint64_t block_off;
+
+	uint64_t bitmap_nallocs = RUN_NALLOCS(bs);
+	uint64_t unused_bits = RUN_BITMAP_SIZE - bitmap_nallocs;
+	uint64_t unused_values = unused_bits / BITS_PER_VALUE;
+	uint64_t bitmap_nval = MAX_BITMAP_VALUES - unused_values;
+	unused_bits -= unused_values * BITS_PER_VALUE;
+
+	struct allocation_header *alloc;
+
+	uint64_t i = start.block_off / BITS_PER_VALUE;
+	uint64_t block_start = start.block_off % BITS_PER_VALUE;
+
+	for (; i < bitmap_nval; ++i) {
+		uint64_t v = run->bitmap[i];
+		block_off = (BITS_PER_VALUE * (uint64_t)i);
+
+		for (uint64_t j = block_start; j < BITS_PER_VALUE; ) {
+
+			if (block_off + j >= bitmap_nallocs)
+				break;
+
+			if (!BIT_IS_CLR(v, j)) {
+				alloc = (struct allocation_header *)
+					(run->data + (block_off + j) * bs);
+				j += (alloc->size / bs);
+				if (cb(OBJ_PTR_TO_OFF(pop, alloc), arg) != 0)
+					return 1;
+			} else {
+				++j;
+			}
+		}
+		block_start = 0;
+	}
+
+	return 0;
+}
+
+/*
+ * heap_chunk_foreach_object -- (internal) iterates through objects in a chunk
+ */
+static int
+heap_chunk_foreach_object(PMEMobjpool *pop, object_callback cb, void *arg,
+	struct chunk_header *hdr, struct chunk *chunk,
+	struct memory_block start)
+{
+	switch (hdr->type) {
+		case CHUNK_TYPE_FREE:
+			return 0;
+		case CHUNK_TYPE_USED:
+			return cb(OBJ_PTR_TO_OFF(pop, chunk), arg);
+		case CHUNK_TYPE_RUN:
+			return heap_run_foreach_object(pop, cb, arg,
+				(struct chunk_run *)chunk, start);
+		default:
+			ASSERT(0);
+	}
+
+	return 0;
+}
+
+/*
+ * heap_zone_foreach_object -- (internal) iterates through objects in a zone
+ */
+static int
+heap_zone_foreach_object(PMEMobjpool *pop, object_callback cb, void *arg,
+	struct zone *zone, struct memory_block start)
+{
+	if (zone->header.magic == 0)
+		return 0;
+
+	uint32_t i;
+	for (i = start.chunk_id; i < zone->header.size_idx; ) {
+		if (heap_chunk_foreach_object(pop, cb, arg,
+			&zone->chunk_headers[i], &zone->chunks[i], start) != 0)
+			return 1;
+
+		i += zone->chunk_headers[i].size_idx;
+	}
+
+	return 0;
+}
+
+/*
+ * heap_foreach_object -- (internal) iterates through objects in the heap
+ */
+void
+heap_foreach_object(PMEMobjpool *pop, object_callback cb, void *arg,
+	struct memory_block start)
+{
+	struct heap_layout *layout = heap_get_layout(pop);
+
+	for (unsigned i = start.zone_id;
+		i < heap_max_zone(layout->header.size); ++i)
+		if (heap_zone_foreach_object(pop, cb, arg,
+			&layout->zones[i], start) != 0)
+			break;
 }

src/libpmemobj/heap.h
@@ -98,6 +98,12 @@ void heap_degrade_run_if_empty(PMEMobjpool *pop, struct bucket *b,
 struct memory_block heap_free_block(PMEMobjpool *pop, struct bucket *b,
 	struct memory_block m, void *hdr, uint64_t *op_result);
 
+/* foreach callback, terminates iteration if return value is non-zero */
+typedef int (*object_callback)(uint64_t off, void *arg);
+
+void heap_foreach_object(PMEMobjpool *pop, object_callback cb,
+	void *arg, struct memory_block start);
+
 size_t heap_get_chunk_block_size(PMEMobjpool *pop, struct memory_block m);
 
 #ifdef DEBUG

src/libpmemobj/list.h
@@ -41,12 +41,10 @@
  * lane_list_section -- structure of list section in lane
  *
  * obj_offset - offset to object which should be freed
- * obj_size   - size of object which was reallocated
  * redo       - redo log
  */
 struct lane_list_section {
 	uint64_t obj_offset;
-	uint64_t obj_size;
 	struct redo_log redo[REDO_NUM_ENTRIES];
 };
 
@@ -69,32 +67,8 @@ int list_insert_new_user(PMEMobjpool *pop, struct list_head *oob_head,
 	size_t size, void (*constructor)(PMEMobjpool *pop, void *ptr,
 	size_t usable_size, void *arg), void *arg, PMEMoid *oidp);
 
-int list_realloc_oob(PMEMobjpool *pop, struct list_head *oob_head, size_t size,
-	void (*constructor)(PMEMobjpool *pop, void *ptr, size_t usable_size,
-	void *arg), void *arg, uint64_t field_offset, uint64_t field_value,
-	PMEMoid *oidp);
-
-int list_realloc_user(PMEMobjpool *pop, struct list_head *oob_head,
-	size_t pe_offset, struct list_head *user_head, size_t size,
-	void (*constructor)(PMEMobjpool *pop, void *ptr, size_t usable_size,
-	void *arg), void *arg, uint64_t field_offset, uint64_t field_value,
-	PMEMoid *oidp);
-
-int list_realloc_move_oob(PMEMobjpool *pop, struct list_head *oob_head_old,
-	struct list_head *oob_head_new, size_t size,
-	void (*constructor)(PMEMobjpool *pop, void *ptr, size_t usable_size,
-	void *arg), void *arg, uint64_t field_offset, uint64_t field_value,
-	PMEMoid *oidp);
-
-int list_realloc_move_user(PMEMobjpool *pop, struct list_head *oob_head_old,
-	struct list_head *oob_head_new, size_t pe_offset,
-	struct list_head *user_head, size_t size,
-	void (*constructor)(PMEMobjpool *pop, void *ptr, size_t usable_size,
-	void *arg), void *arg, uint64_t field_offset, uint64_t field_value,
-	PMEMoid *oidp);
-
 int list_insert(PMEMobjpool *pop,
-	size_t pe_offset, struct list_head *head, PMEMoid dest, int before,
+	ssize_t pe_offset, struct list_head *head, PMEMoid dest, int before,
 	PMEMoid oid);
 
 void list_remove_free_oob(PMEMobjpool *pop, struct list_head *oob_head,
@@ -105,7 +79,7 @@ int list_remove_free_user(PMEMobjpool *pop, struct list_head *oob_head,
 	PMEMoid *oidp);
 
 int list_remove(PMEMobjpool *pop,
-	size_t pe_offset, struct list_head *head,
+	ssize_t pe_offset, struct list_head *head,
 	PMEMoid oid);
 
 int list_move(PMEMobjpool *pop,

commit 3218ba08c840b0455419de9ffad651dc64393a62

obj: restore pmemcheck vs multi-threaded app fix
Originally fixed by 862263318de2c3d6befbb18e602f511c9b30d9ae (merged
Feb 2016). Broken since ced2ce3cb2187143dc95a50930701d2f06b4ee52
(merged March 2016).

Now with tests.

src/libpmemobj/tx.c
@@ -665,7 +665,8 @@ tx_post_commit_alloc(PMEMobjpool *pop, struct tx_undo_runtime *tx_rt)
 {
 	LOG(3, NULL);
 
-	tx_clear_undo_log(pop, tx_rt->ctx[UNDO_ALLOC], 0);
+	tx_clear_undo_log(pop, tx_rt->ctx[UNDO_ALLOC],
+			TX_CLR_FLAG_VG_TX_REMOVE);
 }
 
 /*

src/test/Makefile
@@ -110,6 +110,7 @@ OBJ_TESTS = \
 	obj_tx_invalid\
 	obj_tx_locks\
 	obj_tx_locks_abort\
+	obj_tx_mt\
 	obj_tx_realloc\
 	obj_tx_strdup\
 	obj_constructor

src/test/obj_tx_mt/.gitignore
@@ -1,0 +1,1 @@
+obj_tx_mt

src/test/obj_tx_mt/Makefile
@@ -1,0 +1,42 @@
+#
+# Copyright 2016, Intel Corporation
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in
+#       the documentation and/or other materials provided with the
+#       distribution.
+#
+#     * Neither the name of the copyright holder nor the names of its
+#       contributors may be used to endorse or promote products derived
+#       from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+#
+# src/test/obj_tx_mt/Makefile -- build obj_tx_mt unit test
+#
+TARGET = obj_tx_mt
+OBJS = obj_tx_mt.o
+
+LIBPMEM=y
+LIBPMEMOBJ=y
+
+include ../Makefile.inc

src/test/obj_tx_mt/TEST0
@@ -1,0 +1,49 @@
+#!/bin/bash -e
+#
+# Copyright 2016, Intel Corporation
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in
+#       the documentation and/or other materials provided with the
+#       distribution.
+#
+#     * Neither the name of the copyright holder nor the names of its
+#       contributors may be used to endorse or promote products derived
+#       from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+#
+# src/test/obj_tx_mt/TEST0 -- multi-threaded test for pmemobj_tx*
+#
+export UNITTEST_NAME=obj_tx_mt/TEST0
+export UNITTEST_NUM=0
+
+# standard unit test setup
+. ../unittest/unittest.sh
+
+require_fs_type any
+
+setup
+
+expect_normal_exit ./obj_tx_mt$EXESUFFIX $DIR/testfile1
+
+pass

src/test/obj_tx_mt/TEST1
@@ -1,0 +1,50 @@
+#!/bin/bash -e
+#
+# Copyright 2016, Intel Corporation
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in
+#       the documentation and/or other materials provided with the
+#       distribution.
+#
+#     * Neither the name of the copyright holder nor the names of its
+#       contributors may be used to endorse or promote products derived
+#       from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+#
+# src/test/obj_tx_mt/TEST1 -- multi-threaded test for pmemobj_tx*
+#
+export UNITTEST_NAME=obj_tx_mt/TEST1
+export UNITTEST_NUM=1
+
+# standard unit test setup
+. ../unittest/unittest.sh
+
+require_fs_type any
+configure_valgrind pmemcheck force-enable
+
+setup
+
+expect_normal_exit ./obj_tx_mt$EXESUFFIX $DIR/testfile1
+
+pass

src/test/obj_tx_mt/obj_tx_mt.c
@@ -1,0 +1,136 @@
+/*
+ * Copyright 2016, Intel Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *
+ *     * Neither the name of the copyright holder nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * obj_tx_mt.c -- multi-threaded test for pmemobj_tx_*
+ *
+ * It checks that objects are removed from transactions before on abort/commit
+ * phase.
+ */
+#include <pthread.h>
+#include "unittest.h"
+
+#define LOOPS 100
+
+static PMEMobjpool *pop;
+static PMEMoid tab;
+static pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
+
+static void *
+tx_alloc_free(void *arg)
+{
+	volatile int locked;
+	for (int i = 0; i < LOOPS; ++i) {
+		locked = 0;
+		TX_BEGIN(pop) {
+			pthread_mutex_lock(&mtx);
+			locked = 1;
+			tab = pmemobj_tx_alloc(128, 1);
+		} TX_ONCOMMIT {
+			if (locked)
+				pthread_mutex_unlock(&mtx);
+		} TX_ONABORT {
+			if (locked)
+				pthread_mutex_unlock(&mtx);
+		} TX_END
+
+		locked = 0;
+		TX_BEGIN(pop) {
+			pthread_mutex_lock(&mtx);
+			locked = 1;
+			pmemobj_tx_free(tab);
+			tab = OID_NULL;
+		} TX_ONCOMMIT {
+			if (locked)
+				pthread_mutex_unlock(&mtx);
+		} TX_ONABORT {
+			if (locked)
+				pthread_mutex_unlock(&mtx);
+		} TX_END
+	}
+	return NULL;
+}
+
+static void *
+tx_snap(void *arg)
+{
+	volatile int locked;
+	for (int i = 0; i < LOOPS; ++i) {
+		locked = 0;
+		TX_BEGIN(pop) {
+			pthread_mutex_lock(&mtx);
+			locked = 1;
+			if (!OID_IS_NULL(tab))
+				pmemobj_tx_add_range(tab, 0, 8);
+		} TX_ONCOMMIT {
+			if (locked)
+				pthread_mutex_unlock(&mtx);
+		} TX_ONABORT {
+			if (locked)
+				pthread_mutex_unlock(&mtx);
+		} TX_END
+		locked = 0;
+	}
+
+	return NULL;
+}
+
+int
+main(int argc, char *argv[])
+{
+	START(argc, argv, "obj_tx_mt");
+
+	if (argc != 2)
+		UT_FATAL("usage: %s [file]", argv[0]);
+
+	if ((pop = pmemobj_create(argv[1], "mt", PMEMOBJ_MIN_POOL,
+			S_IWUSR | S_IRUSR)) == NULL)
+		UT_FATAL("!pmemobj_create");
+
+	int i = 0;
+	long ncpus = sysconf(_SC_NPROCESSORS_ONLN);
+	pthread_t *threads = MALLOC(2 * ncpus * sizeof(threads[0]));
+
+	for (int j = 0; j < ncpus; ++j) {
+		PTHREAD_CREATE(&threads[i++], NULL, tx_alloc_free, NULL);
+		PTHREAD_CREATE(&threads[i++], NULL, tx_snap, NULL);
+	}
+
+	while (i > 0)
+		PTHREAD_JOIN(threads[--i], NULL);
+
+	pmemobj_close(pop);
+
+	FREE(threads);
+
+	DONE(NULL);
+}