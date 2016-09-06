accf7761a6e5497b93111d67ae54c4101185c2dc
Generic builder <tmsz.kapela@gmail.com>
2016-09-06 05:11:39 -0700

    doc: automatic docs update

---
+++ cpp_obj/master/cpp_html/search/functions_2.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['delete_5fpersistent',['delete_persistent',['../make__persistent_8hpp.html#a13388e739f3fe00a9c906d8f01d0d6c1',1,'nvml::obj::delete_persistent(typename detail::pp_if_not_array&lt; T &gt;::type ptr)'],['../make__persistent__array_8hpp.html#a6af17d54b190da0a129735c895d1b7a0',1,'nvml::obj::delete_persistent(typename detail::pp_if_array&lt; T &gt;::type ptr, std::size_t N)'],['../make__persistent__array_8hpp.html#a22579298bcf8e2a4708270c396f15ecd',1,'nvml::obj::delete_persistent(typename detail::pp_if_size_array&lt; T &gt;::type ptr)']]],
+  ['delete_5fpersistent_5fatomic',['delete_persistent_atomic',['../make__persistent__array__atomic_8hpp.html#a8e9137d6e143ea796022b621ebb1b236',1,'nvml::obj::delete_persistent_atomic(typename detail::pp_if_array&lt; T &gt;::type &amp;ptr, std::size_t N)'],['../make__persistent__array__atomic_8hpp.html#a94ba4aad4c2411eedb4d2107550f4900',1,'nvml::obj::delete_persistent_atomic(typename detail::pp_if_size_array&lt; T &gt;::type &amp;ptr)'],['../make__persistent__atomic_8hpp.html#ab4829c17b4db1755e79a2fa928879c54',1,'nvml::obj::delete_persistent_atomic(typename detail::pp_if_not_array&lt; T &gt;::type &amp;ptr) noexcept']]],
+  ['drain',['drain',['../classnvml_1_1obj_1_1pool__base.html#a43b185c655a2f636d3f105b5c051d583',1,'nvml::obj::pool_base']]]
+];
---
+++ cpp_obj/master/cpp_html/search/functions_c.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['shared_5fmutex',['shared_mutex',['../classnvml_1_1obj_1_1shared__mutex.html#a343ec83e072d49cd5ec27063481cf554',1,'nvml::obj::shared_mutex::shared_mutex() noexcept=default'],['../classnvml_1_1obj_1_1shared__mutex.html#af34d84d27c1f6f96edc9b4f00398bd97',1,'nvml::obj::shared_mutex::shared_mutex(const shared_mutex &amp;)=delete']]],
+  ['swap',['swap',['../classnvml_1_1obj_1_1p.html#adfd2909bbbe7d3694eefb22d8a009ef3',1,'nvml::obj::p::swap()'],['../classnvml_1_1obj_1_1persistent__ptr.html#a224446552101ff45f098c8a84145c9c8',1,'nvml::obj::persistent_ptr::swap()'],['../p_8hpp.html#a583b96064e11bcb8cb50f3f6718d7862',1,'nvml::obj::swap(p&lt; T &gt; &amp;a, p&lt; T &gt; &amp;b) noexcept'],['../persistent__ptr_8hpp.html#aeff9076f9f19fb718e4f3e20b1eecac2',1,'nvml::obj::swap(persistent_ptr&lt; T &gt; &amp;a, persistent_ptr&lt; T &gt; &amp;b) noexcept']]]
+];
