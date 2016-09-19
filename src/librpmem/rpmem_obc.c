0a5fb5b85290bc8092657cdf4ee9718972293d04
Pawel Lebioda <pawel.lebioda@intel.com>
2016-09-19 12:11:03 +0200

    rpmem: check if open pool attributes are NULL
    Ref: pmem/issues#259

--- src/librpmem/rpmem_obc.c
+++ src/librpmem/rpmem_obc.c
@@ -563,7 +563,8 @@ rpmem_obc_open(struct rpmem_obc *rpc,
 		goto err_msg_resp;
 
 	rpmem_obc_get_res(res, &resp.ibc);
-	memcpy(pool_attr, &resp.pool_attr, sizeof(*pool_attr));
+	if (pool_attr)
+		memcpy(pool_attr, &resp.pool_attr, sizeof(*pool_attr));
 
 	free(msg);
 	return 0;
