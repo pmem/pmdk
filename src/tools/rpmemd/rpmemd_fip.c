7602f30141a88331c228143bc3a2827c9c26f2a7
Jan M Michalski <jan.m.michalski@intel.com>
2016-10-03 12:59:05 +0200

    rpmemd: various fixes

--- src/tools/rpmemd/rpmemd_fip.c
+++ src/tools/rpmemd/rpmemd_fip.c
@@ -647,7 +647,8 @@ rpmemd_fip_init_gpspm(struct rpmemd_fip *fip)
 	return 0;
 err_lane_init:
 	for (unsigned j = 0; j < i; j++)
-		rpmem_fip_lane_fini(&fip->lanes[i].lane);
+		rpmem_fip_lane_fini(&fip->lanes[j].lane);
+	free(fip->lanes);
 err_alloc_lanes:
 	RPMEMD_FI_CLOSE(fip->pres_mr,
 			"unregistering GPSPM messages response buffer");
@@ -681,6 +682,10 @@ rpmemd_fip_fini_gpspm(struct rpmemd_fip *fip)
 			"unregistering GPSPM messages response buffer");
 	if (ret)
 		lret = ret;
+
+	for (unsigned i = 0; i < fip->nlanes; i++)
+		rpmem_fip_lane_fini(&fip->lanes[i].lane);
+	free(fip->lanes);
 
 	free(fip->pmsg);
 	free(fip->pres);
@@ -1080,6 +1085,7 @@ rpmemd_fip_fini(struct rpmemd_fip *fip)
 	rpmemd_fip_fini_memory(fip);
 	rpmemd_fip_fini_fabric_res(fip);
 	fi_freeinfo(fip->fi);
+	free(fip);
 }
 
 /*
@@ -1114,10 +1120,14 @@ rpmemd_fip_accept(struct rpmemd_fip *fip, int timeout)
 		goto err_accept;
 	}
 
+	fi_freeinfo(entry.info);
+
 	ret = rpmem_fip_read_eq(fip->eq, &entry,
 			FI_CONNECTED, &fip->ep->fid, -1);
 	if (ret)
 		goto err_event_connected;
+
+	fi_freeinfo(entry.info);
 
 	return 0;
 err_event_connected:
@@ -1127,6 +1137,7 @@ err_post:
 err_init_ep:
 	rpmemd_fip_fini_cq(fip);
 err_init_cq:
+	fi_freeinfo(entry.info);
 err_event_connreq:
 	return -1;
 }
