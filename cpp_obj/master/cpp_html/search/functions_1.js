2003c15378d5df3a8f8f5a2ec8e064d9361d798c
Generic builder <nvml-bot@intel.com>
2016-10-10 10:33:02 -0700

    doc: automatic docs update

--- cpp_obj/master/cpp_html/search/functions_1.js
+++ cpp_obj/master/cpp_html/search/functions_1.js
@@ -4,5 +4,5 @@ var searchData=
   ['close',['close',['../classnvml_1_1obj_1_1pool__base.html#a228e4679648b2c753e41ae3cf3256909',1,'nvml::obj::pool_base']]],
   ['commit',['commit',['../classnvml_1_1obj_1_1transaction.html#a8f036c0fa7bc23f927202943c13952a6',1,'nvml::obj::transaction']]],
   ['condition_5fvariable',['condition_variable',['../classnvml_1_1obj_1_1condition__variable.html#a21176d22b3e771cc2db739f65f2c3121',1,'nvml::obj::condition_variable::condition_variable() noexcept=default'],['../classnvml_1_1obj_1_1condition__variable.html#a650ab1e3b2c26fdfbe520c5a6bb0b34f',1,'nvml::obj::condition_variable::condition_variable(const condition_variable &amp;)=delete']]],
-  ['create',['create',['../classnvml_1_1obj_1_1pool__base.html#a8d4e09552651eb92ca807130f8faf1d1',1,'nvml::obj::pool_base::create()'],['../classnvml_1_1obj_1_1pool.html#a967d97854e7cfcc913ca3c84a474ebb2',1,'nvml::obj::pool::create()']]]
+  ['create',['create',['../classnvml_1_1obj_1_1pool__base.html#acf5f99143644c56492b8e8f95a20e64b',1,'nvml::obj::pool_base::create()'],['../classnvml_1_1obj_1_1pool.html#a106e32988ee2e780632db1ca20778ad2',1,'nvml::obj::pool::create()']]]
 ];
