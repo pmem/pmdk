027d639ebd8ad0bcc8726819e86888916a2b0bbe
Piotr Balcer <piotr.balcer@intel.com>
2016-10-24 14:49:55 +0200

    obj: increase the pool size for pmalloc_mt test
    This test had narrowly calculated pool size that did not account for
    metadata nor the per-thread allocator caches, which manifested itself as
    sporadic fails in environments with high number of CPUs.

--- src/test/obj_pmalloc_mt/obj_pmalloc_mt.c
+++ src/test/obj_pmalloc_mt/obj_pmalloc_mt.c
@@ -176,7 +176,9 @@ main(int argc, char *argv[])
 
 	if (access(argv[1], F_OK) != 0) {
 		pop = pmemobj_create(argv[1], "TEST",
-		THREADS * OPS_PER_THREAD * ALLOC_SIZE * FRAGMENTATION, 0666);
+		PMEMOBJ_MIN_POOL +
+		(THREADS * OPS_PER_THREAD * ALLOC_SIZE * FRAGMENTATION),
+		0666);
 	} else {
 		if ((pop = pmemobj_open(argv[1], "TEST")) == NULL) {
 			printf("failed to open pool\n");
