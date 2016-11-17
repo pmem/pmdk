e328fb59bb8e1d79e1f8346e25dde6d1d8e4401a
Tomasz Kapela <tomasz.kapela@intel.com>
2016-11-17 13:14:45 +0100

    common: delay clang version check
    Skip message on clang-format version was printed when the script was
    run only for .c files.

--- utils/style_check.sh
+++ utils/style_check.sh
@@ -73,10 +73,10 @@ function run_cstyle() {
 # generate diff with clang-format rules
 #
 function run_clang_check() {
-	check_clang_version
 	if [ $# -eq 0 ]; then
 		return
 	fi
+	check_clang_version
 
 	for file in $@
 	do
@@ -92,10 +92,10 @@ function run_clang_check() {
 # in-place format according to clang-format rules
 #
 function run_clang_format() {
-	check_clang_version
 	if [ $# -eq 0 ]; then
 		return
 	fi
+	check_clang_version
 
 	${clang_format_bin} -style=file -i $@
 }
