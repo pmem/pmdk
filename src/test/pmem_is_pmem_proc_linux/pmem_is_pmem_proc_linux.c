52385afd9294222745630df1f90d40cfbf5d0c36
Pawel Lebioda <pawel.lebioda@intel.com>
2016-11-24 13:21:14 +0100

    pmem: workaround for pmem_is_pmem
    This workaround parses the /proc/self/smaps in a loop if the specified
    memory range is not visible in /proc/self/smaps but the mincore(2)
    result says otherwise.
    
    Ref: pmem/issues#207

--- src/test/pmem_is_pmem_proc_linux/pmem_is_pmem_proc_linux.c
+++ src/test/pmem_is_pmem_proc_linux/pmem_is_pmem_proc_linux.c
@@ -41,7 +41,17 @@
 
 #include <dlfcn.h>
 
-char *Sfile;
+#define MAX_REGIONS 8
+#define MAX_FILES 8
+char *Sfile[MAX_FILES];
+int Nfiles;
+int Nregions;
+int Curfile;
+
+struct region {
+	uintptr_t addr;
+	size_t len;
+} Mincore[MAX_REGIONS];
 
 /*
  * fopen -- interpose on libc fopen()
@@ -55,8 +65,12 @@ fopen(const char *path, const char *mode)
 	static FILE *(*fopen_ptr)(const char *path, const char *mode);
 
 	if (strcmp(path, "/proc/self/smaps") == 0) {
-		UT_OUT("redirecting /proc/self/smaps to %s", Sfile);
-		path = Sfile;
+		UT_ASSERT(Curfile < Nfiles);
+		char *sfile = Sfile[Curfile];
+		Curfile = Curfile + 1;
+
+		UT_OUT("redirecting /proc/self/smaps to %s", sfile);
+		path = sfile;
 	}
 
 	if (fopen_ptr == NULL)
@@ -65,22 +79,66 @@ fopen(const char *path, const char *mode)
 	return (*fopen_ptr)(path, mode);
 }
 
+#define IN_RANGE(a, l, A, L)\
+	((uintptr_t)(a) >= (uintptr_t)(A) &&\
+	(uintptr_t)(a) <= ((uintptr_t)(A) + (L)) &&\
+	((uintptr_t)(a) + l) >= (uintptr_t)(A) &&\
+	((uintptr_t)(a) + l) <= ((uintptr_t)(A) + (L)))
+
+/*
+ * mincore -- interpose on libc mincore(2)
+ *
+ * Return 0 only for specified regions, otherwise return -1 with
+ * errno = ENOMEM.
+ */
+int
+mincore(void *addr, size_t length, unsigned char *vec)
+{
+	for (int i = 0; i < Nregions; i++) {
+		if (IN_RANGE(addr, length, Mincore[i].addr, Mincore[i].len))
+			return 0;
+	}
+
+	errno = ENOMEM;
+	return -1;
+}
+
+
 int
 main(int argc, char *argv[])
 {
 	START(argc, argv, "pmem_is_pmem_proc_linux");
 
-	if (argc < 4 || argc % 2)
-		UT_FATAL("usage: %s file addr len [addr len]...", argv[0]);
+	if (argc < 5)
+		UT_FATAL("usage: %s nfiles file.. nregions "
+			"(addr len)... (addr len)...", argv[0]);
 
-	Sfile = argv[1];
+	Nfiles = atoi(argv[1]);
+	UT_ASSERT(Nfiles < MAX_FILES);
 
-	for (int arg = 2; arg < argc; arg += 2) {
+	for (int i = 0; i < Nfiles; i++) {
+		Sfile[i] = argv[2 + i];
+	}
+
+	Nregions = atoi(argv[2 + Nfiles]);
+	UT_ASSERT(Nregions < MAX_REGIONS);
+	for (int i = 0; i < Nregions; i += 2) {
+		char *str_addr = argv[3 + Nfiles + i + 0];
+		char *str_len  = argv[3 + Nfiles + i + 1];
+
+		Mincore[i].addr = (uintptr_t)strtoull(str_addr, NULL, 16);
+		Mincore[i].len = (size_t)strtoull(str_len, NULL, 10);
+	}
+
+	for (int arg = 2 + Nfiles + 1 + 2 * Nregions; arg < argc; arg += 2) {
 		void *addr;
 		size_t len;
 
 		addr = (void *)strtoull(argv[arg], NULL, 16);
 		len = (size_t)strtoull(argv[arg + 1], NULL, 10);
+
+		Curfile = 0;
+
 		UT_OUT("addr %p, len %zu: %d", addr, len,
 				pmem_is_pmem(addr, len));
 	}
