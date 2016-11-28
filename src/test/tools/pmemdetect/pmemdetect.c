d30dbf582f5cf3e881412f40bb84346955481a36
Pawel Lebioda <pawel.lebioda@intel.com>
2016-11-28 23:27:59 +0100

    test: support for device dax in pmemdetect

--- src/test/tools/pmemdetect/pmemdetect.c
+++ src/test/tools/pmemdetect/pmemdetect.c
@@ -37,37 +37,98 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
+
 #include "mmap.h"
 #include "libpmem.h"
+#include "file.h"
 
 #define SIZE 4096
+
+/*
+ * is_pmem -- checks if given path points to pmem-aware filestem
+ */
+static int
+is_pmem(const char *path)
+{
+	int ret;
+
+	void *addr = util_map_tmpfile(path, SIZE, 0);
+	if (addr == NULL) {
+		fprintf(stderr, "file creation failed\n");
+		return -1;
+	}
+
+	if (pmem_is_pmem(addr, SIZE))
+		ret = 1;
+	else
+		ret = 0;
+
+	util_unmap(addr, SIZE);
+
+	return ret;
+}
+
+/*
+ * is_dev_Dax -- checks if given path points to device dax
+ */
+static int
+is_dev_dax(const char *path)
+{
+	if (!util_file_is_device_dax(path)) {
+		printf("%s -- not device dax\n", path);
+		return 0;
+	}
+
+	if (access(path, W_OK|R_OK)) {
+		printf("%s -- permission denied\n", path);
+		return -1;
+	}
+
+	return 0;
+}
 
 int
 main(int argc, char *argv[])
 {
 	int ret;
 
-	if (argc != 2) {
-		fprintf(stderr, "usage: %s path\n", argv[0]);
+	if (argc != 2 && argc != 3) {
+		fprintf(stderr, "usage: %s [-d] path\n", argv[0]);
 		exit(2);
 	}
 
 	const char *path = argv[1];
+	int dev_dax = 0;
+
+	if (argc == 3 && strcmp(argv[1], "-d") == 0) {
+		path = argv[2];
+		dev_dax = 1;
+	}
 
 	util_init();
 	util_mmap_init();
-	void *addr = util_map_tmpfile(path, SIZE, 0);
-	if (addr == NULL) {
-		fprintf(stderr, "file creation failed\n");
-		exit(2);
-	}
 
-	if (pmem_is_pmem(addr, SIZE))
-		ret = 0;
+	if (dev_dax)
+		ret = is_dev_dax(path);
 	else
-		ret = 1;
+		ret = is_pmem(path);
 
-	util_unmap(addr, SIZE);
+	/*
+	 * Return 0 if 'path' points to PMEM-aware filesystem or device dax.
+	 * Otherwise return 1, if any problem occurred return 2.
+	 */
+	switch (ret) {
+	case 0:
+		ret = 1;
+		break;
+	case 1:
+		ret = 0;
+		break;
+	default:
+		ret = 2;
+		break;
+	}
 
 	return ret;
 }
