24a205643fa88d2264ed438445ccf2ad8b7d01a2
Krzysztof Czurylo <krzysztof.czurylo@intel.com>
2016-12-09 12:21:09 +0100

    win: port util_poolset test
    Due to the problem in Windows implementation of mmap(), selected test
    cases have been modified.  They must be revisited once the issue in
    mmap() is fixed.

--- src/test/util_poolset/mocks_windows.h
+++ src/test/util_poolset/mocks_windows.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2016, Intel Corporation
+ * Copyright 2016, Intel Corporation
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -31,57 +31,26 @@
  */
 
 /*
- * mocks.c -- mocked functions used in util_poolset.c
+ * mocks_windows.h -- redefinitions of libc functions used in util_poolset
+ *
+ * This file is Windows-specific.
+ *
+ * This file should be included (i.e. using Forced Include) by libpmem
+ * files, when compiled for the purpose of util_poolset test.
+ * It would replace default implementation with mocked functions defined
+ * in util_poolset.c.
+ *
+ * These defines could be also passed as preprocessor definitions.
  */
 
-#include "unittest.h"
+#ifndef WRAP_REAL_OPEN
+#define open __wrap_open
+#endif
 
-extern const char *Open_path;
-extern off_t Fallocate_len;
-extern size_t Is_pmem_len;
+#ifndef WRAP_REAL_FALLOCATE
+#define posix_fallocate __wrap_posix_fallocate
+#endif
 
-/*
- * open -- open mock
- */
-FUNC_MOCK(open, int, const char *path, int flags, ...)
-FUNC_MOCK_RUN_DEFAULT {
-	if (strcmp(Open_path, path) == 0) {
-		UT_OUT("mocked open: %s", path);
-		errno = EACCES;
-		return -1;
-	}
-
-	va_list ap;
-	va_start(ap, flags);
-	int mode = va_arg(ap, int);
-	va_end(ap);
-
-	return _FUNC_REAL(open)(path, flags, mode);
-}
-FUNC_MOCK_END
-
-/*
- * posix_fallocate -- posix_fallocate mock
- */
-FUNC_MOCK(posix_fallocate, int, int fd, off_t offset, off_t len)
-FUNC_MOCK_RUN_DEFAULT {
-	if (Fallocate_len == len) {
-		UT_OUT("mocked fallocate: %ju", len);
-		return ENOSPC;
-	}
-	return _FUNC_REAL(posix_fallocate)(fd, offset, len);
-}
-FUNC_MOCK_END
-
-/*
- * pmem_is_pmem -- pmem_is_pmem mock
- */
-FUNC_MOCK(pmem_is_pmem, int, const void *addr, size_t len)
-FUNC_MOCK_RUN_DEFAULT {
-	if (Is_pmem_len == len) {
-		UT_OUT("mocked pmem_is_pmem: %zu", len);
-		return 1;
-	}
-	return _FUNC_REAL(pmem_is_pmem)(addr, len);
-}
-FUNC_MOCK_END
+#ifndef WRAP_REAL_PMEM
+#define pmem_is_pmem __wrap_pmem_is_pmem
+#endif
