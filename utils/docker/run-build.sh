2ee79c579f26cfd3498016d751faa6bb617f6a6e
Wojciech Uss <wojciech.uss@intel.com>
2017-01-20 10:38:49 +0100

    common: split long Travis jobs into debug and nondebug

--- utils/docker/run-build.sh
+++ utils/docker/run-build.sh
@@ -47,6 +47,6 @@ make check-license \
 	&& make cstyle \
 	&& make -j2 USE_LIBUNWIND=1 \
 	&& make -j2 test USE_LIBUNWIND=1 \
-	&& make -j2 pcheck \
+	&& make -j2 pcheck TEST_BUILD=$TEST_BUILD \
 	&& make DESTDIR=/tmp source
 
