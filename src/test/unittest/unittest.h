ec6eca02cb45d68ccb50760ecf9b34298a6203d2
Marcin Ślusarz <marcin.slusarz@gmail.com>
2017-02-11 18:15:56 +0100

    common: add compile error macros for "sparse"
    ... as "sparse" doesn't understand the magic happening inside those
    macros.

--- src/test/unittest/unittest.h
+++ src/test/unittest/unittest.h
@@ -209,7 +209,14 @@ void ut_err(const char *file, int line, const char *func,
 	"assertion failure: %s (0x%llx) != %s (0x%llx)", #lhs,\
 	(unsigned long long)(lhs), #rhs, (unsigned long long)(rhs)), 0)))
 
-#ifndef _MSC_VER
+#if defined(__CHECKER__)
+#define UT_COMPILE_ERROR_ON(cond)
+#define UT_ASSERT_COMPILE_ERROR_ON(cond)
+#elif defined(_MSC_VER)
+#define UT_COMPILE_ERROR_ON(cond) C_ASSERT(!(cond))
+/* XXX - can't be done with C_ASSERT() unless we have __builtin_constant_p() */
+#define UT_ASSERT_COMPILE_ERROR_ON(cond) (void)(cond)
+#else
 #define UT_COMPILE_ERROR_ON(cond) ((void)sizeof(char[(cond) ? -1 : 1]))
 #ifndef __cplusplus
 #define UT_ASSERT_COMPILE_ERROR_ON(cond) UT_COMPILE_ERROR_ON(cond)
@@ -219,10 +226,6 @@ void ut_err(const char *file, int line, const char *func,
  */
 #define UT_ASSERT_COMPILE_ERROR_ON(cond) UT_ASSERT_rt(!(cond))
 #endif /* __cplusplus */
-#else /* _MSC_VER */
-#define UT_COMPILE_ERROR_ON(cond) C_ASSERT(!(cond))
-/* XXX - can't be done with C_ASSERT() unless we have __builtin_constant_p() */
-#define UT_ASSERT_COMPILE_ERROR_ON(cond) (void)(cond)
 #endif /* _MSC_VER */
 
 /* assert a condition is true */
