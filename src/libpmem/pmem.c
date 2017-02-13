c33acc80844b187a33feba09c026b77cc79697c5
Krzysztof Czurylo <krzysztof.czurylo@intel.com>
2017-02-13 19:58:49 +0100

    pmem: make pmem_is_pmem true for Device DAX only
    This is the minimal change required to support new behavior of
    pmem_is_pmem() on Linux.  To minimize the changes, the Linux and Windows
    implementations are separate for now, so the Windows version of libpmem
    is not affected by this patch.
    
    Overview of the changes:
    
    - On Linux pmem_is_pmem() will now return true only for Device DAX
      mappings (unless overriden with PMEM_IS_PMEM_FORCE).
    
    - Only PMEM (Device DAX) regions are tracked for the purpose of
      pmem_is_pmem().  If region is not on the list, it's not PMEM.
    
    - Only the mappings created with pmem_map_file() are tracked.
      I.e. if the user queries a direct pointer to the object allocated
      from pmemobj pool, pmem_is_pmem() would return false.
      [to be fixed in the future]
    
    - Multiple entries for the same or overlapping region are not
      detected/handled properly, but it should never happen in case of
      pmem_map_file() (as MAP_FIXED is not used).
    
    - The internal data structures holding information about all the file
      mappings are allocated using default malloc(), which cannot be overriden
      with custom allocator. [to be fixed in the future]
    
    - Adjacent regions on the tracking list are not merged.
      [to be optimized in the future]
    
    - libpmem depends on libpthread again!

--- src/libpmem/pmem.c
+++ src/libpmem/pmem.c
@@ -175,6 +175,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <limits.h>
+#include <pthread.h>
 
 #ifdef _WIN32
 #include <memoryapi.h>
@@ -551,7 +552,7 @@ pmem_map_file(const char *path, size_t len, int flags, mode_t mode,
 	int fd;
 	int open_flags = O_RDWR;
 	int delete_on_err = 0;
-	int dax = util_file_is_device_dax(path);
+	int device_dax = util_file_is_device_dax(path);
 
 	if (flags & ~(PMEM_FILE_ALL_FLAGS)) {
 		ERR("invalid flag specified %x", flags);
@@ -559,7 +560,7 @@ pmem_map_file(const char *path, size_t len, int flags, mode_t mode,
 		return NULL;
 	}
 
-	if (dax) {
+	if (device_dax) {
 		if (flags & ~(PMEM_DAX_VALID_FLAGS)) {
 			ERR("invalid flag for device dax %x", flags);
 			errno = EINVAL;
@@ -681,6 +682,13 @@ pmem_map_file(const char *path, size_t len, int flags, mode_t mode,
 
 	(void) close(fd);
 
+#ifndef _WIN32
+	/* XXX only Device DAX regions (PMEM) are tracked so far */
+	if (device_dax && util_range_track(addr, len) != 0) {
+		LOG(2, "can't track mapped region");
+	}
+#endif
+
 	return addr;
 
 err:
@@ -700,6 +708,9 @@ pmem_unmap(void *addr, size_t len)
 {
 	LOG(3, "addr %p len %zu", addr, len);
 
+#ifndef _WIN32
+	util_range_untrack(addr, len);
+#endif
 	VALGRIND_REMOVE_PMEM_MAPPING(addr, len);
 	return util_unmap(addr, len);
 }
