012ea0a16833a8ba51fa8a6bd08383706f4c3fbc
Lukasz Dorau <lukasz.dorau@intel.com>
2017-03-16 16:12:47 +0100

    common: grep out the grafted commits later
    Files modified in only two commits:
    1) the newest one not grafted and
    2) the latest one grafted
    should not be skipped.

--- utils/check_license/check-headers.sh
+++ utils/check_license/check-headers.sh
@@ -56,7 +56,8 @@ shift
 
 PATTERN=`mktemp`
 TMP=`mktemp`
-rm -f $PATTERN $TMP
+TMP2=`mktemp`
+rm -f $PATTERN $TMP $TMP2
 
 function exit_if_not_exist()
 {
@@ -151,15 +152,18 @@ for file in $FILES ; do
 		if [ $SHALLOW_CLONE -eq 0 ]; then
 			git log --no-merges --format="%ai %aE" -- $file | sort > $TMP
 		else
-			# grep out the grafted commits (commits with no parents)
-			git log --no-merges --format="%ai %aE qwe%prty" -- $file | grep -v "qwerty" | sort > $TMP
+			# mark the grafted commits (commits with no parents)
+			git log --no-merges --format="%ai %aE grafted-%p-commit" -- $file | sort > $TMP
 		fi
 
 		# skip checking dates for new files
 		[ $(cat $TMP | wc -l) -le 1 ] && continue
 
-		FIRST=`cat $TMP | head -n1`
-		LAST=` cat $TMP | tail -n1`
+		# grep out the grafted commits (commits with no parents)
+		grep -v -e "grafted--commit" $TMP > $TMP2
+
+		FIRST=`head -n1 $TMP2`
+		LAST=` tail -n1 $TMP2`
 
 		# skip checking dates for non-Intel commits
 		AUTHOR_LAST=`echo $LAST | cut -d"@" -f2 | cut -d" " -f1`
@@ -187,7 +191,7 @@ for file in $FILES ; do
 		fi
 	fi
 done
-rm -f $TMP
+rm -f $TMP $TMP2
 
 # check if error found
 if [ $RV -eq 0 ]; then
