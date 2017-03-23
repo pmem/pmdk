fdf9fc8f0df8dde7246c15b2244d7a9a0200aca0
Jan M Michalski <jan.m.michalski@intel.com>
2017-03-23 08:13:32 +0100

    benchmark: fix time measurement
    - calculate average time of getting time from the system
    - take time of getting time from the system into accout in
      a clear and comprehensible way
    - no intermediary statistics involved

--- src/benchmarks/benchmark_time.hpp
+++ src/benchmarks/benchmark_time.hpp
@@ -37,9 +37,11 @@
 typedef struct timespec benchmark_time_t;
 
 void benchmark_time_get(benchmark_time_t *time);
-void benchmark_time_diff_dummy(benchmark_time_t *d, benchmark_time_t *t1,
-			       benchmark_time_t *t2);
 void benchmark_time_diff(benchmark_time_t *d, benchmark_time_t *t1,
 			 benchmark_time_t *t2);
 double benchmark_time_get_secs(benchmark_time_t *t);
 unsigned long long benchmark_time_get_nsecs(benchmark_time_t *t);
+int benchmark_time_compare(const benchmark_time_t *t1,
+			   const benchmark_time_t *t2);
+void benchmark_time_set(benchmark_time_t *time, unsigned long long nsecs);
+unsigned long long benchmark_get_avg_get_time(void);
