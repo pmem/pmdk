12c9db4f120e5d37b41db67ab4b7c65004bf3e4b
Krzysztof Czurylo <krzysztof.czurylo@intel.com>
2017-05-08 13:55:58 +0200

    pmem: fix pmem_is_pmem on invalid memory ranges
    Fixes pmem_is_pmem() behavior when addr+len extends beyond process
    address space.
    
    Ref: pmem/issues#503

--- src/libpmem/pmem_windows.c
+++ src/libpmem/pmem_windows.c
@@ -110,6 +110,11 @@ is_pmem_detect(const void *addr, size_t len)
 {
 	LOG(3, "addr %p len %zu", addr, len);
 
+	if (len > UINTPTR_MAX - (uintptr_t)addr) {
+		len = UINTPTR_MAX - (uintptr_t)addr;
+		LOG(4, "limit len to %zu to not get beyond address space", len);
+	}
+
 	int retval = 1;
 	const void *begin = addr;
 	const void *end = (const void *)((char *)addr + len);
