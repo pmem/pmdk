cc2cd8c7cf095f69d0d7edd1216d255a508a09c7
Katarzyna Wasiuta <katarzyna.wasiuta@intel.com>
2017-06-23 12:32:09 +0200

    examples: corrected layout in cpp doc snippets

--- src/examples/libpmemobj/doc_snippets/pool.cpp
+++ src/examples/libpmemobj/doc_snippets/pool.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016, Intel Corporation
+ * Copyright 2016-2017, Intel Corporation
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -60,7 +60,7 @@ pool_example()
 	pop.close();
 
 	// or open a pmemobj pool
-	pop = pool<root>::open("poolfile", "");
+	pop = pool<root>::open("poolfile", "layout");
 
 	// typical usage schemes
 	auto root_obj = pop.get_root();
@@ -78,7 +78,7 @@ pool_example()
 	pop.close();
 
 	// check pool consistency
-	pool<root>::check("poolfile", "");
+	pool<root>::check("poolfile", "layout");
 }
 //! [pool_example]
 
