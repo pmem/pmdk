5eacb872a3408c435c696bac292435e26bebfff9
Krzysztof Czurylo <krzysztof.czurylo@intel.com>
2017-08-28 14:34:40 +0200

    common: fixes/workarounds for icc

--- src/jemalloc/test/unit/util.c
+++ src/jemalloc/test/unit/util.c
@@ -246,6 +246,12 @@ TEST_BEGIN(test_malloc_snprintf)
 	TEST("_0x1234abc_", "_%#llx_", ((long long)0x1234abc));
 	TEST("_0X1234ABC_", "_%#llX_", ((long long)0x1234ABC));
 
+#ifdef __INTEL_COMPILER
+/* turn off ICC warnings on invalid format string conversion */
+#pragma warning (push)
+#pragma warning (disable: 269)
+#endif
+
 	TEST("_-1234_", "_%qd_", ((long long)-1234));
 	TEST("_1234_", "_%qd_", ((long long)1234));
 	TEST("_-1234_", "_%qi_", ((long long)-1234));
@@ -254,6 +260,10 @@ TEST_BEGIN(test_malloc_snprintf)
 	TEST("_1234_", "_%qu_", ((long long)1234));
 	TEST("_0x1234abc_", "_%#qx_", ((long long)0x1234abc));
 	TEST("_0X1234ABC_", "_%#qX_", ((long long)0x1234ABC));
+
+#ifdef __INTEL_COMPILER
+#pragma warning (pop)
+#endif
 
 	TEST("_-1234_", "_%jd_", ((intmax_t)-1234));
 	TEST("_1234_", "_%jd_", ((intmax_t)1234));
--- src/test/rpmem_fip/rpmem_fip_test.c
+++ src/test/rpmem_fip/rpmem_fip_test.c
@@ -85,10 +85,7 @@ get_persist_method(const char *pm)
 	else if (strcmp(pm, "APM") == 0)
 		return RPMEM_PM_APM;
 	else
-		UT_ASSERT(0);
-
-	/* XXX: cheat icc */
-	__builtin_unreachable();
+		UT_FATAL("unknown method");
 }
 
 /*
