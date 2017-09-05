ed8dbe9e76ec074e5c695c1c3690c73c9d2841a7
Pawel Lebioda <pawel.lebioda@intel.com>
2017-09-05 15:31:47 +0200

    rpmem: fix cleanup if fork() failed
    If fork() failed in rpmem_{open,create} there is a call to kill() with
    pid equals -1, which sends a signal to all processes which the current
    process has permissions to send to.
    
    Ref: pmem/issues634

--- src/librpmem/rpmem_cmd.c
+++ src/librpmem/rpmem_cmd.c
@@ -239,7 +239,7 @@ err_pipe_in:
 int
 rpmem_cmd_wait(struct rpmem_cmd *cmd, int *status)
 {
-	if (!cmd->pid)
+	if (cmd->pid <= 0)
 		return -1;
 
 	if (waitpid(cmd->pid, status, 0) != cmd->pid)
@@ -258,5 +258,6 @@ rpmem_cmd_term(struct rpmem_cmd *cmd)
 	os_close(cmd->fd_out);
 	os_close(cmd->fd_err);
 
+	RPMEM_ASSERT(cmd->pid > 0);
 	return kill(cmd->pid, SIGINT);
 }
