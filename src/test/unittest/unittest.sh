1a3aadbc6da64bbafa66634cff8f91ce75d997c8
welmkows <weronika.lewandowska@intel.com>
2017-10-24 10:49:38 +0200

    test: clean data files on failed

--- src/test/unittest/unittest.sh
+++ src/test/unittest/unittest.sh
@@ -223,6 +223,13 @@ if [ -d "$NON_PMEM_FS_DIR" ]; then
 	NON_PMEM_IS_PMEM=$?
 fi
 
+# writes test working directory to temporary file
+# that allows read location of data after test failure
+if [ -f "$TEMP_LOC" ]; then
+	echo "$DIR" > $TEMP_LOC
+fi
+
+
 #
 # The default is to turn on library logging to level 3 and save it to local files.
 # Tests that don't want it on, should override these environment variables.
