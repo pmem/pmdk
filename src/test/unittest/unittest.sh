e1f79b9fe09c185d148f66a75c19a17eeefdef41
Piotr Balcer <piotr.balcer@intel.com>
2017-11-09 10:47:23 +0100

    common: implement directory support in poolsets
    This patch introduces the infrastructure for dynamically extending
    the layout of a pool by creating a new file in specified directories.

--- src/test/unittest/unittest.sh
+++ src/test/unittest/unittest.sh
@@ -46,7 +46,7 @@ export LC_ALL="C"
 [ "$CHECK_TYPE" ] || export CHECK_TYPE=auto
 [ "$CHECK_POOL" ] || export CHECK_POOL=0
 [ "$VERBOSE" ] || export VERBOSE=0
-[ "$SUFFIX" ] || export SUFFIX="😘⠝⠧⠍⠇ɗNVMLӜ⥺🙋"
+[ -z "$SUFFIX" ] || export SUFFIX="😘⠝⠧⠍⠇ɗNVMLӜ⥺🙋"
 
 TOOLS=../tools
 # Paths to some useful tools
@@ -474,6 +474,7 @@ function create_holey_file() {
 #            z - create zeroed (holey) file
 #            n - create non-zeroed file
 #            h - create non-zeroed file, but with zeroed header (first 4KB)
+#            d - create directory
 #   fsize - (optional) the actual size of the part file (if 'cmd' is not 'x')
 #   mode  - (optional) same format as for 'chmod' command
 #
@@ -570,6 +571,9 @@ function create_poolset() {
 			truncate -s 4K $fpath >> prep$UNITTEST_NUM.log
 			$DD if=/dev/zero bs=$asize count=1 2>>$PREP_LOG_FILE | tr '\0' '\132' >> $fpath
 			truncate -s $asize $fpath >> $PREP_LOG_FILE
+			;;
+		d)
+			mkdir -p $fpath
 			;;
 		esac
 
