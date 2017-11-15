73b934727f13d7254bd4b64f2d1fc6b6b1e8a753
Vishwanath Venkatesan <vishwanath.venkatesan@intel.com>
2017-11-15 01:47:32 -0800

    Cleanup for cache flush routines
    Adding some cleanup for cache flush routines with some renaming.
    Also adding some comments, and remove unwanted duplicate cache functions.
    Rename neon_sse.h to arm_cacheops.h as we no longer use neon sse's for this
    port.
    
    Signed-off-by: Vishwanath Venkatesan <vishwanath.venkatesan@intel.com>

--- src/common/arm_cacheops.h
+++ src/common/arm_cacheops.h
@@ -29,26 +29,32 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+/*
+ * ARM inline assembly to flush and invalidate caches
+ * clwb => dc cvac
+ * clflush | clflushopt => dc civac
+ * fence => dmb ish
+ */
 
-#ifndef NEON_SSE_H
-#define NEON_SSE_H
+#ifndef AARCH64_CACHEOPS_H
+#define AARCH64_CACHEOPS_H
 
 #include <stdlib.h>
 
-static __inline void
+static inline void
 arm_clean_va_to_poc(void const *p __attribute__((unused)))
 {
 	asm volatile("dc cvac, %0" : : "r" (p) : "memory");
 }
 
-static __inline void
+static inline void
 arm_data_memory_barrier(void)
 {
 	asm volatile("dmb ish" : : : "memory");
 }
 
 static inline void
-arm_invalidate_va_to_poc(const void *addr)
+arm_clean_and_invalidate_va_to_poc(const void *addr)
 {
 	asm volatile("dc civac, %0" : : "r" (addr) : "memory");
 }
--- src/libpmem/cpu.c
+++ src/libpmem/cpu.c
@@ -32,6 +32,10 @@
 
 /*
  * cpu.c -- CPU features detection
+ *
+ * These routines do not work AARCH64 platforms, and need new detection
+ * routiones to be added. Currently to ensure msync is not used and ARM
+ * FLUSH instructions are used PMEM_IS_PMEM_FORCE=1 needs to be used.
  */
 
 /*
