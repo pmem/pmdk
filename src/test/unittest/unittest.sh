071f73338134ab85549588e23c9c7488dfab36d1
Piotr Balcer <piotr.balcer@intel.com>
2017-11-17 15:12:42 +0100

    common: implement fs traversal layer

--- src/test/unittest/unittest.sh
+++ src/test/unittest/unittest.sh
@@ -46,7 +46,7 @@ export LC_ALL="C"
 [ "$CHECK_TYPE" ] || export CHECK_TYPE=auto
 [ "$CHECK_POOL" ] || export CHECK_POOL=0
 [ "$VERBOSE" ] || export VERBOSE=0
-[ -z "$SUFFIX" ] || export SUFFIX="😘⠝⠧⠍⠇ɗNVMLӜ⥺🙋"
+[ "$SUFFIX" ] || export SUFFIX="😘⠝⠧⠍⠇ɗNVMLӜ⥺🙋"
 
 TOOLS=../tools
 # Paths to some useful tools
