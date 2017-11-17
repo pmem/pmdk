6346ee2503146aaede7ae30c9d4b49374d3e4ffc
Piotr Balcer <piotr.balcer@intel.com>
2017-11-17 15:12:42 +0100

    common: implement fs traversal layer

---
+++ src/common/fs_linux.c
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2017, Intel Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *
+ *     * Neither the name of the copyright holder nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * fs_linux.c -- file system traversal linux implementation
+ */
+
+#include <fts.h>
+#include "util.h"
+#include "out.h"
+#include "vec.h"
+#include "fs.h"
+
+struct fs {
+	FTS *ft;
+	VEC(, struct fs_entry) entries;
+};
+
+/*
+ * fs_open -- creates fs traversal instance
+ */
+struct fs *
+fs_new(const char *path)
+{
+	struct fs *f = Malloc(sizeof(*f));
+	if (f == NULL)
+		goto error_fs_alloc;
+
+	VEC_INIT(&f->entries);
+
+	const char *paths[2] = {path, NULL};
+	f->ft = fts_open((char * const *)paths, FTS_COMFOLLOW | FTS_XDEV, NULL);
+	if (f->ft == NULL)
+		goto error_fts_open;
+
+	return f;
+
+error_fts_open:
+	Free(f);
+error_fs_alloc:
+	return NULL;
+}
+
+/*
+ * fs_read -- reads an entry from the fs path
+ */
+struct fs_entry *
+fs_read(struct fs *f)
+{
+	FTSENT *entry = fts_read(f->ft);
+	if (entry == NULL)
+		return NULL;
+
+	struct fs_entry e;
+	switch (entry->fts_info) {
+		case FTS_D:
+		e.type = FS_ENTRY_DIRECTORY;
+		break;
+		case FTS_F:
+		e.type = FS_ENTRY_FILE;
+		break;
+		default:
+		e.type = FS_ENTRY_OTHER;
+		break;
+	}
+
+	e.name = entry->fts_name;
+	e.namelen = entry->fts_namelen;
+	e.path = entry->fts_path;
+	e.pathlen = entry->fts_pathlen;
+
+	VEC_PUSH_BACK(&f->entries, e);
+
+	return &VEC_BACK(&f->entries);
+}
+
+/*
+ * fs_delete -- deletes a fs traversal instance
+ */
+void
+fs_delete(struct fs *f)
+{
+	VEC_DELETE(&f->entries);
+	fts_close(f->ft);
+	Free(f);
+}
