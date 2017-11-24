0b9482005082d7edff729337d90b6822423665c2
Krzysztof Czurylo <krzysztof.czurylo@intel.com>
2017-11-24 13:54:21 +0100

    test: remove empty err*.log.match files
    If errX.log.match file is not present, assume there should be no output
    in the corresponding errX.log file. Otherwise, the test would fail.
    
    If there is any output expected on stderr, errX.log.match file must be
    present.

--- src/test/libpmempool_transform/libpmempool_transform.c
+++ src/test/libpmempool_transform/libpmempool_transform.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016, Intel Corporation
+ * Copyright 2016-2017, Intel Corporation
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -51,7 +51,6 @@ main(int argc, char *argv[])
 	int ret = pmempool_transform(argv[1], argv[2],
 			(unsigned)strtoul(argv[3], NULL, 0));
 	UT_OUT("result: %d, errno: %d", ret, errno);
-	UT_ASSERTeq(ret, 0);
 
 	DONE(NULL);
 }
