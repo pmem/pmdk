05ad638e493d9e5e0788d68d7108e470560febee
Marcin Ślusarz <marcin.slusarz@intel.com>
2017-12-05 12:50:00 +0100

    pmem: optimize small memcpy/memset

--- src/libpmem/memcpy/memcpy_sse2.h
+++ src/libpmem/memcpy/memcpy_sse2.h
@@ -45,6 +45,11 @@ memmove_small_sse2(char *dest, const char *src, size_t len)
 {
 	ASSERT(len <= 64);
 
+	if (len <= 8)
+		goto le8;
+	if (len <= 32)
+		goto le32;
+
 	if (len > 48) {
 		/* 49..64 */
 		__m128i xmm0 = _mm_loadu_si128((__m128i *)src);
@@ -56,48 +61,67 @@ memmove_small_sse2(char *dest, const char *src, size_t len)
 		_mm_storeu_si128((__m128i *)(dest + 16), xmm1);
 		_mm_storeu_si128((__m128i *)(dest + 32), xmm2);
 		_mm_storeu_si128((__m128i *)(dest + len - 16), xmm3);
-	} else if (len > 32) {
-		/* 33..48 */
-		__m128i xmm0 = _mm_loadu_si128((__m128i *)src);
-		__m128i xmm1 = _mm_loadu_si128((__m128i *)(src + 16));
-		__m128i xmm2 = _mm_loadu_si128((__m128i *)(src + len - 16));
+		return;
+	}
 
-		_mm_storeu_si128((__m128i *)dest, xmm0);
-		_mm_storeu_si128((__m128i *)(dest + 16), xmm1);
-		_mm_storeu_si128((__m128i *)(dest + len - 16), xmm2);
-	} else if (len > 16) {
+	/* 33..48 */
+	__m128i xmm0 = _mm_loadu_si128((__m128i *)src);
+	__m128i xmm1 = _mm_loadu_si128((__m128i *)(src + 16));
+	__m128i xmm2 = _mm_loadu_si128((__m128i *)(src + len - 16));
+
+	_mm_storeu_si128((__m128i *)dest, xmm0);
+	_mm_storeu_si128((__m128i *)(dest + 16), xmm1);
+	_mm_storeu_si128((__m128i *)(dest + len - 16), xmm2);
+	return;
+
+le32:
+	if (len > 16) {
 		/* 17..32 */
 		__m128i xmm0 = _mm_loadu_si128((__m128i *)src);
 		__m128i xmm1 = _mm_loadu_si128((__m128i *)(src + len - 16));
 
 		_mm_storeu_si128((__m128i *)dest, xmm0);
 		_mm_storeu_si128((__m128i *)(dest + len - 16), xmm1);
-	} else if (len > 8) {
-		/* 9..16 */
-		uint64_t d0 = *(uint64_t *)src;
-		uint64_t d1 = *(uint64_t *)(src + len - 8);
-
-		*(uint64_t *)dest = d0;
-		*(uint64_t *)(dest + len - 8) = d1;
-	} else if (len > 4) {
-		/* 5..8 */
-		uint32_t d0 = *(uint32_t *)src;
-		uint32_t d1 = *(uint32_t *)(src + len - 4);
-
-		*(uint32_t *)dest = d0;
-		*(uint32_t *)(dest + len - 4) = d1;
-	} else if (len > 2) {
-		/* 3..4 */
-		uint16_t d0 = *(uint16_t *)src;
-		uint16_t d1 = *(uint16_t *)(src + len - 2);
-
-		*(uint16_t *)dest = d0;
-		*(uint16_t *)(dest + len - 2) = d1;
-	} else if (len == 2) {
-		*(uint16_t *)dest = *(uint16_t *)src;
-	} else {
-		*(uint8_t *)dest = *(uint8_t *)src;
+		return;
 	}
+
+	/* 9..16 */
+	uint64_t d80 = *(uint64_t *)src;
+	uint64_t d81 = *(uint64_t *)(src + len - 8);
+
+	*(uint64_t *)dest = d80;
+	*(uint64_t *)(dest + len - 8) = d81;
+	return;
+
+le8:
+	if (len <= 2)
+		goto le2;
+
+	if (len > 4) {
+		/* 5..8 */
+		uint32_t d40 = *(uint32_t *)src;
+		uint32_t d41 = *(uint32_t *)(src + len - 4);
+
+		*(uint32_t *)dest = d40;
+		*(uint32_t *)(dest + len - 4) = d41;
+		return;
+	}
+
+	/* 3..4 */
+	uint16_t d20 = *(uint16_t *)src;
+	uint16_t d21 = *(uint16_t *)(src + len - 2);
+
+	*(uint16_t *)dest = d20;
+	*(uint16_t *)(dest + len - 2) = d21;
+	return;
+
+le2:
+	if (len == 2) {
+		*(uint16_t *)dest = *(uint16_t *)src;
+		return;
+	}
+
+	*(uint8_t *)dest = *(uint8_t *)src;
 }
 
 #endif
