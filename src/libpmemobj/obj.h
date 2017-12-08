be4439b58835d95ce2d88b46d1a0d4f8c741d92a
Piotr Balcer <piotr.balcer@intel.com>
2017-12-08 13:20:48 +0100

    obj: implement manual pool extend

--- src/libpmemobj/obj.h
+++ src/libpmemobj/obj.h
@@ -197,7 +197,7 @@ struct pmemobjpool {
 
 	/* padding to align size of this structure to page boundary */
 	/* sizeof(unused2) == 8192 - offsetof(struct pmemobjpool, unused2) */
-	char unused2[1012];
+	char unused2[1004];
 };
 
 /*
