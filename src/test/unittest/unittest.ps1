cf32fdc41d299434a3d4d2c56e996de82ff66adf
Chorazewicz <igor.chorazewicz@intel.com>
2018-02-12 16:46:51 +0100

    test: stop executing powershell script when there is an error

--- src/test/unittest/unittest.ps1
+++ src/test/unittest/unittest.ps1
@@ -30,6 +30,8 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+$ErrorActionPreference="Stop"
+
 . "..\testconfig.ps1"
 
 function verbose_msg {
