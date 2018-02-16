marcinslusarz/nvml

pmem: flush data ASAP in temporal mem[cpy|set]

src/libpmem/memcpy/memcpy_nt_avx512f_clflushopt.c
@@ -30,18 +30,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef PMEM_MEMCPY_AVX512F_H
-#define PMEM_MEMCPY_AVX512F_H
-
-#include <stddef.h>
-
-#include "memcpy_avx.h"
-
-static inline void
-memmove_small_avx512f(char *dest, const char *src, size_t len)
-{
-	/* We can't do better than AVX here. */
-	memmove_small_avx(dest, src, len);
-}
-
-#endif
+#include "pmem.h"
+#define flush flush_clflushopt_nolog
+#define EXPORTED_SYMBOL memmove_movnt_avx512f_clflushopt
+#include "memcpy_nt_avx512f.h"

src/libpmem/memcpy/memcpy_nt_avx_clflushopt.c
@@ -30,18 +30,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef PMEM_MEMCPY_AVX512F_H
-#define PMEM_MEMCPY_AVX512F_H
-
-#include <stddef.h>
-
-#include "memcpy_avx.h"
-
-static inline void
-memmove_small_avx512f(char *dest, const char *src, size_t len)
-{
-	/* We can't do better than AVX here. */
-	memmove_small_avx(dest, src, len);
-}
-
-#endif
+#include "pmem.h"
+#define flush flush_clflushopt_nolog
+#define EXPORTED_SYMBOL memmove_movnt_avx_clflushopt
+#include "memcpy_nt_avx.h"

src/libpmem/memcpy/memcpy_t_avx512f_clwb.c
@@ -30,18 +30,8 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef PMEM_MEMCPY_AVX512F_H
-#define PMEM_MEMCPY_AVX512F_H
-
-#include <stddef.h>
-
-#include "memcpy_avx.h"
-
-static inline void
-memmove_small_avx512f(char *dest, const char *src, size_t len)
-{
-	/* We can't do better than AVX here. */
-	memmove_small_avx(dest, src, len);
-}
-
-#endif
+#include "pmem.h"
+#define flush64b _mm_clwb
+#define flush flush_clwb_nolog
+#define EXPORTED_SYMBOL memmove_mov_avx512f_clwb
+#include "memcpy_t_avx512f.h"

src/libpmem/memset/memset_nt_avx512f_clwb.c
@@ -30,18 +30,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef PMEM_MEMCPY_AVX512F_H
-#define PMEM_MEMCPY_AVX512F_H
-
-#include <stddef.h>
-
-#include "memcpy_avx.h"
-
-static inline void
-memmove_small_avx512f(char *dest, const char *src, size_t len)
-{
-	/* We can't do better than AVX here. */
-	memmove_small_avx(dest, src, len);
-}
-
-#endif
+#include "pmem.h"
+#define flush flush_clwb_nolog
+#define EXPORTED_SYMBOL memset_movnt_avx512f_clwb
+#include "memset_nt_avx512f.h"

src/libpmem/memset/memset_nt_avx_clflushopt.c
@@ -30,18 +30,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef PMEM_MEMCPY_AVX512F_H
-#define PMEM_MEMCPY_AVX512F_H
-
-#include <stddef.h>
-
-#include "memcpy_avx.h"
-
-static inline void
-memmove_small_avx512f(char *dest, const char *src, size_t len)
-{
-	/* We can't do better than AVX here. */
-	memmove_small_avx(dest, src, len);
-}
-
-#endif
+#include "pmem.h"
+#define flush flush_clflushopt_nolog
+#define EXPORTED_SYMBOL memset_movnt_avx_clflushopt
+#include "memset_nt_avx.h"

src/libpmem/memset/memset_nt_sse2_clwb.c
@@ -30,18 +30,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef PMEM_MEMCPY_AVX512F_H
-#define PMEM_MEMCPY_AVX512F_H
-
-#include <stddef.h>
-
-#include "memcpy_avx.h"
-
-static inline void
-memmove_small_avx512f(char *dest, const char *src, size_t len)
-{
-	/* We can't do better than AVX here. */
-	memmove_small_avx(dest, src, len);
-}
-
-#endif
+#include "pmem.h"
+#define flush flush_clwb_nolog
+#define EXPORTED_SYMBOL memset_movnt_sse2_clwb
+#include "memset_nt_sse2.h"

src/libpmem/memset/memset_t_avx512f_clflush.c
@@ -30,18 +30,8 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef PMEM_MEMCPY_AVX512F_H
-#define PMEM_MEMCPY_AVX512F_H
-
-#include <stddef.h>
-
-#include "memcpy_avx.h"
-
-static inline void
-memmove_small_avx512f(char *dest, const char *src, size_t len)
-{
-	/* We can't do better than AVX here. */
-	memmove_small_avx(dest, src, len);
-}
-
-#endif
+#include "pmem.h"
+#define flush64b _mm_clflush
+#define flush flush_clflush_nolog
+#define EXPORTED_SYMBOL memset_mov_avx512f_clflush
+#include "memset_t_avx512f.h"

src/libpmem/memset/memset_t_sse2_clwb.c
@@ -30,18 +30,8 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef PMEM_MEMCPY_AVX512F_H
-#define PMEM_MEMCPY_AVX512F_H
-
-#include <stddef.h>
-
-#include "memcpy_avx.h"
-
-static inline void
-memmove_small_avx512f(char *dest, const char *src, size_t len)
-{
-	/* We can't do better than AVX here. */
-	memmove_small_avx(dest, src, len);
-}
-
-#endif
+#include "pmem.h"
+#define flush64b _mm_clwb
+#define flush flush_clwb_nolog
+#define EXPORTED_SYMBOL memset_mov_sse2_clwb
+#include "memset_t_sse2.h"

pmem/pmdk

src/libpmem/memcpy/memcpy_nt_avx512f_clflushopt.c
@@ -30,18 +30,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef PMEM_MEMCPY_AVX512F_H
-#define PMEM_MEMCPY_AVX512F_H
-
-#include <stddef.h>
-
-#include "memcpy_avx.h"
-
-static inline void
-memmove_small_avx512f(char *dest, const char *src, size_t len)
-{
-	/* We can't do better than AVX here. */
-	memmove_small_avx(dest, src, len);
-}
-
-#endif
+#include "pmem.h"
+#define flush flush_dcache_invalidate_opt_nolog
+#define EXPORTED_SYMBOL memmove_movnt_avx512f_clflushopt
+#include "memcpy_nt_avx512f.h"

src/libpmem/memcpy/memcpy_nt_avx_clflushopt.c
@@ -30,18 +30,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef PMEM_MEMCPY_AVX512F_H
-#define PMEM_MEMCPY_AVX512F_H
-
-#include <stddef.h>
-
-#include "memcpy_avx.h"
-
-static inline void
-memmove_small_avx512f(char *dest, const char *src, size_t len)
-{
-	/* We can't do better than AVX here. */
-	memmove_small_avx(dest, src, len);
-}
-
-#endif
+#include "pmem.h"
+#define flush flush_dcache_invalidate_opt_nolog
+#define EXPORTED_SYMBOL memmove_movnt_avx_clflushopt
+#include "memcpy_nt_avx.h"

src/libpmem/memcpy/memcpy_t_avx512f_clwb.c
@@ -30,18 +30,8 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef PMEM_MEMCPY_AVX512F_H
-#define PMEM_MEMCPY_AVX512F_H
-
-#include <stddef.h>
-
-#include "memcpy_avx.h"
-
-static inline void
-memmove_small_avx512f(char *dest, const char *src, size_t len)
-{
-	/* We can't do better than AVX here. */
-	memmove_small_avx(dest, src, len);
-}
-
-#endif
+#include "pmem.h"
+#define flush64b _mm_clwb
+#define flush flush_dcache_nolog
+#define EXPORTED_SYMBOL memmove_mov_avx512f_clwb
+#include "memcpy_t_avx512f.h"

src/libpmem/memset/memset_nt_avx512f_clwb.c
@@ -30,18 +30,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef PMEM_MEMCPY_AVX512F_H
-#define PMEM_MEMCPY_AVX512F_H
-
-#include <stddef.h>
-
-#include "memcpy_avx.h"
-
-static inline void
-memmove_small_avx512f(char *dest, const char *src, size_t len)
-{
-	/* We can't do better than AVX here. */
-	memmove_small_avx(dest, src, len);
-}
-
-#endif
+#include "pmem.h"
+#define flush flush_dcache_nolog
+#define EXPORTED_SYMBOL memset_movnt_avx512f_clwb
+#include "memset_nt_avx512f.h"

src/libpmem/memset/memset_nt_avx_clflushopt.c
@@ -30,18 +30,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef PMEM_MEMCPY_AVX512F_H
-#define PMEM_MEMCPY_AVX512F_H
-
-#include <stddef.h>
-
-#include "memcpy_avx.h"
-
-static inline void
-memmove_small_avx512f(char *dest, const char *src, size_t len)
-{
-	/* We can't do better than AVX here. */
-	memmove_small_avx(dest, src, len);
-}
-
-#endif
+#include "pmem.h"
+#define flush flush_dcache_invalidate_opt_nolog
+#define EXPORTED_SYMBOL memset_movnt_avx_clflushopt
+#include "memset_nt_avx.h"

src/libpmem/memset/memset_nt_sse2_clwb.c
@@ -30,18 +30,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef PMEM_MEMCPY_AVX512F_H
-#define PMEM_MEMCPY_AVX512F_H
-
-#include <stddef.h>
-
-#include "memcpy_avx.h"
-
-static inline void
-memmove_small_avx512f(char *dest, const char *src, size_t len)
-{
-	/* We can't do better than AVX here. */
-	memmove_small_avx(dest, src, len);
-}
-
-#endif
+#include "pmem.h"
+#define flush flush_dcache_nolog
+#define EXPORTED_SYMBOL memset_movnt_sse2_clwb
+#include "memset_nt_sse2.h"

src/libpmem/memset/memset_t_avx512f_clflush.c
@@ -30,18 +30,8 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef PMEM_MEMCPY_AVX512F_H
-#define PMEM_MEMCPY_AVX512F_H
-
-#include <stddef.h>
-
-#include "memcpy_avx.h"
-
-static inline void
-memmove_small_avx512f(char *dest, const char *src, size_t len)
-{
-	/* We can't do better than AVX here. */
-	memmove_small_avx(dest, src, len);
-}
-
-#endif
+#include "pmem.h"
+#define flush64b _mm_clflush
+#define flush flush_dcache_invalidate_nolog
+#define EXPORTED_SYMBOL memset_mov_avx512f_clflush
+#include "memset_t_avx512f.h"

src/libpmem/memset/memset_t_sse2_clwb.c
@@ -30,18 +30,8 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef PMEM_MEMCPY_AVX512F_H
-#define PMEM_MEMCPY_AVX512F_H
-
-#include <stddef.h>
-
-#include "memcpy_avx.h"
-
-static inline void
-memmove_small_avx512f(char *dest, const char *src, size_t len)
-{
-	/* We can't do better than AVX here. */
-	memmove_small_avx(dest, src, len);
-}
-
-#endif
+#include "pmem.h"
+#define flush64b _mm_clwb
+#define flush flush_dcache_nolog
+#define EXPORTED_SYMBOL memset_mov_sse2_clwb
+#include "memset_t_sse2.h"