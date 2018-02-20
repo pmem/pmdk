38b52c818f674e1e5a241040ec19c7f5aee942b9
Lukasz Dorau <lukasz.dorau@intel.com>
2018-02-20 17:06:18 +0100

    test: add bad blocks unit test

--- src/test/unittest/unittest.sh
+++ src/test/unittest/unittest.sh
@@ -933,6 +933,16 @@ function require_unlimited_vm() {
 }
 
 #
+# require_superuser -- require user with superuser rights
+#
+function require_superuser() {
+	local user_id=$(id -u)
+	[ "$user_id" == "0" ] && return
+	echo "$UNITTEST_NAME: SKIP required: run with superuser rights"
+	exit 0
+}
+
+#
 # require_no_superuser -- require user without superuser rights
 #
 function require_no_superuser() {
@@ -1361,6 +1371,17 @@ function require_command() {
 	if ! command -pv $1 1>/dev/null
 	then
 		msg "$UNITTEST_NAME: SKIP: '$1' command required"
+		exit 0
+	fi
+}
+
+#
+# require_kernel_module -- only allow script to continue if specified kernel module exists
+#
+function require_kernel_module() {
+	local MODULE=$(depmod -n | grep -cw -e "$1.ko")
+	if [ $MODULE == "0" ]; then
+		echo "$UNITTEST_NAME: SKIP: '$1' kernel module required"
 		exit 0
 	fi
 }
