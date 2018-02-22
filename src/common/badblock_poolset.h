cb9b9e889892bc11f53884a426d69c533a84ff2a
Lukasz Dorau <lukasz.dorau@intel.com>
2018-02-22 10:34:24 +0100

    common: handle bad blocks

--- src/common/badblock_poolset.h
+++ src/common/badblock_poolset.h
@@ -31,63 +31,14 @@
  */
 
 /*
- * os_dimm_none.c -- fake dimm functions
+ * badblock_poolset.h - bad block API for poolsets
  */
 
-#include "out.h"
-#include "os.h"
-#include "os_dimm.h"
-/*
- * os_dimm_uid -- returns empty uid
- */
-int
-os_dimm_uid(const char *path, char *uid, size_t *len)
-{
-	LOG(3, "path %s, uid %p, len %lu", path, uid, *len);
-	if (uid == NULL) {
-		*len = 1;
-	} else {
-		*uid = '\0';
-	}
-	return 0;
-}
+#ifndef PMDK_BADBLOCK_POOLSET_H
+#define PMDK_BADBLOCK_POOLSET_H 1
 
-/*
- * os_dimm_usc -- returns fake unsafe shutdown count
- */
-int
-os_dimm_usc(const char *path, uint64_t *usc)
-{
-	LOG(3, "path %s, usc %p", path, usc);
-	*usc = 0;
-	return 0;
-}
+#include "set.h"
 
-/*
- * os_dimm_files_namespace_badblocks -- fake os_dimm_files_namespace_badblocks()
- */
-int
-os_dimm_files_namespace_badblocks(const char *path, struct badblocks *bbs)
-{
-	LOG(3, "path %s", path);
+int os_badblocks_check_poolset(struct pool_set *set, int create);
 
-	os_stat_t st;
-
-	if (os_stat(path, &st)) {
-		ERR("!stat %s", path);
-		return -1;
-	}
-
-	return 0;
-}
-
-/*
- * os_dimm_devdax_clear_badblocks -- fake bad block clearing routine
- */
-int
-os_dimm_devdax_clear_badblocks(const char *path)
-{
-	LOG(3, "path %s", path);
-
-	return 0;
-}
+#endif /* PMDK_BADBLOCK_POOLSET_H */
