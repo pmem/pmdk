8b514a5452cd6cd237ecd34f8ffa202e2188e430
Krzysztof Czurylo <krzysztof.czurylo@intel.com>
2018-03-06 13:09:34 +0100

    test: add missing include in unittest.h
    Fixes compilation error on Fedora 28/rawhide.

--- src/test/unittest/unittest.h
+++ src/test/unittest/unittest.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2017, Intel Corporation
+ * Copyright 2014-2018, Intel Corporation
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -110,6 +110,7 @@ extern "C" {
 #include <sys/mman.h>
 #include <sys/file.h>
 #include <sys/mount.h>
+#include <sys/select.h>
 #include <fcntl.h>
 #include <signal.h>
 #include <errno.h>
