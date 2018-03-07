6b03d1765da3de151fa4f8061b3d2bcf8adcb10a
Krzysztof Czurylo <krzysztof.czurylo@intel.com>
2018-03-07 13:08:22 +0100

    common: workaround for missing macro
    Fixes PMDK compilation on Fedora rawhide.
    Should be fixed in linux/ndctl.h.

--- src/common/os_dimm_ndctl.c
+++ src/common/os_dimm_ndctl.c
@@ -43,6 +43,9 @@
 #include <stdlib.h>
 #include <ndctl/libndctl.h>
 #include <ndctl/libdaxctl.h>
+
+/* XXX: workaround for missing PAGE_SIZE - should be fixed in linux/ndctl.h */
+#include <sys/user.h>
 #include <linux/ndctl.h>
 
 #include "out.h"
