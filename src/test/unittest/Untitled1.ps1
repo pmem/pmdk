b4b4b8d68a7aa159981576e227955e2cf438ea61
Åukasz Plewa <lukasz.plewa@intel.com>
2018-03-14 12:02:00 +0100

    common: windows sdk update

---
+++ src/test/unittest/Untitled1.ps1
@@ -0,0 +1,14 @@
+﻿# standard unit test setup
+
+$Env:UNITTEST_NAME = "util_poolset_parse\TEST0"
+$Env:UNITTEST_NUM = "0"
+
+. ..\unittest\unittest.ps1
+
+
+
+truncate -s 1M C:\test\sparse1
+
+truncate -s 10M C:\test\sparse1
+
+truncate -s 3M C:\test\sparse1
