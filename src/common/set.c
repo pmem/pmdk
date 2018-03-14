6df7eb14b6ca53753a65d8945a7b4adea204c8b5
Lukasz Dorau <lukasz.dorau@intel.com>
2018-03-14 09:24:32 +0100

    common: add os_badblocks_check_poolset()
    Add os_badblocks_check_poolset() to:
    - util_pool_create_uuids(),
    - util_pool_open() and
    - util_pool_open_nocheck()

--- src/common/set.c
+++ src/common/set.c
@@ -69,6 +69,7 @@
 #include "util_pmem.h"
 #include "fs.h"
 #include "os_deep.h"
+#include "badblock_poolset.h"
 
 #define LIBRARY_REMOTE "librpmem.so.1"
 #define SIZE_AUTODETECT_STR "AUTO"
@@ -3053,6 +3054,19 @@ err:
 }
 
 /*
+ * util_print_bad_files_cb -- (internal) callback printing names of pool files
+ *                            containing bad blocks
+ */
+static int
+util_print_bad_files_cb(struct part_file *pf, void *arg)
+{
+	if (pf->has_bad_blocks)
+		ERR("file contains bad blocks -- '%s'", pf->path);
+
+	return 0;
+}
+
+/*
  * util_pool_create_uuids -- create a new memory pool (set or a single file)
  *                           with given uuids
  *
@@ -3129,6 +3143,22 @@ util_pool_create_uuids(struct pool_set **setp, const char *path,
 			util_poolset_append_new_part(set, minsize) != 0) {
 		ERR("cannot create a new part in provided directories");
 		util_poolset_free(set);
+		return -1;
+	}
+
+	int bbs = os_badblocks_check_poolset(set, 1 /* create */);
+	if (bbs < 0) {
+		LOG(1,
+			"WARNING: failed to check pool set for bad blocks -- '%s'",
+			path);
+	}
+
+	if (bbs > 0) {
+		util_poolset_foreach_part_struct(set, util_print_bad_files_cb,
+							NULL);
+		ERR(
+			"pool set contains bad blocks and cannot be created, run 'pmempool' utility to clear bad blocks first");
+
 		return -1;
 	}
 
@@ -3678,6 +3708,22 @@ util_pool_open_nocheck(struct pool_set *set, unsigned flags)
 	ASSERTne(set, NULL);
 	ASSERT(set->nreplicas > 0);
 
+	int bbs = os_badblocks_check_poolset(set, 0 /* not create */);
+	if (bbs < 0) {
+		LOG(1, "WARNING: failed to check pool set for bad blocks");
+	}
+
+	if (bbs > 0) {
+		if (flags & POOL_OPEN_IGNORE_BAD_BLOCKS) {
+			LOG(1,
+				"WARNING: pool set contains bad blocks, ignoring");
+		} else {
+			ERR(
+				"pool set contains bad blocks and cannot be opened, run 'pmempool' utility to try to recover the pool");
+			return -1;
+		}
+	}
+
 	if (set->remote && util_remote_load()) {
 		ERR("the pool set requires a remote replica, "
 			"but the '%s' library cannot be loaded",
@@ -3761,6 +3807,26 @@ util_pool_open(struct pool_set **setp, const char *path, size_t minpartsize,
 	struct pool_set *set = *setp;
 
 	ASSERT(set->nreplicas > 0);
+
+	int bbs = os_badblocks_check_poolset(set, 0 /* not create */);
+	if (bbs < 0) {
+		LOG(1,
+			"WARNING: failed to check pool set for bad blocks -- '%s'",
+			path);
+	}
+
+	if (bbs > 0) {
+		if (flags & POOL_OPEN_IGNORE_BAD_BLOCKS) {
+			LOG(1,
+				"WARNING: pool set contains bad blocks, ignoring -- '%s'",
+				path);
+		} else {
+			ERR(
+				"pool set contains bad blocks and cannot be opened, run 'pmempool' utility to try to recover the pool -- '%s'",
+				path);
+			return -1;
+		}
+	}
 
 	if (set->remote && util_remote_load()) {
 		ERR("the pool set requires a remote replica, "
