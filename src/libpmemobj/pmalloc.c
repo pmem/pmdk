marcinslusarz/nvml

obj: introduce hybrid transactions
This patch revamps the internals of transactions by changing mostly
undo log based solution to a hybrid redo/undo one.
The change was enabled by changing the redo log mechanism to
use: a) checksum for fail-safety and b) entries chaining to enable
large redo logs.

Due to those redo log changes, it has become possible to entirely
eliminate alloc and free undos and even the state field of
transactions because tx post commit has been effectively reduced to
clearing the first entry of the set cache, which also needs to happen
on abort.

The combined effect of the above changes is a dramatic improvement
to transactions performance, especially when performing large number
of allocations/frees.

src/libpmemobj/pmalloc.c
@@ -50,51 +50,65 @@
 #include "alloc_class.h"
 #include "set.h"
 
-#ifdef DEBUG
-/*
- * In order to prevent allocations from inside of a constructor, each lane hold
- * invocation sets the, otherwise unused, runtime part of the lane section
- * to a value that marks an in progress allocation. Likewise, each lane release
- * sets the runtime variable back to NULL.
- *
- * Because this check requires additional hold/release pair for every single
- * allocation, it's done only for debug builds.
- */
-#define ALLOC_INPROGRESS_MARK ((void *)0x1)
-#endif
+#define PMALLOC_REDO_LOG_EXTEND_SIZE 251 /* 4096 - 16 */
+
+enum pmalloc_operation_type {
+	OPERATION_INTERNAL,
+	OPERATION_EXTERNAL,
+
+	MAX_OPERATION_TYPE,
+};
+
+struct lane_alloc_runtime {
+	struct operation_context *ctx[MAX_OPERATION_TYPE];
+};
 
 /*
- * pmalloc_redo_hold -- acquires allocator lane section and returns a pointer to
- * it's redo log
+ * pmalloc_operation_hold_type -- acquires allocator lane section and returns a
+ *	pointer to its operation context
  */
-struct redo_log *
-pmalloc_redo_hold(PMEMobjpool *pop)
+static struct operation_context *
+pmalloc_operation_hold_type(PMEMobjpool *pop, enum pmalloc_operation_type type,
+	int start)
 {
 	struct lane_section *lane;
 	lane_hold(pop, &lane, LANE_SECTION_ALLOCATOR);
+	struct lane_alloc_runtime *rt = lane->runtime;
 
-#ifdef DEBUG
-	ASSERTeq(lane->runtime, NULL);
-	lane->runtime = ALLOC_INPROGRESS_MARK;
-#endif
+	if (start)
+		operation_start(rt->ctx[type]);
+	else
+		operation_init(rt->ctx[type]);
 
-	struct lane_alloc_layout *sec = (void *)lane->layout;
-	return sec->redo;
+	return rt->ctx[type];
 }
 
 /*
- * pmalloc_redo_release -- releases allocator lane section
+ * pmalloc_operation_hold_type -- acquires allocator lane section and returns a
+ *	pointer to it's operation context without starting
+ */
+struct operation_context *
+pmalloc_operation_hold_no_start(PMEMobjpool *pop)
+{
+	return pmalloc_operation_hold_type(pop, OPERATION_EXTERNAL, 0);
+}
+
+/*
+ * pmalloc_operation_hold -- acquires allocator lane section and returns a
+ *	pointer to it's redo log
+ */
+struct operation_context *
+pmalloc_operation_hold(PMEMobjpool *pop)
+{
+	return pmalloc_operation_hold_type(pop, OPERATION_EXTERNAL, 1);
+}
+
+/*
+ * pmalloc_operation_release -- releases allocator lane section
  */
 void
-pmalloc_redo_release(PMEMobjpool *pop)
+pmalloc_operation_release(PMEMobjpool *pop)
 {
-#ifdef DEBUG
-	/* there's no easier way, I've tried */
-	struct lane_section *lane;
-	lane_hold(pop, &lane, LANE_SECTION_ALLOCATOR);
-	lane->runtime = NULL;
-	lane_release(pop);
-#endif
 	lane_release(pop);
 }
 
@@ -134,15 +148,13 @@ int
 pmalloc(PMEMobjpool *pop, uint64_t *off, size_t size,
 	uint64_t extra_field, uint16_t object_flags)
 {
-	struct redo_log *redo = pmalloc_redo_hold(pop);
-
-	struct operation_context ctx;
-	operation_init(&ctx, pop, pop->redo, redo);
+	struct operation_context *ctx =
+		pmalloc_operation_hold_type(pop, OPERATION_INTERNAL, 1);
 
 	int ret = pmalloc_operation(&pop->heap, 0, off, size, NULL, NULL,
-		extra_field, object_flags, 0, &ctx);
+		extra_field, object_flags, 0, ctx);
 
-	pmalloc_redo_release(pop);
+	pmalloc_operation_release(pop);
 
 	return ret;
 }
@@ -160,15 +172,13 @@ pmalloc_construct(PMEMobjpool *pop, uint64_t *off, size_t size,
 	palloc_constr constructor, void *arg,
 	uint64_t extra_field, uint16_t object_flags, uint16_t class_id)
 {
-	struct redo_log *redo = pmalloc_redo_hold(pop);
-	struct operation_context ctx;
-
-	operation_init(&ctx, pop, pop->redo, redo);
+	struct operation_context *ctx =
+		pmalloc_operation_hold_type(pop, OPERATION_INTERNAL, 1);
 
 	int ret = pmalloc_operation(&pop->heap, 0, off, size, constructor, arg,
-			extra_field, object_flags, class_id, &ctx);
+			extra_field, object_flags, class_id, ctx);
 
-	pmalloc_redo_release(pop);
+	pmalloc_operation_release(pop);
 
 	return ret;
 }
@@ -184,15 +194,13 @@ int
 prealloc(PMEMobjpool *pop, uint64_t *off, size_t size,
 	uint64_t extra_field, uint16_t object_flags)
 {
-	struct redo_log *redo = pmalloc_redo_hold(pop);
-	struct operation_context ctx;
-
-	operation_init(&ctx, pop, pop->redo, redo);
+	struct operation_context *ctx =
+		pmalloc_operation_hold_type(pop, OPERATION_INTERNAL, 1);
 
 	int ret = pmalloc_operation(&pop->heap, *off, off, size, NULL, NULL,
-		extra_field, object_flags, 0, &ctx);
+		extra_field, object_flags, 0, ctx);
 
-	pmalloc_redo_release(pop);
+	pmalloc_operation_release(pop);
 
 	return ret;
 }
@@ -207,24 +215,83 @@ prealloc(PMEMobjpool *pop, uint64_t *off, size_t size,
 void
 pfree(PMEMobjpool *pop, uint64_t *off)
 {
-	struct redo_log *redo = pmalloc_redo_hold(pop);
-	struct operation_context ctx;
-
-	operation_init(&ctx, pop, pop->redo, redo);
+	struct operation_context *ctx =
+		pmalloc_operation_hold_type(pop, OPERATION_INTERNAL, 1);
 
 	int ret = pmalloc_operation(&pop->heap, *off, off, 0, NULL, NULL,
-		0, 0, 0, &ctx);
+		0, 0, 0, ctx);
 	ASSERTeq(ret, 0);
 
-	pmalloc_redo_release(pop);
+	pmalloc_operation_release(pop);
+}
+
+/*
+ * alloc_redo_constructor -- constructor of a new redo log section
+ */
+static int
+alloc_redo_constructor(void *base, void *ptr, size_t usable_size, void *arg)
+{
+	PMEMobjpool *pop = base;
+	const struct pmem_ops *p_ops = &pop->p_ops;
+	VALGRIND_ADD_TO_TX(ptr, usable_size);
+
+	struct redo_log *redo = ptr;
+	redo->capacity = PMALLOC_REDO_LOG_EXTEND_SIZE;
+	redo->checksum = 0;
+	redo->next = 0;
+	memset(redo->unused, 0, sizeof(redo->unused));
+
+	pmemops_flush(p_ops, redo, sizeof(*redo));
+
+	pmemops_memset_persist(p_ops, redo->entries, 0,
+		usable_size - sizeof(*redo));
+
+	VALGRIND_REMOVE_FROM_TX(ptr, usable_size);
+
+	return 0;
+}
+
+/*
+ * alloc_redo_external_extend -- redo log extend callback
+ */
+static int
+alloc_redo_external_extend(void *base, uint64_t *redo)
+{
+	size_t s = SIZEOF_REDO_LOG(PMALLOC_REDO_LOG_EXTEND_SIZE);
+
+	return pmalloc_construct(base, redo, s, alloc_redo_constructor, NULL, 0,
+		OBJ_INTERNAL_OBJECT_MASK, 0);
 }
 
 /*
  * pmalloc_construct_rt -- construct runtime part of allocator section
  */
 static void *
-pmalloc_construct_rt(PMEMobjpool *pop)
+pmalloc_construct_rt(PMEMobjpool *pop, void *data)
 {
+	struct lane_alloc_layout *layout = data;
+	struct lane_alloc_runtime *alloc_rt = Malloc(sizeof(*alloc_rt));
+	if (alloc_rt == NULL)
+		goto error_rt_alloc;
+
+	alloc_rt->ctx[OPERATION_INTERNAL] = operation_new(pop, pop->redo,
+		(struct redo_log *)&layout->internal, ALLOC_REDO_INTERNAL_SIZE,
+		NULL);
+	if (alloc_rt->ctx[OPERATION_INTERNAL] == NULL)
+		goto error_internal_alloc;
+
+	alloc_rt->ctx[OPERATION_EXTERNAL] = operation_new(pop, pop->redo,
+		(struct redo_log *)&layout->external, ALLOC_REDO_EXTERNAL_SIZE,
+		alloc_redo_external_extend);
+	if (alloc_rt->ctx[OPERATION_EXTERNAL] == NULL)
+		goto error_external_alloc;
+
+	return alloc_rt;
+error_external_alloc:
+	Free(alloc_rt->ctx[OPERATION_INTERNAL]);
+error_internal_alloc:
+	Free(alloc_rt);
+error_rt_alloc:
 	return NULL;
 }
 
@@ -234,7 +301,10 @@ pmalloc_construct_rt(PMEMobjpool *pop)
 static void
 pmalloc_destroy_rt(PMEMobjpool *pop, void *rt)
 {
-	/* NOP */
+	struct lane_alloc_runtime *alloc_rt = rt;
+	operation_delete(alloc_rt->ctx[OPERATION_INTERNAL]);
+	operation_delete(alloc_rt->ctx[OPERATION_EXTERNAL]);
+	Free(alloc_rt);
 }
 
 /*
@@ -246,7 +316,8 @@ pmalloc_recovery(PMEMobjpool *pop, void *data, unsigned length)
 	struct lane_alloc_layout *sec = data;
 	ASSERT(sizeof(*sec) <= length);
 
-	redo_log_recover(pop->redo, sec->redo, ALLOC_REDO_LOG_SIZE);
+	redo_log_recover(pop->redo, (struct redo_log *)&sec->internal);
+	redo_log_recover(pop->redo, (struct redo_log *)&sec->external);
 
 	return 0;
 }
@@ -261,9 +332,14 @@ pmalloc_check(PMEMobjpool *pop, void *data, unsigned length)
 
 	struct lane_alloc_layout *sec = data;
 
-	int ret = redo_log_check(pop->redo, sec->redo, ALLOC_REDO_LOG_SIZE);
+	int ret = redo_log_check(pop->redo, (struct redo_log *)&sec->internal);
 	if (ret != 0)
-		ERR("allocator lane: redo log check failed");
+		ERR("allocator lane: internal redo log check failed");
+
+	ret = redo_log_check(pop->redo, (struct redo_log *)&sec->external);
+	if (ret != 0)
+		ERR("allocator lane: external redo log check failed");
+
 
 	return ret;
 }

src/test/obj_memops/obj_memops.c
@@ -1,0 +1,227 @@
+/*
+ * Copyright 2018, Intel Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *
+ *     * Neither the name of the copyright holder nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * obj_memops.c -- basic memory operations tests
+ *
+ */
+
+#include <stddef.h>
+#include "obj.h"
+#include "memops.h"
+#include "unittest.h"
+
+#define TEST_ENTRIES 10
+
+#define TEST_VALUES 100
+
+enum fail_types {
+	FAIL_NONE,
+	FAIL_CHECKSUM,
+	FAIL_MODIFY_NEXT,
+	FAIL_MODIFY_VALUE,
+};
+
+struct test_object {
+	struct REDO_LOG(TEST_ENTRIES) redo;
+	uint64_t values[TEST_VALUES];
+};
+
+static void
+clear_test_values(struct test_object *object)
+{
+	memset(object->values, 0, sizeof(uint64_t) * TEST_VALUES);
+}
+
+static int
+redo_log_constructor(void *ctx, void *ptr, size_t usable_size, void *arg)
+{
+	PMEMobjpool *pop = ctx;
+	const struct pmem_ops *p_ops = &pop->p_ops;
+
+	struct redo_log *redo = ptr;
+	redo->capacity = TEST_ENTRIES;
+	redo->checksum = 0;
+	redo->next = 0;
+	memset(redo->unused, 0, sizeof(redo->unused));
+
+	pmemops_flush(p_ops, redo, sizeof(*redo));
+
+	pmemops_memset_persist(p_ops, redo->entries, 0,
+		usable_size - sizeof(*redo));
+
+	return 0;
+}
+
+static int
+pmalloc_redo_extend(void *base, uint64_t *redo)
+{
+	size_t s = SIZEOF_REDO_LOG(TEST_ENTRIES);
+
+	return pmalloc_construct(base, redo, s, redo_log_constructor, NULL, 0,
+		OBJ_INTERNAL_OBJECT_MASK, 0);
+}
+
+static void
+test_set_entries(struct operation_context *ctx, struct test_object *object,
+	size_t nentries, enum fail_types fail)
+{
+	operation_start(ctx);
+
+	for (size_t i = 0; i < nentries; ++i) {
+		operation_add_typed_entry(ctx,
+			&object->values[i], i,
+			REDO_OPERATION_SET, LOG_PERSISTENT);
+	}
+
+	operation_reserve(ctx, nentries);
+
+	if (fail != FAIL_NONE) {
+		operation_cancel(ctx);
+
+		switch (fail) {
+			case FAIL_CHECKSUM:
+				ctx->redo->checksum += 1;
+			break;
+			case FAIL_MODIFY_NEXT:
+				pmalloc_redo_extend(ctx->base,
+					&ctx->redo->next);
+			break;
+			case FAIL_MODIFY_VALUE:
+				ctx->redo->entries[1].offset += 8;
+			break;
+			default:
+				UT_ASSERT(0);
+		}
+
+		redo_log_recover(ctx->redo_ctx, ctx->redo);
+
+		UT_ASSERTeq(object->values[0], 0);
+		UT_ASSERTeq(object->values[1], 0);
+
+		for (size_t i = 0; i < nentries; ++i)
+			UT_ASSERTeq(object->values[i], 0);
+	} else {
+		operation_process(ctx);
+
+		for (size_t i = 0; i < nentries; ++i)
+			UT_ASSERTeq(object->values[i], i);
+	}
+}
+
+static void
+test_same_twice(struct operation_context *ctx, struct test_object *object)
+{
+	operation_start(ctx);
+
+	operation_add_typed_entry(ctx,
+		&object->values[0], 5,
+		REDO_OPERATION_SET, LOG_PERSISTENT);
+
+	operation_add_typed_entry(ctx,
+		&object->values[0], 10,
+		REDO_OPERATION_SET, LOG_PERSISTENT);
+
+	operation_process(ctx);
+
+	UT_ASSERTeq(object->values[0], 5);
+}
+
+static void
+test_merge_op(struct operation_context *ctx, struct test_object *object)
+{
+	operation_start(ctx);
+
+	operation_add_typed_entry(ctx,
+		&object->values[0], 0b01,
+		REDO_OPERATION_OR, LOG_PERSISTENT);
+
+	operation_add_typed_entry(ctx,
+		&object->values[0], 0b10,
+		REDO_OPERATION_OR, LOG_PERSISTENT);
+
+	operation_process(ctx);
+
+	UT_ASSERTeq(object->values[0], 0b11);
+}
+
+int
+main(int argc, char *argv[])
+{
+	START(argc, argv, "obj_memops");
+
+	if (argc != 2)
+		UT_FATAL("usage: %s file-name", argv[0]);
+
+	const char *path = argv[1];
+
+	PMEMobjpool *pop = NULL;
+
+	if ((pop = pmemobj_create(path, "obj_memops",
+			PMEMOBJ_MIN_POOL * 10, S_IWUSR | S_IRUSR)) == NULL)
+		UT_FATAL("!pmemobj_create: %s", path);
+
+	struct test_object *object =
+		pmemobj_direct(pmemobj_root(pop, sizeof(struct test_object)));
+	UT_ASSERTne(object, NULL);
+
+	struct operation_context *ctx = operation_new(pop, pop->redo,
+		(struct redo_log *)&object->redo, TEST_ENTRIES,
+		pmalloc_redo_extend);
+
+	test_set_entries(ctx, object, 10, 0);
+	clear_test_values(object);
+	test_same_twice(ctx, object);
+	clear_test_values(object);
+	test_merge_op(ctx, object);
+	clear_test_values(object);
+	clear_test_values(object);
+	test_set_entries(ctx, object, 100, 0);
+	clear_test_values(object);
+	test_set_entries(ctx, object, 100, FAIL_CHECKSUM);
+	clear_test_values(object);
+	test_set_entries(ctx, object, 10, FAIL_CHECKSUM);
+	clear_test_values(object);
+	test_set_entries(ctx, object, 100, FAIL_MODIFY_NEXT);
+	clear_test_values(object);
+	test_set_entries(ctx, object, 10, FAIL_MODIFY_NEXT);
+	clear_test_values(object);
+	test_set_entries(ctx, object, 100, FAIL_MODIFY_VALUE);
+	clear_test_values(object);
+	test_set_entries(ctx, object, 10, FAIL_MODIFY_VALUE);
+
+	operation_delete(ctx);
+
+	pmemobj_close(pop);
+
+	DONE(NULL);
+}

pmem/pmdk

src/libpmemobj/pmalloc.c
@@ -50,51 +50,65 @@
 #include "alloc_class.h"
 #include "set.h"
 
-#ifdef DEBUG
-/*
- * In order to prevent allocations from inside of a constructor, each lane hold
- * invocation sets the, otherwise unused, runtime part of the lane section
- * to a value that marks an in progress allocation. Likewise, each lane release
- * sets the runtime variable back to NULL.
- *
- * Because this check requires additional hold/release pair for every single
- * allocation, it's done only for debug builds.
- */
-#define ALLOC_INPROGRESS_MARK ((void *)0x1)
-#endif
+#define PMALLOC_REDO_LOG_EXTEND_SIZE 251 /* 4096 - 16 */
+
+enum pmalloc_operation_type {
+	OPERATION_INTERNAL,
+	OPERATION_EXTERNAL,
+
+	MAX_OPERATION_TYPE,
+};
+
+struct lane_alloc_runtime {
+	struct operation_context *ctx[MAX_OPERATION_TYPE];
+};
 
 /*
- * pmalloc_redo_hold -- acquires allocator lane section and returns a pointer to
- * it's redo log
+ * pmalloc_operation_hold_type -- acquires allocator lane section and returns a
+ *	pointer to its operation context
  */
-struct redo_log *
-pmalloc_redo_hold(PMEMobjpool *pop)
+static struct operation_context *
+pmalloc_operation_hold_type(PMEMobjpool *pop, enum pmalloc_operation_type type,
+	int start)
 {
 	struct lane_section *lane;
 	lane_hold(pop, &lane, LANE_SECTION_ALLOCATOR);
+	struct lane_alloc_runtime *rt = lane->runtime;
 
-#ifdef DEBUG
-	ASSERTeq(lane->runtime, NULL);
-	lane->runtime = ALLOC_INPROGRESS_MARK;
-#endif
+	if (start)
+		operation_start(rt->ctx[type]);
+	else
+		operation_init(rt->ctx[type]);
 
-	struct lane_alloc_layout *sec = (void *)lane->layout;
-	return sec->redo;
+	return rt->ctx[type];
 }
 
 /*
- * pmalloc_redo_release -- releases allocator lane section
+ * pmalloc_operation_hold_type -- acquires allocator lane section and returns a
+ *	pointer to it's operation context without starting
+ */
+struct operation_context *
+pmalloc_operation_hold_no_start(PMEMobjpool *pop)
+{
+	return pmalloc_operation_hold_type(pop, OPERATION_EXTERNAL, 0);
+}
+
+/*
+ * pmalloc_operation_hold -- acquires allocator lane section and returns a
+ *	pointer to it's redo log
+ */
+struct operation_context *
+pmalloc_operation_hold(PMEMobjpool *pop)
+{
+	return pmalloc_operation_hold_type(pop, OPERATION_EXTERNAL, 1);
+}
+
+/*
+ * pmalloc_operation_release -- releases allocator lane section
  */
 void
-pmalloc_redo_release(PMEMobjpool *pop)
+pmalloc_operation_release(PMEMobjpool *pop)
 {
-#ifdef DEBUG
-	/* there's no easier way, I've tried */
-	struct lane_section *lane;
-	lane_hold(pop, &lane, LANE_SECTION_ALLOCATOR);
-	lane->runtime = NULL;
-	lane_release(pop);
-#endif
 	lane_release(pop);
 }
 
@@ -134,15 +148,13 @@ int
 pmalloc(PMEMobjpool *pop, uint64_t *off, size_t size,
 	uint64_t extra_field, uint16_t object_flags)
 {
-	struct redo_log *redo = pmalloc_redo_hold(pop);
-
-	struct operation_context ctx;
-	operation_init(&ctx, pop, pop->redo, redo);
+	struct operation_context *ctx =
+		pmalloc_operation_hold_type(pop, OPERATION_INTERNAL, 1);
 
 	int ret = pmalloc_operation(&pop->heap, 0, off, size, NULL, NULL,
-		extra_field, object_flags, 0, &ctx);
+		extra_field, object_flags, 0, ctx);
 
-	pmalloc_redo_release(pop);
+	pmalloc_operation_release(pop);
 
 	return ret;
 }
@@ -160,15 +172,13 @@ pmalloc_construct(PMEMobjpool *pop, uint64_t *off, size_t size,
 	palloc_constr constructor, void *arg,
 	uint64_t extra_field, uint16_t object_flags, uint16_t class_id)
 {
-	struct redo_log *redo = pmalloc_redo_hold(pop);
-	struct operation_context ctx;
-
-	operation_init(&ctx, pop, pop->redo, redo);
+	struct operation_context *ctx =
+		pmalloc_operation_hold_type(pop, OPERATION_INTERNAL, 1);
 
 	int ret = pmalloc_operation(&pop->heap, 0, off, size, constructor, arg,
-			extra_field, object_flags, class_id, &ctx);
+			extra_field, object_flags, class_id, ctx);
 
-	pmalloc_redo_release(pop);
+	pmalloc_operation_release(pop);
 
 	return ret;
 }
@@ -184,15 +194,13 @@ int
 prealloc(PMEMobjpool *pop, uint64_t *off, size_t size,
 	uint64_t extra_field, uint16_t object_flags)
 {
-	struct redo_log *redo = pmalloc_redo_hold(pop);
-	struct operation_context ctx;
-
-	operation_init(&ctx, pop, pop->redo, redo);
+	struct operation_context *ctx =
+		pmalloc_operation_hold_type(pop, OPERATION_INTERNAL, 1);
 
 	int ret = pmalloc_operation(&pop->heap, *off, off, size, NULL, NULL,
-		extra_field, object_flags, 0, &ctx);
+		extra_field, object_flags, 0, ctx);
 
-	pmalloc_redo_release(pop);
+	pmalloc_operation_release(pop);
 
 	return ret;
 }
@@ -207,24 +215,83 @@ prealloc(PMEMobjpool *pop, uint64_t *off, size_t size,
 void
 pfree(PMEMobjpool *pop, uint64_t *off)
 {
-	struct redo_log *redo = pmalloc_redo_hold(pop);
-	struct operation_context ctx;
-
-	operation_init(&ctx, pop, pop->redo, redo);
+	struct operation_context *ctx =
+		pmalloc_operation_hold_type(pop, OPERATION_INTERNAL, 1);
 
 	int ret = pmalloc_operation(&pop->heap, *off, off, 0, NULL, NULL,
-		0, 0, 0, &ctx);
+		0, 0, 0, ctx);
 	ASSERTeq(ret, 0);
 
-	pmalloc_redo_release(pop);
+	pmalloc_operation_release(pop);
+}
+
+/*
+ * alloc_redo_constructor -- constructor of a new redo log section
+ */
+static int
+alloc_redo_constructor(void *base, void *ptr, size_t usable_size, void *arg)
+{
+	PMEMobjpool *pop = base;
+	const struct pmem_ops *p_ops = &pop->p_ops;
+	VALGRIND_ADD_TO_TX(ptr, usable_size);
+
+	struct redo_log *redo = ptr;
+	redo->capacity = PMALLOC_REDO_LOG_EXTEND_SIZE;
+	redo->checksum = 0;
+	redo->next = 0;
+	memset(redo->unused, 0, sizeof(redo->unused));
+
+	pmemops_flush(p_ops, redo, sizeof(*redo));
+
+	pmemops_memset(p_ops, redo->entries, 0,
+		usable_size - sizeof(*redo), 0);
+
+	VALGRIND_REMOVE_FROM_TX(ptr, usable_size);
+
+	return 0;
+}
+
+/*
+ * alloc_redo_external_extend -- redo log extend callback
+ */
+static int
+alloc_redo_external_extend(void *base, uint64_t *redo)
+{
+	size_t s = SIZEOF_REDO_LOG(PMALLOC_REDO_LOG_EXTEND_SIZE);
+
+	return pmalloc_construct(base, redo, s, alloc_redo_constructor, NULL, 0,
+		OBJ_INTERNAL_OBJECT_MASK, 0);
 }
 
 /*
  * pmalloc_construct_rt -- construct runtime part of allocator section
  */
 static void *
-pmalloc_construct_rt(PMEMobjpool *pop)
+pmalloc_construct_rt(PMEMobjpool *pop, void *data)
 {
+	struct lane_alloc_layout *layout = data;
+	struct lane_alloc_runtime *alloc_rt = Malloc(sizeof(*alloc_rt));
+	if (alloc_rt == NULL)
+		goto error_rt_alloc;
+
+	alloc_rt->ctx[OPERATION_INTERNAL] = operation_new(pop, pop->redo,
+		(struct redo_log *)&layout->internal, ALLOC_REDO_INTERNAL_SIZE,
+		NULL);
+	if (alloc_rt->ctx[OPERATION_INTERNAL] == NULL)
+		goto error_internal_alloc;
+
+	alloc_rt->ctx[OPERATION_EXTERNAL] = operation_new(pop, pop->redo,
+		(struct redo_log *)&layout->external, ALLOC_REDO_EXTERNAL_SIZE,
+		alloc_redo_external_extend);
+	if (alloc_rt->ctx[OPERATION_EXTERNAL] == NULL)
+		goto error_external_alloc;
+
+	return alloc_rt;
+error_external_alloc:
+	Free(alloc_rt->ctx[OPERATION_INTERNAL]);
+error_internal_alloc:
+	Free(alloc_rt);
+error_rt_alloc:
 	return NULL;
 }
 
@@ -234,7 +301,10 @@ pmalloc_construct_rt(PMEMobjpool *pop)
 static void
 pmalloc_destroy_rt(PMEMobjpool *pop, void *rt)
 {
-	/* NOP */
+	struct lane_alloc_runtime *alloc_rt = rt;
+	operation_delete(alloc_rt->ctx[OPERATION_INTERNAL]);
+	operation_delete(alloc_rt->ctx[OPERATION_EXTERNAL]);
+	Free(alloc_rt);
 }
 
 /*
@@ -246,7 +316,8 @@ pmalloc_recovery(PMEMobjpool *pop, void *data, unsigned length)
 	struct lane_alloc_layout *sec = data;
 	ASSERT(sizeof(*sec) <= length);
 
-	redo_log_recover(pop->redo, sec->redo, ALLOC_REDO_LOG_SIZE);
+	redo_log_recover(pop->redo, (struct redo_log *)&sec->internal);
+	redo_log_recover(pop->redo, (struct redo_log *)&sec->external);
 
 	return 0;
 }
@@ -261,9 +332,14 @@ pmalloc_check(PMEMobjpool *pop, void *data, unsigned length)
 
 	struct lane_alloc_layout *sec = data;
 
-	int ret = redo_log_check(pop->redo, sec->redo, ALLOC_REDO_LOG_SIZE);
+	int ret = redo_log_check(pop->redo, (struct redo_log *)&sec->internal);
 	if (ret != 0)
-		ERR("allocator lane: redo log check failed");
+		ERR("allocator lane: internal redo log check failed");
+
+	ret = redo_log_check(pop->redo, (struct redo_log *)&sec->external);
+	if (ret != 0)
+		ERR("allocator lane: external redo log check failed");
+
 
 	return ret;
 }

src/test/obj_memops/obj_memops.c
@@ -1,0 +1,227 @@
+/*
+ * Copyright 2018, Intel Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *
+ *     * Neither the name of the copyright holder nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * obj_memops.c -- basic memory operations tests
+ *
+ */
+
+#include <stddef.h>
+#include "obj.h"
+#include "memops.h"
+#include "unittest.h"
+
+#define TEST_ENTRIES 10
+
+#define TEST_VALUES 100
+
+enum fail_types {
+	FAIL_NONE,
+	FAIL_CHECKSUM,
+	FAIL_MODIFY_NEXT,
+	FAIL_MODIFY_VALUE,
+};
+
+struct test_object {
+	struct REDO_LOG(TEST_ENTRIES) redo;
+	uint64_t values[TEST_VALUES];
+};
+
+static void
+clear_test_values(struct test_object *object)
+{
+	memset(object->values, 0, sizeof(uint64_t) * TEST_VALUES);
+}
+
+static int
+redo_log_constructor(void *ctx, void *ptr, size_t usable_size, void *arg)
+{
+	PMEMobjpool *pop = ctx;
+	const struct pmem_ops *p_ops = &pop->p_ops;
+
+	struct redo_log *redo = ptr;
+	redo->capacity = TEST_ENTRIES;
+	redo->checksum = 0;
+	redo->next = 0;
+	memset(redo->unused, 0, sizeof(redo->unused));
+
+	pmemops_flush(p_ops, redo, sizeof(*redo));
+
+	pmemops_memset(p_ops, redo->entries, 0,
+		usable_size - sizeof(*redo), 0);
+
+	return 0;
+}
+
+static int
+pmalloc_redo_extend(void *base, uint64_t *redo)
+{
+	size_t s = SIZEOF_REDO_LOG(TEST_ENTRIES);
+
+	return pmalloc_construct(base, redo, s, redo_log_constructor, NULL, 0,
+		OBJ_INTERNAL_OBJECT_MASK, 0);
+}
+
+static void
+test_set_entries(struct operation_context *ctx, struct test_object *object,
+	size_t nentries, enum fail_types fail)
+{
+	operation_start(ctx);
+
+	for (size_t i = 0; i < nentries; ++i) {
+		operation_add_typed_entry(ctx,
+			&object->values[i], i,
+			REDO_OPERATION_SET, LOG_PERSISTENT);
+	}
+
+	operation_reserve(ctx, nentries);
+
+	if (fail != FAIL_NONE) {
+		operation_cancel(ctx);
+
+		switch (fail) {
+			case FAIL_CHECKSUM:
+				ctx->redo->checksum += 1;
+			break;
+			case FAIL_MODIFY_NEXT:
+				pmalloc_redo_extend(ctx->base,
+					&ctx->redo->next);
+			break;
+			case FAIL_MODIFY_VALUE:
+				ctx->redo->entries[1].offset += 8;
+			break;
+			default:
+				UT_ASSERT(0);
+		}
+
+		redo_log_recover(ctx->redo_ctx, ctx->redo);
+
+		UT_ASSERTeq(object->values[0], 0);
+		UT_ASSERTeq(object->values[1], 0);
+
+		for (size_t i = 0; i < nentries; ++i)
+			UT_ASSERTeq(object->values[i], 0);
+	} else {
+		operation_process(ctx);
+
+		for (size_t i = 0; i < nentries; ++i)
+			UT_ASSERTeq(object->values[i], i);
+	}
+}
+
+static void
+test_same_twice(struct operation_context *ctx, struct test_object *object)
+{
+	operation_start(ctx);
+
+	operation_add_typed_entry(ctx,
+		&object->values[0], 5,
+		REDO_OPERATION_SET, LOG_PERSISTENT);
+
+	operation_add_typed_entry(ctx,
+		&object->values[0], 10,
+		REDO_OPERATION_SET, LOG_PERSISTENT);
+
+	operation_process(ctx);
+
+	UT_ASSERTeq(object->values[0], 5);
+}
+
+static void
+test_merge_op(struct operation_context *ctx, struct test_object *object)
+{
+	operation_start(ctx);
+
+	operation_add_typed_entry(ctx,
+		&object->values[0], 0b01,
+		REDO_OPERATION_OR, LOG_PERSISTENT);
+
+	operation_add_typed_entry(ctx,
+		&object->values[0], 0b10,
+		REDO_OPERATION_OR, LOG_PERSISTENT);
+
+	operation_process(ctx);
+
+	UT_ASSERTeq(object->values[0], 0b11);
+}
+
+int
+main(int argc, char *argv[])
+{
+	START(argc, argv, "obj_memops");
+
+	if (argc != 2)
+		UT_FATAL("usage: %s file-name", argv[0]);
+
+	const char *path = argv[1];
+
+	PMEMobjpool *pop = NULL;
+
+	if ((pop = pmemobj_create(path, "obj_memops",
+			PMEMOBJ_MIN_POOL * 10, S_IWUSR | S_IRUSR)) == NULL)
+		UT_FATAL("!pmemobj_create: %s", path);
+
+	struct test_object *object =
+		pmemobj_direct(pmemobj_root(pop, sizeof(struct test_object)));
+	UT_ASSERTne(object, NULL);
+
+	struct operation_context *ctx = operation_new(pop, pop->redo,
+		(struct redo_log *)&object->redo, TEST_ENTRIES,
+		pmalloc_redo_extend);
+
+	test_set_entries(ctx, object, 10, 0);
+	clear_test_values(object);
+	test_same_twice(ctx, object);
+	clear_test_values(object);
+	test_merge_op(ctx, object);
+	clear_test_values(object);
+	clear_test_values(object);
+	test_set_entries(ctx, object, 100, 0);
+	clear_test_values(object);
+	test_set_entries(ctx, object, 100, FAIL_CHECKSUM);
+	clear_test_values(object);
+	test_set_entries(ctx, object, 10, FAIL_CHECKSUM);
+	clear_test_values(object);
+	test_set_entries(ctx, object, 100, FAIL_MODIFY_NEXT);
+	clear_test_values(object);
+	test_set_entries(ctx, object, 10, FAIL_MODIFY_NEXT);
+	clear_test_values(object);
+	test_set_entries(ctx, object, 100, FAIL_MODIFY_VALUE);
+	clear_test_values(object);
+	test_set_entries(ctx, object, 10, FAIL_MODIFY_VALUE);
+
+	operation_delete(ctx);
+
+	pmemobj_close(pop);
+
+	DONE(NULL);
+}