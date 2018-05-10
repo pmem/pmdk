4e24866cdb7ba1fd7b242207cdb6b841268273c8
Lukasz Dorau <lukasz.dorau@intel.com>
2018-05-10 13:03:38 +0200

    common: add clear-only-given-badblocks functions

--- src/common/os_dimm_windows.c
+++ src/common/os_dimm_windows.c
@@ -176,7 +176,18 @@ os_dimm_files_namespace_badblocks(const char *path, struct badblocks *bbs)
  * os_dimm_devdax_clear_badblocks -- fake bad block clearing routine
  */
 int
-os_dimm_devdax_clear_badblocks(const char *path)
+os_dimm_devdax_clear_badblocks(const char *path, struct badblocks *bbs)
+{
+	LOG(3, "path %s badblocks %p", path, bbs);
+
+	return 0;
+}
+
+/*
+ * os_dimm_devdax_clear_badblocks_all -- fake bad block clearing routine
+ */
+int
+os_dimm_devdax_clear_badblocks_all(const char *path)
 {
 	LOG(3, "path %s", path);
 
