fcfea128769f02c661d713a8c66f9b29ba33a521
Marcin Ślusarz <marcin.slusarz@intel.com>
2018-05-18 14:29:36 +0200

    test: verify fallocate is supported by fs
    ... before using it in util_file_create test.

--- src/test/util_file_create/util_file_create.c
+++ src/test/util_file_create/util_file_create.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2017, Intel Corporation
+ * Copyright 2014-2018, Intel Corporation
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -36,9 +36,46 @@
  * usage: util_file_create minlen len:path [len:path]...
  */
 
+#define _GNU_SOURCE
 #include "unittest.h"
 
 #include "file.h"
+
+#ifdef __linux__
+#include <errno.h>
+#include <fcntl.h>
+static void
+check_filesystem(const char *file)
+{
+	int fd = OPEN(file, O_RDWR | O_CREAT | O_EXCL, 0644);
+
+	if (fallocate(fd, 0, 0, 4096)) {
+		/*
+		 * util_file_create uses posix_fallocate to allocate file space.
+		 * posix_fallocate on Linux is implemented using fallocate
+		 * syscall. This syscall requires file system-specific code on
+		 * the kernel side and not all file systems have this code.
+		 * So when posix_fallocate gets 'not supported' error from
+		 * fallocate it falls back to just writing zeroes.
+		 * This is very slow and can easily make this test fail.
+		 * Detect it and refuse to run the test in such case.
+		 */
+		if (errno == EOPNOTSUPP)
+			UT_FATAL(
+				"File system backing '%s' does not support fallocate syscall.",
+				file);
+		UT_FATAL("!fallocate");
+	}
+
+	CLOSE(fd);
+	UNLINK(file);
+}
+#else
+static void
+check_filesystem(const char *file)
+{
+}
+#endif
 
 int
 main(int argc, char *argv[])
@@ -56,6 +93,9 @@ main(int argc, char *argv[])
 		if (*fname != ':')
 			UT_FATAL("usage: %s minlen len:path...", argv[0]);
 		fname++;
+
+		if (size > (1U << 30))
+			check_filesystem(fname);
 
 		int fd;
 		if ((fd = util_file_create(fname, size, minsize)) == -1)
