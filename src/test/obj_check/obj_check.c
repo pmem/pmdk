c4884530f26eede360b94105ed99705d5ceb8f58
gbrzezin <grzegorz.brzezinski@intel.com>
2018-05-28 13:31:13 +0200

    test: add tests for pmemobj_check

--- src/test/obj_check/obj_check.c
+++ src/test/obj_check/obj_check.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2017, Intel Corporation
+ * Copyright 2015-2018, Intel Corporation
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -48,8 +48,29 @@ main(int argc, char *argv[])
 	}
 
 	const char *path = argv[1];
+	const char *layout = NULL;
+	PMEMobjpool *pop = NULL;
+	int open = 0;
 
-	int ret = pmemobj_check(path, NULL);
+	for (int i = 2; i < argc; ++i) {
+		if (strcmp(argv[i], "-o") == 0)
+			open = 1;
+		else if (strcmp(argv[i], "-l") == 0) {
+			layout = argv[i + 1];
+			i++;
+		} else
+			UT_OUT("Unrecognized argument: %s", argv[i]);
+	}
+
+	if (open) {
+		pop = pmemobj_open(path, layout);
+		if (pop == NULL)
+			UT_OUT("!%s: pmemobj_open", path);
+		else
+			UT_OUT("%s: pmemobj_open: Success", path);
+	}
+
+	int ret = pmemobj_check(path, layout);
 
 	switch (ret) {
 	case 1:
@@ -62,6 +83,9 @@ main(int argc, char *argv[])
 		UT_OUT("error: %s", pmemobj_errormsg());
 		break;
 	}
+
+	if (pop != NULL)
+		pmemobj_close(pop);
 
 	DONE(NULL);
 }
