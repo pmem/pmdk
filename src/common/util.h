790e6e69e4114c2a28f1026ed77b11b8206713ba
Piotr Balcer <piotr.balcer@intel.com>
2018-08-20 12:23:20 +0200

    obj: implement fail-safe log buffer operations
    This allows us to use the existing redo-log code as a basis
    for undo logs to be used in transactions.

--- src/common/util.h
+++ src/common/util.h
@@ -91,6 +91,7 @@ void util_init(void);
 int util_is_zeroed(const void *addr, size_t len);
 int util_checksum(void *addr, size_t len, uint64_t *csump,
 		int insert, size_t skip_off);
+uint64_t util_checksum_seq(const void *addr, size_t len, uint64_t csum);
 int util_parse_size(const char *str, size_t *sizep);
 char *util_fgets(char *buffer, int max, FILE *stream);
 char *util_getexecname(char *path, size_t pathlen);
