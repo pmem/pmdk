9cf7cdc23684b50eaee6b724ecf72db97879dcf9
Oksana Salyk <oksana.salyk@intel.com>
2018-09-06 14:29:27 +0200

    test: (lib)pmempool feature

---
+++ src/test/libpmempool_feature/common.sh
@@ -0,0 +1,135 @@
+#!/usr/bin/env bash
+#
+# Copyright 2018, Intel Corporation
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in
+#       the documentation and/or other materials provided with the
+#       distribution.
+#
+#     * Neither the name of the copyright holder nor the names of its
+#       contributors may be used to endorse or promote products derived
+#       from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+#
+# src/test/libpmempool_feature/common.sh -- common part of libpmempool_feature tests
+#
+
+OUT=out${UNITTEST_NUM}.log
+LOG=grep${UNITTEST_NUM}.log
+
+# enum pmempool_feature values
+PMEMPOOL_FEAT_SINGLEHDR=SINGLEHDR
+PMEMPOOL_FEAT_CHCKSUM_2K=CKSUM_2K
+PMEMPOOL_FEAT_SHUTDOWN_STATE=SHUTDOWN_STATE
+PMEMPOOL_FEAT_INVALID=INVALID
+
+QUERY_PATTERN="query"
+
+exit_func=expect_normal_exit
+
+# libpmempool_feature_query -- query feature
+#
+# usage: libpmempool_feature_query <enum-pmempool_feature>
+function libpmempool_feature_query() {
+	# query feature
+	expect_normal_exit ./libpmempool_feature$EXESUFFIX $DIR/pool.obj q $1
+	cat $OUT | grep "$QUERY_PATTERN" >> $LOG
+
+	# verify query with pmempool info
+	set +e
+	count=$(expect_normal_exit $PMEMPOOL$EXESUFFIX info $DIR/pool.obj | grep -c "$1")
+	set -e
+	if [ "$count" = "0" ]; then
+		echo "pmempool info: $1 is NOT set" >> $LOG
+	else
+		echo "pmempool info: $1 is set" >> $LOG
+	fi
+
+	# check if pool is still valid
+	expect_normal_exit $PMEMPOOL$EXESUFFIX check $DIR/pool.obj >> /dev/null
+}
+
+# libpmempool_feature_enable -- enable feature
+#
+# usage: libpmempool_feature_enable <enum-pmempool_feature> [no-query]
+function libpmempool_feature_enable() {
+	$exit_func ./libpmempool_feature$EXESUFFIX $DIR/pool.obj e $1 &>> $LOG
+	if [ "x$2" != "xno-query" ]; then
+		libpmempool_feature_query $1
+	fi
+}
+
+# libpmempool_feature_disable -- disable feature
+#
+# usage: libpmempool_feature_disable <enum-pmempool_feature> [no-query]
+function libpmempool_feature_disable() {
+	$exit_func ./libpmempool_feature$EXESUFFIX $DIR/pool.obj d $1 &>> $LOG
+	if [ "x$2" != "xno-query" ]; then
+		libpmempool_feature_query $1
+	fi
+}
+
+# libpmempool_feature_test -- misc scenarios for each PMEMPOOL_FEAT_* value
+#
+#	libpmempool_feature_test <enum-pmempool_feature>
+function libpmempool_feature_test() {
+	# create pool
+	expect_normal_exit $PMEMPOOL$EXESUFFIX create obj $DIR/pool.obj
+
+	case "$1" in
+	$PMEMPOOL_FEAT_SINGLEHDR)
+		exit_func=expect_abnormal_exit
+		libpmempool_feature_enable $1 no-query # UNSUPPORTED
+		libpmempool_feature_disable $1 no-query # UNSUPPORTED
+		libpmempool_feature_query $1
+		;;
+	$PMEMPOOL_FEAT_CHCKSUM_2K)
+		# PMEMPOOL_FEAT_CHCKSUM_2K is enabled by default
+		libpmempool_feature_query $1
+
+		# disable PMEMPOOL_FEAT_SHUTDOWN_STATE prior to success
+		exit_func=expect_abnormal_exit
+		libpmempool_feature_disable $1 # should fail
+		exit_func=expect_normal_exit
+		libpmempool_feature_disable $PMEMPOOL_FEAT_SHUTDOWN_STATE
+		libpmempool_feature_disable $1 # should succeed
+
+		libpmempool_feature_enable $1
+		;;
+	$PMEMPOOL_FEAT_SHUTDOWN_STATE)
+		# PMEMPOOL_FEAT_SHUTDOWN_STATE is enabled by default
+		libpmempool_feature_query $1
+
+		libpmempool_feature_disable $1
+
+		# PMEMPOOL_FEAT_SHUTDOWN_STATE requires PMEMPOOL_FEAT_CHCKSUM_2K
+		libpmempool_feature_disable $PMEMPOOL_FEAT_CHCKSUM_2K
+		exit_func=expect_abnormal_exit
+		libpmempool_feature_enable $1 # should fail
+		exit_func=expect_normal_exit
+		libpmempool_feature_enable $PMEMPOOL_FEAT_CHCKSUM_2K
+		libpmempool_feature_enable $1 # should succeed
+		;;
+	esac
+}
