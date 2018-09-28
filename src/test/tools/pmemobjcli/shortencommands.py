b343ba2c200d47c505597f7bab4c16bdf551c541
Michal Reclaw <michalx.reclaw@intel.com>
2018-09-28 13:43:36 +0200

    Added copyrights message and "ignoring extra parameters" for loop
    Signed-off-by: Michal Reclaw <michalx.reclaw@intel.com>

--- src/test/tools/pmemobjcli/shortencommands.py
+++ src/test/tools/pmemobjcli/shortencommands.py
@@ -1,3 +1,34 @@
+# Copyright 2018, Intel Corporation
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in
+#       the documentation and/or other materials provided with the
+#       distribution.
+#
+#     * Neither the name of the copyright holder nor the names of its
+#       contributors may be used to endorse or promote products derived
+#       from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
 import sys
 import re
 import os.path
@@ -12,6 +43,9 @@ def validate_input():
             if not os.path.isfile(sys.argv[2]):
                 print("Error: input file %s does not exist." % sys.argv[2])
                 return False
+            if len(sys.argv) > 4:
+                for index in range(4, len(sys.argv)):
+                    print("Warning: Ignoring extra parameter: " + sys.argv[index])
         else:
             print("Error: Input file path empty. Please enter a correct path to input file.")
             return False
