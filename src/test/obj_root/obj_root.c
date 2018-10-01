20b928d7b44365a105f8cf2a708d9e563ab38c30
Marcin Ślusarz <marcin.slusarz@intel.com>
2018-10-01 19:21:21 +0200

    test: extract heavy part of obj_root test to separate "long" test
    This test causes heavy swapping.

--- src/test/obj_root/obj_root.c
+++ src/test/obj_root/obj_root.c
@@ -42,12 +42,16 @@ int
 main(int argc, char *argv[])
 {
 	START(argc, argv, "obj_root");
-	if (argc != 2)
-		UT_FATAL("usage: obj_root <file>");
+	if (argc < 2)
+		UT_FATAL("usage: obj_root <file> [l]");
 
 	const char *path = argv[1];
 	PMEMobjpool *pop = NULL;
 	os_stat_t st;
+	int long_test = 0;
+
+	if (argc >= 3 && argv[2][0] == 'l')
+		long_test = 1;
 
 	os_stat(path, &st);
 	UT_ASSERTeq(st.st_size, FILE_SIZE);
@@ -60,8 +64,10 @@ main(int argc, char *argv[])
 	UT_ASSERT(OID_EQUALS(oid, OID_NULL));
 	UT_ASSERTeq(errno, EINVAL);
 
-	oid = pmemobj_root(pop, PMEMOBJ_MAX_ALLOC_SIZE);
-	UT_ASSERT(!OID_EQUALS(oid, OID_NULL));
+	if (long_test) {
+		oid = pmemobj_root(pop, PMEMOBJ_MAX_ALLOC_SIZE);
+		UT_ASSERT(!OID_EQUALS(oid, OID_NULL));
+	}
 
 	oid = pmemobj_root(pop, 1);
 	UT_ASSERT(!OID_EQUALS(oid, OID_NULL));
