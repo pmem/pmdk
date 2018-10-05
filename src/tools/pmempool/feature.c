92a8cab5406c23694e4c54c7634374b277ae9c7d
Lukasz Dorau <lukasz.dorau@intel.com>
2018-10-05 15:01:51 +0200

    pool: handle the POOL_FEAT_CHECK_BAD_BLOCKS compat feature

--- src/tools/pmempool/feature.c
+++ src/tools/pmempool/feature.c
@@ -99,7 +99,8 @@ static void
 print_usage(const char *appname)
 {
 	printf("Usage: %s feature [<args>] <file>\n", appname);
-	printf("feature: SINGLEHDR, CKSUM_2K, SHUTDOWN_STATE\n");
+	printf(
+		"feature: SINGLEHDR, CKSUM_2K, SHUTDOWN_STATE, CHECK_BAD_BLOCKS\n");
 }
 
 /*
