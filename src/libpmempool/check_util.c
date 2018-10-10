pmem/pmdk

commit 3db003275583d2176b7622a08177a5af6a680c9a

pool: make pmempool check do not check bad blocks by default
Make pmempool-check check bad blocks
only if the CHECK_BAD_BLOCKS compat feature is set.

src/libpmempool/pool.c
@@ -213,6 +213,7 @@ void
 pool_params_from_header(struct pool_params *params, const struct pool_hdr *hdr)
 {
 	memcpy(params->signature, hdr->signature, sizeof(params->signature));
+	memcpy(&params->features, &hdr->features, sizeof(params->features));
 
 	/*
 	 * Check if file is a part of pool set by comparing the UUID with the

src/libpmempool/check_bad_blocks.c
@@ -56,6 +56,12 @@ check_bad_blocks(PMEMpoolcheck *ppc)
 
 	int ret;
 
+	if (!(ppc->pool->params.features.compat & POOL_FEAT_CHECK_BAD_BLOCKS)) {
+		/* skipping checking poolset for bad blocks */
+		ppc->result = CHECK_RESULT_CONSISTENT;
+		return;
+	}
+
 	if (ppc->pool->set_file->poolset) {
 		ret = badblocks_check_poolset(ppc->pool->set_file->poolset, 0);
 	} else {

src/libpmempool/pool.h
@@ -69,6 +69,7 @@ enum pool_type {
 struct pool_params {
 	enum pool_type type;
 	char signature[POOL_HDR_SIG_LEN];
+	features_t features;
 	size_t size;
 	mode_t mode;
 	int is_poolset;

src/test/pmempool_check/TEST31
@@ -71,6 +71,8 @@ create_poolset $POOLSET 10M:$DIR/testfile0:z 10M:$MOUNT_DIR/testfile1:z 10M:$DIR
 expect_normal_exit $PMEMPOOL$EXESUFFIX rm $POOLSET
 expect_normal_exit $PMEMPOOL$EXESUFFIX create obj --layout pmempool$SUFFIX $POOLSET
 
+turn_on_checking_bad_blocks $POOLSET
+
 # inject bad block:
 FIRST_SECTOR=$(expect_normal_exit $EXTENTS $MOUNT_DIR/testfile1 -l 0)
 ndctl_inject_error $NAMESPACE $FIRST_SECTOR 1

src/test/pmempool_check/TEST32
@@ -68,6 +68,8 @@ create_poolset $POOLSET AUTO:$FULLDEV:x \
 expect_normal_exit $PMEMPOOL$EXESUFFIX rm $POOLSET
 expect_normal_exit $PMEMPOOL$EXESUFFIX create obj --layout pmempool$SUFFIX $POOLSET
 
+turn_on_checking_bad_blocks $POOLSET
+
 # inject bad block: OFF=11 LEN=1
 ndctl_inject_error $NAMESPACE 11 1
 

src/test/pmempool_check/out31.log.match
@@ -29,7 +29,7 @@ POOL Header:
 Signature                : PMEMOBJ
 Major                    : $(nW)
 Mandatory features       : $(*)
-Not mandatory features   : 0x0
+Not mandatory features   : $(*)
 Forced RO                : 0x0
 Pool set UUID            : $(nW)
 UUID                     : $(nW)
@@ -89,7 +89,7 @@ POOL Header:
 Signature                : PMEMOBJ
 Major                    : $(nW)
 Mandatory features       : $(*)
-Not mandatory features   : 0x0
+Not mandatory features   : $(*)
 Forced RO                : 0x0
 Pool set UUID            : $(nW)
 UUID                     : $(nW)

src/test/pmempool_check/out32.log.match
@@ -22,7 +22,7 @@ POOL Header:
 Signature                : PMEMOBJ
 Major                    : $(nW)
 Mandatory features       : $(*)
-Not mandatory features   : 0x0
+Not mandatory features   : $(*)
 Forced RO                : 0x0
 Pool set UUID            : $(nW)
 UUID                     : $(nW)
@@ -75,7 +75,7 @@ POOL Header:
 Signature                : PMEMOBJ
 Major                    : $(nW)
 Mandatory features       : $(*)
-Not mandatory features   : 0x0
+Not mandatory features   : $(*)
 Forced RO                : 0x0
 Pool set UUID            : $(nW)
 UUID                     : $(nW)

commit fc0b0032d2ec604df79c52bbdad0835fa367bd6f

pool: zero all features in pmempool_check_init()
This patch fixes regression introduced by 3db00327.
The libpmempool_bttdev tests run on pmem FS fail without this fix.

src/libpmempool/check_util.c
@@ -99,16 +99,11 @@ check_data_alloc(void)
 {
 	LOG(3, NULL);
 
-	struct check_data *data = malloc(sizeof(*data));
+	struct check_data *data = calloc(1, sizeof(*data));
 	if (data == NULL) {
-		ERR("!malloc");
+		ERR("!calloc");
 		return NULL;
 	}
-
-	memset(&data->step_data, 0, sizeof(location));
-	data->check_status_cache = NULL;
-	data->error = NULL;
-	data->step = 0;
 
 	TAILQ_INIT(&data->infos);
 	TAILQ_INIT(&data->questions);

src/libpmempool/libpmempool.c
@@ -181,17 +181,12 @@ pmempool_errormsgW(void)
 static void
 pmempool_ppc_set_default(PMEMpoolcheck *ppc)
 {
+	/* all other fields should be zeroed */
 	const PMEMpoolcheck ppc_default = {
 		.args		= {
-			.path		= NULL,
-			.backup_path	= NULL,
 			.pool_type	= PMEMPOOL_POOL_TYPE_DETECT,
-			.flags		= 0
 		},
-		.data		= NULL,
-		.pool		= NULL,
 		.result		= CHECK_RESULT_CONSISTENT,
-		.sync_required	= false
 	};
 	*ppc = ppc_default;
 }
@@ -253,9 +248,9 @@ pmempool_check_initU(struct pmempool_check_argsU *args, size_t args_size)
 		return NULL;
 	}
 
-	PMEMpoolcheck *ppc = malloc(sizeof(*ppc));
+	PMEMpoolcheck *ppc = calloc(1, sizeof(*ppc));
 	if (ppc == NULL) {
-		ERR("!malloc");
+		ERR("!calloc");
 		return NULL;
 	}
 

src/libpmempool/pool.c
@@ -505,18 +505,14 @@ pool_data_alloc(PMEMpoolcheck *ppc)
 {
 	LOG(3, NULL);
 
-	struct pool_data *pool = malloc(sizeof(*pool));
+	struct pool_data *pool = calloc(1, sizeof(*pool));
 	if (!pool) {
-		ERR("!malloc");
+		ERR("!calloc");
 		return NULL;
 	}
 
 	TAILQ_INIT(&pool->arenas);
-	pool->narenas = 0;
-	pool->blk_no_layout = 0;
 	pool->uuid_op = UUID_NOP;
-	pool->set_file = NULL;
-	pool->bttc.valid = false;
 
 	if (pool_params_parse(ppc, &pool->params, 0))
 		goto error;