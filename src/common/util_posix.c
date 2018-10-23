dd2e02f775c1464854794d4bf1ebcbeb2f6df44d
Szymon Romik <szymon.romik@intel.com>
2018-10-23 15:05:29 +0200

    common: fix klocwork issue #222263

--- src/common/util_posix.c
+++ src/common/util_posix.c
@@ -134,10 +134,12 @@ util_getexecname(char *path, size_t pathlen)
 #else
 	cc = readlink("/proc/self/exe", path, pathlen);
 #endif
-	if (cc == -1)
-		strcpy(path, "unknown");
-	else
+	if (cc == -1) {
+		strncpy(path, "unknown", pathlen);
+		path[pathlen - 1] = '\0';
+	} else {
 		path[cc] = '\0';
+	}
 
 	return path;
 }
