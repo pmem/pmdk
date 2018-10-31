b9b431e6a10ca90609ec1d27b842cd2f33e3bb8d
Dawid Paluszkiewicz <dawid.paluszkiewicz@intel.com>
2018-10-31 14:56:11 +0100

    pool: creating max size pool bug fix
    Ref: pmem/issues#968

--- src/tools/pmempool/create.c
+++ src/tools/pmempool/create.c
@@ -180,12 +180,12 @@ pmempool_create_help(const char *appname)
  * pmempool_create_obj -- create pmem obj pool
  */
 static int
-pmempool_create_obj(struct pmempool_create *pcp, const char **message)
+pmempool_create_obj(struct pmempool_create *pcp)
 {
 	PMEMobjpool *pop = pmemobj_create(pcp->fname, pcp->layout,
 			pcp->params.size, pcp->params.mode);
 	if (!pop) {
-		*message = pmemobj_errormsg();
+		outv_err("'%s' -- %s\n", pcp->fname, pmemobj_errormsg());
 		return -1;
 	}
 
@@ -198,7 +198,7 @@ pmempool_create_obj(struct pmempool_create *pcp, const char **message)
  * pmempool_create_blk -- create pmem blk pool
  */
 static int
-pmempool_create_blk(struct pmempool_create *pcp, const char **message)
+pmempool_create_blk(struct pmempool_create *pcp)
 {
 	int ret = 0;
 
@@ -211,7 +211,7 @@ pmempool_create_blk(struct pmempool_create *pcp, const char **message)
 	PMEMblkpool *pbp = pmemblk_create(pcp->fname, pcp->params.blk.bsize,
 			pcp->params.size, pcp->params.mode);
 	if (!pbp) {
-		*message = pmemblk_errormsg();
+		outv_err("'%s' -- %s\n", pcp->fname, pmemblk_errormsg());
 		return -1;
 	}
 
@@ -234,13 +234,13 @@ pmempool_create_blk(struct pmempool_create *pcp, const char **message)
  * pmempool_create_log -- create pmem log pool
  */
 static int
-pmempool_create_log(struct pmempool_create *pcp, const char **message)
+pmempool_create_log(struct pmempool_create *pcp)
 {
 	PMEMlogpool *plp = pmemlog_create(pcp->fname,
 					pcp->params.size, pcp->params.mode);
 
 	if (!plp) {
-		*message = pmemlog_errormsg();
+		outv_err("'%s' -- %s\n", pcp->fname, pmemlog_errormsg());
 		return -1;
 	}
 
@@ -436,6 +436,42 @@ pmempool_create_parse_args(struct pmempool_create *pcp, const char *appname,
 	return 0;
 }
 
+static int
+allocate_max_size_available_file(const char *name_of_file, mode_t mode,
+		off_t max_size)
+{
+	int fd = os_open(name_of_file, O_CREAT | O_EXCL | O_RDWR, mode);
+	if (fd == -1) {
+		outv_err("!open '%s' failed", name_of_file);
+		return -1;
+	}
+
+	off_t offset = 0;
+	off_t length = max_size - (max_size % (off_t)Pagesize);
+	int ret;
+	do {
+		ret = os_posix_fallocate(fd, offset, length);
+		if (ret == 0)
+			offset += length;
+		else if (ret != ENOSPC) {
+			os_close(fd);
+			if (os_unlink(name_of_file) == -1)
+				outv_err("!unlink '%s' failed", name_of_file);
+			errno = ret;
+			outv_err("!space allocation for '%s' failed",
+					name_of_file);
+			return -1;
+		}
+
+		length /= 2;
+		length -= (length % (off_t)Pagesize);
+	} while (length > (off_t)Pagesize);
+
+	os_close(fd);
+
+	return 0;
+}
+
 /*
  * pmempool_create_func -- main function for create command
  */
@@ -595,6 +631,14 @@ pmempool_create_func(const char *appname, int argc, char *argv[])
 			}
 			outv(1, "Available space is %s\n",
 				out_get_size_str(pc.params.size, 2));
+			if (allocate_max_size_available_file(pc.fname,
+					pc.params.mode, (off_t)pc.params.size))
+				return -1;
+			/*
+			 * We are going to create pool based
+			 * on file size instead of the pc.params.size.
+			 */
+			pc.params.size = 0;
 		} else {
 			if (!pc.fexists) {
 				outv(1, "No size option passed "
@@ -625,41 +669,19 @@ pmempool_create_func(const char *appname, int argc, char *argv[])
 		}
 	}
 
-	const char *message = NULL;
-
-	while (1) {
-		switch (pc.params.type) {
-		case PMEM_POOL_TYPE_BLK:
-			ret = pmempool_create_blk(&pc, &message);
-			break;
-		case PMEM_POOL_TYPE_LOG:
-			ret = pmempool_create_log(&pc, &message);
-			break;
-		case PMEM_POOL_TYPE_OBJ:
-			ret = pmempool_create_obj(&pc, &message);
-			break;
-		default:
-			ret = -1;
-			break;
-		}
-
-		if (ret == -1) {
-			if (pc.max_size && errno == ENOSPC) {
-				if (pc.params.size > Pagesize) {
-					pc.params.size -= Pagesize;
-				} else {
-					outv_err("'%s' -- %s\n", pc.fname,
-						message);
-					break;
-				}
-			} else {
-				outv_err("'%s' -- %s\n", pc.fname, message);
-				break;
-			}
-		} else {
-			break;
-		}
-
+	switch (pc.params.type) {
+	case PMEM_POOL_TYPE_BLK:
+		ret = pmempool_create_blk(&pc);
+		break;
+	case PMEM_POOL_TYPE_LOG:
+		ret = pmempool_create_log(&pc);
+		break;
+	case PMEM_POOL_TYPE_OBJ:
+		ret = pmempool_create_obj(&pc);
+		break;
+	default:
+		ret = -1;
+		break;
 	}
 
 	if (ret) {
