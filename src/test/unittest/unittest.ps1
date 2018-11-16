be1bda1a84ed487dd98ae0709c8774f75108eb51
Marcin Ślusarz <marcin.slusarz@intel.com>
2018-11-16 20:48:14 +0100

    test: add 5%/8M to required free space
    File systems require some space for metadata.

--- src/test/unittest/unittest.ps1
+++ src/test/unittest/unittest.ps1
@@ -1288,6 +1288,15 @@ function enable_log_append() {
 #
 function require_free_space() {
 	$req_free_space = (convert_to_bytes $args[0])
+
+	$pct = 5 * $req_free_space / 100
+	$abs = (convert_to_bytes 8M)
+	if ($pct -gt $abs) {
+		$req_free_space = $req_free_space + $pct
+	} else {
+		$req_free_space = $req_free_space + $abs
+	}
+
 	$path = $DIR -replace '\\\\\?\\', ''
 	$device_name = (Get-Item $path).PSDrive.Root
 	$filter = "Name='$($device_name -replace '\\', '\\')'"
