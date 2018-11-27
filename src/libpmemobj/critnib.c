4e91085a57d6ef3db2b56a6051bd7aed380e4fa6
Adam Borowski <adam.borowski@intel.com>
2018-11-27 16:34:32 +0100

    obj: critnib: return EEXIST instead of EINVAL if a key already exists
    Behaviour change from cuckoo.

--- src/libpmemobj/critnib.c
+++ src/libpmemobj/critnib.c
@@ -342,7 +342,7 @@ alloc_leaf(struct critnib *__restrict c)
  *
  * Returns:
  *  • 0 on success
- *  • EINVAL if such a key already exists
+ *  • EEXIST if such a key already exists
  *  • ENOMEM if we're out of memory
  *
  * Takes a global write lock but doesn't stall any readers.
@@ -401,7 +401,7 @@ critnib_insert(struct critnib *c, uint64_t key, void *value)
 
 		os_mutex_unlock(&c->mutex);
 
-		return EINVAL;
+		return EEXIST;
 	}
 
 	/* and convert that to an index. */
