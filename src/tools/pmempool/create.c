1bb0693ea5b71d4ce1c787d1446b06e58b633138
Dawid Paluszkiewicz <dawid.paluszkiewicz@intel.com>
2019-01-04 10:30:18 +0100

    pool: change off_t to os_off_t
    off_t is 4 bytes on windows and its value often exceeds available size

--- src/tools/pmempool/create.c
+++ src/tools/pmempool/create.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2018, Intel Corporation
+ * Copyright 2014-2019, Intel Corporation
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -438,7 +438,7 @@ pmempool_create_parse_args(struct pmempool_create *pcp, const char *appname,
 
 static int
 allocate_max_size_available_file(const char *name_of_file, mode_t mode,
-		off_t max_size)
+		os_off_t max_size)
 {
 	int fd = os_open(name_of_file, O_CREAT | O_EXCL | O_RDWR, mode);
 	if (fd == -1) {
@@ -446,8 +446,8 @@ allocate_max_size_available_file(const char *name_of_file, mode_t mode,
 		return -1;
 	}
 
-	off_t offset = 0;
-	off_t length = max_size - (max_size % (off_t)Pagesize);
+	os_off_t offset = 0;
+	os_off_t length = max_size - (max_size % (os_off_t)Pagesize);
 	int ret;
 	do {
 		ret = os_posix_fallocate(fd, offset, length);
@@ -464,8 +464,8 @@ allocate_max_size_available_file(const char *name_of_file, mode_t mode,
 		}
 
 		length /= 2;
-		length -= (length % (off_t)Pagesize);
-	} while (length > (off_t)Pagesize);
+		length -= (length % (os_off_t)Pagesize);
+	} while (length > (os_off_t)Pagesize);
 
 	os_close(fd);
 
@@ -632,7 +632,8 @@ pmempool_create_func(const char *appname, int argc, char *argv[])
 			outv(1, "Available space is %s\n",
 				out_get_size_str(pc.params.size, 2));
 			if (allocate_max_size_available_file(pc.fname,
-					pc.params.mode, (off_t)pc.params.size))
+					pc.params.mode,
+					(os_off_t)pc.params.size))
 				return -1;
 			/*
 			 * We are going to create pool based
