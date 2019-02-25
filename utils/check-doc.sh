d23727fc586a501f09dcdcb7082873efa190f4d2
Marcin Ślusarz <marcin.slusarz@intel.com>
2019-02-25 20:35:24 +0100

    common: introduce GITHUB_REPO and DOCKERHUB_REPO on Travis
    Will be needed by #3436.

--- utils/check-doc.sh
+++ utils/check-doc.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright 2016-2017, Intel Corporation
+# Copyright 2016-2019, Intel Corporation
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -46,10 +46,10 @@ if [[ -z "$TRAVIS" ]]; then
 	exit 1
 fi
 
-if [[ $TRAVIS_REPO_SLUG != "pmem/pmdk" \
+if [[ $TRAVIS_REPO_SLUG != "$GITHUB_REPO" \
 	|| $TRAVIS_EVENT_TYPE != "pull_request" ]];
 then
-	echo "SKIP: $0 can only be executed for pull requests to pmem/pmdk"
+	echo "SKIP: $0 can only be executed for pull requests to ${GITHUB_REPO}"
 	exit 0
 fi
 
