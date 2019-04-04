222694f9feaa7a93acedbee9dcb97e04017c3473
Oksana Salyk <oksana.salyk@intel.com>
2019-04-04 13:31:50 +0200

    test: fix libpmempool_bttdev windows test
    When allocating large pools on the windows we use up a lot of resources,
    with automatic page file size management we eliminate the risk
    of exceeding the available resources.

--- src/test/unittest/unittest.ps1
+++ src/test/unittest/unittest.ps1
@@ -1324,3 +1324,14 @@ function require_free_physical_memory() {
 		exit 0
 	}
 }
+
+#
+# require_automatic_managed_pagefile -- check if automatic page file management is enabled
+#
+function require_automatic_managed_pagefile() {
+	$c = Get-WmiObject Win32_computersystem -EnableAllPrivileges
+	if($c.AutomaticManagedPagefile -eq $false){
+		msg "${Env:UNITTEST_NAME}: SKIP automatic managed page file is disabled"
+		exit 0
+		}
+}
