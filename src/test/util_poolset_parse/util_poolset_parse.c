7a9d8caa709d48a6194b59d9b04ce1020cb464e1
Oksana Salyk <oksana.salyk@intel.com>
2019-05-06 13:52:30 +0200

    test: util_poolset with fault_injection

--- src/test/util_poolset_parse/util_poolset_parse.c
+++ src/test/util_poolset_parse/util_poolset_parse.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2017, Intel Corporation
+ * Copyright 2015-2019, Intel Corporation
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -39,6 +39,7 @@
 #include "set.h"
 #include "unittest.h"
 #include "pmemcommon.h"
+#include "fault_injection.h"
 
 #define LOG_PREFIX "parser"
 #define LOG_LEVEL_VAR "PARSER_LOG_LEVEL"
@@ -54,23 +55,43 @@ main(int argc, char *argv[])
 	common_init(LOG_PREFIX, LOG_LEVEL_VAR, LOG_FILE_VAR,
 			MAJOR_VERSION, MINOR_VERSION);
 
-	if (argc < 2)
+	if (argc < 3)
 		UT_FATAL("usage: %s set-file-name ...", argv[0]);
 
 	struct pool_set *set;
 	int fd;
 
-	for (int i = 1; i < argc; i++) {
-		const char *path = argv[i];
+	switch (argv[1][0]) {
+	case 't':
+		for (int i = 2; i < argc; i++) {
+			const char *path = argv[i];
 
+			fd = OPEN(path, O_RDWR);
+
+			int ret = util_poolset_parse(&set, path, fd);
+			if (ret == 0)
+				util_poolset_free(set);
+
+			CLOSE(fd);
+		}
+		break;
+	case 'f':
+		if (!common_fault_injection_enabled())
+			break;
+
+		const char *path = argv[2];
 		fd = OPEN(path, O_RDWR);
 
+		common_inject_fault_at(PMEM_MALLOC, 1,
+				"util_poolset_directories_load");
 		int ret = util_poolset_parse(&set, path, fd);
-		if (ret == 0)
-			util_poolset_free(set);
+		UT_ASSERTne(ret, 0);
+		UT_ASSERTeq(errno, ENOMEM);
 
 		CLOSE(fd);
 	}
+
+
 
 	common_fini();
 
