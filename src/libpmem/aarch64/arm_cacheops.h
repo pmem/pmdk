651a13210809cfae5ef26789253aefacd512d15e
Adam Borowski <adam.borowski@intel.com>
2019-06-13 18:02:28 +0200

    pmem: some docs about asm and concepts on ARM
    I don't know much about ARM assembly myself, thus there's a few uncertain
    points (marked by "???").

--- src/libpmem/aarch64/arm_cacheops.h
+++ src/libpmem/aarch64/arm_cacheops.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2017, Intel Corporation
+ * Copyright 2014-2019, Intel Corporation
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -34,6 +34,40 @@
  * clwb => dc cvac
  * clflush | clflushopt => dc civac
  * fence => dmb ish
+ */
+
+/*
+ * Cache instructions on ARM:
+ * ARMv8.0-a    DC CVAC  - cache clean to Point of Coherency
+ *                         Meant for thread synchronization, usually implies
+ *                         real memory flush but may mean less.
+ * ARMv8.2-a    DC CVAP  - cache clean to Point of Persistency
+ *                         Meant exactly for our use.
+ * ARMv8.5-a    DC CVADP - cache clean to Point of Deep Persistency
+ *                         As of mid-2019 not on any commercially available CPU.
+ * Any of the above may be disabled for EL0, but it's probably safe to consider
+ * that a system configuration error.
+ * Other flags include I (like "DC CIVAC") that invalidates the cache line, but
+ * we don't want that.
+ *
+ * Memory fences:
+ * * DMB [ISH]    MFENCE
+ * * DMB [ISH]ST  SFENCE
+ * * DMB [ISH]LD  LFENCE
+ * We care about persistence not synchronization thus ISH should be enough?
+ *
+ * Memory domains:
+ * * non-shareable - local to a single core
+ * * inner shareable (ISH) - usu. one or multiple processor sockets
+ * * outer shareable (OSH) - usu. including GPU
+ * * full system (SY) - anything that can possibly access memory
+ * ??? What about RDMA?  No libfabric on ARM thus not a concern for now.
+ *
+ * Exception (privilege) levels:
+ * * EL0 - userspace (ring 3)
+ * * EL1 - kernel (ring 0)
+ * * EL2 - hypervisor (ring -1)
+ * * EL3 - "secure world" (ring -3)
  */
 
 #ifndef AARCH64_CACHEOPS_H
