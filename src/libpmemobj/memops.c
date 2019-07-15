d61e1f95fe21c391a8b24d08c374f12cf722bf78
Piotr Balcer <piotr.balcer@intel.com>
2019-07-15 23:46:59 +0200

    obj: prevent ulog_foreach from ever reading uninitialized data
    This patch changes snapshotting so that memory that's directly
    after the snapshot that is being created will be zeroed. This
    ensures that it's safe to read later on (during abort).

--- src/libpmemobj/memops.c
+++ src/libpmemobj/memops.c
@@ -397,6 +397,20 @@ operation_add_buffer(struct operation_context *ctx,
 
 	size_t curr_size = MIN(real_size, ctx->ulog_curr_capacity);
 	size_t data_size = curr_size - sizeof(struct ulog_entry_buf);
+	size_t entry_size = ALIGN_UP(curr_size, CACHELINE_SIZE);
+
+	size_t next_entry_offset = ctx->ulog_curr_offset + entry_size;
+	struct ulog_entry_base *next_entry = NULL;
+	if (next_entry_offset == ctx->ulog_curr_capacity) {
+		struct ulog *u = ulog_next(ctx->ulog_curr, ctx->p_ops);
+		if (u != NULL)
+			next_entry = (struct ulog_entry_base *)u->data;
+	} else {
+		next_entry = (struct ulog_entry_base *)(ctx->ulog_curr->data +
+			next_entry_offset);
+	}
+	if (next_entry != NULL && next_entry->offset != 0)
+		ulog_clobber_entry(next_entry, ctx->p_ops);
 
 	/* create a persistent log entry */
 	struct ulog_entry_buf *e = ulog_entry_buf_create(ctx->ulog_curr,
@@ -404,7 +418,6 @@ operation_add_buffer(struct operation_context *ctx,
 		ctx->ulog_curr_gen_num,
 		dest, src, data_size,
 		type, ctx->p_ops);
-	size_t entry_size = ALIGN_UP(curr_size, CACHELINE_SIZE);
 	ASSERT(entry_size == ulog_entry_size(&e->base));
 	ASSERT(entry_size <= ctx->ulog_curr_capacity);
 
