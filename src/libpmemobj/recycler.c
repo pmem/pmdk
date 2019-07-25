3e157c8b960cbf92db7047bb5814c243fda4ef6b
Piotr Balcer <piotr.balcer@intel.com>
2019-07-25 17:25:24 +0200

    obj: fix recycler not locating unused chunks
    In scenarios where free() is done very rarely the memory in the recycler
    would not be looked at because the estimates would indicate that no memory
    was freed and there's no reason to search the tree. Normally that's OK,
    because eventually the heap would trigger a forced search of the entire
    recycler for the allocation class, locating all the memory that might have
    been missed. But, the forced recalculation erronously looked at the
    estimates and required that at least some memory were freed since the
    last recycler run - which might have not been the case.
    This patch makes the forced recycling run without looking at the estimates.
    
    Reported-by: Denny Zhao <denny.zhao@memverge.com>

--- src/libpmemobj/recycler.c
+++ src/libpmemobj/recycler.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2018, Intel Corporation
+ * Copyright 2016-2019, Intel Corporation
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -285,7 +285,7 @@ recycler_recalc(struct recycler *r, int force)
 
 	uint64_t units = r->unaccounted_total;
 
-	if (units == 0 || (!force && units < (r->recalc_threshold)))
+	if (!force && units < r->recalc_threshold)
 		return runs;
 
 	if (util_mutex_trylock(&r->lock) != 0)
