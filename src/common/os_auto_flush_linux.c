be62d72b2f47c4d8f9724dc672100f1cf438052b
Weronika Lewandowska <weronika.lewandowska@intel.com>
2019-08-05 14:23:47 +0200

    common: fix persistent domain termination
    Ref: pmem/issues#1093
    Ref: pmem/issues#1096
    Ref: pmem/issues#1097

--- src/common/os_auto_flush_linux.c
+++ src/common/os_auto_flush_linux.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018, Intel Corporation
+ * Copyright 2018-2019, Intel Corporation
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -81,8 +81,9 @@ check_cpu_cache(const char *domain_path)
 		goto end;
 	}
 
+	domain_value[len - 1] = '\0';
 	LOG(15, "detected persistent_domain: %s", domain_value);
-	if (strncmp(domain_value, "cpu_cache", strlen("cpu_cache")) == 0) {
+	if (strcmp(domain_value, "cpu_cache") == 0) {
 		LOG(15, "cpu_cache in persistent_domain: %s", domain_path);
 		cpu_cache = 1;
 	} else {
