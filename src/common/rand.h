7029b933f4637878d049dc313e497ae409038d7f
Adam Borowski <adam.borowski@intel.com>
2019-09-09 13:34:41 +0200

    common: fix linkage in C++ mode

--- src/common/rand.h
+++ src/common/rand.h
@@ -10,6 +10,10 @@
 
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef uint64_t rng_t[4];
 
 uint64_t hash64(uint64_t x);
@@ -17,5 +21,9 @@ uint64_t rnd64_r(rng_t *rng);
 void randomize_r(rng_t *rng, uint64_t seed);
 uint64_t rnd64(void);
 void randomize(uint64_t seed);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
