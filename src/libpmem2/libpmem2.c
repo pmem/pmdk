e3fbd57a338a433e24b954b89fb6e80efb015c9f
Adam Borowski <kilobyte@angband.pl>
2019-10-09 15:56:06 +0200

    pmem2: copy in error messaging functions

--- src/libpmem2/libpmem2.c
+++ src/libpmem2/libpmem2.c
@@ -68,3 +68,35 @@ libpmem2_fini(void)
 	/* XXX LOG(3, NULL); placeholder */
 	/* XXX common_fini placeholder */
 }
+
+/*
+ * pmem_errormsgU -- return last error message
+ */
+#ifndef _WIN32
+static inline
+#endif
+const char *
+pmem2_errormsgU(void)
+{
+	return out_get_errormsg();
+}
+
+#ifndef _WIN32
+/*
+ * pmem_errormsg -- return last error message
+ */
+const char *
+pmem2_errormsg(void)
+{
+	return pmem2_errormsgU();
+}
+#else
+/*
+ * pmem_errormsgW -- return last error message as wchar_t
+ */
+const wchar_t *
+pmem2_errormsgW(void)
+{
+	return out_get_errormsgW();
+}
+#endif
