5fb24ef2ded18dccb8b5d2f15dce3be342896b1e
Jan M Michalski <jan.m.michalski@intel.com>
2019-10-12 17:20:59 +0200

    pmem2: pmem_map for Linux

--- src/libpmem2/pmem2.h
+++ src/libpmem2/pmem2.h
@@ -53,6 +53,14 @@ extern "C" {
 #define PMEM2_LOG_LEVEL_VAR "PMEM2_LOG_LEVEL"
 #define PMEM2_LOG_FILE_VAR "PMEM2_LOG_FILE"
 
+struct pmem2_map {
+	enum file_type file_type; /* TYPE_NORMAL or TYPE_DEVDAX */
+	int map_sync; /* if mapping is MAP_SYNC */
+	void *addr; /* base address */
+	size_t length; /* effective length of the mapping */
+	size_t alignment;
+};
+
 #ifdef __cplusplus
 }
 #endif
