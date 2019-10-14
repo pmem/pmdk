22ba7d9b5ce64cb66547e03e1da2019d311f07ac
Oksana Salyk <oksana.salyk@intel.com>
2019-10-14 14:45:33 +0200

    pmem2: prepare libpmem2 header file

--- src/include/libpmem2.h
+++ src/include/libpmem2.h
@@ -43,6 +43,9 @@
 #ifndef LIBPMEM2_H
 #define LIBPMEM2_H 1
 
+#include <stddef.h>
+#include <stdint.h>
+
 #ifdef _WIN32
 #include <pmemcompat.h>
 
@@ -57,6 +60,203 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+struct pmem2_config;
+struct pmem2_map;
+struct pmem2_badblock_iterator;
+struct pmem2_badblock {
+	size_t offset;
+	size_t length;
+};
+
+enum pmem2_granularity {
+	PMEM2_GRANULARITY_BYTE,
+	PMEM2_GRANULARITY_CACHE_LINE,
+	PMEM2_GRANULARITY_PAGE,
+};
+
+#define PMEM2_E_OK			0
+#define PMEM2_E_EXTERNAL		1
+#define PMEM2_E_INVALID_ARG		2
+#define PMEM2_E_INVALID_HANDLE		3
+#define PMEM2_E_NOMEM			4
+#define PMEM2_E_MAP_RANGE		5
+#define PMEM2_E_INV_FSIZE		6
+#define PMEM2_E_UNKNOWN_FILETYPE	7
+#define PMEM2_E_MAP_FAILED		8
+#define PMEM2_E_NOSUPP			9
+
+#define PMEM2_F_MEM_NODRAIN	(1U << 0)
+
+#define PMEM2_F_MEM_NONTEMPORAL	(1U << 1)
+#define PMEM2_F_MEM_TEMPORAL	(1U << 2)
+
+#define PMEM2_F_MEM_WC	(1U << 3)
+#define PMEM2_F_MEM_WB	(1U << 4)
+
+#define PMEM2_F_MEM_NOFLUSH	(1U << 5)
+
+#define PMEM2_F_MEM_VALID_FLAGS (PMEM2_F_MEM_NODRAIN | \
+		PMEM2_F_MEM_NONTEMPORAL | \
+		PMEM2_F_MEM_TEMPORAL | \
+		PMEM2_F_MEM_WC | \
+		PMEM2_F_MEM_WB | \
+		PMEM2_F_MEM_NOFLUSH)
+
+#define PMEM2_ADDRESS_ANY		0 /* default */
+#define PMEM2_ADDRESS_FIXED_REPLACE	1
+#define PMEM2_ADDRESS_FIXED_NOREPLACE	2
+
+#define PMEM2_SHARED	0 /* default */
+#define PMEM2_PRIVATE	1
+
+#define PMEM2_PROT_FROM_FD	0 /* default */
+#define PMEM2_PROT_EXEC		(1U<<29)
+#define PMEM2_PROT_READ		(1U<<30)
+#define PMEM2_PROT_WRITE	(1U<<31)
+
+typedef void (*pmem2_persist_fn)(void *ptr, size_t size);
+typedef void (*pmem2_flush_fn)(void *ptr, size_t size);
+typedef void (*pmem2_drain_fn)(void);
+
+static inline int pmem2_config_new(struct pmem2_config **cfg)
+{
+	return PMEM2_E_NOSUPP;
+}
+
+static inline int pmem2_config_set_fd(struct pmem2_config *cfg, int fd)
+{
+	return PMEM2_E_NOSUPP;
+}
+
+static inline int pmem2_config_delete(struct pmem2_config *cfg)
+{
+	return PMEM2_E_NOSUPP;
+}
+
+static inline pmem2_persist_fn *pmem2_get_persist_fn(struct pmem2_map *map)
+{
+	return NULL;
+}
+static inline pmem2_flush_fn *pmem2_get_flush_fn(struct pmem2_map *map)
+{
+	return NULL;
+}
+static inline pmem2_drain_fn *pmem2_get_drain_fn(struct pmem2_map *map)
+{
+	return NULL;
+}
+
+static inline int pmem2_config_set_required_store_granularity(
+		struct pmem2_config *cfg, enum pmem2_granularity g)
+{
+	return PMEM2_E_NOSUPP;
+}
+
+static inline enum pmem2_granularity pmem2_map_get_store_granularity(
+		struct pmem2_map *map)
+{
+	return PMEM2_E_NOSUPP;
+}
+
+static inline int pmem2_config_set_offset(struct pmem2_config *cfg,
+		size_t offset)
+{
+	return PMEM2_E_NOSUPP;
+}
+
+static inline int pmem2_config_set_length(struct pmem2_config *cfg,
+		size_t length)
+{
+	return PMEM2_E_NOSUPP;
+}
+
+typedef void (*pmem2_memmove_fn)(void *pmemdest, const void *src, size_t len,
+		unsigned flags);
+typedef void (*pmem2_memcpy_fn)(void *pmemdest, const void *src, size_t len,
+		unsigned flags);
+typedef void (*pmem2_memset_fn)(void *pmemdest, int c, size_t len,
+		unsigned flags);
+
+static inline pmem2_memmove_fn *pmem2_get_memmove_fn(struct pmem2_map *map)
+{
+	return NULL;
+}
+
+static inline pmem2_memcpy_fn *pmem2_get_memcpy_fn(struct pmem2_map *map)
+{
+	return NULL;
+}
+
+static inline pmem2_memset_fn *pmem2_get_memset_fn(struct pmem2_map *map)
+{
+	return NULL;
+}
+
+static inline struct pmem2_config *pmem2_get_config(struct pmem2_map *map)
+{
+	return NULL;
+}
+
+static inline int pmem2_config_set_address(struct pmem2_config *cfg,
+		unsigned type, void *addr)
+{
+	return PMEM2_E_NOSUPP;
+}
+
+static inline int pmem2_get_device_id(const struct pmem2_config *cfg, char *id,
+		size_t *len)
+{
+	return PMEM2_E_NOSUPP;
+}
+
+static inline int pmem2_get_device_usc(const struct pmem2_config *cfg,
+		uint64_t *usc)
+{
+	return PMEM2_E_NOSUPP;
+}
+
+static inline int pmem2_badblock_iterator_new(const struct pmem2_config *cfg,
+		struct pmem2_badblock_iterator **pbb)
+{
+	return PMEM2_E_NOSUPP;
+}
+
+static inline int pmem2_badblock_next(struct pmem2_badblock_iterator *pbb,
+		struct pmem2_badblock *bb)
+{
+	return PMEM2_E_NOSUPP;
+}
+
+static inline void pmem2_badblock_iterator_delete(
+		struct pmem2_badblock_iterator **pbb)
+{
+	;
+}
+
+static inline int pmem2_badblock_clear(const struct pmem2_config *cfg,
+		const struct pmem2_badblock *bb)
+{
+	return PMEM2_E_NOSUPP;
+}
+
+static inline int pmem2_config_set_sharing(struct pmem2_config *cfg,
+		unsigned type)
+{
+	return PMEM2_E_NOSUPP;
+}
+
+static inline int pmem2_config_set_protection(struct pmem2_config *cfg,
+		unsigned flag)
+{
+	return PMEM2_E_NOSUPP;
+}
+
+static inline int pmem2_config_use_anonymous_mapping(struct pmem2_config *cfg,
+		unsigned on)
+{
+	return PMEM2_E_NOSUPP;
+}
 
 #ifndef _WIN32
 const char *pmem2_errormsg(void);
