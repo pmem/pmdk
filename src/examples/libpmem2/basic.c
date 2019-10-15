e2248518c13c184b35bcd32d6e20ccd08588b2b7
Oksana Salyk <oksana.salyk@intel.com>
2019-10-15 12:46:37 +0200

    pmem2: add libpmem2 basic example

---
+++ src/examples/libpmem2/basic.c
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2019, Intel Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *
+ *     * Neither the name of the copyright holder nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * basic.c -- simple example for the libpmem2
+ */
+
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#ifndef _WIN32
+#include <unistd.h>
+#else
+#include <io.h>
+#endif
+#include <libpmem2.h>
+
+int
+main(int argc, char *argv[])
+{
+	int fd;
+	struct pmem2_config *cfg;
+	struct pmem2_map *map;
+	pmem2_persist_fn persist;
+
+	if (argc != 2) {
+		fprintf(stderr, "usage: %s file\n", argv[0]);
+		exit(1);
+	}
+
+	if ((fd = open(argv[1], O_RDWR)) < 0) {
+		perror("open");
+		exit(1);
+	}
+
+	if (pmem2_config_new(&cfg)) {
+		fprintf(stderr, "%s", pmem2_errormsg());
+		exit(1);
+	}
+
+	if (pmem2_config_set_fd(cfg, fd)) {
+		fprintf(stderr, "%s", pmem2_errormsg());
+		exit(1);
+	}
+
+	if (pmem2_map(cfg, &map)) {
+		fprintf(stderr, "%s", pmem2_errormsg());
+		exit(1);
+	}
+
+	char *addr = pmem2_map_get_address(map);
+	size_t size = pmem2_map_get_size(map);
+
+	strcpy(addr, "hello, persistent memory");
+
+	persist = pmem2_get_persist_fn(map);
+
+	/* remove the condition after adding a function implementation */
+	if (0)
+		persist(addr, size);
+
+	pmem2_unmap(&map);
+	pmem2_config_delete(&cfg);
+	close(fd);
+}
