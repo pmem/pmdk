28d21d56f8fbcb91b3a2441497a10c9cf0f8f4c0
Łukasz Plewa <lukasz.plewa@intel.com>
2019-10-15 12:07:27 +0200

    pmem2: pmem2_config implementation

--- src/test/unittest/ut_pmem2_utils.c
+++ src/test/unittest/ut_pmem2_utils.c
@@ -31,12 +31,23 @@
  */
 
 /*
- * pmem2_utils.h -- libpmem2 utilities functions
+ * ut_pmem2_utils.c -- utility helper functions for libpmem2 tests
  */
 
-#ifndef PMEM2_UTILS_H
-#define PMEM2_UTILS_H 1
+#include "unittest.h"
+#include "ut_pmem2_utils.h"
 
-void *pmem2_malloc(size_t size, int *err);
-
-#endif /* PMEM2_UTILS_H */
+/*
+ * ut_pmem2_expect_return -- veryfies error code and prints appropriate
+ *	error message in case of error
+ */
+void ut_pmem2_expect_return(const char *file, int line, const char *func,
+	int value, int expected)
+{
+	if (value != expected) {
+		ut_fatal(file, line, func,
+			"unexpected return code (got %d, expected: %d): %s",
+			value, expected,
+			(value == 0 ? "success" : pmem2_errormsg()));
+	}
+}
