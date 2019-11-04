fd80b520374b6e546448dafe0180906e3c532c28
Rafal Borawski <rafal.borawski@intel.com>
2019-11-04 14:41:13 +0100

    pmreorder: add --version support
    Ref: #4234

--- src/tools/pmreorder/pmreorder.py
+++ src/tools/pmreorder/pmreorder.py
@@ -1,6 +1,5 @@
-#!/usr/bin/python
 #
-# Copyright 2018, Intel Corporation
+# Copyright 2018-2019, Intel Corporation
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -42,6 +41,17 @@ import reorderengines
 
 
 def main():
+    pmreorder_version = "unknown"
+
+    '''
+    There should be given argv[1] to provide proper usage of -v or
+    --version flag. Argv[1] is sent from the installed script. We check
+    if that parameter was given, and it works because argv[1] is always
+    a version of the pmreorder.
+    '''
+    if len(sys.argv) > 1 and sys.argv[1][0] != "-":
+        pmreorder_version = sys.argv[1]
+
     # TODO unicode support
     # TODO parameterize reorder engine type
     parser = argparse.ArgumentParser(description="Store reordering tool")
@@ -67,6 +77,10 @@ def main():
     parser.add_argument("-x", "--extended-macros",
                         help="list of pairs MARKER=ENGINE or " +
                         "json config file")
+    parser.add_argument("-v", "--version",
+                        help="print version of the pmreorder",
+                        action="version",
+                        version="%(prog)s (" + pmreorder_version + ")")
     engines_keys = list(reorderengines.engines.keys())
     parser.add_argument("-r", "--default-engine",
                         help="set default reorder engine " +
