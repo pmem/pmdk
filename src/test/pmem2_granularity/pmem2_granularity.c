125df8851923e1b4694d09ce625ec166c94ee84e
Weronika Lewandowska <weronika.lewandowska@intel.com>
2019-11-29 08:59:10 +0100

    tests: pmem2_granularity generic part

---
+++ src/test/pmem2_granularity/pmem2_granularity.c
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2019, Intel Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *
+ *     * Neither the name of the copyright holder nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * pmem2_granularity.c -- test for graunlarity functionality
+ */
+
+#include <string.h>
+#include "unittest.h"
+#include "ut_pmem2_utils.h"
+#include "ut_pmem2_config.h"
+#include "config.h"
+
+#define PMEM2_GRANULARITY_INVAL 99
+
+static size_t
+get_size(const char *file)
+{
+	os_stat_t stbuf;
+	STAT(file, &stbuf);
+
+	return (size_t)stbuf.st_size;
+}
+
+static void
+prepare_config(struct pmem2_config *cfg, int *fd, size_t length,
+		size_t offset, int access)
+{
+	pmem2_config_init(cfg);
+	cfg->offset = offset;
+	cfg->length = length;
+#ifdef _WIN32
+	cfg->handle = (HANDLE)_get_osfhandle(*fd);
+#else
+	cfg->fd = *fd;
+#endif
+}
+
+/*
+ * test_granularity_inval -- pass invalid granularity value
+ */
+static int
+test_granularity_inval(const struct test_case *tc, int argc, char *argv[])
+{
+	if (argc != 1)
+		UT_FATAL("usage: test_granularity_inval <file>");
+
+	char *file = argv[0];
+	int fd = OPEN(file, O_RDWR);
+
+	struct pmem2_config cfg;
+	size_t size = get_size(file);
+
+	prepare_config(&cfg, &fd, size, 0, O_RDWR);
+	enum pmem2_granularity g = PMEM2_GRANULARITY_INVAL;
+	int ret = pmem2_config_set_required_store_granularity(&cfg, g);
+	UT_PMEM2_EXPECT_RETURN(ret, PMEM2_E_INVALID_ARG);
+
+	CLOSE(fd);
+
+	return 1;
+}
+
+/*
+ * test_cases -- available test cases
+ */
+static struct test_case test_cases[] = {
+	TEST_CASE(test_granularity_inval),
+};
+
+#define NTESTS (sizeof(test_cases) / sizeof(test_cases[0]))
+
+int
+main(int argc, char *argv[])
+{
+	START(argc, argv, "pmem2_granularity");
+	TEST_CASE_PROCESS(argc, argv, test_cases, NTESTS);
+	DONE(NULL);
+}
