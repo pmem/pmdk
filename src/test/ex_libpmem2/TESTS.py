1b2eda8ceb945b39cd93cc4d1b481ad66e1acf77
Oksana Salyk <oksana.salyk@intel.com>
2019-12-02 13:06:26 +0100

    test: set length and offset in pmem2 advanced example

--- src/test/ex_libpmem2/TESTS.py
+++ src/test/ex_libpmem2/TESTS.py
@@ -41,9 +41,12 @@ import testframework as t
 class EX_LIBPMEM2(t.Test):
     test_type = t.Medium
 
+    offset = str(97 * t.KiB)
+    length = str(65 * t.KiB)
+
     def get_path(self, ctx, file_name):
         path = futils.get_examples_dir(ctx)
-        filepath = ctx.create_non_zero_file(16 * t.MiB,
+        filepath = ctx.create_non_zero_file(1 * t.MiB,
                                             Path(ctx.testdir, file_name))
         return path, filepath
 
@@ -62,3 +65,21 @@ class TEST1(EX_LIBPMEM2):
     def run(self, ctx):
         test_path, file_path = self.get_path(ctx, 'testfile1')
         ctx.exec(path.join(test_path, 'ex_pmem2_basic'), file_path)
+
+
+@t.windows_exclude
+class TEST2(EX_LIBPMEM2):
+
+    def run(self, ctx):
+        test_path, file_path = self.get_path(ctx, 'testfile2')
+        ctx.exec(path.join(test_path, 'libpmem2', 'advanced'),
+                 file_path, self.offset, self.length)
+
+
+@t.windows_only
+class TEST3(EX_LIBPMEM2):
+
+    def run(self, ctx):
+        test_path, file_path = self.get_path(ctx, 'testfile3')
+        ctx.exec(path.join(test_path, 'ex_pmem2_advanced'),
+                 file_path, self.offset, self.length)
