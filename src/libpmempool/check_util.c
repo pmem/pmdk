pmem/pmdk

commit 8986b6d40307cb7649359eafbb704493629b61b5

pool: fix vs static analyzer libpmempool

src/libpmempool/check_util.c
@@ -359,8 +359,12 @@ check_status_create(PMEMpoolcheck *ppc, enum pmempool_check_msg_type type,
 			p > 0) {
 		char buff[UTIL_MAX_ERR_MSG];
 		util_strerror(errno, buff, UTIL_MAX_ERR_MSG);
-		snprintf(st->msg + p, MAX_MSG_STR_SIZE - (size_t)p,
+		int ret = snprintf(st->msg + p, MAX_MSG_STR_SIZE - (size_t)p,
 			": %s", buff);
+		if (ret < 0 || ret >= (int)(MAX_MSG_STR_SIZE - (size_t)p)) {
+			ERR("!snprintf");
+			return -1;
+		}
 	}
 
 	st->status.type = type;
@@ -621,9 +625,13 @@ check_get_time_str(time_t time)
 
 	if (tm)
 		strftime(str_buff, STR_MAX, TIME_STR_FMT, tm);
-	else
-		snprintf(str_buff, STR_MAX, "unknown");
-
+	else {
+		int ret = snprintf(str_buff, STR_MAX, "unknown");
+		if (ret < 0) {
+			ERR("failed to get time str");
+			return "";
+		}
+	}
 	return str_buff;
 }
 

src/libpmempool/check_btt_map_flog.c
@@ -97,6 +97,7 @@ map_read(PMEMpoolcheck *ppc, struct arena *arenap)
 	uint64_t mapoff = arenap->offset + arenap->btt_info.mapoff;
 	arenap->mapsize = btt_map_size(arenap->btt_info.external_nlba);
 
+	ASSERT(arenap->mapsize != 0);
 	arenap->map = malloc(arenap->mapsize);
 	if (!arenap->map) {
 		ERR("!malloc");
@@ -108,8 +109,9 @@ map_read(PMEMpoolcheck *ppc, struct arena *arenap)
 	}
 
 	uint32_t i;
-	for (i = 0; i < arenap->btt_info.external_nlba; i++)
+	for (i = 0; i < arenap->btt_info.external_nlba; i++) {
 		arenap->map[i] = le32toh(arenap->map[i]);
+	}
 
 	return 0;
 

src/libpmempool/check_pool_hdr.c
@@ -907,9 +907,11 @@ init_location_data(PMEMpoolcheck *ppc, location *loc)
 	unsigned nfiles = pool_set_files_count(ppc->pool->set_file);
 	if (ppc->result != CHECK_RESULT_PROCESS_ANSWERS) {
 		if (nfiles > 1) {
-			snprintf(loc->prefix, PREFIX_MAX_SIZE,
+			int ret = snprintf(loc->prefix, PREFIX_MAX_SIZE,
 				"replica %u part %u: ",
 				loc->replica, loc->part);
+			if (ret < 0 || ret >= PREFIX_MAX_SIZE)
+				FATAL("!snprintf");
 		} else
 			loc->prefix[0] = '\0';
 		loc->step = 0;

src/libpmempool/pool.c
@@ -385,9 +385,8 @@ pool_params_parse(const PMEMpoolcheck *ppc, struct pool_params *params,
 		memcpy(&pbp, addr, sizeof(pbp));
 		params->blk.bsize = le32toh(pbp.bsize);
 	} else if (params->type == POOL_TYPE_OBJ) {
-		struct pmemobjpool pop;
-		memcpy(&pop, addr, sizeof(pop));
-		memcpy(params->obj.layout, pop.layout,
+		struct pmemobjpool *pop = addr;
+		memcpy(params->obj.layout, pop->layout,
 			PMEMOBJ_MAX_LAYOUT);
 	}
 

commit b722b29b9542cf91e686d6822ba37678cd8ba5aa

pool: fix possible memory leak
Introduced by 8986b6d403. Fix attempt in a90bcb14a8.

src/libpmempool/check_util.c
@@ -364,7 +364,7 @@ check_status_create(PMEMpoolcheck *ppc, enum pmempool_check_msg_type type,
 			": %s", buff);
 		if (ret < 0 || ret >= (int)(MAX_MSG_STR_SIZE - (size_t)p)) {
 			ERR("snprintf: %d", ret);
-			free(st);
+			status_release(st);
 			return -1;
 		}
 	}