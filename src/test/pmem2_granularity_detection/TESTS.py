ffbe0d0e9257477d6e885dda659bd7aa2c3e9a7e
Lugin <leszek.lugin@intel.com>
2019-12-11 16:06:36 +0100

    test: (py) fix get_test_tool_path

--- src/test/pmem2_granularity_detection/TESTS.py
+++ src/test/pmem2_granularity_detection/TESTS.py
@@ -44,7 +44,8 @@ class TEST0(PMEM2_CONFIG):
     """test granularity detection with PMEM2_FORCE_GRANULARITY set to page"""
     def run(self, ctx):
         ctx.env['PMEM2_FORCE_GRANULARITY'] = "page"
-        ctx.exec(f.get_test_tool_path(ctx, "gran_detecto"), '-p', ctx.testdir)
+        ctx.exec(f.get_test_tool_path(ctx.build, "gran_detecto"),
+                 '-p', ctx.testdir)
 
 
 class TEST1(PMEM2_CONFIG):
@@ -52,14 +53,16 @@ class TEST1(PMEM2_CONFIG):
     set to cache_line"""
     def run(self, ctx):
         ctx.env['PMEM2_FORCE_GRANULARITY'] = "cache_line"
-        ctx.exec(f.get_test_tool_path(ctx, "gran_detecto"), '-c', ctx.testdir)
+        ctx.exec(f.get_test_tool_path(ctx.build, "gran_detecto"),
+                 '-c', ctx.testdir)
 
 
 class TEST2(PMEM2_CONFIG):
     """test granularity detection with PMEM2_FORCE_GRANULARITY set to byte"""
     def run(self, ctx):
         ctx.env['PMEM2_FORCE_GRANULARITY'] = "byte"
-        ctx.exec(f.get_test_tool_path(ctx, "gran_detecto"), '-b', ctx.testdir)
+        ctx.exec(f.get_test_tool_path(ctx.build, "gran_detecto"),
+                 '-b', ctx.testdir)
 
 
 class TEST3(PMEM2_CONFIG):
@@ -67,7 +70,8 @@ class TEST3(PMEM2_CONFIG):
     set to CaCHe_Line"""
     def run(self, ctx):
         ctx.env['PMEM2_FORCE_GRANULARITY'] = "CaCHe_Line"
-        ctx.exec(f.get_test_tool_path(ctx, "gran_detecto"), '-c', ctx.testdir)
+        ctx.exec(f.get_test_tool_path(ctx.build, "gran_detecto"),
+                 '-c', ctx.testdir)
 
 
 class TEST4(PMEM2_CONFIG):
@@ -75,4 +79,5 @@ class TEST4(PMEM2_CONFIG):
     set to CACHELINE"""
     def run(self, ctx):
         ctx.env['PMEM2_FORCE_GRANULARITY'] = "CACHELINE"
-        ctx.exec(f.get_test_tool_path(ctx, "gran_detecto"), '-c', ctx.testdir)
+        ctx.exec(f.get_test_tool_path(ctx.build, "gran_detecto"),
+                 '-c', ctx.testdir)
