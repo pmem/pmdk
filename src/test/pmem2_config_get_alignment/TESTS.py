adc3853ca3bc94581b0bca230942f63d41387677
Rafal Borawski <rafal.borawski@intel.com>
2019-12-18 16:51:19 +0100

    pmem2: cleanup pmem2 tests

--- src/test/pmem2_config_get_alignment/TESTS.py
+++ src/test/pmem2_config_get_alignment/TESTS.py
@@ -38,29 +38,29 @@ import testframework as t
 @t.no_testdir()
 class TEST0(t.BaseTest):
     test_type = t.Short
+    test_case = 'test_notset_fd'
 
     def run(self, ctx):
-        ctx.exec('pmem2_config_get_alignment', 'notset_fd', 'x')
+        ctx.exec('pmem2_config_get_alignment', self.test_case)
 
 
 class TEST1(t.BaseTest):
     test_type = t.Short
+    test_case = 'test_get_alignment_success'
 
     def run(self, ctx):
-        size = 16 * t.MiB
-        filepath = ctx.create_holey_file(size, 'testfile')
-        ctx.exec('pmem2_config_get_alignment', 'get_alignment_success',
-                 filepath)
+        filepath = ctx.create_holey_file(16 * t.MiB, 'testfile',)
+        ctx.exec('pmem2_config_get_alignment', self.test_case, filepath)
 
 
 @t.windows_exclude
 @t.require_granularity(t.ANY)
 class TEST2(t.BaseTest):
     test_type = t.Short
+    test_case = 'test_directory'
 
     def run(self, ctx):
-        ctx.exec('pmem2_config_get_alignment', 'directory',
-                 ctx.testdir)
+        ctx.exec('pmem2_config_get_alignment', self.test_case, ctx.testdir)
 
 
 # XXX must be divided into separate requirements (alignment 4k and 2M)
@@ -69,8 +69,9 @@ class TEST2(t.BaseTest):
 @t.require_devdax(t.DevDax('devdax1'))
 class TEST3(t.BaseTest):
     test_type = t.Short
+    test_case = 'test_get_alignment_success'
 
     def run(self, ctx):
         dd = ctx.devdaxes.devdax1
         ctx.exec('pmem2_config_get_alignment',
-                 'get_alignment_success', dd.path, str(dd.alignment))
+                 self.test_case, dd.path, str(dd.alignment))
