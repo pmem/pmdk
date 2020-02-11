9f24120eb04c7072991040504164b7afad93e666
Marcin Ślusarz <marcin.slusarz@intel.com>
2020-02-11 19:45:56 +0100

    pmem: bump copyright year on cpu.h

--- src/libpmem2/x86_64/cpu.h
+++ src/libpmem2/x86_64/cpu.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2018, Intel Corporation
+ * Copyright 2016-2020, Intel Corporation
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
