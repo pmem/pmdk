83ca19ee5865d057a178909ccfdc4ac223f230ea
Grzegorz Brzezinski <grzegorz.brzezinski@intel.com>
2020-02-14 12:47:56 +0100

    test: test for the aof example

--- src/examples/libpmem2/persistdata.c
+++ src/examples/libpmem2/persistdata.c
@@ -54,9 +54,18 @@
 #define MIN_SIZE 1 << 20
 
 /*
+ * aof_header -- AOF header
+ */
+struct aof_header {
+	uint64_t signature[AOF_SIGNATURE_LEN];
+	uint64_t end_offset;
+};
+
+/*
  * aof -- append-only file application context
  */
 struct aof {
+	struct aof_header *header;
 	struct pmem2_map *map;
 	void *tail;
 
@@ -70,28 +79,17 @@ struct aof {
 };
 
 /*
- * aof_header -- AOF header
- */
-struct aof_header {
-	uint64_t signature[AOF_SIGNATURE_LEN];
-	uint64_t end_offset;
-};
-
-/*
  * print_usage -- print short description of usage
  */
 static void
 print_usage(void)
 {
-	printf("Usage:\taof <file> append \"STR\"\n"
-		"\taof <file> appendv N \"STR_1\" \"STR_2\" ... \"STR_N\"\n"
-		"\taof <file> rewind\n"
-		"\taof <file> walk\n"
+	printf("Usage:\taof <file> <COMMAND_1> [COMMAND_2 ...]\n"
 		"\taof help\n"
-		"Available commnads:\n"
-		"append DATA\t\t\t- add new element to the <file>\n"
-		"appendv N DATA ... DATA_N\t- add N elements to the <file>\n"
-		"rewind\t\t\t\t- reset the current write point to the beginning\n"
+		"Available commands:\n"
+		"append DATA\t\t\t- add a new element to the AOF\n"
+		"appendv N DATA_1 ... DATA_N\t- add N new elements to the AOF\n"
+		"rewind\t\t\t\t- remove the AOF content\n"
 		"dump\t\t\t\t- dump the file content to the console\n"
 		"help\t\t\t\t- print this help info\n");
 }
@@ -109,13 +107,14 @@ aof_init(struct aof *ctx)
 	}
 
 	struct pmem2_config *cfg;
+	struct pmem2_source *src;
 	int ret = pmem2_config_new(&cfg);
 	if (ret) {
 		fprintf(stderr, "pmem2_config_new: %s\n", pmem2_errormsg());
 		goto err_config_new;
 	}
 
-	ret = pmem2_config_set_fd(cfg, fd);
+	ret = pmem2_source_from_fd(&src, fd);
 	if (ret) {
 		fprintf(stderr, "pmem2_config_set_fd: %s\n", pmem2_errormsg());
 		goto err_cfg_set_fd;
@@ -130,7 +129,7 @@ aof_init(struct aof *ctx)
 		goto err_cfg_set_req_granularity;
 	}
 
-	ret = pmem2_map(cfg, &ctx->map);
+	ret = pmem2_map(cfg, src, &ctx->map);
 	if (ret) {
 		fprintf(stderr, "pmem2_map: %s\n", pmem2_errormsg());
 		goto err_map;
@@ -141,10 +140,19 @@ aof_init(struct aof *ctx)
 		goto err_map_size;
 	}
 
+	ret = pmem2_source_delete(&src);
+	if (ret || src != NULL) {
+		fprintf(stderr, "pmem2_source_delete: %s\n", pmem2_errormsg());
+		goto err_source_delete;
+	}
+
 	ret = pmem2_config_delete(&cfg);
 	if (ret) {
 		fprintf(stderr, "pmem2_config_delete: %s\n", pmem2_errormsg());
-		goto err_config_delete;
+		(void) pmem2_source_delete(&src);
+		(void) pmem2_unmap(&ctx->map);
+		(void) close(fd);
+		exit(ret);
 	}
 
 	ctx->flush = pmem2_get_flush_fn(ctx->map);
@@ -155,15 +163,15 @@ aof_init(struct aof *ctx)
 
 	return;
 
-err_config_delete:
+err_source_delete:
 err_map_size:
-	pmem2_unmap(&ctx->map);
+	(void) pmem2_unmap(&ctx->map);
+err_map:
 err_cfg_set_req_granularity:
 err_cfg_set_fd:
-err_map:
 	(void) pmem2_config_delete(&cfg);
 err_config_new:
-	close(fd);
+	(void) close(fd);
 	exit(ret);
 }
 
@@ -193,134 +201,133 @@ aof_is_initialized(struct aof_header *header)
  * aof_header_init -- init AOF header
  */
 static void
-aof_header_init(struct aof_header *header, struct aof *ctx)
+aof_header_init(struct aof *ctx)
 {
-	memcpy(header->signature, AOF_SIGNATURE, AOF_SIGNATURE_LEN);
-	ctx->persist(&header->signature, AOF_SIGNATURE_LEN);
+	ctx->header->end_offset = sizeof(*ctx->header);
+	ctx->persist(&ctx->header->end_offset, sizeof(ctx->header->end_offset));
 
-	header->end_offset = sizeof(*header);
-	ctx->persist(&header->end_offset, sizeof(header->end_offset));
+	memcpy(ctx->header->signature, AOF_SIGNATURE, AOF_SIGNATURE_LEN);
+	ctx->persist(&ctx->header->signature, AOF_SIGNATURE_LEN);
+
+	/* tail depends on the header that's why is updated in this place */
+	ctx->tail = (void *)((uintptr_t)ctx->header + ctx->header->end_offset);
 }
 
 /*
  * aof_header_update -- update AOF header
  */
 static void
-aof_header_update(struct aof_header *header, size_t off, struct aof *ctx)
+aof_header_update(size_t off, struct aof *ctx)
 {
-	header->end_offset += off;
-	ctx->persist(&header->end_offset, sizeof(header->end_offset));
+	ctx->header->end_offset += off;
+	ctx->persist(&ctx->header->end_offset, sizeof(ctx->header->end_offset));
+
+	/* tail depends on the header that's why is updated in this place */
+	ctx->tail = (void *)((uintptr_t)ctx->header + ctx->header->end_offset);
 }
 
 /*
- * aof_append -- append new string to AOF
+ * aof_append -- append a new string to AOF
  */
 static int
-aof_append(int argc, char *argv, struct aof_header *header, struct aof *ctx)
+aof_append(char *argv[], struct aof *ctx)
 {
-	char *data = argv;
-
-	if (!data)
-		return -1;
+	char *data = *argv;
 
 	void *addr = ctx->tail;
 	size_t map_size = pmem2_map_get_size(ctx->map);
-	void *end_ptr = (void *)((uintptr_t)header + map_size);
+	void *end_ptr = (void *)((uintptr_t)ctx->header + map_size);
 	size_t len = strlen(data);
 	if ((void *)((uintptr_t)addr + len) > end_ptr) {
-		fprintf(stderr, "aof_append: No space left in the file");
+		fprintf(stderr, "aof_append: no space left in the file");
 		return -1;
 	}
 
 	memcpy(addr, data, len);
 	ctx->persist(addr, len);
 
-	aof_header_update(header, len, ctx);
+	aof_header_update(len, ctx);
 
-	return 2;
+	return 1;
 }
 
 /*
  * aof_appendv -- append N new strings to AOF
  */
 static int
-aof_appendv(int argc, char *argv[], struct aof_header *header, struct aof *ctx)
+aof_appendv(int argc, char *argv[], struct aof *ctx)
 {
-	size_t n_new_strings = strtoul(*argv, NULL, 0);
-	++argv;
+	size_t n_new_strings = strtoul(argv[0], NULL, 0);
 	if ((!n_new_strings) ||
-	    (n_new_strings == ULONG_MAX && errno == ERANGE)) {
-		fprintf(stderr, "aof_appendv: wrong N argument\n");
+		(n_new_strings == ULONG_MAX && errno == ERANGE)) {
+		fprintf(stderr, "aof_appendv: invalid N argument: %s\n",
+			argv[0]);
 		return -1;
 	}
 
-	/* n_args - number of needed arguments (N + number of strings) */
+	/* n_args - number of needed arguments (1 + number of strings) */
 	size_t n_args = n_new_strings + 1;
 	if (argc < n_args) {
-		fprintf(stderr, "aof_appendv: Wrong number of strings\n");
+		fprintf(stderr,
+			"aof_appendv: a too small number of strings provided\n");
 		return -1;
 	}
 
 	void *addr = ctx->tail;
 	size_t map_size = pmem2_map_get_size(ctx->map);
-	void *end_ptr = (void *)((uintptr_t)header + map_size);
-	size_t strings_len = 0;
+	void *end_ptr = (void *)((uintptr_t)ctx->header + map_size);
+	size_t total_len = 0;
 	size_t len;
 	char *data = NULL;
 
-	unsigned i;
-	for (i = 0; i < n_new_strings; ++i) {
-		data = *argv;
-		argv += 1;
-		if (!data) {
-			fprintf(stderr, "aof_appendv: Invalid data\n");
-			return -1;
-		}
-
+	unsigned arg_id;
+	for (arg_id = 1; arg_id <= n_new_strings; ++arg_id) {
+		data = argv[arg_id];
 		len = strlen(data);
 		if ((void *)((uintptr_t)addr + len) > end_ptr) {
 			fprintf(stderr,
-				"aof_appendv: No space left in the file\n");
+				"aof_appendv: no space left in the file\n");
 			return -1;
 		}
 
 		memcpy(addr, data, len);
 		ctx->flush(addr, len);
 
-		strings_len += len;
+		total_len += len;
 		addr = (void *)((uintptr_t)addr + len);
 	}
 	ctx->drain();
-	aof_header_update(header, strings_len, ctx);
+	aof_header_update(total_len, ctx);
 
-	return (n_args + 1 /* + 1 argument (command name) */);
+	return n_args;
 }
 
 /*
  * aof_dump -- dump the AOF file content
  */
 static int
-aof_dump(struct aof_header *header, struct aof *ctx)
+aof_dump(struct aof *ctx)
 {
-	char *elem = (void *)((uintptr_t)header + sizeof(*header));
-	fwrite(elem, 1, strlen(elem), stdout);
+	char *elem = (void *)((uintptr_t)ctx->header + sizeof(*ctx->header));
+	size_t total_len = (size_t)((uintptr_t)ctx->tail - (uintptr_t)elem);
+	fwrite(elem, 1, total_len, stdout);
 	printf("\n");
 
-	return 1;
+	return 0;
 }
 
 /*
- * aof_rewind -- reset the current end offset to the length of the header
+ * aof_rewind -- remove the AOF file content
  */
 static int
-aof_rewind(struct aof_header *header, struct aof *ctx)
+aof_rewind(struct aof *ctx)
 {
-	header->end_offset = sizeof(*header);
-	ctx->persist(&header->end_offset, sizeof(header->end_offset));
+	ctx->header->end_offset = sizeof(*ctx->header);
+	ctx->persist(&ctx->header->end_offset, sizeof(ctx->header->end_offset));
 
-	ctx->tail = (void *)((uintptr_t)header + header->end_offset);
+	ctx->tail = (void *)((uintptr_t)ctx->header + ctx->header->end_offset);
 
-	return 1;
+	return 0;
 }
 
 int
@@ -340,27 +347,28 @@ main(int argc, char *argv[])
 
 	aof_init(&ctx);
 
-	struct aof_header *header = pmem2_map_get_address(ctx.map);
-	if (!aof_is_initialized(header))
-		aof_header_init(header, &ctx);
+	ctx.header = pmem2_map_get_address(ctx.map);
+	if (!aof_is_initialized(ctx.header))
+		aof_header_init(&ctx);
 
-	ctx.tail = (void *)((uintptr_t)header + header->end_offset);
+	ctx.tail = (void *)((uintptr_t)ctx.header + ctx.header->end_offset);
 
 	int ret;
+	argc -= 2;
 	argv += 2;
-	while (argc > 2) {
-		ctx.command = *argv;
-		if (strcmp(ctx.command, "append") == 0) {
-			ret = aof_append(argc, *(argv + 1), header, &ctx);
-		} else if (strcmp(ctx.command, "appendv") == 0) {
-			ret = aof_appendv(argc, (argv + 1), header, &ctx);
-		} else if (strcmp(ctx.command, "dump") == 0) {
-			ret = aof_dump(header, &ctx);
-		} else if (strcmp(ctx.command, "rewind") == 0) {
-			ret = aof_rewind(header, &ctx);
+	while (argc > 0) {
+		if (strcmp(argv[0], "append") == 0 && argc > 1) {
+			ret = aof_append((&argv[1]), &ctx);
+		} else if (strcmp(argv[0], "appendv") == 0) {
+			ret = aof_appendv(argc, &argv[1], &ctx);
+		} else if (strcmp(argv[0], "dump") == 0) {
+			ret = aof_dump(&ctx);
+		} else if (strcmp(argv[0], "rewind") == 0) {
+			ret = aof_rewind(&ctx);
 		} else {
-			fprintf(stderr, "aof_appendv: %s - unknown command\n",
-				ctx.command);
+			fprintf(stderr,
+				"aof: %s - unknown command or or a too small number of arguments\n",
+				argv[0]);
 			print_usage();
 			ret = -1;
 		}
@@ -369,6 +377,11 @@ main(int argc, char *argv[])
 			ret = 1;
 			break;
 		} else {
+			/*
+			 * One argument consumed by the name of the performed
+			 * operation.
+			 */
+			ret += 1;
 			argc -= ret;
 			argv += ret;
 		}
--- src/test/ex_libpmem2/TESTS.py
+++ src/test/ex_libpmem2/TESTS.py
@@ -16,10 +16,10 @@ class EX_LIBPMEM2(t.Test):
     offset = str(97 * t.KiB)
     length = str(65 * t.KiB)
 
-    def get_path(self, ctx, file_name):
+    def get_path(self, ctx, file_name, create_file_fn, file_size):
         path = futils.get_examples_dir(ctx)
-        filepath = ctx.create_non_zero_file(1 * t.MiB,
-                                            Path(ctx.testdir, file_name))
+        filepath = create_file_fn(file_size,
+                                  Path(ctx.testdir, file_name))
         return path, filepath
 
 
@@ -27,7 +27,8 @@ class EX_LIBPMEM2(t.Test):
 class TEST0(EX_LIBPMEM2):
 
     def run(self, ctx):
-        test_path, file_path = self.get_path(ctx, 'testfile0')
+        test_path, file_path = self.get_path(
+            ctx, 'testfile0', ctx.create_non_zero_file, 1 * t.MiB)
         ctx.exec(path.join(test_path, 'libpmem2', 'basic'), file_path)
 
 
@@ -35,7 +36,8 @@ class TEST0(EX_LIBPMEM2):
 class TEST1(EX_LIBPMEM2):
 
     def run(self, ctx):
-        test_path, file_path = self.get_path(ctx, 'testfile1')
+        test_path, file_path = self.get_path(
+            ctx, 'testfile1', ctx.create_non_zero_file, 1 * t.MiB)
         ctx.exec(path.join(test_path, 'ex_pmem2_basic'), file_path)
 
 
@@ -43,7 +45,8 @@ class TEST1(EX_LIBPMEM2):
 class TEST2(EX_LIBPMEM2):
 
     def run(self, ctx):
-        test_path, file_path = self.get_path(ctx, 'testfile2')
+        test_path, file_path = self.get_path(
+            ctx, 'testfile2', ctx.create_non_zero_file, 1 * t.MiB)
         ctx.exec(path.join(test_path, 'libpmem2', 'advanced'),
                  file_path, self.offset, self.length)
 
@@ -52,6 +55,19 @@ class TEST2(EX_LIBPMEM2):
 class TEST3(EX_LIBPMEM2):
 
     def run(self, ctx):
-        test_path, file_path = self.get_path(ctx, 'testfile3')
+        test_path, file_path = self.get_path(
+            ctx, 'testfile3', ctx.create_non_zero_file, 1 * t.MiB)
         ctx.exec(path.join(test_path, 'ex_pmem2_advanced'),
                  file_path, self.offset, self.length)
+
+
+class TEST4(EX_LIBPMEM2):
+
+    def run(self, ctx):
+        test_path, file_path = self.get_path(
+            ctx, 'testfile', ctx.create_holey_file, 16 * t.MiB)
+        ctx.exec(path.join(test_path, 'libpmem2', 'persistdata'), file_path,
+                 'appendv', '4', 'PMDK ', 'is ', 'the best ', 'open source ',
+                 'append', 'project in the world.', 'dump', 'rewind', 'dump',
+                 'appendv', '2', 'End of ', 'file.', 'dump',
+                 log_file='out4.log')
