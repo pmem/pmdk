fbda2add5d4d61bad69950fe03d3d85ee14e5dff
Lukasz Dorau <lukasz.dorau@intel.com>
2020-02-21 13:15:40 +0100

    common: adjust push-image.sh script to GHA CI
    The 'push-image.sh' script is called in the following way now:
    
    $ push-image.sh $OS-$OS_VER
    
    but 'OS' and 'OS_VER' variables cannot be set in command line
    in GHA yaml script, so value of tag '$' has to be set manually
    inside the script.

--- utils/docker/images/push-image.sh
+++ utils/docker/images/push-image.sh
@@ -3,8 +3,7 @@
 # Copyright 2016-2020, Intel Corporation
 
 #
-# push-image.sh <OS-VER> - pushes the Docker image tagged with OS-VER
-#                          to the Docker Hub.
+# push-image.sh - pushes the Docker image to the Docker Hub.
 #
 # The script utilizes $DOCKER_USER and $DOCKER_PASSWORD variables to log in to
 # Docker Hub. The variables can be set in the Travis project's configuration
@@ -15,17 +14,13 @@ set -e
 
 source $(dirname $0)/../set-ci-vars.sh
 
-function usage {
-	echo "Usage:"
-	echo "    push-image.sh <OS-VER>"
-	echo "where <OS-VER>, for example, can be 'ubuntu-16.04', provided " \
-		"a Docker image tagged with ${DOCKERHUB_REPO}:ubuntu-16.04 exists " \
-		"locally."
-}
+if [[ -z "$OS" ]]; then
+	echo "OS environment variable is not set"
+	exit 1
+fi
 
-# Check if the first argument is nonempty
-if [[ -z "$1" ]]; then
-	usage
+if [[ -z "$OS_VER" ]]; then
+	echo "OS_VER environment variable is not set"
 	exit 1
 fi
 
@@ -34,12 +29,13 @@ if [[ -z "${DOCKERHUB_REPO}" ]]; then
 	exit 1
 fi
 
+TAG="1.9-${OS}-${OS_VER}"
+
 # Check if the image tagged with pmdk/OS-VER exists locally
-if [[ ! $(docker images -a | awk -v pattern="^${DOCKERHUB_REPO}:1.9-$1\$" \
+if [[ ! $(docker images -a | awk -v pattern="^${DOCKERHUB_REPO}:${TAG}\$" \
 	'$1":"$2 ~ pattern') ]]
 then
-	echo "ERROR: wrong argument."
-	usage
+	echo "ERROR: Docker image tagged ${DOCKERHUB_REPO}:${TAG} does not exists locally."
 	exit 1
 fi
 
@@ -47,4 +43,4 @@ fi
 docker login -u="$DOCKER_USER" -p="$DOCKER_PASSWORD"
 
 # Push the image to the repository
-docker push ${DOCKERHUB_REPO}:1.9-$1
+docker push ${DOCKERHUB_REPO}:${TAG}
