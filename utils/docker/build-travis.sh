pmem/pmdk

commit 696786d4305b73f71ea89b1245b9a23f0510e533

common: unify code for Travis and GitHub Actions CIs

.github/workflows/gha.yml
@@ -43,6 +43,9 @@ jobs:
        - name: Clone the git repo
          uses: actions/checkout@v1
 
+       - name: Set CI vars
+         run: cd $WORKDIR && ${{ matrix.CONFIG }} ./set-ci-vars.sh
+
        - name: Pull or rebuild the image
          run: cd $WORKDIR && ${{ matrix.CONFIG }} ./pull-or-rebuild-image.sh
 

.travis.yml
@@ -38,6 +38,7 @@ before_install:
   - echo $TRAVIS_COMMIT_RANGE
   - export HOST_WORKDIR=`pwd`
   - cd utils/docker
+  - ./set-ci-vars.sh
   - ./pull-or-rebuild-image.sh
 
 script:

utils/docker/build-travis.sh
@@ -14,14 +14,14 @@ set -e
 source $(dirname $0)/set-vars.sh
 source $(dirname $0)/valid-branches.sh
 
-if [[ "$TRAVIS_EVENT_TYPE" != "cron" && "$TRAVIS_BRANCH" != "coverity_scan" \
+if [[ "$CI_EVENT_TYPE" != "cron" && "$CI_BRANCH" != "coverity_scan" \
 	&& "$COVERITY" -eq 1 ]]; then
 	echo "INFO: Skip Coverity scan job if build is triggered neither by " \
 		"'cron' nor by a push to 'coverity_scan' branch"
 	exit 0
 fi
 
-if [[ ( "$TRAVIS_EVENT_TYPE" == "cron" || "$TRAVIS_BRANCH" == "coverity_scan" )\
+if [[ ( "$CI_EVENT_TYPE" == "cron" || "$CI_BRANCH" == "coverity_scan" )\
 	&& "$COVERITY" -ne 1 ]]; then
 	echo "INFO: Skip regular jobs if build is triggered either by 'cron'" \
 		" or by a push to 'coverity_scan' branch"
@@ -51,7 +51,7 @@ if [[ $MAKE_PKG -eq 0 ]] ; then command="./run-build.sh"; fi
 if [[ $MAKE_PKG -eq 1 ]] ; then command="./run-build-package.sh"; fi
 if [[ $COVERAGE -eq 1 ]] ; then command="./run-coverage.sh"; ci_env=`bash <(curl -s https://codecov.io/env)`; fi
 
-if [[ ( "$TRAVIS_EVENT_TYPE" == "cron" || "$TRAVIS_BRANCH" == "coverity_scan" )\
+if [[ ( "$CI_EVENT_TYPE" == "cron" || "$CI_BRANCH" == "coverity_scan" )\
 	&& "$COVERITY" -eq 1 ]]; then
 	command="./run-coverity.sh"
 fi
@@ -61,7 +61,7 @@ if [[ -f $CI_FILE_SKIP_BUILD_PKG_CHECK ]]; then BUILD_PACKAGE_CHECK=n; else BUIL
 if [ -z "$NDCTL_ENABLE" ]; then ndctl_enable=; else ndctl_enable="--env NDCTL_ENABLE=$NDCTL_ENABLE"; fi
 
 # Only run doc update on $GITHUB_REPO master or stable branch
-if [[ -z "${TRAVIS_BRANCH}" || -z "${TARGET_BRANCHES[${TRAVIS_BRANCH}]}" || "$TRAVIS_PULL_REQUEST" != "false" || "$TRAVIS_REPO_SLUG" != "${GITHUB_REPO}" ]]; then
+if [[ -z "${CI_BRANCH}" || -z "${TARGET_BRANCHES[${CI_BRANCH}]}" || "$CI_EVENT_TYPE" == "pull_request" || "$CI_REPO_SLUG" != "${GITHUB_REPO}" ]]; then
 	AUTO_DOC_UPDATE=0
 fi
 
@@ -95,11 +95,11 @@ docker run --rm --privileged=true --name=$containerName -i $TTY \
 	--env BUILD_PACKAGE_CHECK=$BUILD_PACKAGE_CHECK \
 	--env SCRIPTSDIR=$SCRIPTSDIR \
 	--env TRAVIS=$TRAVIS \
-	--env TRAVIS_COMMIT_RANGE=$TRAVIS_COMMIT_RANGE \
-	--env TRAVIS_COMMIT=$TRAVIS_COMMIT \
-	--env TRAVIS_REPO_SLUG=$TRAVIS_REPO_SLUG \
-	--env TRAVIS_BRANCH=$TRAVIS_BRANCH \
-	--env TRAVIS_EVENT_TYPE=$TRAVIS_EVENT_TYPE \
+	--env CI_COMMIT_RANGE=$CI_COMMIT_RANGE \
+	--env CI_COMMIT=$CI_COMMIT \
+	--env CI_REPO_SLUG=$CI_REPO_SLUG \
+	--env CI_BRANCH=$CI_BRANCH \
+	--env CI_EVENT_TYPE=$CI_EVENT_TYPE \
 	--env GITHUB_TOKEN=$GITHUB_TOKEN \
 	--env COVERITY_SCAN_TOKEN=$COVERITY_SCAN_TOKEN \
 	--env COVERITY_SCAN_NOTIFICATION_EMAIL=$COVERITY_SCAN_NOTIFICATION_EMAIL \

utils/docker/pull-or-rebuild-image.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright 2016-2019, Intel Corporation
+# Copyright 2016-2020, Intel Corporation
 
 #
 # pull-or-rebuild-image.sh - rebuilds the Docker image used in the
@@ -23,36 +23,14 @@ set -e
 
 source $(dirname $0)/set-vars.sh
 
-function get_commit_range_from_last_merge {
-	# get commit id of the last merge
-	LAST_MERGE=$(git log --merges --pretty=%H -1)
-	LAST_COMMIT=$(git log --pretty=%H -1)
-	if [ "$LAST_MERGE" == "$LAST_COMMIT" ]; then
-		# GitHub Actions commits its own merge in case of pull requests
-		# so the first merge commit has to be skipped.
-		LAST_MERGE=$(git log --merges --pretty=%H -2 | tail -n1)
-	fi
-	if [ "$LAST_MERGE" == "" ]; then
-		# possible in case of shallow clones
-		COMMIT_RANGE=""
-	else
-		COMMIT_RANGE="$LAST_MERGE..HEAD"
-		# make sure it works now
-		if ! git rev-list $COMMIT_RANGE >/dev/null; then
-			COMMIT_RANGE=""
-		fi
-	fi
-	echo $COMMIT_RANGE
-}
-
-if [[ "$TRAVIS_EVENT_TYPE" != "cron" && "$TRAVIS_BRANCH" != "coverity_scan" \
+if [[ "$CI_EVENT_TYPE" != "cron" && "$CI_BRANCH" != "coverity_scan" \
 	&& "$COVERITY" -eq 1 ]]; then
 	echo "INFO: Skip Coverity scan job if build is triggered neither by " \
 		"'cron' nor by a push to 'coverity_scan' branch"
 	exit 0
 fi
 
-if [[ ( "$TRAVIS_EVENT_TYPE" == "cron" || "$TRAVIS_BRANCH" == "coverity_scan" )\
+if [[ ( "$CI_EVENT_TYPE" == "cron" || "$CI_BRANCH" == "coverity_scan" )\
 	&& "$COVERITY" -ne 1 ]]; then
 	echo "INFO: Skip regular jobs if build is triggered either by 'cron'" \
 		" or by a push to 'coverity_scan' branch"
@@ -71,35 +49,11 @@ if [[ -z "$HOST_WORKDIR" ]]; then
 	exit 1
 fi
 
-# TRAVIS_COMMIT_RANGE is usually invalid for force pushes - fix it when used
-# with non-upstream repository
-if [ -n "$TRAVIS_COMMIT_RANGE" -a "$TRAVIS_REPO_SLUG" != "$GITHUB_REPO" ]; then
-	if ! git rev-list $TRAVIS_COMMIT_RANGE; then
-		TRAVIS_COMMIT_RANGE=$(get_commit_range_from_last_merge)
-	fi
-fi
-
-# Fix Travis commit range
-if [ -n "$TRAVIS_COMMIT_RANGE" ]; then
-	# $TRAVIS_COMMIT_RANGE contains "..." instead of ".."
-	# https://github.com/travis-ci/travis-ci/issues/4596
-	PR_COMMIT_RANGE="${TRAVIS_COMMIT_RANGE/.../..}"
-fi
-
-# Set the commit range in case of GitHub Actions
-if [ -n "$GITHUB_ACTIONS" ]; then
-	PR_COMMIT_RANGE=$(get_commit_range_from_last_merge)
-fi
-
 # Find all the commits for the current build
-if [ -n "$PR_COMMIT_RANGE" ]; then
-	commits=$(git rev-list $PR_COMMIT_RANGE)
-elif [ -n "$TRAVIS" ]; then
-	commits=$TRAVIS_COMMIT
-elif [ -n "$GITHUB_ACTIONS" ]; then
-	commits=$GITHUB_SHA
+if [ -n "$CI_COMMIT_RANGE" ]; then
+	commits=$(git rev-list $CI_COMMIT_RANGE)
 else
-	commits=$(git log --pretty=%H -1)
+	commits=$CI_COMMIT
 fi
 
 echo "Commits in the commit range:"
@@ -132,9 +86,9 @@ for file in $files; do
 		# repository's stable-* or master branch, and the Travis build is not
 		# of the "pull_request" type). In that case, create the empty
 		# file.
-		if [[ "$TRAVIS_REPO_SLUG" == "$GITHUB_REPO" \
-			&& ($TRAVIS_BRANCH == stable-* || $TRAVIS_BRANCH == devel-* || $TRAVIS_BRANCH == master) \
-			&& $TRAVIS_EVENT_TYPE != "pull_request" \
+		if [[ "$CI_REPO_SLUG" == "$GITHUB_REPO" \
+			&& ($CI_BRANCH == stable-* || $CI_BRANCH == devel-* || $CI_BRANCH == master) \
+			&& $CI_EVENT_TYPE != "pull_request" \
 			&& $PUSH_IMAGE == "1" ]]
 		then
 			echo "The image will be pushed to Docker Hub"

utils/check-commits.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright 2016-2019, Intel Corporation
+# Copyright 2016-2020, Intel Corporation
 
 #
 # Used to check whether all the commit messages in a pull request
@@ -11,9 +11,9 @@
 
 if [ -z "$1" ]; then
 	# on Travis run this check only for pull requests
-	if [ -n "$TRAVIS_REPO_SLUG" ]; then
-		if [[ "$TRAVIS_REPO_SLUG" != "$GITHUB_REPO" \
-			|| $TRAVIS_EVENT_TYPE != "pull_request" ]];
+	if [ -n "$CI_REPO_SLUG" ]; then
+		if [[ "$CI_REPO_SLUG" != "$GITHUB_REPO" \
+			|| $CI_EVENT_TYPE != "pull_request" ]];
 		then
 			echo "SKIP: $0 can only be executed for pull requests to $GITHUB_REPO"
 			exit 0

utils/docker/run-coverity.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright 2017-2019, Intel Corporation
+# Copyright 2017-2020, Intel Corporation
 
 #
 # run-coverity.sh - runs the Coverity scan build
@@ -16,8 +16,8 @@ echo -n | openssl s_client -connect scan.coverity.com:443 | \
 	sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' | \
 	sudo tee -a /etc/ssl/certs/ca-;
 
-export COVERITY_SCAN_PROJECT_NAME="$TRAVIS_REPO_SLUG"
-[[ "$TRAVIS_EVENT_TYPE" == "cron" ]] \
+export COVERITY_SCAN_PROJECT_NAME="$CI_REPO_SLUG"
+[[ "$CI_EVENT_TYPE" == "cron" ]] \
 	&& export COVERITY_SCAN_BRANCH_PATTERN="master" \
 	|| export COVERITY_SCAN_BRANCH_PATTERN="coverity_scan"
 export COVERITY_SCAN_BUILD_COMMAND="make -j$(nproc) all"

utils/docker/run-doc-update.sh
@@ -13,7 +13,7 @@ REPO_NAME="pmdk"
 ORIGIN="https://${GITHUB_TOKEN}@github.com/${BOT_NAME}/${REPO_NAME}"
 UPSTREAM="https://github.com/${USER_NAME}/${REPO_NAME}"
 # master or stable-* branch
-TARGET_BRANCH=${TRAVIS_BRANCH}
+TARGET_BRANCH=${CI_BRANCH}
 VERSION=${TARGET_BRANCHES[$TARGET_BRANCH]}
 
 if [ -z $VERSION ]; then

utils/docker/set-ci-vars.sh
@@ -1,0 +1,75 @@
+#!/usr/bin/env bash
+#
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright 2020, Intel Corporation
+
+#
+# set-ci-vars.sh -- set CI variables common for both:
+#                   Travis and GitHub Actions CIs
+#
+
+set -e
+
+function get_commit_range_from_last_merge {
+	# get commit id of the last merge
+	LAST_MERGE=$(git log --merges --pretty=%H -1)
+	LAST_COMMIT=$(git log --pretty=%H -1)
+	if [ "$LAST_MERGE" == "$LAST_COMMIT" ]; then
+		# GitHub Actions commits its own merge in case of pull requests
+		# so the first merge commit has to be skipped.
+		LAST_MERGE=$(git log --merges --pretty=%H -2 | tail -n1)
+	fi
+	if [ "$LAST_MERGE" == "" ]; then
+		# possible in case of shallow clones
+		# or new repos with no merge commits yet
+		# - pick up the first commit
+		LAST_MERGE=$(git log --pretty=%H | tail -n1)
+	fi
+	COMMIT_RANGE="$LAST_MERGE..HEAD"
+	# make sure it works now
+	if ! git rev-list $COMMIT_RANGE >/dev/null; then
+		COMMIT_RANGE=""
+	fi
+	echo $COMMIT_RANGE
+}
+
+COMMIT_RANGE_FROM_LAST_MERGE=$(get_commit_range_from_last_merge)
+
+if [ -n "$TRAVIS" ]; then
+	CI_COMMIT=$TRAVIS_COMMIT
+	CI_COMMIT_RANGE="${TRAVIS_COMMIT_RANGE/.../..}"
+	CI_BRANCH=$TRAVIS_BRANCH
+	CI_EVENT_TYPE=$TRAVIS_EVENT_TYPE
+	CI_REPO_SLUG=$TRAVIS_REPO_SLUG
+
+	# CI_COMMIT_RANGE is usually invalid for force pushes - fix it when used
+	# with non-upstream repository
+	if [ -n "$CI_COMMIT_RANGE" -a "$CI_REPO_SLUG" != "$GITHUB_REPO" ]; then
+		if ! git rev-list $CI_COMMIT_RANGE; then
+			CI_COMMIT_RANGE=$COMMIT_RANGE_FROM_LAST_MERGE
+		fi
+	fi
+
+elif [ -n "$GITHUB_ACTIONS" ]; then
+	CI_COMMIT=$GITHUB_SHA
+	CI_COMMIT_RANGE=$COMMIT_RANGE_FROM_LAST_MERGE
+	CI_BRANCH=$(echo $GITHUB_REF | cut -d'/' -f3)
+	CI_EVENT_TYPE=$GITHUB_EVENT_NAME
+	CI_REPO_SLUG=$GITHUB_REPOSITORY
+
+else
+	CI_COMMIT=$(git log --pretty=%H -1)
+	CI_COMMIT_RANGE=$COMMIT_RANGE_FROM_LAST_MERGE
+fi
+
+export CI_COMMIT=$CI_COMMIT
+export CI_COMMIT_RANGE=$CI_COMMIT_RANGE
+export CI_BRANCH=$CI_BRANCH
+export CI_EVENT_TYPE=$CI_EVENT_TYPE
+export CI_REPO_SLUG=$CI_REPO_SLUG
+
+echo CI_COMMIT=$CI_COMMIT
+echo CI_COMMIT_RANGE=$CI_COMMIT_RANGE
+echo CI_BRANCH=$CI_BRANCH
+echo CI_EVENT_TYPE=$CI_EVENT_TYPE
+echo CI_REPO_SLUG=$CI_REPO_SLUG

commit 3f40d812f273141499b5bde45073c3f8a09643d9

common: fix CI building
This patch fixes regression introduced by commit 696786d.

It turned out that passing CI_* variables through environment
does not work - they are not passed through at all.

The 'set-ci-vars.sh' script has to be called directly
in other CI scripts.

.github/workflows/gha.yml
@@ -43,9 +43,6 @@ jobs:
        - name: Clone the git repo
          uses: actions/checkout@v1
 
-       - name: Set CI vars
-         run: cd $WORKDIR && ${{ matrix.CONFIG }} ./set-ci-vars.sh
-
        - name: Pull or rebuild the image
          run: cd $WORKDIR && ${{ matrix.CONFIG }} ./pull-or-rebuild-image.sh
 

.travis.yml
@@ -38,7 +38,6 @@ before_install:
   - echo $TRAVIS_COMMIT_RANGE
   - export HOST_WORKDIR=`pwd`
   - cd utils/docker
-  - ./set-ci-vars.sh
   - ./pull-or-rebuild-image.sh
 
 script:

utils/docker/build-travis.sh
@@ -11,6 +11,7 @@
 
 set -e
 
+source $(dirname $0)/set-ci-vars.sh
 source $(dirname $0)/set-vars.sh
 source $(dirname $0)/valid-branches.sh
 

utils/docker/images/push-image.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright 2016-2019, Intel Corporation
+# Copyright 2016-2020, Intel Corporation
 
 #
 # push-image.sh <OS-VER> - pushes the Docker image tagged with OS-VER
@@ -12,6 +12,8 @@
 #
 
 set -e
+
+source $(dirname $0)/../set-ci-vars.sh
 
 function usage {
 	echo "Usage:"

utils/docker/pull-or-rebuild-image.sh
@@ -21,6 +21,7 @@
 
 set -e
 
+source $(dirname $0)/set-ci-vars.sh
 source $(dirname $0)/set-vars.sh
 
 if [[ "$CI_EVENT_TYPE" != "cron" && "$CI_BRANCH" != "coverity_scan" \