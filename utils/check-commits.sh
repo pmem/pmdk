9e080cc4ae39ab10e3cc4c9755a370e691d34d52
Lukasz Dorau <lukasz.dorau@intel.com>
2020-02-28 09:39:30 +0100

    common: fix check-commits.sh to make it work on GitHub Actions CI

--- utils/check-commits.sh
+++ utils/check-commits.sh
@@ -10,7 +10,7 @@
 #
 
 if [ -z "$1" ]; then
-	# on Travis run this check only for pull requests
+	# on CI run this check only for pull requests
 	if [ -n "$CI_REPO_SLUG" ]; then
 		if [[ "$CI_REPO_SLUG" != "$GITHUB_REPO" \
 			|| $CI_EVENT_TYPE != "pull_request" ]];
@@ -19,13 +19,13 @@ if [ -z "$1" ]; then
 			exit 0
 		fi
 	fi
-	# TRAVIS_COMMIT_RANGE can be invalid for force pushes - use another
+	# CI_COMMIT_RANGE can be invalid for force pushes - use another
 	# method to determine the list of commits
-	if [[ $(git rev-list $TRAVIS_COMMIT_RANGE) || -n "$TRAVIS_COMMIT_RANGE" ]]; then
-		MERGE_BASE=$(echo $TRAVIS_COMMIT_RANGE | cut -d. -f1)
+	if [[ $(git rev-list $CI_COMMIT_RANGE 2>/dev/null) || -n "$CI_COMMIT_RANGE" ]]; then
+		MERGE_BASE=$(echo $CI_COMMIT_RANGE | cut -d. -f1)
 		[ -z $MERGE_BASE ] && \
 			MERGE_BASE=$(git log --pretty="%cN:%H" | grep GitHub | head -n1 | cut -d: -f2)
-		range=$MERGE_BASE..$TRAVIS_COMMIT
+		range=$MERGE_BASE..$CI_COMMIT
 	else
 		merge_base=$(git log --pretty="%cN:%H" | grep GitHub | head -n1 | cut -d: -f2)
 		range=$merge_base..HEAD
