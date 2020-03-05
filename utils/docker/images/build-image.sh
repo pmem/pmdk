378978a03fbbf5c1ba29ca4fcdd68b262253c4cd
Lukasz Dorau <lukasz.dorau@intel.com>
2020-03-05 15:44:13 +0100

    common: make Docker images and files architecture-specific

--- utils/docker/images/build-image.sh
+++ utils/docker/images/build-image.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright 2016-2019, Intel Corporation
+# Copyright 2016-2020, Intel Corporation
 
 #
-# build-image.sh <OS-VER> - prepares a Docker image with <OS>-based
+# build-image.sh <OS-VER-ARCH> - prepares a Docker image with <OS>-based
 #                           environment for building PMDK project, according
 #                           to the Dockerfile.<OS-VER> file located
 #                           in the same directory.
@@ -15,10 +15,10 @@ set -e
 
 function usage {
 	echo "Usage:"
-	echo "    build-image.sh <OS-VER>"
-	echo "where <OS-VER>, for example, can be 'ubuntu-16.04', provided " \
-		"a Dockerfile named 'Dockerfile.ubuntu-16.04' exists in the " \
-		"current directory."
+	echo "    build-image.sh <OS-VER-ARCH>"
+	echo "where <OS-VER-ARCH>, for example, can be 'ubuntu-16.04-x86_64', "\
+		"provided a Dockerfile named 'Dockerfile.ubuntu-16.04' "\
+		"exists in the current directory."
 }
 
 # Check if the first argument is nonempty
@@ -27,9 +27,12 @@ if [[ -z "$1" ]]; then
 	exit 1
 fi
 
+# Dockerfile's "extension"
+EXT=$(echo "$1" | cut -d'-' -f1-2)
+
 # Check if the file Dockerfile.OS-VER exists
-if [[ ! -f "Dockerfile.$1" ]]; then
-	echo "ERROR: wrong argument."
+if [[ ! -f "Dockerfile.$EXT" ]]; then
+	echo "ERROR: Dockerfile.$EXT does not exist."
 	usage
 	exit 1
 fi
@@ -39,9 +42,9 @@ if [[ -z "${DOCKERHUB_REPO}" ]]; then
 	exit 1
 fi
 
-# Build a Docker image tagged with ${DOCKERHUB_REPO}:OS-VER
+# Build a Docker image tagged with ${DOCKERHUB_REPO}:OS-VER-ARCH
 tag=${DOCKERHUB_REPO}:1.9-$1
 docker build -t $tag \
 	--build-arg http_proxy=$http_proxy \
 	--build-arg https_proxy=$https_proxy \
-	-f Dockerfile.$1 .
+	-f Dockerfile.$EXT .
