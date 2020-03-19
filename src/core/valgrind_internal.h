f65ae329e5817f96d60cda84045e537de0eb19b7
Grzegorz Brzezinski <grzegorz.brzezinski@intel.com>
2020-03-19 12:20:13 +0100

    pmem2: add PMEM2_API_START() and PMEM2_API_END() to pmem2 API

--- src/core/valgrind_internal.h
+++ src/core/valgrind_internal.h
@@ -119,6 +119,7 @@ extern unsigned _On_valgrind;
 
 void pobj_emit_log(const char *func, int order);
 void pmem_emit_log(const char *func, int order);
+void pmem2_emit_log(const char *func, int order);
 extern int _Pmreorder_emit;
 
 #define Pmreorder_emit __builtin_expect(_Pmreorder_emit, 0)
@@ -244,6 +245,13 @@ extern int _Pmreorder_emit;
 	if (Pmreorder_emit)\
 		pmem_emit_log(__func__, 1);
 
+#define PMEM2_API_START()\
+	if (Pmreorder_emit)\
+		pmem2_emit_log(__func__, 0);
+#define PMEM2_API_END()\
+	if (Pmreorder_emit)\
+		pmem2_emit_log(__func__, 1);
+
 #else
 
 #define Pmreorder_emit (0)
@@ -339,6 +347,10 @@ extern int _Pmreorder_emit;
 #define PMEM_API_START() do {} while (0)
 
 #define PMEM_API_END() do {} while (0)
+
+#define PMEM2_API_START() do {} while (0)
+
+#define PMEM2_API_END() do {} while (0)
 
 #endif
 
