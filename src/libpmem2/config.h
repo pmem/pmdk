b1c813436536a133ef23f2cc3d395825ee025ae2
osalyk <oksana.salyk@intel.com>
2020-03-25 10:08:38 +0100

    pmem2: add protection flags to pmem2_map

--- src/libpmem2/config.h
+++ src/libpmem2/config.h
@@ -21,6 +21,7 @@ struct pmem2_config {
 	int addr_request; /* address request type */
 	enum pmem2_granularity requested_max_granularity;
 	enum pmem2_sharing_type sharing; /* the way the file will be mapped */
+	unsigned protection_flag;
 };
 
 void pmem2_config_init(struct pmem2_config *cfg);
