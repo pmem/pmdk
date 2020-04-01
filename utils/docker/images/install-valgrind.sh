f150e6d873fde753a3c2a1f6b266024a16abc201
Marcin Ślusarz <marcin.slusarz@intel.com>
2020-04-01 12:26:26 +0200

    common: update pmemcheck in Docker images to the latest version
    ... with optimizations

--- utils/docker/images/install-valgrind.sh
+++ utils/docker/images/install-valgrind.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright 2016-2019, Intel Corporation
+# Copyright 2016-2020, Intel Corporation
 
 #
 # install-valgrind.sh - installs valgrind for persistent memory
@@ -22,7 +22,8 @@ install_custom-pmem_from_source() {
   git clone https://github.com/pmem/valgrind.git
   cd valgrind
   # valgrind v3.15 with pmemcheck
-  git checkout c27a8a2f973414934e63f1e94bc84c0a580e3840
+  # 2020.04.01 Merge pull request #78 from marcinslusarz/opt3
+  git checkout 759686fd66cc0105df8311cfe676b0b2f9e89196
   ./autogen.sh
   ./configure
   make -j$(nproc)
