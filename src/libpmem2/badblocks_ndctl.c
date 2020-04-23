pmem/pmdk

commit 831d125fe7e12c8c0f7b3d7fe80acc3072961626

pmem2: make extent functions return pmem2 errors
This patch changes pmem2 extents API. The new API is following:

int pmem2_extents_create_get(int fd, struct extents **exts);
void pmem2_extents_destroy(struct extents **exts);

src/libpmem2/badblocks_ndctl.c
@@ -506,7 +506,6 @@ badblocks_get(const char *file, struct badblocks *bbs)
 
 	VEC(bbsvec, struct bad_block) bbv = VEC_INITIALIZER;
 	struct extents *exts = NULL;
-	long extents = 0;
 	int fd = -1;
 
 	unsigned long long bb_beg;
@@ -531,25 +530,19 @@ badblocks_get(const char *file, struct badblocks *bbs)
 		goto exit_free_all;
 	}
 
-	exts = Zalloc(sizeof(struct extents));
-	if (exts == NULL) {
-		ERR("!Zalloc");
-		goto error_free_all;
-	}
-
 	fd = os_open(file, O_RDONLY);
 	if (fd == -1) {
 		ERR("!open %s", file);
 		goto error_free_all;
 	}
 
-	extents = pmem2_extents_count(fd, exts);
-	if (extents < 0) {
-		LOG(1, "counting file's extents failed -- '%s'", file);
+	int ret = pmem2_extents_create_get(fd, &exts);
+	if (ret) {
+		LOG(1, "getting file's extents failed -- '%s'", file);
 		goto error_free_all;
 	}
 
-	if (extents == 0) {
+	if (exts->extents_count == 0) {
 		/* dax device has no extents */
 		bb_found = (int)bbs->bb_cnt;
 
@@ -560,17 +553,6 @@ badblocks_get(const char *file, struct badblocks *bbs)
 		}
 
 		goto exit_free_all;
-	}
-
-	exts->extents = Zalloc(exts->extents_count * sizeof(struct extent));
-	if (exts->extents == NULL) {
-		ERR("!Zalloc");
-		goto error_free_all;
-	}
-
-	if (pmem2_extents_get(fd, exts)) {
-		LOG(1, "getting file's extents failed -- '%s'", file);
-		goto error_free_all;
 	}
 
 	bb_found = 0;
@@ -641,12 +623,9 @@ error_free_all:
 	bbs->bb_cnt = 0;
 
 exit_free_all:
-	if (exts) {
-		Free(exts->extents);
-		Free(exts);
-	}
+	pmem2_extents_destroy(&exts);
 
-	if (extents > 0 && bb_found > 0) {
+	if (bb_found > 0) {
 		bbs->bbv = VEC_ARR(&bbv);
 		bbs->bb_cnt = (unsigned)VEC_SIZE(&bbv);
 

src/libpmem2/extent.h
@@ -27,8 +27,8 @@ struct extents {
 	struct extent *extents;
 };
 
-long pmem2_extents_count(int fd, struct extents *exts);
-int pmem2_extents_get(int fd, struct extents *exts);
+int pmem2_extents_create_get(int fd, struct extents **exts);
+void pmem2_extents_destroy(struct extents **exts);
 
 #ifdef __cplusplus
 }

src/libpmem2/extent_linux.c
@@ -20,173 +20,147 @@
 #include "alloc.h"
 
 /*
- * pmem2_extents_common -- (internal) common part of getting extents
- *                          of the given file
- *
- * Returns: number of extents the file consists of or -1 in case of an error.
- * Sets: struct fiemap and struct extents.
+ * pmem2_extents_create_get -- allocate extents structure and get extents
+ *                             of the given file
  */
-static long
-pmem2_extents_common(int fd, struct extents *exts, struct fiemap **pfmap)
+int
+pmem2_extents_create_get(int fd, struct extents **exts)
 {
-	LOG(3, "fd %i exts %p pfmap %p", fd, exts, pfmap);
+	LOG(3, "fd %i extents %p", fd, exts);
 
-	os_stat_t st;
-	if (os_fstat(fd, &st) < 0) {
-		ERR("!fstat %d", fd);
-		return -1;
-	}
+	ASSERT(fd > 2);
+	ASSERTne(exts, NULL);
 
 	enum pmem2_file_type pmem2_type;
+	struct extents *pexts = NULL;
+	struct fiemap *fmap = NULL;
+	os_stat_t st;
+
+	if (os_fstat(fd, &st) < 0) {
+		ERR("!fstat %d", fd);
+		return PMEM2_E_ERRNO;
+	}
 
 	int ret = pmem2_get_type_from_stat(&st, &pmem2_type);
-	if (ret) {
-		errno = pmem2_err_to_errno(ret);
-		return -1;
-	}
+	if (ret)
+		return ret;
 
 	/* directories do not have any extents */
 	if (pmem2_type == PMEM2_FTYPE_DIR) {
 		ERR(
 			"checking extents does not make sense in case of directories");
-		return -1;
+		return PMEM2_E_INVALID_FILE_TYPE;
 	}
 
-	if (exts->extents_count == 0) {
-		LOG(10, "fd %i: block size: %li", fd, (long int)st.st_blksize);
-		exts->blksize = (uint64_t)st.st_blksize;
-	}
+	/* allocate extents structure */
+	pexts = pmem2_zalloc(sizeof(struct extents), &ret);
+	if (ret)
+		return ret;
 
-	/* devdax does not have any extents */
+	/* save block size */
+	LOG(10, "fd %i: block size: %li", fd, (long int)st.st_blksize);
+	pexts->blksize = (uint64_t)st.st_blksize;
+
+	/* DAX device does not have any extents */
 	if (pmem2_type == PMEM2_FTYPE_DEVDAX) {
+		*exts = pexts;
 		return 0;
 	}
 
 	ASSERTeq(pmem2_type, PMEM2_FTYPE_REG);
 
-	struct fiemap *fmap = Zalloc(sizeof(struct fiemap));
-	if (fmap == NULL) {
-		ERR("!malloc");
-		return -1;
-	}
+	fmap = pmem2_zalloc(sizeof(struct fiemap), &ret);
+	if (ret)
+		goto error_free;
 
 	fmap->fm_start = 0;
 	fmap->fm_length = (size_t)st.st_size;
 	fmap->fm_flags = 0;
 	fmap->fm_extent_count = 0;
+	fmap->fm_mapped_extents = 0;
 
 	if (ioctl(fd, FS_IOC_FIEMAP, fmap) != 0) {
-		ERR("!ioctl %d", fd);
+		ERR("!fiemap ioctl() for fd=%d failed", fd);
+		ret = PMEM2_E_ERRNO;
 		goto error_free;
 	}
 
-	if (exts->extents_count == 0) {
-		exts->extents_count = fmap->fm_mapped_extents;
-		LOG(4, "fd: %i: number of extents: %u",
-			fd, exts->extents_count);
+	size_t newsize = sizeof(struct fiemap) +
+		fmap->fm_mapped_extents * sizeof(struct fiemap_extent);
 
-	} else if (exts->extents_count != fmap->fm_mapped_extents) {
-		ERR("number of extents differs (was: %u, is: %u)",
-			exts->extents_count, fmap->fm_mapped_extents);
+	struct fiemap *newfmap = pmem2_realloc(fmap, newsize, &ret);
+	if (ret)
 		goto error_free;
-	}
-
-	*pfmap = fmap;
-
-	return exts->extents_count;
-
-error_free:
-	Free(fmap);
-
-	return -1;
-}
-
-/*
- * pmem2_extents_count -- get number of extents of the given file
- *                        (and optionally read its block size)
- */
-long
-pmem2_extents_count(int fd, struct extents *exts)
-{
-	LOG(3, "fd %i extents %p", fd, exts);
-
-	struct fiemap *fmap = NULL;
-
-	ASSERTne(exts, NULL);
-	memset(exts, 0, sizeof(*exts));
-
-	long ret = pmem2_extents_common(fd, exts, &fmap);
-
-	Free(fmap);
-
-	return ret;
-}
-
-/*
- * pmem2_extents_get -- get extents of the given file
- *                      (and optionally read its block size)
- */
-int
-pmem2_extents_get(int fd, struct extents *exts)
-{
-	LOG(3, "fd %i extents %p", fd, exts);
-
-	struct fiemap *fmap = NULL;
-	int ret = -1;
-
-	ASSERTne(exts, NULL);
-
-	if (exts->extents_count == 0)
-		return 0;
-
-	ASSERTne(exts->extents, NULL);
-
-	if (pmem2_extents_common(fd, exts, &fmap) <= 0)
-		goto error_free;
-
-	struct fiemap *newfmap = Realloc(fmap, sizeof(struct fiemap) +
-						fmap->fm_mapped_extents *
-						sizeof(struct fiemap_extent));
-	if (newfmap == NULL) {
-		ERR("!Realloc");
-		goto error_free;
-	}
 
 	fmap = newfmap;
-	fmap->fm_extent_count = fmap->fm_mapped_extents;
-
 	memset(fmap->fm_extents, 0, fmap->fm_mapped_extents *
 					sizeof(struct fiemap_extent));
+	fmap->fm_extent_count = fmap->fm_mapped_extents;
+	fmap->fm_mapped_extents = 0;
 
 	if (ioctl(fd, FS_IOC_FIEMAP, fmap) != 0) {
-		ERR("!ioctl %d", fd);
+		ERR("!fiemap ioctl() for fd=%d failed", fd);
+		ret = PMEM2_E_ERRNO;
 		goto error_free;
 	}
 
-	if (fmap->fm_extent_count > 0) {
-		LOG(10, "file with fd %i has %u extents:",
-			fd, fmap->fm_extent_count);
-	}
+	LOG(4, "file with fd=%i has %u extents:", fd, fmap->fm_mapped_extents);
 
+	/* save number of extents */
+	pexts->extents_count = fmap->fm_mapped_extents;
+
+	pexts->extents = pmem2_malloc(
+				pexts->extents_count * sizeof(struct extent),
+				&ret);
+	if (ret)
+		goto error_free;
+
+	/* save extents */
 	unsigned e;
-	for (e = 0; e < fmap->fm_extent_count; e++) {
-		exts->extents[e].offset_physical =
-						fmap->fm_extents[e].fe_physical;
-		exts->extents[e].offset_logical =
-						fmap->fm_extents[e].fe_logical;
-		exts->extents[e].length = fmap->fm_extents[e].fe_length;
+	for (e = 0; e < fmap->fm_mapped_extents; e++) {
+		pexts->extents[e].offset_physical =
+			fmap->fm_extents[e].fe_physical;
+		pexts->extents[e].offset_logical =
+			fmap->fm_extents[e].fe_logical;
+		pexts->extents[e].length =
+			fmap->fm_extents[e].fe_length;
 
 		LOG(10, "   #%u: off_phy: %lu off_log: %lu len: %lu",
 			e,
-			exts->extents[e].offset_physical,
-			exts->extents[e].offset_logical,
-			exts->extents[e].length);
+			pexts->extents[e].offset_physical,
+			pexts->extents[e].offset_logical,
+			pexts->extents[e].length);
 	}
 
-	ret = 0;
+	*exts = pexts;
+
+	Free(fmap);
+
+	return 0;
 
 error_free:
+	if (pexts) {
+		Free(pexts->extents);
+		Free(pexts);
+	}
 	Free(fmap);
 
 	return ret;
+}
+
+/*
+ * pmem2_extents_destroy -- free extents structure
+ */
+void
+pmem2_extents_destroy(struct extents **exts)
+{
+	LOG(3, "extents %p", exts);
+
+	ASSERTne(exts, NULL);
+
+	if (*exts) {
+		Free((*exts)->extents);
+		Free(*exts);
+		*exts = NULL;
+	}
 }

src/libpmem2/extent_none.c
@@ -5,29 +5,27 @@
  * extent_none.c - fake implementation of the FS extent query API
  */
 
+#include "libpmem2.h"
 #include "out.h"
 #include "extent.h"
 
 /*
- * pmem2_extents_count -- get number of extents of the given file
- *                        (and optionally read its block size)
+ * pmem2_extents_create_get -- allocate extents structure and get extents
+ *                             of the given file
  */
-long
-pmem2_extents_count(int fd, struct extents *exts)
+int
+pmem2_extents_create_get(int fd, struct extents **exts)
 {
 	LOG(3, "fd %i extents %p", fd, exts);
 
-	return -1;
+	return PMEM2_E_NOSUPP;
 }
 
 /*
- * pmem2_extents_get -- get extents of the given file
- *                      (and optionally read its block size)
+ * pmem2_extents_destroy -- free extents structure
  */
-int
-pmem2_extents_get(int fd, struct extents *exts)
+void
+pmem2_extents_destroy(struct extents **exts)
 {
-	LOG(3, "fd %i extents %p", fd, exts);
-
-	return -1;
+	LOG(3, "extents %p", exts);
 }

src/test/tools/extents/extents.c
@@ -11,6 +11,7 @@
 #include <fcntl.h>
 #include <errno.h>
 
+#include "libpmem2.h"
 #include "os.h"
 #include "extent.h"
 
@@ -30,6 +31,7 @@ static const char *usage_str =
 int
 main(int argc, char *argv[])
 {
+	struct extents *exts = NULL;
 	long unsigned offset = 0;
 	unsigned extent = 0;
 	char *error;
@@ -80,31 +82,17 @@ main(int argc, char *argv[])
 
 	const char *file = argv[optind];
 
-	struct extents *exts = malloc(sizeof(struct extents));
-	if (exts == NULL)
-		return -1;
-
 	int fd = os_open(file, O_RDONLY);
 	if (fd == -1) {
 		perror(file);
 		goto exit_free;
 	}
 
-	long count = pmem2_extents_count(fd, exts);
-	if (count < 0)
-		goto exit_free;
-
-	if (count == 0) {
-		ret = 0;
-		goto exit_free;
-	}
-
-	exts->extents = malloc(exts->extents_count * sizeof(struct extent));
-	if (exts->extents == NULL)
-		goto exit_free;
-
-	ret = pmem2_extents_get(fd, exts);
+	ret = pmem2_extents_create_get(fd, &exts);
 	if (ret)
+		goto exit_free;
+
+	if (exts->extents_count == 0)
 		goto exit_free;
 
 	switch (mode) {
@@ -152,9 +140,7 @@ main(int argc, char *argv[])
 	}
 
 exit_free:
-	if (exts->extents)
-		free(exts->extents);
-	free(exts);
+	pmem2_extents_destroy(&exts);
 
 	if (fd != -1)
 		close(fd);

src/test/util_extent/util_extent.c
@@ -8,6 +8,7 @@
 
 #include "unittest.h"
 #include "extent.h"
+#include "libpmem2.h"
 
 /*
  * test_size -- test if sum of all file's extents sums up to the file's size
@@ -17,26 +18,17 @@ test_size(int fd, size_t size)
 {
 	size_t total_length = 0;
 
-	struct extents *exts = MALLOC(sizeof(struct extents));
+	struct extents *exts = NULL;
 
-	UT_ASSERT(pmem2_extents_count(fd, exts) >= 0);
-
+	UT_ASSERTeq(pmem2_extents_create_get(fd, &exts), 0);
+	UT_ASSERT(exts->extents_count > 0);
 	UT_OUT("exts->extents_count: %u", exts->extents_count);
 
-	if (exts->extents_count > 0) {
-		exts->extents = MALLOC(exts->extents_count *
-							sizeof(struct extent));
+	unsigned e;
+	for (e = 0; e < exts->extents_count; e++)
+		total_length += exts->extents[e].length;
 
-		UT_ASSERTeq(pmem2_extents_get(fd, exts), 0);
-
-		unsigned e;
-		for (e = 0; e < exts->extents_count; e++)
-			total_length += exts->extents[e].length;
-
-		FREE(exts->extents);
-	}
-
-	FREE(exts);
+	pmem2_extents_destroy(&exts);
 
 	UT_ASSERTeq(total_length, size);
 }

commit fed2aba68cd8cc2419370699343a1b94de66016f

pmem2: fix regression in badblocks_get()
Fix regression in badblocks_get() introduced by the commit 831d125.

Now (b773100) the condition in the line no. 628 looks like:

   if (bb_found > 0) {

Earlier, before the commit 831d125, it looked like:

   if (extents > 0 && bb_found > 0) {

so it was false for DAX devices (which have no extents).

Now the array of bad blocks in a DAX device (bbs) is cleared
and zeroed in the above 'if' condition (lines no. 629-630)
by being assigned to the empty in this case 'bbv' vector.

This patch adds the new 'exit_free_exts' label,
so this 'if' condition is skipped for DAX devices.

src/libpmem2/badblocks_ndctl.c
@@ -552,7 +552,7 @@ badblocks_get(const char *file, struct badblocks *bbs)
 				bbs->bbv[b].length);
 		}
 
-		goto exit_free_all;
+		goto exit_free_exts;
 	}
 
 	bb_found = 0;
@@ -623,8 +623,6 @@ error_free_all:
 	bbs->bb_cnt = 0;
 
 exit_free_all:
-	pmem2_extents_destroy(&exts);
-
 	if (bb_found > 0) {
 		bbs->bbv = VEC_ARR(&bbv);
 		bbs->bb_cnt = (unsigned)VEC_SIZE(&bbv);
@@ -634,6 +632,9 @@ exit_free_all:
 		/* sanity check */
 		ASSERTeq((unsigned)bb_found, bbs->bb_cnt);
 	}
+
+exit_free_exts:
+	pmem2_extents_destroy(&exts);
 
 	if (fd != -1)
 		close(fd);