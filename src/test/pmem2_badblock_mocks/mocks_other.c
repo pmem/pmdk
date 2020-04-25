9dbfbfaafedfd57d7d467e16c3cc7af01c677211
Lukasz Dorau <lukasz.dorau@intel.com>
2020-04-25 23:08:50 +0200

    test: add pmem2_badblock_mocks test

---
+++ src/test/pmem2_badblock_mocks/mocks_other.c
@@ -0,0 +1,134 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* Copyright 2019-2020, Intel Corporation */
+
+/*
+ * mocks_other.c -- mocked various functions
+ *                  used in pmem2_badblock_mocks.c
+ */
+
+#include <sys/stat.h>
+
+#include "unittest.h"
+#include "out.h"
+#include "pmem2_badblock_mocks.h"
+#include "extent.h"
+
+/*
+ * fstat - mock fstat
+ *
+ *        fd == 99 - regular file, did not found any matching device
+ * 100 <= fd < 200 - regular file
+ * 200 <= fd < 300 - character device
+ * 300 <= fd < 400 - directory
+ * 400 <= fd       - block device
+ *
+ * fd % 100 <  50 - namespace mode
+ * fd % 100 >= 50 - region mode
+ */
+FUNC_MOCK(fstat, int, int fd, struct stat *buf)
+FUNC_MOCK_RUN_DEFAULT {
+	ASSERTne(buf, NULL);
+
+	memset(buf, 0, sizeof(struct stat));
+
+	/* default: regular file */
+	buf->st_mode = __S_IFREG;
+
+	/* default block size */
+	buf->st_blksize = BLK_SIZE_1KB;
+
+	/* default: did not found any matching device */
+	buf->st_ino = MODE_NO_DEV;
+
+	if (fd < FD_REG_FILE)
+		/* regular file, did not found any matching device */
+		return 0;
+
+	/*
+	 * st->st_ino ==  0 - did not found any matching device
+	 * st->st_ino <  50 - namespace mode
+	 * st->st_ino >= 50 - region mode
+	 */
+	buf->st_ino = (__ino_t)(fd % MASK_MODE);
+
+	if (fd < FD_CHR_DEV) {
+		/* regular file */
+		return 0;
+	}
+
+	if (fd < FD_DIRECTORY) {
+		/* character device */
+		buf->st_mode = __S_IFCHR;
+		return 0;
+	}
+
+	if (fd < FD_BLK_DEV) {
+		/* directory */
+		buf->st_mode = __S_IFDIR;
+		return 0;
+	}
+
+	/* block device */
+	buf->st_mode = __S_IFBLK;
+
+	return 0;
+}
+FUNC_MOCK_END
+
+/*
+ * os_stat -- mock os_stat
+ */
+FUNC_MOCK(os_stat, int,
+	const char *pathname, os_stat_t *buf)
+FUNC_MOCK_RUN_DEFAULT {
+	int fd = get_fd(pathname);
+	return fstat(fd, buf);
+}
+FUNC_MOCK_END
+
+/*
+ * os_open -- mock os_open
+ */
+FUNC_MOCK(os_open, int,
+	const char *pathname, int flags)
+FUNC_MOCK_RUN_DEFAULT {
+	int fd = get_fd(pathname);
+	if (fd)
+		return fd;
+
+	return _FUNC_REAL(os_open)(pathname, flags);
+}
+FUNC_MOCK_END
+
+/*
+ * pmem2_extents_create_get -- allocate extents structure and get extents
+ *                             of the given file
+ */
+FUNC_MOCK(pmem2_extents_create_get, int,
+		int fd, struct extents **exts)
+FUNC_MOCK_RUN_DEFAULT {
+	return get_extents(fd, exts);
+}
+FUNC_MOCK_END
+
+/*
+ * fallocate -- mock fallocate
+ */
+FUNC_MOCK(fallocate, int,
+		int fd, int mode, __off_t offset, __off_t len)
+FUNC_MOCK_RUN_DEFAULT {
+	UT_OUT("fallocate(%i, %i, %lu, %lu)", fd, mode, offset, len);
+	return 0;
+}
+FUNC_MOCK_END
+
+int fcntl(int fildes, int cmd, ...);
+/*
+ * fcntl -- mock fcntl
+ */
+FUNC_MOCK(fcntl, int,
+		int fildes, int cmd)
+FUNC_MOCK_RUN_DEFAULT {
+	return O_RDWR;
+}
+FUNC_MOCK_END
