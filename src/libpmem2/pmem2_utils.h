04b111477159db63294ef45ae32a28009062c6e1
Adam Borowski <kilobyte@angband.pl>
2020-04-30 13:17:28 +0200

    pmem2: whe returning PMEM2_E_ERRNO, ensure errno is set

--- src/libpmem2/pmem2_utils.h
+++ src/libpmem2/pmem2_utils.h
@@ -11,8 +11,20 @@
 #include <errno.h>
 
 #include "os.h"
+#include "out.h"
 
-#define PMEM2_E_ERRNO (-errno)
+static inline int pmem2_assert_errno()
+{
+	if (!errno) {
+		ERR("errno unset");
+		ASSERTinfo(0, "errno unset");
+		return -EINVAL;
+	}
+
+	return -errno;
+}
+
+#define PMEM2_E_ERRNO (pmem2_assert_errno())
 
 void *pmem2_malloc(size_t size, int *err);
 void *pmem2_zalloc(size_t size, int *err);
