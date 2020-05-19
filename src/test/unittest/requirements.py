e69ae0af240b7c9a6a8987ce3fe046cde48a8dab
Lukasz Dorau <lukasz.dorau@intel.com>
2020-05-19 10:19:15 +0200

    test: add the Requirements class for checking various requirements

---
+++ src/test/unittest/requirements.py
@@ -0,0 +1,64 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright 2020, Intel Corporation
+#
+"""Various requirements"""
+
+
+import subprocess as sp
+import os
+
+import configurator as conf
+import context as ctx
+import futils
+
+
+NDCTL_MIN_VERSION = '63'
+
+
+class Requirements:
+    """Various requirements"""
+    def __init__(self):
+        config = conf.Configurator().config
+        self.msg = futils.Message(config.unittest_log_level)
+
+    def _check_pkgconfig(self, pkg, min_ver):
+        """
+        Run pkg-config to check if a package is installed
+        """
+        proc = sp.run(['pkg-config', pkg, '--atleast-version', min_ver],
+                      stdout=sp.PIPE, stderr=sp.STDOUT)
+        return proc.returncode == 0
+
+    def _check_ndctl_req_is_met(self, tc):
+        """
+        Check if all conditions for the ndctl requirement are met
+        """
+        pv = self.msg.print_verbose
+        require_ndctl, _ = ctx.get_requirement(tc, 'require_ndctl', ())
+        if not require_ndctl:
+            return True
+
+        ndctl_enable = os.environ.get('NDCTL_ENABLE')
+        if ndctl_enable == 'n':
+            pv('{}: SKIP: libndctl is disabled (NDCTL_ENABLE == \'n\')'
+               .format(tc))
+            return False
+
+        is_ndctl = self._check_pkgconfig('libndctl', NDCTL_MIN_VERSION)
+        if not is_ndctl:
+            pv('{}: SKIP: libndctl (>=v{}) is not installed'
+               .format(tc, NDCTL_MIN_VERSION))
+            return False
+        return True
+
+    def check_if_all_requirements_are_met(self, tc):
+        if not self._check_ndctl_req_is_met(tc):
+            return False
+        """More requirements can be checked here"""
+        return True
+
+
+def require_ndctl(tc):
+    """Enable test only if ndctl is installed"""
+    ctx.add_requirement(tc, 'require_ndctl', True)
+    return tc
