affde67c3ee94d8ec4ffa0afed2c232373207eb0
Krzysztof Swiecicki <krzysztof.swiecicki@intel.com>
2020-09-29 10:37:44 +0200

    set: setup simple build for libpmemset

---
+++ src/include/libpmemset.h
@@ -0,0 +1,20 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/* Copyright 2020, Intel Corporation */
+
+/*
+ * libpmemset.h -- definitions of libpmemset entry points (EXPERIMENTAL)
+ *
+ * See libpmemset(7) for details.
+ */
+
+#ifndef LIBPMEMSET_H
+#define LIBPMEMSET_H 1
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+#endif	/* libpmemset.h */
---
+++ src/libpmemset/pmemset.h
@@ -0,0 +1,27 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/* Copyright 2020, Intel Corporation */
+
+/*
+ * pmemset.h -- internal definitions for libpmemset
+ */
+#ifndef PMEMSET_H
+#define PMEMSET_H
+
+#include "libpmemset.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define PMEMSET_MAJOR_VERSION 0
+#define PMEMSET_MINOR_VERSION 0
+
+#define PMEMSET_LOG_PREFIX "libpmemset"
+#define PMEMSET_LOG_LEVEL_VAR "PMEMSET_LOG_LEVEL"
+#define PMEMSET_LOG_FILE_VAR "PMEMSET_LOG_FILE"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
