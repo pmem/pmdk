5b70cb6d850d78ffdc7ef43c0b454b81c2a1ebee
Weronika Lewandowska <weronika.lewandowska@intel.com>
2021-03-31 12:44:27 +0200

    set: merge pmemset part and add map config

--- src/test/pmemset_part/TESTS.py
+++ src/test/pmemset_part/TESTS.py
@@ -38,108 +38,87 @@ class PMEMSET_PART_ASYNC(t.Test):
 
 
 class TEST0(PMEMSET_PART):
-    """test pmemset_part allocation with error injection"""
-    test_case = "test_part_new_enomem"
-
-
-class TEST1(PMEMSET_PART):
-    """create a new part from a source with invalid path assigned"""
-    test_case = "test_part_new_invalid_source_file"
-    create_file = False
-
-
-class TEST2(PMEMSET_PART):
-    """create a new part from a source with valid path assigned"""
-    test_case = "test_part_new_valid_source_file"
-
-
-class TEST3(PMEMSET_PART):
-    """create a new part from a source with valid pmem2_source assigned"""
-    test_case = "test_part_new_valid_source_pmem2"
-
-
-class TEST4(PMEMSET_PART):
     """create a new part from a source with valid pmem2_source and map it"""
     test_case = "test_part_map_valid_source_pmem2"
 
 
-class TEST5(PMEMSET_PART):
+class TEST1(PMEMSET_PART):
     """create a new part from a source with valid file path and map it"""
     test_case = "test_part_map_valid_source_file"
 
 
-class TEST6(PMEMSET_PART):
+class TEST2(PMEMSET_PART):
     """test pmemset_part_map with invalid offset"""
     test_case = "test_part_map_invalid_offset"
 
 
 @t.windows_exclude
 @t.require_valgrind_enabled('memcheck')
-class TEST7(PMEMSET_PART):
+class TEST3(PMEMSET_PART):
     """create a new part from a source with valid file path and map it"""
     test_case = "test_part_map_valid_source_file"
 
 
 @t.windows_exclude
 @t.require_valgrind_enabled('memcheck')
-class TEST8(PMEMSET_PART):
+class TEST4(PMEMSET_PART):
     """create a new part from a source with valid pmem2 source and map it"""
     test_case = "test_part_map_valid_source_pmem2"
 
 
-class TEST9(PMEMSET_PART):
+class TEST5(PMEMSET_PART):
     """test if data is unavailable after pmemset_delete"""
     test_case = "test_unmap_part"
 
 
-class TEST10(PMEMSET_PART):
+class TEST6(PMEMSET_PART):
     """test pmemset_part_map allocation with error injection"""
     test_case = "test_part_map_enomem"
 
 
-class TEST11(PMEMSET_PART):
+class TEST7(PMEMSET_PART):
     """get the first (earliest in the memory) mapping from the set"""
     test_case = "test_part_map_first"
 
 
-class TEST12(PMEMSET_PART):
+class TEST8(PMEMSET_PART):
     """get the descriptor from the part map"""
     test_case = "test_part_map_descriptor"
 
 
-class TEST13(PMEMSET_PART):
+class TEST9(PMEMSET_PART):
     """test retrieving next mapping from the set"""
     test_case = "test_part_map_next"
 
 
-class TEST14(PMEMSET_PART):
+class TEST10(PMEMSET_PART):
     """test pmemset_get_store_granularity function"""
     test_case = "test_part_map_gran_read"
 
 
-class TEST15(PMEMSET_PART):
+class TEST11(PMEMSET_PART):
     """test pmemset_part_map_drop function"""
     test_case = "test_part_map_drop"
 
 
-class TEST16(PMEMSET_PART):
+class TEST12(PMEMSET_PART):
     """test reading part mapping by address"""
     test_case = "test_part_map_by_addr"
 
 
-class TEST17(PMEMSET_PART):
+class TEST13(PMEMSET_PART):
     """test creating a new part from file with unaligned size"""
     test_case = "test_part_map_unaligned_size"
     file_size = 16 * t.MiB - 1
 
 
-class TEST18(PMEMSET_PART):
+class TEST14(PMEMSET_PART):
     """turn on full coalescing feature then create two mappings"""
     test_case = "test_part_map_full_coalesce_before"
     file_size = 64 * t.KiB
 
 
-class TEST19(PMEMSET_PART):
+class TEST15(PMEMSET_PART):
     """
     map a part, turn on the full coalescing feature and map a part second time
     """
@@ -147,13 +126,13 @@ class TEST19(PMEMSET_PART):
     file_size = 64 * t.KiB
 
 
-class TEST20(PMEMSET_PART):
+class TEST16(PMEMSET_PART):
     """turn on opportunistic coalescing feature then create two mappings"""
     test_case = "test_part_map_opp_coalesce_before"
     file_size = 64 * t.KiB
 
 
-class TEST21(PMEMSET_PART):
+class TEST17(PMEMSET_PART):
     """
     map a part, turn on the opportunistic coalescing feature and
     map a part second time
@@ -162,7 +141,7 @@ class TEST21(PMEMSET_PART):
     file_size = 64 * t.KiB
 
 
-class TEST22(PMEMSET_PART):
+class TEST18(PMEMSET_PART):
     """
     map two parts to the pmemset, iterate over
     the set to find them, then remove them
@@ -170,7 +149,7 @@ class TEST22(PMEMSET_PART):
     test_case = "test_remove_part_map"
 
 
-class TEST23(PMEMSET_PART):
+class TEST19(PMEMSET_PART):
     """
     Enable the part coalescing feature, map two parts to the pmemset.
     If no error returned those parts should appear as single part mapping.
@@ -179,7 +158,7 @@ class TEST23(PMEMSET_PART):
     test_case = "test_full_coalescing_before_remove_part_map"
 
 
-class TEST24(PMEMSET_PART):
+class TEST20(PMEMSET_PART):
     """
     Map two parts to the pmemset, iterate over the set to find first mapping
     and delete it. Turn on part coalescing and map new part. Lastly iterate
@@ -188,7 +167,7 @@ class TEST24(PMEMSET_PART):
     test_case = "test_full_coalescing_after_remove_first_part_map"
 
 
-class TEST25(PMEMSET_PART):
+class TEST21(PMEMSET_PART):
     """
     Map two parts to the pmemset, iterate over the set to find second mapping
     and delete it. Turn on part coalescing and map new part. Lastly iterate
@@ -197,7 +176,7 @@ class TEST25(PMEMSET_PART):
     test_case = "test_full_coalescing_after_remove_second_part_map"
 
 
-class TEST26(PMEMSET_PART):
+class TEST22(PMEMSET_PART):
     """
     Map hundred parts to the pmemset, iterate over the set to find the middle
     part mapping and delete it, repeat the process until there is not mappings
