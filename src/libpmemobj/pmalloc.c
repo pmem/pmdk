341bdaece89bff27eb963383fb2d1aef0b97eb87
Piotr Balcer <piotr.balcer@intel.com>
2021-05-18 16:31:46 +0200

    obj: refactor bucket abstraction
    This patch cleans up the previously leaky bucket
    abstraction layer in preparation for heap segmentation.
    
    Proper bucket locking is now enforced at the type level,
    so it's easier to use and modify.

--- src/libpmemobj/pmalloc.c
+++ src/libpmemobj/pmalloc.c
@@ -10,6 +10,7 @@
  */
 
 #include <inttypes.h>
+#include "bucket.h"
 #include "ctl.h"
 #include "libpmemobj/ctl.h"
 #include "valgrind_internal.h"
@@ -716,14 +717,20 @@ CTL_READ_HANDLER(size)(void *ctx,
 	}
 
 	/* take buckets for arena */
-	struct bucket **buckets;
+	struct bucket_locked **buckets;
 	buckets = heap_get_arena_buckets(&pop->heap, arena_id);
 
 	/* calculate number of reservation for arena using buckets */
 	unsigned size = 0;
 	for (int i = 0; i < MAX_ALLOCATION_CLASSES; ++i) {
-		if (buckets[i] != NULL && buckets[i]->is_active)
-			size += buckets[i]->active_memory_block->m.size_idx;
+		if (buckets[i] != NULL) {
+			struct bucket *b = bucket_acquire(buckets[i]);
+			struct memory_block_reserved *active =
+				bucket_active_block(b);
+
+			size += active ? active->m.size_idx : 0;
+			bucket_release(b);
+		}
 	}
 
 	*arena_size = size * CHUNKSIZE;
