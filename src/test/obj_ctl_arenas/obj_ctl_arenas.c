4a1c40e0a76ee16a68973b3b4e379d9084897290
Piotr Balcer <piotr@balcer.eu>
2021-08-06 15:24:23 +0200

    obj: add heap.arenas_default_max global ctl
    This allows applications to reduce or increase the number
    of arenas that are created on startup.

--- src/test/obj_ctl_arenas/obj_ctl_arenas.c
+++ src/test/obj_ctl_arenas/obj_ctl_arenas.c
@@ -256,7 +256,7 @@ main(int argc, char *argv[])
 	START(argc, argv, "obj_ctl_arenas");
 
 	if (argc != 3)
-		UT_FATAL("usage: %s poolset [n|s|c|f|q|m|a|g|p|d]", argv[0]);
+		UT_FATAL("usage: %s poolset [n|s|c|f|q|m|a|g|p|d|b]", argv[0]);
 
 	const char *path = argv[1];
 	char t = argv[2][0];
@@ -264,6 +264,7 @@ main(int argc, char *argv[])
 	int ret = 0;
 
 	int keys_created = 0;
+	size_t target_narenas = 0;
 	if (t == 'g') {
 		os_tls_key_t *keys = MALLOC(sizeof(os_tls_key_t) * MAX_KEYS);
 		int i;
@@ -290,6 +291,17 @@ main(int argc, char *argv[])
 
 		ret = pmemobj_ctl_set(NULL,
 			"heap.arenas_assignment_type", &atype);
+		UT_ASSERTeq(ret, 0);
+	} else if (t == 'b') {
+		size_t narenas = 0;
+		ret = pmemobj_ctl_get(pop,
+			"heap.arenas_default_max", &narenas);
+		UT_ASSERTeq(ret, 0);
+		UT_ASSERTne(narenas, 0);
+
+		target_narenas = narenas + 1;
+		ret = pmemobj_ctl_set(pop,
+			"heap.arenas_default_max", &target_narenas);
 		UT_ASSERTeq(ret, 0);
 	}
 
@@ -529,6 +541,11 @@ main(int argc, char *argv[])
 			"heap.arenas_assignment_type", &atype);
 		UT_ASSERTeq(ret, 0);
 		UT_ASSERTeq(atype, POBJ_ARENAS_ASSIGNMENT_THREAD_KEY);
+	} else if (t == 'b') {
+		unsigned narenas = 0;
+		ret = pmemobj_ctl_get(pop, "heap.narenas.total", &narenas);
+		UT_ASSERTeq(ret, 0);
+		UT_ASSERTeq(narenas, target_narenas);
 	} else {
 		UT_ASSERT(0);
 	}
