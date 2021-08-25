27730bb4c917072e4239fbd36741e165640304ba
Krzysztof Swiecicki <krzysztof.swiecicki@intel.com>
2021-08-25 11:15:59 -0400

    pmem2: add machine safe read/write source operations

--- src/test/pmem2_badblock/pmem2_badblock.c
+++ src/test/pmem2_badblock/pmem2_badblock.c
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BSD-3-Clause
-/* Copyright 2020, Intel Corporation */
+/* Copyright 2021, Intel Corporation */
 
 /*
  * pmem2_badblock.c -- pmem2 bad block tests
@@ -9,12 +9,13 @@
 #include "unittest.h"
 #include "ut_pmem2.h"
 
+#include <errno.h>
+
 /*
  * test_pmem2_badblock_count -- counts the number of bb in the file
  */
 static int
-test_pmem2_badblock_count(const struct test_case *tc, int argc,
-					char *argv[])
+test_pmem2_badblock_count(const struct test_case *tc, int argc, char *argv[])
 {
 	if (argc < 1)
 		UT_FATAL(
@@ -36,9 +37,69 @@ test_pmem2_badblock_count(const struct test_case *tc, int argc,
 	while ((ret = pmem2_badblock_next(bbctx, &bb)) == 0)
 		++count;
 
-	UT_OUT("BB: %d\n", count);
+	printf("BB: %d\n", count);
 
 	pmem2_badblock_context_delete(&bbctx);
+
+	pmem2_source_delete(&src);
+	CLOSE(fd);
+
+	return 1;
+}
+
+/*
+ * test_pmem2_src_mcsafe_badblock_read -- test mcsafe read operation with
+ *                                        encountered badblock
+ */
+static int
+test_pmem2_src_mcsafe_badblock_read(const struct test_case *tc, int argc,
+		char *argv[])
+{
+	if (argc < 1)
+		UT_FATAL(
+			"usage: test_pmem2_src_mcsafe_badblock_read <file>");
+
+	char *file = argv[0];
+	int fd = OPEN(file, O_RDWR);
+
+	struct pmem2_source *src;
+	int ret = pmem2_source_from_fd(&src, fd);
+	UT_ASSERTeq(ret, 0);
+
+	size_t bufsize = 4096;
+	void *buf = MALLOC(bufsize);
+	ret = pmem2_source_pread_mcsafe(src, buf, bufsize, 0);
+	UT_PMEM2_EXPECT_RETURN(ret, PMEM2_E_IO_FAIL);
+
+	pmem2_source_delete(&src);
+	CLOSE(fd);
+
+	return 1;
+}
+
+/*
+ * test_pmem2_src_mcsafe_badblock_write -- test mcsafe write operation with
+ *                                         encountered badblock
+ */
+static int
+test_pmem2_src_mcsafe_badblock_write(const struct test_case *tc, int argc,
+		char *argv[])
+{
+	if (argc < 1)
+		UT_FATAL(
+			"usage: test_pmem2_src_mcsafe_badblock_write <file>");
+
+	char *file = argv[0];
+	int fd = OPEN(file, O_RDWR);
+
+	struct pmem2_source *src;
+	int ret = pmem2_source_from_fd(&src, fd);
+	UT_ASSERTeq(ret, 0);
+
+	size_t bufsize = 4096;
+	void *buf = MALLOC(bufsize);
+	ret = pmem2_source_pwrite_mcsafe(src, buf, bufsize, 0);
+	UT_PMEM2_EXPECT_RETURN(ret, PMEM2_E_IO_FAIL);
 
 	pmem2_source_delete(&src);
 	CLOSE(fd);
@@ -51,6 +112,8 @@ test_pmem2_badblock_count(const struct test_case *tc, int argc,
  */
 static struct test_case test_cases[] = {
 	TEST_CASE(test_pmem2_badblock_count),
+	TEST_CASE(test_pmem2_src_mcsafe_badblock_read),
+	TEST_CASE(test_pmem2_src_mcsafe_badblock_write),
 };
 
 #define NTESTS (sizeof(test_cases) / sizeof(test_cases[0]))
