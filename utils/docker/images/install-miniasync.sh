8e3599c53a8fcc1a086342c40334ac233fa933c2
Łukasz Plewa <lukasz.plewa@intel.com>
2021-12-01 17:23:32 +0100

    common: add miniasync to docker files

---
+++ utils/docker/images/install-miniasync.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright 2021, Intel Corporation
+
+#
+# install-miniasync.sh - installs miniasync library
+#
+
+set -e
+
+git clone https://github.com/pmem/miniasync.git
+cd miniasync
+
+# XXX: as miniasync in activly in development,
+# we need to consider better solution
+# valgrind v3.17 with pmemcheck
+# 2021.12.1 Merge pull request #20 from pmem/coverity-badge
+git checkout 2453713fd223c154dff205a19aede6699364d222
+mkdir build && cd build
+
+cmake ..
+make -j$(nproc)
+make -j$(nproc) install
+
+cd ../..
+rm -rf ./miniasync
