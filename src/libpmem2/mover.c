f19ac428e01c8ac324e5a4e37c5b8fff387faae5
?ukasz Plewa <lukasz.plewa@intel.com>
2021-12-16 18:37:52 +0100

    pmem2: add asynchronous memcpy

---
+++ src/libpmem2/mover.c
@@ -0,0 +1,149 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* Copyright 2021, Intel Corporation */
+
+/*
+ * mover.c -- default pmem2 data mover
+ */
+
+#include "libpmem2.h"
+#include "mover.h"
+#include "map.h"
+#include "out.h"
+#include "pmem2_utils.h"
+#include "util.h"
+#include <inttypes.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+struct data_mover {
+	struct vdm base; /* must be first */
+	struct pmem2_map *map;
+};
+
+struct data_mover_op {
+	struct vdm_operation op;
+	struct data_mover *mover; /* xxx: shoud be keept in vdm_operation ? */
+	int complete;
+};
+
+/*
+ * sync_operation_check -- always returns COMPLETE because sync mover operations
+ * are complete immediately after starting.
+ */
+static enum future_state
+sync_operation_check(void *op)
+{
+	LOG(3, "op %p", op);
+	struct data_mover_op *sync_op = op;
+
+	int complete;
+	util_atomic_load_explicit32(&sync_op->complete, &complete,
+		memory_order_acquire);
+
+	return complete ? FUTURE_STATE_COMPLETE : FUTURE_STATE_IDLE;
+}
+
+/*
+ * sync_operation_new -- creates a new sync operation
+ */
+static void *
+sync_operation_new(struct vdm *vdm, const struct vdm_operation *operation)
+{
+	LOG(3, "vdm %p, operation %p", vdm, operation);
+	struct data_mover *vdm_sync = (struct data_mover *)vdm;
+	/* XXX: backport membuf from miniasync */
+	struct data_mover_op *sync_op = malloc(sizeof(*sync_op));
+	if (sync_op == NULL)
+		return NULL;
+
+	sync_op->complete = 0;
+	sync_op->mover = vdm_sync;
+	sync_op->op = *operation;
+
+	return sync_op;
+}
+
+/*
+ * sync_operation_delete -- deletes sync operation
+ */
+static void
+sync_operation_delete(void *op, struct vdm_operation_output *output)
+{
+	LOG(3, "op %p, output %p", op, output);
+	struct data_mover_op *sync_op = (struct data_mover_op *)op;
+	switch (sync_op->op.type) {
+		case VDM_OPERATION_MEMCPY:
+			output->type = VDM_OPERATION_MEMCPY;
+			output->memcpy.dest = sync_op->op.memcpy.dest;
+			break;
+		default:
+			FATAL("unsupported operation type");
+	}
+	free(sync_op);
+}
+
+/*
+ * sync_operation_start -- start (and perform) a synchronous memory operation
+ */
+static int
+sync_operation_start(void *op, struct future_notifier *n)
+{
+	LOG(3, "op %p, notifier %p", op, n);
+	struct data_mover_op *sync_op = (struct data_mover_op *)op;
+	if (n)
+		n->notifier_used = FUTURE_NOTIFIER_NONE;
+
+	pmem2_memcpy_fn memcpy_fn = pmem2_get_memcpy_fn(sync_op->mover->map);
+
+	memcpy_fn(sync_op->op.memcpy.dest, sync_op->op.memcpy.src,
+		sync_op->op.memcpy.n, PMEM2_F_MEM_NONTEMPORAL);
+
+	util_atomic_store_explicit32(&sync_op->complete,
+		1, memory_order_release);
+
+	return 0;
+}
+
+static struct vdm data_mover_vdm = {
+	.op_new = sync_operation_new,
+	.op_delete = sync_operation_delete,
+	.op_check = sync_operation_check,
+	.op_start = sync_operation_start,
+};
+
+/*
+ * mover_new -- creates a new synchronous data mover
+ */
+int
+mover_new(struct pmem2_map *map, struct vdm **vdm)
+{
+	LOG(3, "map %p, vdm %p", map, vdm);
+	int ret;
+	struct data_mover *dms = pmem2_malloc(sizeof(*map), &ret);
+	if (dms == NULL)
+		return ret;
+
+	dms->base = data_mover_vdm;
+	dms->map = map;
+	*vdm = (struct vdm *)dms;
+	return 0;
+}
+
+/*
+ * mover_delete -- deletes a synchronous data mover
+ */
+void
+mover_delete(struct vdm *dms)
+{
+	free((struct data_mover *)dms);
+}
+
+struct vdm_operation_future
+pmem2_memcpy_async(struct pmem2_map *map,
+	void *pmemdest,	const void *src, size_t len, unsigned flags)
+{
+	LOG(3, "map %p, pmemdest %p, src %p, len %" PRIu64 ", flags %u",
+		map, pmemdest, src, len, flags);
+	SUPPRESS_UNUSED(flags);
+	return vdm_memcpy(map->vdm, pmemdest, (void *)src, len, 0);
+}
---
+++ src/test/pmem2_mover/pmem2_mover.c
@@ -0,0 +1,80 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* Copyright 2019-2022, Intel Corporation */
+
+/*
+ * pmem2_mover.c -- pmem2 mover tests
+ */
+
+#define PMEM2_USE_MINIASYNC 1
+#include "libpmem2.h"
+#include "unittest.h"
+#include "ut_pmem2.h"
+#include "ut_pmem2_setup_integration.h"
+
+/*
+ * map_valid -- return valid mapped pmem2_map
+ */
+static struct pmem2_map *
+map_valid(struct pmem2_config *cfg, struct pmem2_source *src)
+{
+	struct pmem2_map *map = NULL;
+	int ret = pmem2_map_new(&map, cfg, src);
+	UT_PMEM2_EXPECT_RETURN(ret, 0);
+	UT_ASSERTne(map, NULL);
+
+	return map;
+}
+
+/*
+ * test_mover_basic -- test basic functionality of pmem2 default mover
+ */
+static int
+test_mover_basic(const struct test_case *tc, int argc, char *argv[])
+{
+	if (argc < 1)
+		UT_FATAL("usage: test_mover_basic <file>");
+
+	char *file = argv[0];
+	int fd = OPEN(file, O_RDWR);
+
+	struct pmem2_source *src;
+	struct pmem2_config *cfg;
+	PMEM2_PREPARE_CONFIG_INTEGRATION(&cfg, &src, fd,
+		PMEM2_GRANULARITY_PAGE);
+
+	struct pmem2_map *map = map_valid(cfg, src);
+	char *data = pmem2_map_get_address(map);
+	pmem2_memset_fn memset_fn = pmem2_get_memset_fn(map);
+	memset_fn(data, 0xBA, 4096, 0);
+	memset_fn(data + 4096, 0xAB, 4096, 0);
+	struct vdm_operation_future cpy =
+		pmem2_memcpy_async(map, data, data + 4096, 4096, 0);
+
+	FUTURE_BUSY_POLL(&cpy);
+
+	if (memcmp(data, data + 4096, 4096))
+		UT_FATAL("data should be equal");
+
+	pmem2_map_delete(&map);
+	pmem2_config_delete(&cfg);
+	pmem2_source_delete(&src);
+	CLOSE(fd);
+	return 1;
+}
+
+/*
+ * test_cases -- available test cases
+ */
+static struct test_case test_cases[] = {
+	TEST_CASE(test_mover_basic)
+};
+
+#define NTESTS (sizeof(test_cases) / sizeof(test_cases[0]))
+
+int
+main(int argc, char *argv[])
+{
+	START(argc, argv, "pmem2_mover");
+	TEST_CASE_PROCESS(argc, argv, test_cases, NTESTS);
+	DONE(NULL);
+}
