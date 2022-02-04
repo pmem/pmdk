efc4877a10d1ba3ad322599c236135f3d7d8f1fe
Mike Papinski <48183086+MikePapinski@users.noreply.github.com>
2022-02-04 14:01:35 +0100

    common: add GHA unittest for self-hosted runners

---
+++ utils/gha-runners/build-pmdk.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright 2022, Intel Corporation
+
+#
+# build-pmdk.sh - Script for building pmdk project
+#
+
+FULL_PATH=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
+PMDK_PATH="${FULL_PATH}/../.."
+
+set -o pipefail
+
+#
+# build_pmdk -- build pmdk from source
+#
+function build_pmdk {
+	echo "********** make pmdk **********"
+	cd ${PMDK_PATH} && make clean
+	cd ${PMDK_PATH} && make EXTRA_CFLAGS=-DUSE_VALGRIND -j$(nproc)
+	echo "********** make pmdk test **********"
+	cd ${PMDK_PATH}/ && make test
+}
+
+build_pmdk
---
+++ utils/gha-runners/get-system-info.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright 2022, Intel Corporation
+
+#
+# get-system-info.sh - Script for printing system info
+#
+
+function system_info {
+	echo "********** system_info **********"
+	cat /etc/os-release | grep -oP "PRETTY_NAME=\K.*"
+	uname -r
+	echo "libndctl: $(pkg-config --modversion libndctl || echo 'libndctl not found')"
+	echo "valgrind: $(pkg-config --modversion valgrind || echo 'valgrind not found')"
+	echo "******************** memory-info *******************"
+	sudo ipmctl show -dimm || true
+	sudo ipmctl show -topology || true
+	echo "*************** list-existing-namespaces ***************"
+	sudo ndctl list -M -N
+	echo "*************** installed-packages ***************"
+	# Instructions below will return some minor errors, as they are dependent on the Linux distribution.
+	zypper se --installed-only 2>/dev/null || true
+	apt list --installed 2>/dev/null || true
+	yum list installed 2>/dev/null || true
+	echo "**********/proc/cmdline**********"
+	cat /proc/cmdline
+	echo "**********/proc/modules**********"
+	cat /proc/modules
+	echo "**********/proc/cpuinfo**********"
+	cat /proc/cpuinfo
+	echo "**********/proc/meminfo**********"
+	cat /proc/meminfo
+	echo "**********/proc/swaps**********"
+	cat /proc/swaps
+	echo "**********/proc/version**********"
+	cat /proc/version
+	echo "**********check-updates**********"
+	# Instructions below will return some minor errors, as they are dependent on the Linux distribution.
+	sudo zypper list-updates 2>/dev/null || true
+	sudo apt-get update 2>/dev/null || true
+	sudo apt upgrade --dry-run 2>/dev/null || true
+	sudo dnf check-update 2>/dev/null || true
+	echo "**********list-enviroment**********"
+	env
+}
