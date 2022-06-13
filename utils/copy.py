3f062adc58d15bfa6a316fbd8b9c19fb13820d23
Krzysztof Swiecicki <krzysztof.swiecicki@intel.com>
2022-06-13 12:03:58 +0200

    common: add copy script

---
+++ utils/copy.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import os, sys, shutil
+
+copy_prefixes = sys.argv[3]
+copy_suffixes = sys.argv[4]
+
+# get absolute input and output paths
+input_path = os.path.join(
+    os.getenv('MESON_SOURCE_ROOT'),
+    os.getenv('MESON_SUBDIR'),
+    sys.argv[1])
+
+output_path = os.path.join(
+    os.getenv('MESON_BUILD_ROOT'),
+    os.getenv('MESON_SUBDIR'),
+    sys.argv[2])
+
+# make sure destination directory exists
+os.makedirs(output_path, exist_ok=True)
+
+# copy files with matching prefixes or suffixes
+for file in os.listdir(input_path):
+    if file.startswith(copy_prefixes) or file.endswith(copy_suffixes):
+        shutil.copyfile(os.path.join(input_path, file), os.path.join(output_path, file))
