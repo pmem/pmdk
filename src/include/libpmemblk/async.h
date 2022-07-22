b245ef050ee6009518930c0bca2b9444245b1fe7
blazej-smorawski <blazej.smorawki@gmail.com>
2022-07-22 14:36:18 +0200

    blk: merge

--- src/include/libpmemblk/async.h
+++ src/include/libpmemblk/async.h
@@ -75,27 +75,33 @@ struct pmemblk_read_async_future pmemblk_read_async(PMEMblkpool *pbp, void *buf,
 /* END of pmemblk_read_async future */
 
 /* START of pmemblk_write_async future */
-struct pmemblk_write_async_future_data {
+enum pmemblk_write_stages{
+	PMEMBLK_WRITE_INITIALIZED = 0,
+	PMEMBLK_WRITE_WAITING_FOR_LANE = 1,
+	PMEMBLK_WRITE_IN_PROGRESS = 2,
+	PMEMBLK_WRITE_COMPLETE = 20,
+};
+struct pmemblk_write_async_data {
 	PMEMblkpool *pbp;
 	void *buf;
 	long long blockno;
 
+	int stage;
 	struct {
 		struct btt_write_async_future btt_write_fut;
-		int btt_write_started;
 		unsigned lane;
 	} internal;
 };
 
-struct pmemblk_write_async_future_output {
+struct pmemblk_write_async_output {
 	int return_value;
 };
 
-FUTURE(pmemblk_write_async_future, struct pmemblk_write_async_future_data,
-		struct pmemblk_write_async_future_output);
+FUTURE(pmemblk_write_async_fut, struct pmemblk_write_async_data,
+		struct pmemblk_write_async_output);
 
-struct pmemblk_write_async_future pmemblk_write_async(PMEMblkpool *pbp,
-		void *buf, long long blockno);
+struct pmemblk_write_async_fut pmemblk_write_async(PMEMblkpool *pbp, void *buf,
+		long long blockno);
 /* END of pmemblk_write_async future */
 #endif
 
