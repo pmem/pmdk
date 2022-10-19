2a70b3117e71ef626fbf957c254ff3e5d150f96d
Weronika Lewandowska <weronika.lewandowska@intel.com>
2022-10-19 14:03:18 +0200

    pmem2: suppress unused params

--- src/libpmem2/usc_none.c
+++ src/libpmem2/usc_none.c
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BSD-3-Clause
-/* Copyright 2020, Intel Corporation */
+/* Copyright 2020-2022, Intel Corporation */
 
 /*
  * usc_none.c -- pmem2 usc function for non supported platform
@@ -11,13 +11,19 @@
 int
 pmem2_source_device_id(const struct pmem2_source *src, char *id, size_t *len)
 {
+	SUPPRESS_UNUSED(src, id, len);
+
 	ERR("Cannot read device id - ndctl is not available");
+
 	return PMEM2_E_NOSUPP;
 }
 
 int
 pmem2_source_device_usc(const struct pmem2_source *src, uint64_t *usc)
 {
+	SUPPRESS_UNUSED(src, usc);
+
 	ERR("Cannot read device usc - ndctl is not available");
+
 	return PMEM2_E_NOSUPP;
 }
