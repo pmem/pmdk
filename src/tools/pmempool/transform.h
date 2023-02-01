3a86e7763997167035483877ce2bb65249cbf6f2
osalyk <oksana.salyk@intel.com>
2023-02-01 07:29:51 -0500

    common: remove windows from tools

--- src/tools/pmempool/transform.h
+++ src/tools/pmempool/transform.h
@@ -5,16 +5,5 @@
  * transform.h -- pmempool transform command header file
  */
 
-#ifdef _WIN32
-#define WIN_DEPR_STR "Windows support is deprecated."
-#define WIN_DEPR_ATTR __declspec(deprecated(WIN_DEPR_STR))
-#endif
-
-#ifdef _WIN32
-WIN_DEPR_ATTR
-#endif
 int pmempool_transform_func(const char *appname, int argc, char *argv[]);
-#ifdef _WIN32
-WIN_DEPR_ATTR
-#endif
 void pmempool_transform_help(const char *appname);
