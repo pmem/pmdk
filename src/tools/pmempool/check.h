261e7c30148a703cac4d40e813fa80e315c03f68
osalyk <oksana.salyk@intel.com>
2023-02-01 07:29:51 -0500

    common: remove windows from tools

--- src/tools/pmempool/check.h
+++ src/tools/pmempool/check.h
@@ -5,16 +5,5 @@
  * check.h -- pmempool check command header file
  */
 
-#ifdef _WIN32
-#define WIN_DEPR_STR "Windows support is deprecated."
-#define WIN_DEPR_ATTR __declspec(deprecated(WIN_DEPR_STR))
-#endif
-
-#ifdef _WIN32
-WIN_DEPR_ATTR
-#endif
 int pmempool_check_func(const char *appname, int argc, char *argv[]);
-#ifdef _WIN32
-WIN_DEPR_ATTR
-#endif
 void pmempool_check_help(const char *appname);
