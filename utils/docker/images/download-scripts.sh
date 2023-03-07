8fc45aba071df85b677311cf5ec02b1ae7282e01
Łukasz Stolarczuk <lukasz.stolarczuk@intel.com>
2023-03-07 17:07:57 +0100

    common: move on to the new coverage uploader
    the old, bash uploader is long EOL now.
    
    Downloading and integrity check of the uploader script is done
    in docker building process (on the latest Ubuntu LTS).

--- utils/docker/images/download-scripts.sh
+++ utils/docker/images/download-scripts.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright 2020, Intel Corporation
+# Copyright 2020-2023, Intel Corporation
 
 #
-# download-scripts.sh - downloads specific version of codecov's bash
-#		script to generate and upload reports. It's useful,
-#		since unverified version may break coverage results.
+# download-scripts.sh - downloads latest version of
+#			codecov's uploader to generate and upload reports.
 #
 
 set -e
-
-# master: Merge pull request #342 from codecov/revert-proj-name-..., 18.08.2020
-CODECOV_VERSION="e877c1280cc6e902101fb5df2981ed1c962da7f0"
 
 if [ "${SKIP_SCRIPTS_DOWNLOAD}" ]; then
 	echo "Variable 'SKIP_SCRIPTS_DOWNLOAD' is set; skipping scripts' download"
@@ -20,13 +16,24 @@ fi
 
 mkdir -p /opt/scripts
 
-# Download codecov's bash script
-git clone https://github.com/codecov/codecov-bash
-cd codecov-bash
-git checkout $CODECOV_VERSION
+if ! [ -x "$(command -v curl)" ]; then
+	echo "Error: curl is not installed."
+	return 1
+fi
 
-git apply ../0001-fix-generating-gcov-files-and-turn-off-verbose-log.patch
+# Download codecov and check integrity
+mkdir -p codecov-tmp
+pushd codecov-tmp
+
+curl https://keybase.io/codecovsecurity/pgp_keys.asc | gpg --no-default-keyring --keyring trustedkeys.gpg --import
+curl -Os https://uploader.codecov.io/latest/linux/codecov
+curl -Os https://uploader.codecov.io/latest/linux/codecov.SHA256SUM
+curl -Os https://uploader.codecov.io/latest/linux/codecov.SHA256SUM.sig
+gpgv codecov.SHA256SUM.sig codecov.SHA256SUM
+shasum -a 256 -c codecov.SHA256SUM
+chmod +x codecov
+
 mv -v codecov /opt/scripts/codecov
 
-cd ..
-rm -rf codecov-bash
+popd
+rm -rf codecov-tmp
