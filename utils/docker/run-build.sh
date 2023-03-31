8be3fae537fa572213c7352b977e45c99c0457e0
Tomasz Gromadzki <tomasz.gromadzki@intel.com>
2023-03-31 20:43:37 +0200

    test: use separate files for each test
    use a separate file for each test to avoid interferences between tests

--- utils/docker/run-build.sh
+++ utils/docker/run-build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright 2016-2022, Intel Corporation
+# Copyright 2016-2023, Intel Corporation
 
 #
 # run-build.sh - is called inside a Docker container; prepares the environment
