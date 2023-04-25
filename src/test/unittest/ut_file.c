8dcb79aa7acc31ad9551c5ce1037dc8e182b9dbc
Łukasz Plewa <lukasz.plewa@intel.com>
2023-04-25 12:25:27 +0200

    pmem2: fix valgrind instrumentation in pmem2_map_from_exisiting
    fixes: pmem/pmdk#5597

--- src/test/unittest/ut_file.c
+++ src/test/unittest/ut_file.c
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BSD-3-Clause
-/* Copyright 2014-2020, Intel Corporation */
+/* Copyright 2014-2023, Intel Corporation */
 
 /*
  * ut_file.c -- unit test file operations
@@ -370,7 +370,7 @@ void *
 ut_file_map(const char *file, int line, const char *func, int fd, size_t size)
 {
 	void *addr = NULL;
-	ut_mmap(file, line, func, addr, size, PROT_READ | PROT_WRITE,
+	addr = ut_mmap(file, line, func, NULL, size, PROT_READ | PROT_WRITE,
 		MAP_SHARED, fd, 0);
 	return addr;
 }
