4f5cba857ad982a61433bb2105de4d65abbfa653
Gromadzki, Tomasz <tomasz.gromadzki@intel.com>
2023-05-10 20:19:45 +0200

    common: remove windows specific files from tests
    Remove all Windows specyfic files incl. test scripts and
    VC++ project files.
    getopt test has been removed as it is no longer needed as
    PMDK uses system getopt call on Linux platform
    
    Signed-off-by: Gromadzki, Tomasz <tomasz.gromadzki@intel.com>

--- utils/check_license/file-exceptions.sh
+++ utils/check_license/file-exceptions.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -e
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright 2016-2021, Intel Corporation
+# Copyright 2016-2023, Intel Corporation
 
 # file-exceptions.sh - filter out files not checked for copyright and license
 
-grep -v -E -e '/queue.h$' -e '/getopt.h$' -e '/getopt.c$' -e 'src/core/valgrind/' \
-	 -e '/testconfig\...$' -e'src/deps/miniasync/LICENSE'
+grep -v -E -e '/queue.h$' -e 'src/core/valgrind/' -e '/testconfig\...$' \
+	 -e'src/deps/miniasync/LICENSE'
