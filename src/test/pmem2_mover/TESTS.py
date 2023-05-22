62643d85394fb7d975d90f8d26ccda0d54630193
Tomasz Gromadzki <tomasz.gromadzki@intel.com>
2023-05-22 12:31:54 +0200

    test: disable pmem2_mover/TEST[0-5, 9] w/ pmemcheck until #5595 is fixed
    Signed-off-by: Tomasz Gromadzki <tomasz.gromadzki@intel.com>

--- src/test/pmem2_mover/TESTS.py
+++ src/test/pmem2_mover/TESTS.py
@@ -1,6 +1,6 @@
 #!../env.py
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright 2022, Intel Corporation
+# Copyright 2022-2023, Intel Corporation
 #
 
 
@@ -39,31 +39,49 @@ class PMEM2_MOVER_MT(PMEM2_MOVER):
         ctx.exec('pmem2_mover', self.test_case, self.filepath, self.thread_num)
 
 
+# XXX disable the test for 'pmemcheck'
+# until https://github.com/pmem/pmdk/issues/5595 is fixed.
+@t.require_valgrind_disabled('pmemcheck')
 class TEST0(PMEM2_MOVER):
     """verify pmem2 mover memcpy functionality"""
     test_case = "test_mover_memcpy_basic"
 
 
+# XXX disable the test for 'pmemcheck'
+# until https://github.com/pmem/pmdk/issues/5595 is fixed.
+@t.require_valgrind_disabled('pmemcheck')
 class TEST1(PMEM2_MOVER):
     """verify pmem2 mover memmove functionality"""
     test_case = "test_mover_memmove_basic"
 
 
+# XXX disable the test for 'pmemcheck'
+# until https://github.com/pmem/pmdk/issues/5595 is fixed.
+@t.require_valgrind_disabled('pmemcheck')
 class TEST2(PMEM2_MOVER):
     """verify pmem2 mover memset functionality"""
     test_case = "test_mover_memset_basic"
 
 
+# XXX disable the test for 'pmemcheck'
+# until https://github.com/pmem/pmdk/issues/5595 is fixed.
+@t.require_valgrind_disabled('pmemcheck')
 class TEST3(PMEM2_MOVER_MT):
     """verify pmem2 mover multi-threaded memcpy functionality"""
     test_case = "test_mover_memcpy_multithreaded"
 
 
+# XXX disable the test for 'pmemcheck'
+# until https://github.com/pmem/pmdk/issues/5595 is fixed.
+@t.require_valgrind_disabled('pmemcheck')
 class TEST4(PMEM2_MOVER_MT):
     """verify pmem2 mover multi-threaded memmove functionality"""
     test_case = "test_mover_memmove_multithreaded"
 
 
+# XXX disable the test for 'pmemcheck'
+# until https://github.com/pmem/pmdk/issues/5595 is fixed.
+@t.require_valgrind_disabled('pmemcheck')
 class TEST5(PMEM2_MOVER_MT):
     """verify pmem2 mover multi-threaded memset functionality"""
     test_case = "test_mover_memset_multithreaded"
@@ -90,6 +108,9 @@ class TEST8(PMEM2_MOVER_MT):
     test_case = "test_mover_memset_multithreaded"
 
 
+# XXX disable the test for 'pmemcheck'
+# until https://github.com/pmem/pmdk/issues/5595 is fixed.
+@t.require_valgrind_disabled('pmemcheck')
 class TEST9(PMEM2_MOVER):
     """verify pmem2 mover functionality"""
     test_case = "test_miniasync_mover"
