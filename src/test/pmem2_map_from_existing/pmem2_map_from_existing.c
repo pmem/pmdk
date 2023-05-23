1ddbf47d52bfa974747786a83a2f1bd85f5bfd96
Tomasz Gromadzki <tomasz.gromadzki@intel.com>
2023-05-23 15:09:49 +0200

    test: fix pmem2_map_from_existing/TEST[0-2] w/ memcheck
    Fixes: #5594
    
    Signed-off-by: Tomasz Gromadzki <tomasz.gromadzki@intel.com>

--- src/test/pmem2_map_from_existing/pmem2_map_from_existing.c
+++ src/test/pmem2_map_from_existing/pmem2_map_from_existing.c
@@ -37,6 +37,7 @@ test_two_same_mappings(const struct test_case *tc, int argc, char *argv[])
 	UT_ASSERTeq(map2, NULL);
 
 	pmem2_map_delete(&map1);
+	pmem2_source_delete(&src);
 	CLOSE(fd);
 	return 1;
 }
@@ -69,6 +70,7 @@ test_mapping_overlap_bottom(const struct test_case *tc, int argc, char *argv[])
 	UT_ASSERTeq(map2, NULL);
 
 	pmem2_map_delete(&map1);
+	pmem2_source_delete(&src);
 	CLOSE(fd);
 	return 1;
 }
@@ -101,6 +103,7 @@ test_mapping_overlap_upper(const struct test_case *tc, int argc, char *argv[])
 	UT_ASSERTeq(map2, NULL);
 
 	pmem2_map_delete(&map1);
+	pmem2_source_delete(&src);
 	CLOSE(fd);
 	return 1;
 }
