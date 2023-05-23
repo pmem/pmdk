6f533b16d13db75371c51b6577ad83413793745e
Tomasz Gromadzki <tomasz.gromadzki@intel.com>
2023-05-23 12:53:32 +0200

    test: (fix) remove obsolete data structure
    Signed-off-by: Tomasz Gromadzki <tomasz.gromadzki@intel.com>

--- src/test/obj_tx_alloc_mt/obj_tx_alloc_mt.c
+++ src/test/obj_tx_alloc_mt/obj_tx_alloc_mt.c
@@ -23,15 +23,8 @@ static unsigned Threads;
 static unsigned Ops_per_thread;
 static unsigned Tx_per_thread;
 
-struct action {
-	struct pobj_action pact;
-	os_mutex_t lock;
-	os_cond_t cond;
-};
-
 struct root {
 	uint64_t offs[MAX_THREADS][MAX_OPS_PER_THREAD];
-	struct action actions[MAX_THREADS][MAX_OPS_PER_THREAD];
 };
 
 struct worker_args {
@@ -177,11 +170,6 @@ main(int argc, char *argv[])
 		args[i].pop = pop;
 		args[i].r = r;
 		args[i].idx = i;
-		for (unsigned j = 0; j < Ops_per_thread; ++j) {
-			struct action *a = &r->actions[i][j];
-			util_mutex_init(&a->lock);
-			util_cond_init(&a->cond);
-		}
 	}
 	pmemobj_persist(pop, r, sizeof(struct root));
 
