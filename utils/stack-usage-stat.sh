64df9d00b0a16040136365ae20e50b66c483d1f3
Tomasz Gromadzki <tomasz.gromadzki@intel.com>
2023-10-23 21:23:44 +0200

    common: script to combine statck usage statistics into one file
    Signed-off-by: Tomasz Gromadzki <tomasz.gromadzki@intel.com>

---
+++ utils/stack-usage-stat.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright 2023, Intel Corporation
+#
+# utils/stack-usage-stat.sh -- combine stack usage into a file
+# The script shall be run from the main PMDK folder.
+#
+
+if [ ! -d "src/stat" ] ; then
+	mkdir src/stat
+fi
+
+fgrep -r -e static -e dynamic src | grep '.su:' | \
+    grep -e nondebug | \
+    gawk '{print $2 " "  $1 " " $3 " "  $4 " "  $5}' | \
+    sort -n -r > src/stat/stack-usage-nondebug.txt
+
+fgrep -r -e static -e dynamic src | grep '.su:' | \
+    grep -e '\/debug\/' | \
+    gawk '{print $2 " "  $1 " " $3 " "  $4 " "  $5}' | \
+    sort -n -r > src/stat/stack-usage-debug.txt
