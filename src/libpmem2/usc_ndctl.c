d412c3656b968f0e701511203e6602d9151c82f8
Tomasz Gromadzki <tomasz.gromadzki@intel.com>
2023-10-31 13:35:35 +0100

    common: remove obsolete headers files
    Signed-off-by: Tomasz Gromadzki <tomasz.gromadzki@intel.com>

--- src/libpmem2/usc_ndctl.c
+++ src/libpmem2/usc_ndctl.c
@@ -5,7 +5,6 @@
  * usc_ndctl.c -- pmem2 usc function for platforms using ndctl
  */
 #include <ndctl/libndctl.h>
-#include <daxctl/libdaxctl.h>
 #include <sys/types.h>
 #include <sys/sysmacros.h>
 #include <fcntl.h>
