c471a17a6b518c4eb9070739ce614e19946a5cda
Tomasz Gromadzki <tomasz.gromadzki@intel.com>
2023-11-06 16:12:45 +0100

    common: stack usage in one file
    Callect stack usage of all libpmem and libpmemobj library into
    one file.
    
    Signed-off-by: Tomasz Gromadzki <tomasz.gromadzki@intel.com>

---
+++ utils/call_stacks_analysis/stack-usage-stats.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright 2023, Intel Corporation
+#
+#
+# Combine stack usage into a file. The script shall be run from the main PMDK folder.
+#
+
+for build in debug nondebug; do
+	grep -v ^$ src/$build/core/*.su src/$build/common/*.su \
+			src/$build/libpmem/*.su src/$build/libpmemobj/*.su | \
+		gawk -F "[:\t]" '{print $6 " " $5 " : " $1 ":" $2 " " $7}' | \
+		sort -n -r > $(dirname "$0")/stack-usage-$build.txt
+done
