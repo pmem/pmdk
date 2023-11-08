c3a3ce9a8bd48ae077dff8d8d22a8feb2a23a7f0
Jan Michalski <jan.michalski@intel.com>
2023-11-08 18:10:23 -0500

    common: pass the validation step
    Signed-off-by: Jan Michalski <jan.michalski@intel.com>

--- utils/call_stacks_analysis/make_api.sh
+++ utils/call_stacks_analysis/make_api.sh
@@ -18,6 +18,12 @@ for link in $LINK_FILES; do
 	fi
 done
 
+API=$WD/api.txt
+
 grep ";" $LINK_FILES | \
 	grep -v -e'*' -e'}' -e'_pobj_cache' | \
-	gawk -F "[;\t]" '{ print $3 }' | sort |  uniq  > $WD/api.txt
+	gawk -F "[;\t]" '{ print $3 }' | sort |  uniq  > $API
+
+for api in libpmem_init libpmem_fini libpmemobj_init libpmemobj_fini; do
+	echo $api >> $API
+done
