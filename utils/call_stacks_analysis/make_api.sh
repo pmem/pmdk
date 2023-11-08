4eebd95fb2e97b227696fafab492fe0306e9819b
Jan Michalski <jan.michalski@intel.com>
2023-11-07 22:59:35 -0500

    common: call stack analysis - usability fixes
    Signed-off-by: Jan Michalski <jan.michalski@intel.com>

---
+++ utils/call_stacks_analysis/make_api.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright 2023, Intel Corporation
+#
+#
+# Generate a list of all libpmem and libpmemobj public API functions.
+#
+
+WD=$(realpath $(dirname $0))
+TOP=$(realpath $(dirname $0)/../..)
+
+LINK_FILES="$TOP/src/libpmem/libpmem.link.in $TOP/src/libpmemobj/libpmemobj.link.in"
+
+for link in $LINK_FILES; do
+	if [ ! -f $link ]; then
+		echo "$link is missing"
+		exit 1
+	fi
+done
+
+grep ";" $LINK_FILES | \
+	grep -v -e'*' -e'}' -e'_pobj_cache' | \
+	gawk -F "[;\t]" '{ print $3 }' | sort |  uniq  > $WD/api.txt
---
+++ utils/call_stacks_analysis/make_stack_usage.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright 2023, Intel Corporation
+#
+#
+# Combine stack usage into a file.
+#
+
+BUILD=${1-nondebug} # debug or nondebug
+
+WD=$(realpath $(dirname $0))
+TOP=$(realpath $(dirname $0)/../..)
+
+SU_FILES="$TOP/src/$BUILD/core/*.su $TOP/src/$BUILD/common/*.su \
+	$TOP/src/$BUILD/libpmem/*.su $TOP/src/$BUILD/libpmemobj/*.su"
+
+grep -v ^$ $SU_FILES | \
+	gawk -F "[:\t]" '{print $6 " " $5 " : " $1 ":" $2 " " $7}' | \
+	sort -n -r > $WD/stack_usage.txt
