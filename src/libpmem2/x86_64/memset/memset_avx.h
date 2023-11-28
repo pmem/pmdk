98c6dd9e5ef92b7874220ca572a5f5660dc002be
Tomasz Gromadzki <tomasz.gromadzki@intel.com>
2023-11-28 18:08:50 +0100

    common: disable On_pmemcheck access outside Valgrind context
    Signed-off-by: Tomasz Gromadzki <tomasz.gromadzki@intel.com>

--- src/libpmem2/x86_64/memset/memset_avx.h
+++ src/libpmem2/x86_64/memset/memset_avx.h
@@ -84,11 +84,15 @@ memset_small_avx(char *dest, __m256i ymm, size_t len, flush_fn flush)
 	 * path) in the optimized version.
 	 * libc's memset also does that, so we can't use it here.
 	 */
+#if VG_PMEMCHECK_ENABLED
 	if (On_pmemcheck) {
 		memset_nodrain_generic(dest, (uint8_t)m256_get2b(ymm),
 						len, PMEM2_F_MEM_NOFLUSH, NULL,
 						NULL);
-	} else {
+	}
+	else
+#endif
+	{
 		memset_small_avx_noflush(dest, ymm, len);
 	}
 
