ab907bdd37220386be128f92a02aa00bf9e87e00
Tomasz Gromadzki <tomasz.gromadzki@intel.com>
2023-12-07 22:28:32 +0100

    common: separate test to verify if Valgrind enabled in tests
    Signed-off-by: Tomasz Gromadzki <tomasz.gromadzki@intel.com>

---
+++ src/test/valgrind_check/valgrind_check.c
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* Copyright 2024, Intel Corporation */
+
+/*
+ * valgrind_check.c -- unit test Valgrind enabled during build
+ *
+ * usage: valgrind_check
+ *
+ */
+
+#include "unittest.h"
+
+int
+main(int argc, char *argv[])
+{
+#ifndef VALGRIND_ENABLED
+	return 0;
+#else
+#if VALGRIND_ENABLED
+	return 0;
+#else
+	return 1;
+#endif
+#endif
+}
