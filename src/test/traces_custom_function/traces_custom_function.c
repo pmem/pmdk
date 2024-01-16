c685cb6a3f40239b985887d539f46d4c08a26629
Tomasz Gromadzki <tomasz.gromadzki@intel.com>
2024-01-16 23:45:07 +0100

    common: fix trace_custom_function test

--- src/test/traces_custom_function/traces_custom_function.c
+++ src/test/traces_custom_function/traces_custom_function.c
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BSD-3-Clause
-/* Copyright 2014-2017, Intel Corporation */
+/* Copyright 2014-2024, Intel Corporation */
 
 /*
  * traces_custom_function.c -- unit test for traces with custom print or
@@ -19,6 +19,8 @@
 #include <stdarg.h>
 #include "pmemcommon.h"
 #include "unittest.h"
+#include "log_internal.h"
+#include "out.h"
 
 /*
  * print_custom_function -- Custom function to handle output
@@ -63,9 +65,44 @@ vsnprintf_custom_function(char *str, size_t size, const char *format,
 	return ret_val;
 }
 
+static void
+ut_log_function(enum core_log_level level, const char *file_name,
+	const int line_no, const char *function_name,
+	const char *message_format, ...)
+{
+	if (file_name) {
+		/* extract base_file_name */
+		const char *base_file_name = strrchr(file_name, '/');
+		if (!base_file_name)
+			base_file_name = file_name;
+		else
+			/* skip '/' */
+			base_file_name++;
+
+		char message[1024] = "";
+		va_list arg;
+		va_start(arg, message_format);
+		if (vsnprintf(message, sizeof(message), message_format, arg)
+				< 0) {
+			va_end(arg);
+			return;
+		}
+		va_end(arg);
+
+		/* remove '\n' from the end of the line */
+		/* '\n' is added by out_log */
+		message[strlen(message)-1] = '\0';
+
+		out_log(base_file_name, line_no, function_name, 1, "%s",
+			message);
+	}
+}
+
 int
 main(int argc, char *argv[])
 {
+	core_log_set_function(ut_log_function);
+
 	START(argc, argv, "traces_custom_function");
 
 	if (argc != 2)
--- src/test/traces_pmem/traces_pmem.c
+++ src/test/traces_pmem/traces_pmem.c
@@ -7,9 +7,42 @@
 
 #include "unittest.h"
 
+static void
+ut_log_function(enum core_log_level level, const char *file_name,
+	const int line_no, const char *function_name,
+	const char *message_format, ...)
+{
+	if (file_name) {
+		/* extract base_file_name */
+		const char *base_file_name = strrchr(file_name, '/');
+		if (!base_file_name)
+			base_file_name = file_name;
+		else
+			/* skip '/' */
+			base_file_name++;
+
+		char message[1024] = "";
+		va_list arg;
+		va_start(arg, message_format);
+		if (vsnprintf(message, sizeof(message), message_format, arg) < 0) {
+			va_end(arg);
+			return;
+		}
+		va_end(arg);
+
+		/* remove '\n' from the end of the line,
+		as it is added by out_log */
+		message[strlen(message)-1] = '\0';
+
+		out_log(base_file_name, line_no, function_name, 1, message);
+	}
+}
+
 int
 main(int argc, char *argv[])
 {
+	core_log_set_function(ut_log_function);
+
 	START(argc, argv, "traces_pmem");
 
 	UT_ASSERT(!pmem_check_version(PMEM_MAJOR_VERSION,
