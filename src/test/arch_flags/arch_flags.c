26b5db481d9f8f360428a0589104465134ed2cd4
Tomasz Gromadzki <tomasz.gromadzki@intel.com>
2024-01-16 22:46:55 +0100

    common: common test log function
    Signed-off-by: Tomasz Gromadzki <tomasz.gromadzki@intel.com>

--- src/test/arch_flags/arch_flags.c
+++ src/test/arch_flags/arch_flags.c
@@ -10,8 +10,6 @@
 #include "unittest.h"
 #include "pool_hdr.h"
 #include "pmemcommon.h"
-#include "log_internal.h"
-#include "out.h"
 
 #define FATAL_USAGE()\
 UT_FATAL(\
@@ -97,7 +95,7 @@ arch_flags_log_function(enum core_log_level level, const char *file_name,
 int
 main(int argc, char *argv[])
 {
-	core_log_set_function(arch_flags_log_function);
+	core_log_set_function(ut_log_function);
 	START(argc, argv, "arch_flags");
 
 	common_init(ARCH_FLAGS_LOG_PREFIX,
--- src/test/unittest/unittest.h
+++ src/test/unittest/unittest.h
@@ -88,6 +88,8 @@ extern "C" {
 #include "os_thread.h"
 #include "util.h"
 
+#include "log_internal.h"
+
 int ut_get_uuid_str(char *);
 #define UT_MAX_ERR_MSG 128
 #define UT_POOL_HDR_UUID_STR_LEN 37 /* uuid string length */
@@ -732,6 +734,10 @@ void __asan_unpoison_memory_region(void const volatile *addr, size_t size);
 #define ASAN_UNPOISON_MEMORY_REGION(addr, size) \
 	((void)(addr), (void)(size))
 #endif
+
+void ut_log_function(enum core_log_level level, const char *file_name,
+	const int line_no, const char *function_name,
+	const char *message_format, ...);
 
 #ifdef __cplusplus
 }
