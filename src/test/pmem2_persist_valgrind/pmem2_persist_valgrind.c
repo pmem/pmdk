6f0d95e3ff3c09e537305d291d76d49886875ab8
Tomasz Gromadzki <tomasz.gromadzki@intel.com>
2024-01-18 12:53:20 +0100

    common: remove unneeded logging initialization
    Signed-off-by: Tomasz Gromadzki <tomasz.gromadzki@intel.com>

--- src/test/pmem2_persist_valgrind/pmem2_persist_valgrind.c
+++ src/test/pmem2_persist_valgrind/pmem2_persist_valgrind.c
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: BSD-3-Clause
-/* Copyright 2020, Intel Corporation */
+/* Copyright 2020-2024, Intel Corporation */
 
 /*
  * pmem2_persist_valgrind.c -- pmem2_persist_valgrind tests
  */
 
-#include "out.h"
 #include "unittest.h"
 #include "ut_pmem2_utils.h"
 
@@ -208,9 +207,6 @@ int
 main(int argc, char *argv[])
 {
 	START(argc, argv, "pmem2_persist_valgrind");
-	out_init("pmem2_persist_valgrind", "TEST_LOG_LEVEL", "TEST_LOG_FILE", 0,
-			0);
 	TEST_CASE_PROCESS(argc, argv, test_cases, NTESTS);
-	out_fini();
 	DONE(NULL);
 }
