6a9fea9ec2a8d8c9e7c67bab32fafc2850562ce2
Tomasz Gromadzki <tomasz.gromadzki@intel.com>
2024-01-22 09:13:13 +0100

    common: errno explicitly provided to ERR() macro.
    Remove "!..." logic from error message format string.
    Use separate argument to either use or not use errno in error message.
    Remove non-posix version of util_strerror.
    
    Signed-off-by: Tomasz Gromadzki <tomasz.gromadzki@intel.com>

--- src/tools/pmempool/output.c
+++ src/tools/pmempool/output.c
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BSD-3-Clause
-/* Copyright 2014-2023, Intel Corporation */
+/* Copyright 2014-2024, Intel Corporation */
 
 /*
  * output.c -- definitions of output printing related functions
@@ -774,7 +774,7 @@ out_get_incompat_features_str(uint32_t incompat)
 		/* print the value and the left square bracket */
 		ret = util_snprintf(str_buff, STR_MAX, "0x%x [", incompat);
 		if (ret < 0) {
-			ERR("snprintf for incompat features: %d", ret);
+			ERR(0, "snprintf for incompat features: %d", ret);
 			return "<error>";
 		}
 
