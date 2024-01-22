17afb33b9466fc73edeb58fe68625803687512e0
Tomasz Gromadzki <tomasz.gromadzki@intel.com>
2024-01-22 13:24:34 +0100

    common: create pmem pool always with shutdown state
    Does not allow create pmem pool wihout support
    of unsafe shutdown counter.
    Disable unsafe shutdown counter for non-pmem tests.
    Disable unsafe shutdown counter for pmem tests in Dockers.
    
    Signed-off-by: Tomasz Gromadzki <tomasz.gromadzki@intel.com>

--- src/common/shutdown_state.c
+++ src/common/shutdown_state.c
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BSD-3-Clause
-/* Copyright 2017-2020, Intel Corporation */
+/* Copyright 2017-2024, Intel Corporation */
 
 /*
  * shutdown_state.c -- unsafe shudown detection
@@ -71,9 +71,7 @@ shutdown_state_add_part(struct shutdown_state *sds, int fd,
 
 	int ret = pmem2_source_device_usc(src, &usc);
 
-	if (ret == PMEM2_E_NOSUPP) {
-		usc = 0;
-	} else if (ret != 0) {
+	if (ret != 0) {
 		if (ret == -EPERM) {
 			/* overwrite error message */
 			ERR(
@@ -84,7 +82,7 @@ shutdown_state_add_part(struct shutdown_state *sds, int fd,
 	}
 
 	ret = pmem2_source_device_id(src, NULL, &len);
-	if (ret != PMEM2_E_NOSUPP && ret != 0) {
+	if (ret != 0) {
 		ERR("cannot read uuid of %d", fd);
 		goto err;
 	}
@@ -98,7 +96,7 @@ shutdown_state_add_part(struct shutdown_state *sds, int fd,
 	}
 
 	ret = pmem2_source_device_id(src, uid, &len);
-	if (ret != PMEM2_E_NOSUPP && ret != 0) {
+	if (ret != 0) {
 		ERR("cannot read uuid of %d", fd);
 		Free(uid);
 		goto err;
