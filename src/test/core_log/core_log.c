0a16cc6ae36fd7b99170d1a4eee9b4a5c1fe217f
Jan Michalski <jan.michalski@intel.com>
2024-02-13 14:30:57 -0500

    common: test all TLS log messages for length
    Signed-off-by: Jan Michalski <jan.michalski@intel.com>

--- src/test/core_log/core_log.c
+++ src/test/core_log/core_log.c
@@ -11,22 +11,23 @@
 
 #include "log_internal.h"
 #include "unittest.h"
+#include "unittest.h"
+#include "call_all.h"
 
 #define NO_ARGS_CONSUMED 0
 
 #define BIG_BUF_SIZE 1024
 
-/*
- * test_CORE_LOG_MAX_ERRNO_MSG --
- * _CORE_LOG_MAX_ERRNO_MSG >= max(strlen(strerror(errnnum)))
- *     for errnum in errnums
- */
-static int
-test_CORE_LOG_MAX_ERRNO_MSG(const struct test_case *tc, int argc, char *argv[])
+#define MAX_STRERROR_NUM 0x54
+
+static void
+call_all_strerror_r(int *max_strerror_len, int *max_streerror_num)
 {
 	char buf[BIG_BUF_SIZE];
-	int max_strerror_len = 0;
 	int correct_strerror_calls = 0;
+
+	*max_strerror_len = 0;
+	*max_streerror_num = 0;
 
 	/*
 	 * In general, valid errno values are all positive values of type int,
@@ -51,11 +52,28 @@ test_CORE_LOG_MAX_ERRNO_MSG(const struct test_case *tc, int argc, char *argv[])
 		++correct_strerror_calls;
 
 		int len = strlen(buf);
-		if (len > max_strerror_len) {
-			max_strerror_len = len;
+		if (len > *max_strerror_len) {
+			*max_strerror_len = len;
+			*max_streerror_num = errnum;
 		}
 	}
 	UT_ASSERT(correct_strerror_calls == 132);
+}
+
+/* tests */
+
+/*
+ * test_CORE_LOG_MAX_ERRNO_MSG --
+ * _CORE_LOG_MAX_ERRNO_MSG >= max(strlen(strerror(errnnum)))
+ *     for errnum in errnums
+ */
+static int
+test_CORE_LOG_MAX_ERRNO_MSG(const struct test_case *tc, int argc, char *argv[])
+{
+	int max_strerror_len = 0;
+	int max_streerror_num = 0;
+	
+	call_all_strerror_r(&max_strerror_len, &max_streerror_num);
 
 	/*
 	 * The key aim of this test is to make sure the assumed errno message
@@ -63,11 +81,47 @@ test_CORE_LOG_MAX_ERRNO_MSG(const struct test_case *tc, int argc, char *argv[])
 	 */
 	UT_ASSERT(max_strerror_len + 1 <= _CORE_LOG_MAX_ERRNO_MSG);
 
+	/*
+	 * Other tests in this group makes use of this value so just make sure
+	 * the generated strerror will be as long as it is possible.
+	 */
+	UT_ASSERTeq(max_streerror_num, MAX_STRERROR_NUM);
+
+	return NO_ARGS_CONSUMED;
+}
+
+static void
+log_function(void *context, enum core_log_level level,
+	const char *file_name, const int line_no, const char *function_name,
+	const char *message)
+{
+	size_t *max_message_len = (size_t *)context;
+	size_t len = strlen(message);
+
+	if (len > *max_message_len) {
+		*max_message_len = len;
+	}
+}
+
+static int
+test_ERR_W_ERRNO(const struct test_case *tc, int argc, char *argv[])
+{
+	size_t max_message_len = 0;
+	core_log_set_function(log_function, &max_message_len);
+
+	call_all_CORE_LOG_ERROR_LAST();
+	call_all_ERR_WO_ERRNO();
+	call_all_CORE_LOG_ERROR_W_ERRNO_LAST(MAX_STRERROR_NUM);
+	call_all_ERR_W_ERRNO(MAX_STRERROR_NUM);
+
+	UT_ASSERTeq(max_message_len, 0); /* XXX */
+
 	return NO_ARGS_CONSUMED;
 }
 
 static struct test_case test_cases[] = {
-	TEST_CASE(test_CORE_LOG_MAX_ERRNO_MSG)
+	TEST_CASE(test_CORE_LOG_MAX_ERRNO_MSG),
+	TEST_CASE(test_ERR_W_ERRNO),
 };
 
 #define NTESTS ARRAY_SIZE(test_cases)
@@ -76,6 +130,9 @@ int
 main(int argc, char *argv[])
 {
 	START(argc, argv, "core_log");
+
+	UT_COMPILE_ERROR_ON(sizeof(PATH) != 128);
+
 	TEST_CASE_PROCESS(argc, argv, test_cases, NTESTS);
 	DONE(NULL);
 }
