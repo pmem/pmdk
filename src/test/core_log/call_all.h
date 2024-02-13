36325f68942dd9ad430a2158e93b48cf79a78457
Jan Michalski <jan.michalski@intel.com>
2024-02-13 14:30:57 -0500

    common: test all TLS log messages for length
    Signed-off-by: Jan Michalski <jan.michalski@intel.com>

---
+++ src/test/core_log/call_all.h
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/* Copyright 2024, Intel Corporation */
+
+/*
+ * call_all.h --
+ */
+
+#ifndef CALL_ALL_H
+#define CALL_ALL_H
+
+#define PATH \
+	"Ut/imperdiet/dictum/dui/in/posuere/augue/accumsan/ut/Cras/et/neque/id/elit/porta/malesuada/Class/aptent/taciti/sociosqu//litora"
+
+void call_all_CORE_LOG_ERROR_LAST(void);
+void call_all_ERR_WO_ERRNO(void);
+void call_all_CORE_LOG_ERROR_W_ERRNO_LAST(int errnum);
+void call_all_ERR_W_ERRNO(int errnum);
+
+#endif /* CALL_ALL_H */
