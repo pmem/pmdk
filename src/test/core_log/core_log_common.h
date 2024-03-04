46efe7971160377d41c21f1c838c9f324df8b8ef
Jan Michalski <jan.michalski@intel.com>
2024-03-04 14:44:04 -0500

    test: introduce core_log() unit tests (fix)
    - to be squashed before the merge
    
    Signed-off-by: Jan Michalski <jan.michalski@intel.com>

---
+++ src/test/core_log/core_log_common.h
@@ -0,0 +1,67 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/* Copyright 2024, Intel Corporation */
+
+/*
+ * core_log.c -- unit test for core_log() and core_log_va()
+ */
+
+#include <stdbool.h>
+
+#include "unittest.h"
+
+#define NO_ARGS_CONSUMED 0
+
+#define FILE_NAME "dummy.c"
+#define LINE_NO 1234
+#define FUNC_NAME "dummy_func"
+#define MSG_FORMAT "dummy msg format"
+#define DUMMY_ARG 456
+#define LAST_ERROR_MSG_MOCK ((char *)0x1a547e58)
+#define DUMMY_ERRNO1 500
+#define DUMMY_ERRNO2 (DUMMY_ERRNO1 + 1)
+#define BASIC_MESSAGE_LEN 131
+
+#define VALIDATED_CALL 127
+#define NOT_CALLED VALIDATED_CALL
+#define CALLED (VALIDATED_CALL + 1)
+
+/* XXX to be removed when introduced to the unittest.sh */
+#define UT_ASSERTstreq(a, b) \
+	UT_ASSERTeq(strcmp((a), (b)), 0)
+
+extern struct common_ctx {
+	bool use_last_error_msg;
+} Common;
+
+extern struct vsnprintf_ctx {
+	int ret;
+} Vsnprintf_;
+
+extern struct strerror_r_ctx {
+	char *exp__buf;
+	size_t exp__buflen;
+	bool before_glibc_2_13;
+	int error;
+} Strerror_r;
+
+extern struct log_function_ctx {
+	enum core_log_level exp_level;
+} Log_function_;
+
+/* mocks */
+
+extern unsigned RCOUNTER(last_error_msg_get);
+extern unsigned RCOUNTER(vsnprintf);
+extern unsigned RCOUNTER(__xpg_strerror_r);
+extern unsigned RCOUNTER(core_log_default_function);
+
+/* helpers */
+
+void reset_mocks(void);
+
+void test_no_space_for_strerror_r_helper(int core_message_length);
+
+void test_strerror_r_fail_helper(bool before_glibc_2_13);
+
+void test_log_function_call_helper(enum core_log_level level,
+	bool call_log_function);
