1c2e3fff4ac02da4fa0ab4a652b3fdbfc643ce9c
Jan Michalski <jan.michalski@intel.com>
2024-03-07 16:12:17 -0500

    common: add core_log_set_function() MT unit test
    Signed-off-by: Jan Michalski <jan.michalski@intel.com>

--- src/test/core_log_function_mt/TESTS.py
+++ src/test/core_log_function_mt/TESTS.py
@@ -9,37 +9,25 @@ from testframework import granularity as g
 
 
 @g.require_granularity(g.ANY)
+# The 'nondebug' build is chosen arbitrarily to ensure these tests are run only
+# once. No dynamic libraries are used nor .static_* builds are available.
 @t.require_build('nondebug')
 class CORE_LOG_MT(t.BaseTest):
     test_type = t.Short
 
     def run(self, ctx):
-        ctx.exec('core_log_threshold_mt', self.test_case)
+        ctx.exec('core_log_function_mt', self.test_case)
 
 
-class TEST0(CORE_LOG_MT):
-    test_case = 'test_threshold_set_get'
+class SET_CALL(CORE_LOG_MT):
+    test_case = 'test_function_set_call'
 
 
 @t.require_valgrind_enabled('helgrind')
-class TEST1(TEST0):
+class TEST0(SET_CALL):
     pass
 
 
 @t.require_valgrind_enabled('drd')
-class TEST2(TEST0):
-    pass
-
-
-class TEST3(CORE_LOG_MT):
-    test_case = 'test_threshold_aux_set_get'
-
-
-@t.require_valgrind_enabled('helgrind')
-class TEST4(TEST3):
-    pass
-
-
-@t.require_valgrind_enabled('drd')
-class TEST5(TEST3):
+class TEST1(SET_CALL):
     pass
