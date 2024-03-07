f599a304d76363e9cb281d3fd55252c369f915cf
Jan Michalski <jan.michalski@intel.com>
2024-03-07 15:08:23 -0500

    test: don't configure the log function for PMEMOBJ...
    ... nor PMEMCORE.
    
    Signed-off-by: Jan Michalski <jan.michalski@intel.com>

--- src/test/unittest/unittest.h
+++ src/test/unittest/unittest.h
@@ -121,32 +121,9 @@ void ut_err(const char *file, int line, const char *func,
 	const char *fmt, ...)
 	__attribute__((format(printf, 4, 5)));
 
-void
-ut_log_function(enum core_log_level level, const char *file_name,
-	const int line_no, const char *function_name,
-	const char *message);
-
-#ifdef USE_LOG_PMEMCORE
-#define LOG_SET_PMEMCORE_FUNC core_log_set_function(ut_log_function)
-#else
-#define LOG_SET_PMEMCORE_FUNC
-#endif
-
-#ifdef USE_LOG_PMEMOBJ
-#define LOG_SET_PMEMOBJ_FUNC \
-	pmemobj_log_set_function((pmemobj_log_function *)ut_log_function)
-#else
-#define LOG_SET_PMEMOBJ_FUNC
-#endif
-
 /* indicate the start of the test */
 #define START(argc, argv, ...)\
-	do {\
-		ut_start(__FILE__, __LINE__, __func__, argc, argv,\
-			__VA_ARGS__);\
-		LOG_SET_PMEMCORE_FUNC; \
-		LOG_SET_PMEMOBJ_FUNC; \
-	} while (0)
+	ut_start(__FILE__, __LINE__, __func__, argc, argv, __VA_ARGS__)
 
 /* normal exit from test */
 #define DONE(...)\
