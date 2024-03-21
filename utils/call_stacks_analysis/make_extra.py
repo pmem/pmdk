ac9007d8d07dcef17e8b2c58d2867c99f1ab97fd
Tomasz Gromadzki <tomasz.gromadzki@intel.com>
2024-03-21 08:29:13 +0100

    common: include new logging functions in linkers files
    Signed-off-by: Tomasz Gromadzki <tomasz.gromadzki@intel.com>

--- utils/call_stacks_analysis/make_extra.py
+++ utils/call_stacks_analysis/make_extra.py
@@ -22,12 +22,14 @@ def dict_extend(dict_, key, values):
 def inlines(calls: Calls) -> Calls:
         # common
         calls['core_init'] = ['util_init', 'core_log_init', 'out_init']
-        calls['core_fini'] = ['out_fini']
+        calls['core_fini'] = ['out_fini', 'core_log_fini', 'last_error_msg_fini']
         calls['common_init'] = ['core_init', 'util_mmap_init']
         calls['common_fini'] = ['util_mmap_fini', 'core_fini']
         calls['Last_errormsg_key_alloc'] = ['_Last_errormsg_key_alloc']
         calls['_Last_errormsg_key_alloc'] = ['os_once', 'os_tls_key_create']
         calls['core_log_va'] = ['core_log_default_function']
+        calls['core_log'] = ['core_log_va']
+        calls['core_log_set_function'] = ['core_log']
 
         # libpmem
         calls['flush_empty'] = ['flush_empty_nolog']
